06b8481bafcf9b1eaf47736b74275d7982f583aa
Matt Elwell <matthewe@solidstategroup.com>
2018-06-21 14:51:21 +0100

    update logging configuration when running in development and production environments update allowed hosts config for heroku and docker configurations

--- src/app/settings/heroku-master.py
+++ src/app/settings/heroku-master.py
@@ -3,8 +3,6 @@ import os
 
 import dj_database_url
 
-ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOST']]
-
 DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'])
 
 DEBUG = False
@@ -12,22 +10,29 @@ DEBUG = False
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'app.handlers.MakeFileHandler',
-            'filename': 'logs/django.log',
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
         },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
         'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler'
-        }
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
     },
     'loggers': {
         'django': {
-            'handlers': ['file', 'console'],
-            'level': 'DEBUG',
+            'handlers': ['console'],
             'propagate': True,
+            'level': 'INFO'
         },
-    },
+        'gunicorn': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
 }
