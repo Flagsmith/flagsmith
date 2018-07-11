42fb51f2a9485df2f1aeedfc8337fb3de0840b02
Matt Elwell <matthewe@solidstategroup.com>
2018-07-11 15:35:32 +0100

    Refactor project features view into nested router

--- src/projects/views.py
+++ src/projects/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 
 from features.models import Feature, FeatureState, FLAG
-from features.serializers import FeatureSerializer
+from features.serializers import CreateFeatureSerializer
 from environments.serializers import EnvironmentSerializerLight
 from projects.models import Project
 from projects.serializers import ProjectSerializer
@@ -28,64 +28,3 @@ class ProjectViewSet(viewsets.ModelViewSet):
         project = self.get_object()
         environments = project.environments.all()
         return Response(EnvironmentSerializerLight(environments, many=True).data)
-
-    @detail_route(methods=["GET", "POST", "PUT", "DELETE"])
-    def features(self, request, pk):
-        project = self.get_object()
-
-        self.queryset = Feature.objects.filter(project=project)
-        self.serializer_class = FeatureSerializer
-
-        if request.method == "POST":
-            data = {
-                "project": project.id,
-                "name": request.data["name"],
-                "initial_value":
-                    request.data["initial_value"] if "initial_value" in request.data else None,
-                "description":
-                    request.data["description"] if "description" in request.data else None,
-                "type":
-                    request.data["type"] if "type" in request.data else FLAG,
-                "default_enabled":
-                    request.data["default_enabled"] if "default_enabled" in request.data else False,
-            }
-
-            f_serializer = FeatureSerializer(data=data)
-
-            if f_serializer.is_valid():
-                f_serializer.save()
-                return Response(f_serializer.data, status=status.HTTP_201_CREATED)
-            else:
-                return Response(f_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        elif request.method == "PUT":
-            data = {
-                "id": request.data["id"],
-                "project": project.id,
-                "name": request.data["name"],
-                "initial_value": request.data["initial_value"]
-            }
-
-            serializer = FeatureSerializer(data=data)
-
-            if serializer.is_valid():
-                feature_to_update = Feature.objects.get(pk=data["id"])
-                feature_updated = serializer.update(feature_to_update, serializer.validated_data)
-                return Response(FeatureSerializer(feature_updated).data,
-                                status=status.HTTP_201_CREATED)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        elif request.method == "DELETE":
-            feature = Feature.objects.get(pk=request.data["id"])
-            feature.delete()
-
-            feature_states = FeatureState.objects.filter(feature=feature)
-            for fs in feature_states:
-                fs.delete()
-
-            return Response(status=status.HTTP_200_OK)
-
-        else:
-            serializer = FeatureSerializer(instance=self.queryset, many=True)
-            return Response(serializer.data)
