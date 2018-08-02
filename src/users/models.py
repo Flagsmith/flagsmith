ffd251e9053d17f65131a07d6e06630d96739f2e
Stuart Dines <me@stuartdines.com>
2018-08-02 11:01:58 +1000

    Add str / unicode compatability
    By using `python_2_unicode_compatible` we don't need to explicitly set
    both the `__str__` and the `__unicode__` methods. After providing one it
    will add a reference for the other function.

--- src/users/models.py
+++ src/users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.template.loader import get_template
 
 from app.utils import create_hash
+from django.utils.encoding import python_2_unicode_compatible
 from organisations.models import Organisation
 
 
@@ -42,6 +43,7 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
 
+@python_2_unicode_compatible
 class FFAdminUser(AbstractUser):
     organisations = models.ManyToManyField(Organisation, related_name="users", blank=True)
     email = models.EmailField(unique=True, null=False)
@@ -68,6 +70,7 @@ class FFAdminUser(AbstractUser):
         return "%s %s" % (self.first_name, self.last_name)
 
 
+@python_2_unicode_compatible
 class Invite(models.Model):
     email = models.EmailField()
     hash = models.CharField(max_length=100, default=create_hash, unique=True)
