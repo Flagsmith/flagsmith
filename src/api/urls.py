96321d30f201c0269d00418e203487856f402c50
Stuart Dines <me@stuartdines.com>
2018-08-09 13:23:59 +1000

    Add `app_name` support
    App names need to be added for URLs that are included in later versions
    of Django

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,22 +2,33 @@ from django.conf.urls import url, include
 
 from features.views import SDKFeatureStates
 
+app_name = "api"
+
 urlpatterns = [
-    url(r'^v1/', include([
-        url(r'^organisations/', include('organisations.urls')),
-        url(r'^projects/', include('projects.urls')),
-        url(r'^environments/', include('environments.urls')),
-        url(r'^users/', include('users.urls')),
-        url(r'^auth/', include('rest_auth.urls')),
-        url(r'^auth/register/', include('rest_auth.registration.urls')),
-        url(r'^account/', include('allauth.urls')),
-        url(r'^e2etests/', include('e2etests.urls')),
+    url(
+        r'^v1/',
+        include(
+            (
+                [
+                    url(r'^organisations/', include('organisations.urls')),
+                    url(r'^projects/', include('projects.urls')),
+                    url(r'^environments/', include('environments.urls')),
+                    url(r'^users/', include('users.urls')),
+                    url(r'^auth/', include('rest_auth.urls')),
+                    url(r'^auth/register/', include('rest_auth.registration.urls')),
+                    url(r'^account/', include('allauth.urls')),
+                    url(r'^e2etests/', include('e2etests.urls')),
 
-        # Client SDK urls
-        url(r'^flags/(?P<identifier>\w+)', SDKFeatureStates.as_view()),
-        url(r'^flags/', SDKFeatureStates.as_view()),
+                    # Client SDK urls
+                    url(r'^flags/(?P<identifier>\w+)', SDKFeatureStates.as_view()),
+                    url(r'^flags/', SDKFeatureStates.as_view()),
 
-        # API documentation
-        url(r'^docs/', include('docs.urls', namespace='docs'))
-    ], namespace='v1'))
+                    # API documentation
+                    url(r'^docs/', include(('docs.urls', "docs"), namespace='docs'))
+                ],
+                "api"
+            ),
+            namespace='v1'
+        )
+    )
 ]
