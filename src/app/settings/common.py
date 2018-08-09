d74c293053d57ea4beaddd335dbfb53722dd9774
Stuart Dines <me@stuartdines.com>
2018-08-09 13:22:22 +1000

    Add middleware support for Django 2
    `MIDDLEWARE_CLASSES` was renamed `MIDDLEWARE` which will also work with
    Django 1.11.
    
    `SessionAuthenticationMiddleware` in Django 1.11 was only used as a
    backwards compatibility shim that had no functionality therefore it can
    be removed for all versions of supported Django.

--- src/app/settings/common.py
+++ src/app/settings/common.py
@@ -94,13 +94,12 @@ REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'users.serializers.UserRegisterSerializer'
 }
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware'
