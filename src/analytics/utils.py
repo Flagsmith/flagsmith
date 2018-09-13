2f101836a72a7606149a79befae3c8ed80ba83b4
Matt Elwell <matthewe@solidstategroup.com>
2018-09-13 15:19:22 +0100

    Generate new uuid for every call to GA

--- src/analytics/utils.py
+++ src/analytics/utils.py
@@ -1,3 +1,5 @@
+import uuid
+
 from six.moves.urllib.parse import quote # python 2/3 compatible urllib import
 import requests
 
@@ -6,8 +8,7 @@ from django.conf import settings
 GOOGLE_ANALYTICS_BASE_URL = "https://www.google-analytics.com"
 GOOGLE_ANALYTICS_COLLECT_URL = GOOGLE_ANALYTICS_BASE_URL + "/collect"
 GOOGLE_ANALYTICS_BATCH_URL = GOOGLE_ANALYTICS_BASE_URL + "/batch"
-DEFAULT_DATA = "v=1&tid=" + settings.GOOGLE_ANALYTICS_KEY + "&cid=" + \
-               settings.GOOGLE_ANALYTICS_CLIENT_ID + "&"
+DEFAULT_DATA = "v=1&tid=" + settings.GOOGLE_ANALYTICS_KEY
 
 
 def track_request(uri):
@@ -21,9 +22,9 @@ def track_request(uri):
 
 
 def track_event(category, action, label='', value=''):
-    data = DEFAULT_DATA + "t=event" + \
+    data = DEFAULT_DATA + "&t=event" + \
            "&ec=" + category + \
-           "&ea=" + action
+           "&ea=" + action + "&cid=" + str(uuid.uuid4())
     data = data + "&el=" + label if label else data
     data = data + "&ev=" + value if value else data
     print data
