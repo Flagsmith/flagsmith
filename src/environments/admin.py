79dc8f29557924ccfad04c449b4146967f3c5ec8
Pavlo Maks <pavlo@solidstategroup.com>
2018-10-22 16:45:44 +0100

    admin work for user traits and traits values

--- src/environments/admin.py
+++ src/environments/admin.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
-from .models import Identity, Environment
+from .models import Identity, Environment, Trait, TraitValue
 
 
 @admin.register(Environment)
@@ -20,3 +21,35 @@ class IdentityAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'created_date', 'environment', )
     list_filter = ('created_date', 'environment', )
     search_fields = ('identifier', )
+
+
+class TraitValueInline(admin.StackedInline):
+    model = TraitValue
+    extra = 0
+    show_change_link = True
+
+
+@admin.register(Trait)
+class TraitAdmin(SimpleHistoryAdmin):
+    inlines = [
+        TraitValueInline,
+    ]
+    date_hierarchy = 'created_date'
+    list_display = ('__str__', 'created_date', 'identity', )
+    list_filter = ('created_date', 'identity', )
+    raw_id_fields = ('identity', )
+    search_fields = ('identity__identifier', 'trait_key', )
+
+
+@admin.register(TraitValue)
+class TraitValue(SimpleHistoryAdmin):
+    list_display = ('trait', 'type', 'boolean_value',
+                    'integer_value', 'string_value', )
+    list_filter = ('type', 'boolean_value', )
+    list_select_related = ('trait',)
+    raw_id_fields = ('trait', )
+    search_fields = (
+        'string_value',
+        'trait__identity__environment__name',
+        'trait__identity__identifier',
+    )
