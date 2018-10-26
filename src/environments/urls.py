9e896405a7ccd4e4ea17eb3a436732da252cfca9
Pavlo Maks <pavlo@solidstategroup.com>
2018-10-26 10:38:02 +0100

    added traits router

--- src/environments/urls.py
+++ src/environments/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework_nested import routers
 
 from features.views import FeatureStateViewSet
-from .views import IdentityViewSet, EnvironmentViewSet
+from .views import IdentityViewSet, EnvironmentViewSet, TraitViewSet
 
 router = routers.DefaultRouter()
 router.register(r'', EnvironmentViewSet, base_name="environment")
@@ -10,19 +10,15 @@ router.register(r'', EnvironmentViewSet, base_name="environment")
 environments_router = routers.NestedSimpleRouter(router, r'', lookup="environment")
 environments_router.register(r'identities', IdentityViewSet, base_name="environment-identities")
 
-identity_feature_states_router = routers.NestedSimpleRouter(environments_router,
-                                                            r'identities',
-                                                            lookup="identity")
-identity_feature_states_router.register(r'featurestates', FeatureStateViewSet,
-                                        base_name="identity-featurestates")
-
-environments_router.register(r'featurestates', FeatureStateViewSet,
-                             base_name="environment-featurestates")
+identity_router = routers.NestedSimpleRouter(environments_router, r'identities', lookup="identity")
+identity_router.register(r'featurestates', FeatureStateViewSet, base_name="identity-featurestates")
+environments_router.register(r'featurestates', FeatureStateViewSet, base_name="environment-featurestates")
+identity_router.register(r'traits', TraitViewSet, base_name="identities-traits")
 
 app_name = "environments"
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^', include(environments_router.urls)),
-    url(r'^', include(identity_feature_states_router.urls))
+    url(r'^', include(identity_router.urls))
 ]
