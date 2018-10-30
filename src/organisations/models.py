01fc182ab7cf8a702f4ad49eaf3b5ba450372a13
Tom <tom@solidstategroup.com>
2018-10-30 15:37:37 +0000

    Add pending cancellation flag

--- src/organisations/models.py
+++ src/organisations/models.py
@@ -8,6 +8,7 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Organisation(models.Model):
     name = models.CharField(max_length=2000)
+    pending_cancellation = models.BooleanField(default=False)
     has_requested_features = models.BooleanField(default=False)
     webhook_notification_email = models.EmailField(null=True, blank=True)
     created_date = models.DateTimeField('DateCreated', auto_now_add=True)
