170313eec52b11129ee1fb97ea0c2225a0760149
Luke Fanning <luke@solidstategroup.com>
2019-01-09 15:09:08 +0000

    Accept dot and dash for endpoints containing identifier or trait key

--- src/api/urls.py
+++ src/api/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
         url(r'^e2etests/', include('e2etests.urls')),
 
         # Client SDK urls
-        url(r'^flags/(?P<identifier>\w+)', SDKFeatureStates.as_view()),
+        url(r'^flags/(?P<identifier>[-\w.]+)', SDKFeatureStates.as_view()),
         url(r'^flags/', SDKFeatureStates.as_view()),
 
-        url(r'^identities/(?P<identifier>\w+)/traits/(?P<trait_key>\w+)', SDKTraits.as_view()),
-        url(r'^identities/(?P<identifier>\w+)/', SDKIdentities.as_view()),
+        url(r'^identities/(?P<identifier>[-\w.]+)/traits/(?P<trait_key>[-\w.]+)', SDKTraits.as_view()),
+        url(r'^identities/(?P<identifier>[-\w.]+)/', SDKIdentities.as_view()),
 
         # API documentation
         url(r'^docs/', include('docs.urls', namespace='docs'))
