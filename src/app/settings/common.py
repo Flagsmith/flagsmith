863205951b00f4d25aa68a14eb7430c254706fef
ola.okejimi <ola.okejimi@ft.com>
2019-02-14 14:38:05 +0000

    enable csfr verification

--- src/app/settings/common.py
+++ src/app/settings/common.py
@@ -116,7 +116,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
