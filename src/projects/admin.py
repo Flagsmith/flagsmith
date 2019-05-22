73853a851b3fc187ea918c9b412f7cbd8cacda38
Matt Elwell <matthewe@solidstategroup.com>
2019-05-22 13:50:32 +0000

    Feature/user segments

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from environments.models import Environment
 from features.models import Feature
 from projects.models import Project
+from segments.models import Segment
 
 
 class EnvironmentInline(admin.StackedInline):
@@ -20,14 +21,21 @@ class FeatureInline(admin.StackedInline):
     show_change_link = True
 
 
+class SegmentInline(admin.StackedInline):
+    model = Segment
+    extra = 0
+    show_change_link = True
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     inlines = [
         EnvironmentInline,
-        FeatureInline
+        FeatureInline,
+        SegmentInline
     ]
-    list_display = ('name', 'organisation', 'created_date', )
-    list_filter = ('created_date', 'organisation', 'environments', )
+    list_display = ('name', 'organisation', 'created_date',)
+    list_filter = ('created_date', 'organisation', 'environments',)
     list_select_related = ('organisation',)
-    search_fields = ('organisation__name', )
+    search_fields = ('organisation__name',)
--- src/segments/models.py
+++ src/segments/models.py
@@ -3,27 +3,12 @@ from django.db import models
 
 from django.utils.encoding import python_2_unicode_compatible
 
-# Condition Operation Value Types
-EXACT_MATCH = "ExactMatch"
-GREATER_THAN = "GreaterThan"
-LESS_THAN = "LessThan"
+from projects.models import Project
+
 
 @python_2_unicode_compatible
 class Segment(models.Model):
     name = models.CharField(max_length=2000)
     description = models.TextField(null=True, blank=True)
-
-@python_2_unicode_compatible
-class SegmentCondition(models.Model):
-    CONDITION_OPERATION_TYPES = (
-        (EXACT_MATCH, 'ExactMatch'),
-        (GREATER_THAN, 'GreaterThan'),
-        (LESS_THAN, 'LessThan')
-    )
-
-    segment = models.ForeignKey(Segment, related_name='segment_conditions')
-    trait_key = models.CharField(max_length=200)
-    condition_type = models.CharField(max_length=20, choices=CONDITION_OPERATION_TYPES, default=EXACT_MATCH,
-                            null=False, blank=False)
-    match_value = models.TextField(null=True, blank=True)
-    
+    rules = models.TextField(null=False, blank=False)
+    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name="segments")
