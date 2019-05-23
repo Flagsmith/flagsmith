b7b08bfef4e0dd8ec030a1bbe6a55173488b2c5b
Matthew Elwell <matthew.elwell@solidstategroup.com>
2019-05-23 17:38:30 +0100

    add update endpoint

--- src/segments/serializers.py
+++ src/segments/serializers.py
@@ -36,31 +36,45 @@ class SegmentSerializer(serializers.ModelSerializer):
         :param validated_data: validated json data
         :return: created Segment object
         """
-        segment_rules_data = validated_data.pop('rules', [])
+        rules_data = validated_data.pop('rules', [])
         segment = Segment.objects.create(**validated_data)
-        self._create_segment_rules(segment_rules_data, segment=segment)
+        self._create_segment_rules(rules_data, segment=segment)
         return segment
 
-    def _create_segment_rules(self, segment_rules_data, segment=None, rule=None):
+    def update(self, instance, validated_data):
+        rules_data = validated_data.pop('rules', [])
+        self._update_segment_rules(rules_data, segment=instance)
+        return super().update(instance, validated_data)
+
+    def _update_segment_rules(self, rules_data, segment=None):
+        """
+        Since we don't have a unique identifier for the rules / conditions for the update, we assume that the client
+        passes up the new configuration for the rules of the segment and simply wipe the old ones and create new ones
+        """
+        segment.rules = []
+        self._create_segment_rules(rules_data, segment=segment)
+        segment.save()
+
+    def _create_segment_rules(self, rules_data, segment=None, rule=None):
         if all(x is None for x in {segment, rule}):
             raise RuntimeError("Can't create rule without parent segment or rule")
 
-        for segment_rule_data in segment_rules_data:
-            child_rules = segment_rule_data.pop('rules', [])
-            conditions = segment_rule_data.pop('conditions', [])
+        for rule_data in rules_data:
+            child_rules = rule_data.pop('rules', [])
+            conditions = rule_data.pop('conditions', [])
 
-            child_rule = self._create_segment_rule(segment_rule_data, segment, rule)
+            child_rule = self._create_segment_rule(rule_data, segment, rule)
 
             self._create_conditions(conditions, child_rule)
 
             self._create_segment_rules(child_rules, rule=child_rule)
 
     @staticmethod
-    def _create_segment_rule(segment_rule_data, segment, rule):
+    def _create_segment_rule(rule_data, segment, rule):
         if segment:
-            segment_rule = SegmentRule.objects.create(segment=segment, **segment_rule_data)
+            segment_rule = SegmentRule.objects.create(segment=segment, **rule_data)
         else:
-            segment_rule = SegmentRule.objects.create(rule=rule, **segment_rule_data)
+            segment_rule = SegmentRule.objects.create(rule=rule, **rule_data)
 
         return segment_rule
 
