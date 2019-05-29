ae93b8952dfe3975ea885dee42d1cc06ee5a8b1c
Matthew Elwell <matthew.elwell@solidstategroup.com>
2019-05-29 18:26:23 +0100

    add endpoint for creating feature states based on segments

---
+++ src/features/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from rest_framework_nested import routers
+
+from features.views import FeatureStateCreateViewSet
+
+router = routers.DefaultRouter()
+router.register(r'', FeatureStateCreateViewSet, base_name="featurestates")
+
+
+app_name = "features"
+
+urlpatterns = [
+    url(r'^featurestates', include(router.urls))
+]
