e2310be0df586b13f19a03351239c77d6291e3c7
Matthew Elwell <matthew.elwell@solidstategroup.com>
2019-06-05 09:33:34 +0100

    Revert "allow more characters"
    This reverts commit 2cf0ac47

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,7 +4,6 @@ from features.views import SDKFeatureStates
 from environments.views import SDKIdentities, SDKTraits
 from segments.views import SDKSegments
 
-
 urlpatterns = [
     url(r'^v1/', include([
         url(r'^organisations/', include('organisations.urls')),
@@ -16,13 +15,12 @@ urlpatterns = [
         url(r'^account/', include('allauth.urls')),
         url(r'^e2etests/', include('e2etests.urls')),
 
-        # TODO: remove identifier from URL and move to e.g. GET parameter
         # Client SDK urls
-        url(r"^flags/(?P<identifier>[\w $&+,:;=?@#|'<>.-^*()%!]+)", SDKFeatureStates.as_view()),
+        url(r'^flags/(?P<identifier>[-\w@%.]+)', SDKFeatureStates.as_view()),
         url(r'^flags/', SDKFeatureStates.as_view()),
 
-        url(r"^identities/(?P<identifier>[\w $&+,:;=?@#|'<>.-^*()%!]+)/traits/(?P<trait_key>[-\w.]+)", SDKTraits.as_view()),
-        url(r"^identities/(?P<identifier>[\w $&+,:;=?@#|'<>.-^*()%!]+)/", SDKIdentities.as_view()),
+        url(r'^identities/(?P<identifier>[-\w@%.]+)/traits/(?P<trait_key>[-\w.]+)', SDKTraits.as_view()),
+        url(r'^identities/(?P<identifier>[-\w@%.]+)/', SDKIdentities.as_view()),
 
         url(r'^segments/', SDKSegments.as_view()),
 
