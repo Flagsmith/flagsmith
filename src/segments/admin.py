e88eeb90729c0c58e56ba4daf1a681ff861e7e8f
Matthew Elwell <matthew.elwell@solidstategroup.com>
2019-06-11 13:57:10 +0100

    Revert "Merge branch 'feature/server-side-segment-rules' into 'develop'"
    This reverts commit 2ef6343a4bd44e1199ab2687cd60da6f4607bb48, reversing
    changes made to 24a9354c4fdee5d7345de1bbc6317daf839a5147.

--- src/segments/admin.py
+++ src/segments/admin.py
@@ -3,30 +3,27 @@ from django.contrib import admin
 from segments.models import SegmentRule, Condition, Segment
 
 
-class ConditionsInline(admin.StackedInline):
-    model = Condition
-    extra = 0
-    show_change_link = True
-
-
-class ChildRulesInline(admin.StackedInline):
-    model = SegmentRule
-    extra = 0
-    show_change_link = True
-
-
-@admin.register(SegmentRule)
-class RulesAdmin(admin.ModelAdmin):
-    model = SegmentRule
-    inlines = [ConditionsInline, ChildRulesInline]
-
-
 class RulesInline(admin.StackedInline):
     model = SegmentRule
     extra = 0
     show_change_link = True
 
 
+class ConditionsInline(admin.StackedInline):
+    model = Condition
+    extra = 0
+    show_change_link = True
+
+
 @admin.register(Segment)
 class SegmentAdmin(admin.ModelAdmin):
-    inlines = [RulesInline]
+    inlines = [
+        RulesInline
+    ]
+
+
+@admin.register(SegmentRule)
+class SegmentRule(admin.ModelAdmin):
+    inlines = [
+        ConditionsInline
+    ]
