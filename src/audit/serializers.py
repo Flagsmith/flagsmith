f8a93c768c96caf0f1080783ac01ab2002d46001
Matt Elwell <mathewe@solidstategroup.com>
2019-08-27 16:43:40 +0100

    Basic audit log implementation

---
+++ src/audit/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from audit.models import AuditLog
+from users.serializers import UserListSerializer
+
+
+class AuditLogSerializer(serializers.ModelSerializer):
+    author = UserListSerializer()
+
+    class Meta:
+        model = AuditLog
+        fields = ('created_date', 'log', 'author')
