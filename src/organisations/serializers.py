ff763e7632b65cb4deaeedc0ecdb2aac5983426e
Matthew Elwell <matthew.elwell@solidstategroup.com>
2019-09-11 18:04:37 +0100

    add number of seats to organisation object

--- src/organisations/serializers.py
+++ src/organisations/serializers.py
@@ -4,7 +4,13 @@ from . import models
 
 
 class OrganisationSerializer(serializers.ModelSerializer):
+    num_seats = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Organisation
         fields = ('id', 'name', 'created_date', 'webhook_notification_email', 'paid_subscription',
-        'free_to_use_subscription', 'subscription_date', 'plan', 'pending_cancellation')
+                  'free_to_use_subscription', 'subscription_date', 'plan', 'pending_cancellation', 'num_seats')
+
+    def get_num_seats(self, instance):
+        return instance.users.count()
+
