3ddc4ce907d261f842033db1b8fa4b7974b9ad33
Matt Elwell <mathewe@solidstategroup.com>
2019-09-13 10:44:30 +0100

    update audit log docs

--- src/audit/views.py
+++ src/audit/views.py
@@ -1,11 +1,38 @@
+import coreapi
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, mixins
 
 from audit.models import AuditLog
 from audit.serializers import AuditLogSerializer
 
+project_query_param = openapi.Parameter('project', openapi.IN_QUERY, description='ID of the project to filter on',
+                                        type=openapi.TYPE_INTEGER)
+environment_query_param = openapi.Parameter('environment', openapi.IN_QUERY,
+                                            description='ID of the environment to filter on '
+                                                        '(Note `id` required, not `api_key`)',
+                                            type=openapi.TYPE_INTEGER)
 
+
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    manual_parameters=[project_query_param, environment_query_param]))
 class AuditLogViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = AuditLogSerializer
 
     def get_queryset(self):
-        return AuditLog.objects.filter(project__organisation__in=self.request.user.organisations.all())
+        q = Q(project__organisation__in=self.request.user.organisations.all())
+        if 'project' in self.request.query_params:
+            project_id = self._get_value_as_int(self.request.query_params.get('project'))
+            q = q & Q(project__id=project_id)
+        if 'environment' in self.request.query_params:
+            environment_id = self._get_value_as_int(self.request.query_params.get('environment'))
+            q = q & (Q(environment__id=environment_id) | Q(environment=None))
+        return AuditLog.objects.filter(q)
+
+    def _get_value_as_int(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            return None
