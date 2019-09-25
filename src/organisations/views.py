cf18e92ca19c4d68678d052fef6edc4faa632fe2
Matt Elwell <mathewe@solidstategroup.com>
2019-09-25 16:07:42 +0100

    Return organisation on update_subscription endpoint

--- src/organisations/views.py
+++ src/organisations/views.py
@@ -100,7 +100,7 @@ class OrganisationViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(status=status.HTTP_200_OK)
+        return Response(OrganisationSerializerFull(instance=self.get_object()).data, status=status.HTTP_200_OK)
 
 
 class InviteViewSet(viewsets.ModelViewSet):
