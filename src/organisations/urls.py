e34976a7aac1dabca534630d7988363394a05d04
Matthew Elwell <matthew.elwell@solidstategroup.com>
2020-02-11 23:19:19 +0000

    Add organisation level webhooks and add previous state to feature state webhook

--- src/organisations/urls.py
+++ src/organisations/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework_nested import routers
 
-from organisations.views import InviteViewSet
+from organisations.views import InviteViewSet, OrganisationWebhookViewSet
 from users.views import FFAdminUserViewSet
 from . import views
 
@@ -11,6 +11,7 @@ router.register(r'', views.OrganisationViewSet, basename="organisation")
 organisations_router = routers.NestedSimpleRouter(router, r'', lookup="organisation")
 organisations_router.register(r'invites', InviteViewSet, basename="organisation-invites")
 organisations_router.register(r'users', FFAdminUserViewSet, basename='organisation-users')
+organisations_router.register(r'webhooks', OrganisationWebhookViewSet, basename='organisation-webhooks')
 
 app_name = "organisations"
 
