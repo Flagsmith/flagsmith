062f59e3cd7f1a0c69f96a219e0041d502631663
Matthew Elwell <matthewe@solidstategroup.com>
2020-04-20 14:16:22 +0100

    Add middleware for denying access to admin pages based on IP address

---
+++ src/app/middleware.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+
+
+class AdminWhitelistMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path.startswith('/admin'):
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            ip = x_forwarded_for.split(',')[0] if x_forwarded_for else request.META.get('REMOTE_ADDR')
+            if settings.ALLOWED_ADMIN_IP_ADDRESSES and ip not in settings.ALLOWED_ADMIN_IP_ADDRESSES:
+                # IP address not allowed!
+                raise PermissionDenied()
+
+        return self.get_response(request)
