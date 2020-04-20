b2123d2191a873a9cff2d613dd796f27497960c4
Matthew Elwell <matthewe@solidstategroup.com>
2020-04-20 12:28:56 +0100

    Make static files storage configurable

--- src/app/settings/common.py
+++ src/app/settings/common.py
@@ -314,3 +314,11 @@ CACHES = {
         'LOCATION': FLAGS_CACHE_LOCATION,
     }
 }
+
+if os.environ.get('USE_S3_STORAGE') in ['True', 'true']:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-west-2')
+    AWS_LOCATION = 'static'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ADDRESSING_STYLE = 'virtual'
