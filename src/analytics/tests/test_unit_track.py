b947436be45ae008555623deb2629ec7bd96db8f
Ben Rometsch <ben@solidstategroup.com>
2020-04-28 17:07:49 +0100

    More work on test for InfluxDB

--- src/analytics/tests/test_unit_track.py
+++ src/analytics/tests/test_unit_track.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 import pytest
 
-from analytics.track import track_request_googleanalytics
+from analytics.track import track_request_googleanalytics, track_request_influxdb
 
 
 @pytest.mark.parametrize("request_uri, expected_ga_requests", (
@@ -33,3 +33,26 @@ def test_track_request_googleanalytics(MockEnvironment, mock_requests, request_u
 
     # Then
     assert mock_requests.post.call_count == expected_ga_requests
+
+
+@pytest.mark.parametrize("request_uri", (
+        ("/api/v1/flags/"),
+        ("/api/v1/identities/"),
+        ("/api/v1/traits/"),
+        ("/api/v1/features/"),
+        ("/health")
+))
+@mock.patch("analytics.track.requests")
+@mock.patch("analytics.track.Environment")
+def test_track_request_influxdb(MockEnvironment, mock_requests, request_uri):
+    """
+    Verify that the correct number of calls are made to InfluxDB for the various uris.
+    """
+    # Given
+    request = mock.MagicMock()
+    request.path = request_uri
+    environment_api_key = "test"
+    request.headers = {"X-Environment-Key": environment_api_key}
+
+    # When
+    track_request_influxdb(request)
