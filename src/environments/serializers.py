e57f2686ed8de1e5f1c80ee8ff5c4d979ed35096
Ben Rometsch <ben@solidstategroup.com>
2020-07-14 16:36:26 +0100

    Added serializers for API keys

--- src/environments/serializers.py
+++ src/environments/serializers.py
@@ -18,13 +18,13 @@ class EnvironmentSerializerFull(serializers.ModelSerializer):
 
     class Meta:
         model = Environment
-        fields = ('id', 'name', 'feature_states', 'project', 'api_key')
+        fields = ('id', 'name', 'feature_states', 'project', 'api_key', 'amplitude_api_key')
 
 
 class EnvironmentSerializerLight(serializers.ModelSerializer):
     class Meta:
         model = Environment
-        fields = ('id', 'name', 'api_key', 'project')
+        fields = ('id', 'name', 'api_key', 'project', 'amplitude_api_key')
         read_only_fields = ('api_key',)
 
     def create(self, validated_data):
