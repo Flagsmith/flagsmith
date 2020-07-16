77731c9a61f58fb54306ef899b479e4f633ca991
Matthew Elwell <matthewe@solidstategroup.com>
2020-07-16 20:25:07 +0100

    Use a separate transaction to verify if the project still exists

--- src/features/signals.py
+++ src/features/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.dispatch import receiver
 from simple_history.signals import post_create_historical_record
 
@@ -12,14 +13,14 @@ logger = get_logger(__name__)
 
 @receiver(post_create_historical_record, sender=HistoricalFeatureSegment)
 def create_feature_segment_audit_log(instance, history_user, history_instance, **kwargs):
-    # check if the signal has been triggered by the feature segment being deleted
     deleted = history_instance.history_type == "-"
 
-    # if the feature segment has been deleted, this could have been from a cascade delete. We need to verify that
-    # the project still exists.
+    # if the feature segment has been deleted, this could have been a cascade delete from the project being deleted
+    # if it is, then we can skip creating the audit log.
     project = instance.feature.project
-    if deleted and not Project.objects.filter(id=project.id).exists():
-        project = None
+    with transaction.atomic():
+        if deleted and not Project.objects.filter(id=project.id).exists():
+            return
 
     message = FEATURE_SEGMENT_UPDATED_MESSAGE % (instance.feature.name, instance.environment.name)
     AuditLog.create_record(
@@ -27,5 +28,5 @@ def create_feature_segment_audit_log(instance, history_user, history_instance, *
         obj_type=RelatedObjectType.FEATURE,
         log_message=message,
         author=history_user,
-        project=project
+        project=instance.feature.project
     )
