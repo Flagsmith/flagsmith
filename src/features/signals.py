ed26156e73f53de2c9ce5df296d0541730eb661b
Matthew Elwell <matthewe@solidstategroup.com>
2020-07-16 20:09:21 +0100

    Fix issue with creating audit log records when cascade deleting

--- src/features/signals.py
+++ src/features/signals.py
@@ -2,6 +2,7 @@ from django.dispatch import receiver
 from simple_history.signals import post_create_historical_record
 
 from audit.models import AuditLog, RelatedObjectType, FEATURE_SEGMENT_UPDATED_MESSAGE
+from projects.models import Project
 from util.logging import get_logger
 # noinspection PyUnresolvedReferences
 from .models import HistoricalFeatureSegment
@@ -10,12 +11,21 @@ logger = get_logger(__name__)
 
 
 @receiver(post_create_historical_record, sender=HistoricalFeatureSegment)
-def create_feature_segment_audit_log(instance, history_user, **kwargs):
+def create_feature_segment_audit_log(instance, history_user, history_instance, **kwargs):
+    # check if the signal has been triggered by the feature segment being deleted
+    deleted = history_instance.history_type == "-"
+
+    # if the feature segment has been deleted, this could have been from a cascade delete. We need to verify that
+    # the project still exists.
+    project = instance.feature.project
+    if deleted and not Project.objects.filter(id=project.id).exists():
+        project = None
+
     message = FEATURE_SEGMENT_UPDATED_MESSAGE % (instance.feature.name, instance.environment.name)
     AuditLog.create_record(
         obj=instance.feature,
         obj_type=RelatedObjectType.FEATURE,
         log_message=message,
         author=history_user,
-        project=instance.feature.project
+        project=project
     )
