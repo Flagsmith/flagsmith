3e05871b94870511623d59ab351a089ffc059a1e
Matthew Elwell <matthewe@solidstategroup.com>
2020-09-07 15:05:36 +0000

    Fix issue deleting an organisation

--- src/features/signals.py
+++ src/features/signals.py
@@ -1,9 +1,7 @@
-from django.db import transaction
 from django.dispatch import receiver
 from simple_history.signals import post_create_historical_record
 
 from audit.models import AuditLog, RelatedObjectType, FEATURE_SEGMENT_UPDATED_MESSAGE
-from projects.models import Project
 from util.logging import get_logger
 # noinspection PyUnresolvedReferences
 from .models import HistoricalFeatureSegment
@@ -13,14 +11,10 @@ logger = get_logger(__name__)
 
 @receiver(post_create_historical_record, sender=HistoricalFeatureSegment)
 def create_feature_segment_audit_log(instance, history_user, history_instance, **kwargs):
-    deleted = history_instance.history_type == "-"
-
-    # if the feature segment has been deleted, this could have been a cascade delete from the project being deleted
-    # if it is, then we can skip creating the audit log.
-    project = instance.feature.project
-    with transaction.atomic():
-        if deleted and not Project.objects.filter(id=project.id).exists():
-            return
+    # due to referential integrity issues that come from cascade deletes, we skip creating
+    # audit logs for deleted feature segments for now
+    # TODO: handle audit log in middleware instead
+    project = None if history_instance.history_type == "-" else instance.feature.project
 
     message = FEATURE_SEGMENT_UPDATED_MESSAGE % (instance.feature.name, instance.environment.name)
     AuditLog.create_record(
@@ -28,5 +22,5 @@ def create_feature_segment_audit_log(instance, history_user, history_instance, *
         obj_type=RelatedObjectType.FEATURE,
         log_message=message,
         author=history_user,
-        project=instance.feature.project
+        project=project
     )
