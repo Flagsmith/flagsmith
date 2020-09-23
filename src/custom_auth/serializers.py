7267871c80dd0639493133d41f52ede46af13713
Pavlo Maks <pavlo@solidstategroup.com>
2020-09-23 16:25:47 +0100

    return api key on account register

--- src/custom_auth/serializers.py
+++ src/custom_auth/serializers.py
@@ -12,8 +12,7 @@ class CustomTokenSerializer(serializers.ModelSerializer):
 class CustomUserCreateSerializer(UserCreateSerializer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # not returning key on registration as need to confirm account first
-        # self.fields["key"] = serializers.SerializerMethodField()
+        self.fields["key"] = serializers.SerializerMethodField()
 
     def get_key(self, instance):
         token, _ = Token.objects.get_or_create(user=instance)
