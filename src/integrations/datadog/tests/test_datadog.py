bbee7907b812adb2c8293e0146eed3d51d92c32e
Pavlo Maks <pavlo@solidstategroup.com>
2020-09-25 15:19:47 +0100

    unittests

--- src/integrations/datadog/tests/test_datadog.py
+++ src/integrations/datadog/tests/test_datadog.py
@@ -1,15 +1,76 @@
-from unittest import mock
+from unittest import mock, SkipTest
 
+from integrations.datadog.datadog import DataDogWrapper
 from pytest import fail
 
 
 # TODO: look into using https://github.com/betamaxpy/betamax or https://github.com/getsentry/responses
+@SkipTest
 @mock.patch("integrations.datadog.datadog.requests")
 def test_datadog_track_event(mock_requests):
     # TODO: implement me
     fail()
 
 
+@SkipTest
 def test_datadog_generate_event_data():
     # TODO: implement me
     fail()
+
+
+def test_datatog_when_generate_event_data_with_correct_values_then_success():
+    # Given
+    log = "some log data"
+    email = "tes@email.com"
+    env = "test"
+    data_dog = DataDogWrapper(base_url="http://test.com", api_key='123key')
+
+    # When
+    event_data = data_dog.generate_event_data(log=log,
+                                              email=email,
+                                              environment_name=env)
+
+    # Then
+    expected_event_text = f"{log} by user {email}"
+
+    assert event_data['text'] == expected_event_text
+    assert len(event_data['tags']) == 1
+    assert event_data['tags'][0] == "env:" + env
+
+
+def test_datatog_when_generate_event_data_with_with_missing_values_then_success():
+    # Given no log or email data
+    log = None
+    email = None
+    env = "test"
+    data_dog = DataDogWrapper(base_url="http://test.com", api_key='123key')
+
+    # When
+    event_data = data_dog.generate_event_data(log=log,
+                                              email=email,
+                                              environment_name=env)
+
+    # Then
+    expected_event_text = f"{log} by user {email}"
+    assert event_data['text'] == expected_event_text
+    assert len(event_data['tags']) == 1
+    assert event_data['tags'][0] == f"env:{env}"
+
+
+def test_datatog_when_generate_event_data_with_with_missing_env_then_success():
+    # Given environment
+    log = "some log data"
+    email = "tes@email.com"
+    env = None
+    data_dog = DataDogWrapper(base_url="http://test.com", api_key='123key')
+
+    # When
+    event_data = data_dog.generate_event_data(log=log,
+                                              email=email,
+                                              environment_name=env)
+
+    # Then
+    expected_event_text = f"{log} by user {email}"
+    assert event_data['text'] == expected_event_text
+    assert len(event_data['tags']) == 1
+    assert event_data['tags'][0] == f"env:{env}"
