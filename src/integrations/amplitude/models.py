b601e8be8a9f3b6fa0a373a52ce38ff464f58a9e
Pavlo Maks <pavlo@solidstategroup.com>
2020-09-28 16:58:54 +0100

    amplitude config model

---
+++ src/integrations/amplitude/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from environments.models import Environment
+
+
+class AmplitudeConfiguration(models.Model):
+    api_key = models.CharField(max_length=100, blank=False, null=False)
+    organisation = models.OneToOneField(Environment, related_name='amplitude_config', on_delete=models.CASCADE)
+    created_date = models.DateTimeField('DateCreated', auto_now_add=True)
