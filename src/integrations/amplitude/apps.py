1f3a4ca677f957e915fdce715e5a416f0fc02f84
Pavlo Maks <pavlo@solidstategroup.com>
2020-09-29 14:52:52 +0100

    removed not needed files

--- src/integrations/amplitude/apps.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.apps import AppConfig
-
-
-class AmplitudeIntegrationConfig(AppConfig):
-    name = 'integrations.amplitude'
--- src/integrations/amplitude/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from integrations.amplitude.models import AmplitudeConfiguration
-
-
-class AmplitudeConfigurationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AmplitudeConfiguration
-        fields = ('api_key')
