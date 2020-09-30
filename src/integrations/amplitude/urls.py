cd48a37a16173f0db55745aabb7169ecc7f5f1e3
Pavlo Maks <pavlo@solidstategroup.com>
2020-09-30 15:50:50 +0100

    code cleanup

--- src/integrations/amplitude/urls.py
+++ src/integrations/amplitude/urls.py
@@ -1,16 +1,2 @@
-from django.conf.urls import url, include
-from django.urls import path
-from rest_framework import routers
-
-from .views import AmplitudeConfigurationViewSet
-
-# app_name = "amplitude"
-
-router = routers.DefaultRouter()
-router.register(r'amplitude', AmplitudeConfigurationViewSet)
-# router.register(r'amplitude', AmplitudeConfigurationViewSet, basename='amplitude')
-
-urlpatterns = [
-    url(r'^integrations/', include(router.urls))
-    # path('', include(router.urls))
-]
+# We dont need to setup routes here as
+# we nesting them under environments
