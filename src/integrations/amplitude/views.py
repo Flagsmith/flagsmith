40e240de9defdcf2dc41cc6e28bd1a98cc7ee00e
Pavlo Maks <pavlo@solidstategroup.com>
2020-10-01 15:53:51 +0100

    extra check for duplicate on create

--- src/integrations/amplitude/views.py
+++ src/integrations/amplitude/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 
+from environments.models import Environment
 from integrations.amplitude.serializers import AmplitudeConfigurationSerializer
 from .models import AmplitudeConfiguration
 
@@ -14,12 +16,22 @@ class AmplitudeConfigurationViewSet(viewsets.ModelViewSet):
         environment = get_object_or_404(self.request.user.get_permitted_environments(['VIEW_ENVIRONMENT']),
                                         api_key=environment_api_key)
 
-        return AmplitudeConfiguration.objects.filter(environment)
+        return AmplitudeConfiguration.objects.filter(environment=environment)
 
     def perform_create(self, serializer):
         environment = self.get_environment_from_request()
+
+        if AmplitudeConfiguration.objects.filter(environment=environment).exists():
+            raise ValidationError("AmplitudeConfiguration for environment already exist.")
+
         serializer.save(environment=environment)
 
     def perform_update(self, serializer):
         environment = self.get_environment_from_request()
         serializer.save(environment=environment)
+
+    def get_environment_from_request(self):
+        """
+        Get environment object from URL parameters in request.
+        """
+        return Environment.objects.get(api_key=self.kwargs["environment_api_key"])
