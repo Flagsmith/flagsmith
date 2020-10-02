dea99951d8e99a0470d0bcff551ebef36e0fee8b
Pavlo Maks <pavlo@solidstategroup.com>
2020-10-02 13:29:50 +0100

    implemented get events for organisation

--- src/analytics/influxdb_wrapper.py
+++ src/analytics/influxdb_wrapper.py
@@ -1,13 +1,17 @@
 from django.conf import settings
+from influxdb_client import InfluxDBClient
 from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
-from influxdb_client import InfluxDBClient
 
+url = settings.INFLUXDB_URL
+token = settings.INFLUXDB_TOKEN
+org = settings.INFLUXDB_ORG
+read_bucket = settings.INFLUXDB_BUCKET + "_downsampled_15m"
 
 influxdb_client = InfluxDBClient(
-    url=settings.INFLUXDB_URL,
-    token=settings.INFLUXDB_TOKEN,
-    org=settings.INFLUXDB_ORG
+    url=url,
+    token=token,
+    org=org
 )
 
 
@@ -28,3 +32,23 @@ class InfluxDBWrapper:
 
     def write(self):
         self.write_api.write(bucket=settings.INFLUXDB_BUCKET, record=self.record)
+
+    def get_events_for_organisation(self, organisation_id):
+        query_api = influxdb_client.query_api()
+        query = ' from(bucket:"%s") \
+                |> range(start: -30d, stop: now()) \
+                |> filter(fn:(r) => r._measurement == "api_call") \
+                |> filter(fn: (r) => r["_field"] == "request_count") \
+                |> filter(fn: (r) => r["organisation_id"] == "%s") \
+                |> drop(columns: ["organisation", "resource",  "project", "project_id"]) \
+                |> sum()' % (read_bucket, organisation_id)
+
+        # we should get only one record back
+        # just in case iterate over and sum them up
+        result = query_api.query(org=org, query=query)
+        total = 0
+        for table in result:
+            for record in table.records:
+                total += record.get_value()
+
+        return total
