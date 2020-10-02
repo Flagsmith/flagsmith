f13dd69e4dae62e0d95946ecfafe0857c4fcd872
Pavlo Maks <pavlo@solidstategroup.com>
2020-10-02 13:37:05 +0100

    added unit test

--- src/analytics/tests/test_influxdb_wrapper.py
+++ src/analytics/tests/test_influxdb_wrapper.py
@@ -2,7 +2,7 @@ from unittest import mock
 
 import analytics
 from analytics.influxdb_wrapper import InfluxDBWrapper
-
+from influxdb_client import InfluxDBClient
 
 def test_write(monkeypatch):
     # Given
@@ -19,3 +19,22 @@ def test_write(monkeypatch):
 
     # Then
     mock_write_api.write.assert_called()
+
+
+def test_query(monkeypatch):
+    # Given
+    mock_influxdb_client = mock.MagicMock()
+    monkeypatch.setattr(analytics.influxdb_wrapper, "influxdb_client", mock_influxdb_client)
+    monkeypatch.setattr(analytics.influxdb_wrapper, "read_bucket", "test_downsampled_15m")
+
+    mock_query_api = mock.MagicMock()
+    mock_influxdb_client.query_api.return_value = mock_query_api
+
+    influxdb = InfluxDBWrapper("name", "field_name", "field_value")
+
+    # When
+    influxdb.get_events_for_organisation(123)
+
+    # Then
+    mock_query_api.query.assert_called()
+
