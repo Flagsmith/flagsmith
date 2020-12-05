3d718173d29cc3b43e242840e13d63d892e859b7
Matthew Elwell <matthewe@solidstategroup.com>
2020-12-05 14:43:56 +0000

    Refactor sentry integration

--- src/app/settings/common.py
+++ src/app/settings/common.py
@@ -18,10 +18,8 @@ from importlib import reload
 import dj_database_url
 import environ
 import requests
-import sentry_sdk
 from corsheaders.defaults import default_headers
 from django.core.management.utils import get_random_secret_key
-from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env()
 
@@ -120,6 +118,7 @@ INSTALLED_APPS = [
     # Third party integrations
     "integrations.datadog",
     "integrations.amplitude",
+    "integrations.sentry",
     # Rate limiting admin endpoints
     "axes",
 ]
@@ -408,13 +407,4 @@ AXES_BLACKLISTED_URLS = [
 
 # Sentry tracking
 SENTRY_SDK_DSN = env("SENTRY_SDK_DSN", default=None)
-if SENTRY_SDK_DSN:
-    sentry_sdk.init(
-        dsn=env.str("SENTRY_SDK_DSN"),
-        integrations=[DjangoIntegration()],
-        traces_sample_rate=env.float("SENTRY_TRACE_SAMPLE_RATE", default=1.0),
-        environment=env("ENVIRONMENT", default="unknown"),
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True,
-    )
+SENTRY_TRACE_SAMPLE_RATE = env.float("SENTRY_TRACE_SAMPLE_RATE", default=1.0)
---
+++ src/integrations/sentry/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "apps.integrations.sentry.SentryConfig"
