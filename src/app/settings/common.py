b0da934ebe0551ed72d6941dc04ad74712f3a45b
Ben Rometsch <ben@solidstategroup.com>
2020-12-07 19:17:27 +0000

    Temporarily removed axes due to login issues

--- src/app/settings/common.py
+++ src/app/settings/common.py
@@ -156,7 +156,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
-    "app.middleware.AxesMiddleware",
+    #"app.middleware.AxesMiddleware",
 ]
 
 if GOOGLE_ANALYTICS_KEY:
