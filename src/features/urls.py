d2a54a813fe250e1418602bce3d653ea1116a027
Matthew Elwell <matthewe@solidstategroup.com>
2021-03-17 21:22:12 +0000

    Feature/improve feature state viewset

--- src/features/urls.py
+++ src/features/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework_nested import routers
 
-from features.views import FeatureStateCreateViewSet
+from features.views import SimpleFeatureStateViewSet
 from features.feature_segments.views import FeatureSegmentViewSet
 
 router = routers.DefaultRouter()
-router.register(r"featurestates", FeatureStateCreateViewSet, basename="featurestates")
+router.register(r"featurestates", SimpleFeatureStateViewSet, basename="featurestates")
 router.register(r"feature-segments", FeatureSegmentViewSet, basename="feature-segment")
 
 
