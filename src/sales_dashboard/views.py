5372624e6bfd2a53be622c8610053a5e9c8b0f4b
Matthew Elwell <matthewe@solidstategroup.com>
2021-03-24 20:30:19 +0000

    Add influx data to sales dashboard

--- src/sales_dashboard/views.py
+++ src/sales_dashboard/views.py
@@ -2,18 +2,20 @@ import json
 
 from app_analytics.influxdb_wrapper import (
     get_event_list_for_organisation,
+    get_top_organisations,
     get_events_for_organisation,
 )
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
-from django.core.paginator import Paginator
-from django.db.models import Count, Q
+from django.db.models import Count, Q, When, Case, Value, IntegerField
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template import loader
 from django.utils.safestring import mark_safe
+
 from django.views.generic import ListView
 
-from organisations.models import Organisation, UserOrganisation
+from organisations.models import Organisation
 from projects.models import Project
 from users.models import FFAdminUser
 
@@ -27,14 +29,27 @@ class OrganisationList(ListView):
 
     def get_queryset(self):
         queryset = Organisation.objects.annotate(
-            num_projects=Count("projects", distinct=True),
-            num_users=Count("users", distinct=True),
-            num_features=Count("projects__features", distinct=True),
-            num_segments=Count("projects__segments", distinct=True),
-        ).all()
+            num_projects=Count("projects"),
+            num_users=Count("users"),
+            num_flags=Count("projects__features"),
+            num_segments=Count("projects__segments"),
+        )
+
+        # Annotate the queryset with the organisations usage for the given time periods
+        # and order the queryset with it.
+        if settings.INFLUXDB_TOKEN:
+            for date_range, limit in (("30d", ""), ("7d", ""), ("24h", "100")):
+                key = f"num_{date_range}_calls"
+                org_calls = get_top_organisations(date_range, limit)
+                if org_calls:
+                    whens = [When(id=k, then=Value(v)) for k, v in org_calls.items()]
+                    queryset = queryset.annotate(
+                        **{key: Case(*whens, default=0, output_field=IntegerField())}
+                    ).order_by(f"-{key}")
 
         if "search" in self.request.GET:
-            queryset = queryset.filter(name__icontains=self.request.GET["search"])
+            search_term = self.request.GET["search"]
+            queryset = queryset.filter(name__icontains=search_term)
 
         return queryset
 
@@ -66,6 +81,12 @@ def organisation_info(request, organisation_id):
         "identities": mark_safe(json.dumps(event_list["identities"])),
         "flags": mark_safe(json.dumps(event_list["flags"])),
         "labels": mark_safe(json.dumps(labels)),
+        "api_calls": {
+            # TODO: this could probably be reduced to a single influx request
+            #  rather than 3
+            range_: get_events_for_organisation(organisation_id, date_range=range_)
+            for range_ in ("24h", "7d", "30d")
+        },
     }
 
     return HttpResponse(template.render(context, request))
