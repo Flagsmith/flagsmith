399e2a63c174e07a850788f87b3066eebf4361e7
Ben Rometsch <ben@solidstategroup.com>
2021-03-26 10:23:46 +0000

    Merge branch 'bug/fix-docker-gunicorn-threads' into 'develop'
    Fixed gunicorn threads command line
    
    See merge request flagsmith/bullet-train-api!317

--- src/integrations/common/models.py
+++ src/integrations/common/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class IntegrationsModel(models.Model):
-    base_url = models.URLField(blank=False, null=False)
+    base_url = models.URLField(blank=False, null=True)
     api_key = models.CharField(max_length=100, blank=False, null=False)
 
     class Meta:
---
+++ src/integrations/mixpanel/apps.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class MixpanelIntegrationConfig(AppConfig):
+    name = "integrations.mixpanel"
