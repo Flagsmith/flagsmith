594b2af16e3ee4461aeb19862c1cad42cd4fee55
Matthew Elwell <matthewe@solidstategroup.com>
2021-05-21 17:32:40 +0000

    Prevent user registration without invite

--- src/custom_auth/oauth/serializers.py
+++ src/custom_auth/oauth/serializers.py
@@ -1,9 +1,14 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import PermissionDenied
 
-from custom_auth.oauth.github import GithubUser
-from custom_auth.oauth.google import get_user_info
+from organisations.invites.models import Invite
+
+from ..constants import USER_REGISTRATION_WITHOUT_INVITE_ERROR_MESSAGE
+from .github import GithubUser
+from .google import get_user_info
 
 GOOGLE_URL = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&"
 UserModel = get_user_model()
@@ -19,10 +24,24 @@ class OAuthLoginSerializer(serializers.Serializer):
         abstract = True
 
     def create(self, validated_data):
-        user_data = self.get_user_info()
-        email = user_data.pop("email")
-        user, _ = UserModel.objects.get_or_create(email=email, defaults=user_data)
+        user = self._get_user()
         return Token.objects.get_or_create(user=user)[0]
+
+    def _get_user(self):
+        user_data = self.get_user_info()
+        email = user_data.get("email")
+        existing_user = UserModel.objects.filter(email=email).first()
+
+        if not existing_user:
+            if not (
+                settings.ALLOW_REGISTRATION_WITHOUT_INVITE
+                or Invite.objects.filter(email=email).exists()
+            ):
+                raise PermissionDenied(USER_REGISTRATION_WITHOUT_INVITE_ERROR_MESSAGE)
+
+            return UserModel.objects.create(**user_data)
+
+        return existing_user
 
     def get_user_info(self):
         raise NotImplementedError("`get_user_info()` must be implemented.")
