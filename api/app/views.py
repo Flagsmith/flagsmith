593bee3fd5bad02c66c37d1cc55979f9896d22e5
Ben Rometsch <ben.rometsch@gmail.com>
2021-07-05 18:14:55 +0100

    Settings overrides

--- api/app/views.py
+++ api/app/views.py
@@ -19,15 +19,39 @@ def index(request):
     #    template = loader.get_template("index.html")
 
     context = {
-        # "linkedin_api_key": settings.FRONTEND_LINKEDIN,
+        "linkedin_api_key": settings.FRONTEND_LINKEDIN_API_KEY,
     }
 
     return HttpResponse(template.render(context, request))
 
 
 def project_overrides(request):
-    project_overrides = {}
-    project_overrides["api"] = settings.FRONTEND_API_URL
-    project_overrides["assetURL"] = settings.FRONTEND_ASSET_URL
+    config_mapping_dict = {
+        "FRONTEND_API_URL": "api",
+        "FRONTEND_MAINTENANCE_MODE": "maintenance",
+        "FRONTEND_ASSET_URL": "assetURL",
+        "FRONTEND_LINKEDIN_API_KEY": "linkedin_api_key",
+        "FRONTEND_PREVENT_SIGNUP": "preventSignup",
+        "FRONTEND_FLAGSMITH_ON_FLAGSMITH_API_URL": "flagsmith",
+        "FRONTEND_GOOGLE_ANALYTICS_API_KEY": "ga",
+        "FRONTEND_CRISP_CHAT_API_KEY": "crispChat",
+        "FRONTEND_LINKEDIN_API_KEY": "sha",
+        "FRONTEND_MIXPANEL_API_KEY": "mixpanel",
+        "FRONTEND_SENTRY_API_KEY": "sentry",
+        "FRONTEND_FLAGSMITH_ON_FLAGSMITH_API_KEY": "flagsmithClientAPI",
+        "FRONTEND_DISABLE_INFLUXDB_FEATURES": "disableInflux",
+        "FRONTEND_FLAGSMITH_ANALYTICS": "flagsmithAnalytics",
+        "FRONTEND_AMPLITUDE_API_KEY": "amplitude",
+    }
 
-    return HttpResponse("window.projectOverrides = " + json.dumps(project_overrides))
+    project_overrides = {}
+
+    for key, value in config_mapping_dict.items():
+        settings_value = getattr(settings, key, None)
+        if settings_value:
+            project_overrides[value] = settings_value
+
+    return HttpResponse(
+        "window.projectOverrides = "
+        + json.dumps(project_overrides, indent=4, separators=(",", ":"))
+    )
