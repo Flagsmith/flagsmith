1eaec5853e1c8e97614ac13d5181913d82f9fc9d
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-07-20 18:26:05 +0530

    test(audit_logs): Add test to check the number of query executed
    The test will make sure that we are not executing more than 2 queries
    in the API(one for the result and the other one for pagination)

---
+++ api/tests/integration/audit/test_audit_logs.py
@@ -0,0 +1,13 @@
+from django.urls import reverse
+from rest_framework import status
+
+
+def test_audit_logs_only_makes_two_queries(
+    admin_client, project, environment, django_assert_num_queries
+):
+    url = reverse("api-v1:audit-list")
+
+    with django_assert_num_queries(2):
+        res = admin_client.get(url, {"project": project})
+
+    assert res.status_code == status.HTTP_200_OK
