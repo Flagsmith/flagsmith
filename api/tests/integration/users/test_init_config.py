7137f2bcef096af564d7b404cf78b236192c4b40
gagandeep trivedi <gagandeeptrivedi47@gmail.com>
2021-08-02 16:39:45 +0530

    Create a Form view to help create a superuser over http (#230)
    * feat(github-162): Add api to help intialize the project
    
    The api can be used to create the first superuser and
    update the site model with name and doamin
    
    * fixup! feat(github-162): Add api to help intialize the project
    
    * Use signals instead of apps ready
    
    * fixup! fix signal
    
    * config_init: Return 404 when user already exists
    
    * test(config-init): Add integration tests
    
    * tests(config/init): Remove unit style testcases
    
    * fixup! test(config-init): Add integration tests
    
    * Add test cases for valid form submission
    
    * Add test case for invalid form
    
    * change the warning from user to runtime
    
    * Add test for warn_insecure
    
    * fixup! Add test case for invalid form
    
    * fixup! Add test case for invalid form
    
    Co-authored-by: Matthew Elwell <matthewe@solidstategroup.com>

---
+++ api/tests/integration/users/test_init_config.py
@@ -0,0 +1,73 @@
+import pytest
+from django.contrib.sites.models import Site
+from django.urls import reverse
+from rest_framework import status
+
+from users.models import FFAdminUser
+
+
+def test_returns_404_when_user_exists(admin_user, django_client):
+    # Given
+    url = reverse("api-v1:users:config-init")
+
+    # When
+    get_response = django_client.get(url)
+    post_response = django_client.post(url)
+
+    # Then
+    assert get_response.status_code == status.HTTP_404_NOT_FOUND
+    assert post_response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db()
+def test_returns_200_when_no_user_exists(django_client):
+    # Given
+    url = reverse("api-v1:users:config-init")
+
+    # When
+    response = django_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_valid_request_creates_admin_and_updates_site(db, django_client):
+    # Given
+    url = reverse("api-v1:users:config-init")
+    form_data = {
+        "username": "test-admin",
+        "email": "test@email.com",
+        "password": "test123",
+        "site_name": "test_site",
+        "site_domain": "test.com",
+    }
+
+    # When
+    response = django_client.post(url, data=form_data)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert FFAdminUser.objects.filter(email=form_data.get("email")).count() == 1
+    assert Site.objects.filter(name=form_data.get("site_name")).count() == 1
+
+
+def test_invalid_form_does_not_change_anything(db, django_client):
+    # Given
+    url = reverse("api-v1:users:config-init")
+    form_data = {
+        "username": "test-admin",
+        "email": "invalid_email",  # Invalid email
+        "password": "test123",
+        "site_name": "test_site",
+        "site_domain": "test.com",
+    }
+
+    # When
+    response = django_client.post(url, data=form_data)
+
+    # Then
+    # Re renders the form with error
+    assert response.status_code == status.HTTP_200_OK
+
+    assert Site.objects.filter(name=form_data.get("site_name")).count() == 0
+    assert FFAdminUser.objects.filter(email=form_data.get("email")).count() == 0
--- api/users/apps.py
+++ api/users/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+from .signals import warn_insecure
 
 
-class AuthenticationConfig(AppConfig):
+class UsersConfig(AppConfig):
     name = "users"
+
+    def ready(self):
+        post_migrate.connect(warn_insecure, self)
