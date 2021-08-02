96f50065021f2556017384b602564354379dc9bd
gagandeep trivedi <gagandeeptrivedi47@gmail.com>
2021-08-02 18:29:26 +0530

    Add an API endpoint to support cloning of environment  (#203)
    * feat(env_clone): Add an api to support environment cloning
    
    * fixup! feat(env_clone): Add an api to support environment cloning
    
    * docs(env_clone): fix swagger docs
    
    * squash! feat(env_clone): Add an api to support environment cloning
    
    * test(integration): Create a fixture to return env dict response
    
    * test(clone_env): Add integration test to check 200
    
    * test: remove environment_dict fixture
    
    * refactor(clone-env): move clonning logic to serializer
    
    * fix: Add permission check for env clone action
    
    * refactor(clone-env): move permission check to has_permission
    
    * fixup! refactor(clone-env): move permission check to has_permission
    
    * squash! refactor permission for clone
    
    * test(env_clone): Add test to check sourece state env after clone
    
    * wip: permission changes
    
    * use Q objects to generate filter

--- api/environments/tests/test_models.py
+++ api/environments/tests/test_models.py
@@ -55,6 +55,47 @@ class EnvironmentTestCase(TestCase):
         self.environment.save()
         self.assertFalse(FeatureState.objects.get().enabled)
 
+    def test_clone_does_not_modify_the_original_instance(self):
+        # Given
+        self.environment.save()
+
+        # When
+        clone = self.environment.clone(name="Cloned env")
+        clone.save()
+
+        # Then
+        self.assertNotEqual(clone.name, self.environment.name)
+        self.assertNotEqual(clone.api_key, self.environment.api_key)
+
+    def test_clone_save_creates_feature_states(self):
+        # Given
+        self.environment.save()
+
+        # When
+        clone = self.environment.clone(name="Cloned env")
+        clone.save()
+
+        # Then
+        feature_states = FeatureState.objects.filter(environment=clone)
+        assert feature_states.count() == 1
+
+    def test_clone_does_not_modify_source_feature_state(self):
+        # Given
+        self.environment.save()
+        source_feature_state_before_clone = FeatureState.objects.filter(
+            environment=self.environment
+        ).first()
+
+        # When
+        clone = self.environment.clone(name="Cloned env")
+        clone.save()
+        source_feature_state_after_clone = FeatureState.objects.filter(
+            environment=self.environment
+        ).first()
+
+        # Then
+        assert source_feature_state_before_clone == source_feature_state_after_clone
+
     @mock.patch("environments.models.environment_cache")
     def test_get_from_cache_stores_environment_in_cache_on_success(self, mock_cache):
         # Given
