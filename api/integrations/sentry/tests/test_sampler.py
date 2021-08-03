24f717608bfa275c18d0915671c5ae9df23f8e7f
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-08-03 14:48:43 +0530

    squash! use @override_settings for updating setting

--- api/integrations/sentry/tests/test_sampler.py
+++ api/integrations/sentry/tests/test_sampler.py
@@ -1,12 +1,12 @@
-from django.conf import settings
+from django.test import override_settings
 
 from integrations.sentry.samplers import block_health_sampler
 
+SAMPLE_RATE = 10
 
+
+@override_settings(SENTRY_TRACE_SAMPLE_RATE=SAMPLE_RATE)
 def test_block_health_sampler_empty_context_returns_defaut_sample_rate():
-    # Given
-    settings.SENTRY_TRACE_SAMPLE_RATE = 10
-
     # When
     sample_rate = block_health_sampler({})
 
@@ -14,9 +14,9 @@ def test_block_health_sampler_empty_context_returns_defaut_sample_rate():
     assert sample_rate == 10
 
 
+@override_settings(SENTRY_TRACE_SAMPLE_RATE=SAMPLE_RATE)
 def test_block_health_sampler_returns_0_for_health_check_transaction():
     # Given
-    settings.SENTRY_TRACE_SAMPLE_RATE = 10
     ctx = {"wsgi_environ": {"PATH_INFO": "/health"}}
 
     # When
@@ -26,9 +26,9 @@ def test_block_health_sampler_returns_0_for_health_check_transaction():
     assert sample_rate == 0
 
 
+@override_settings(SENTRY_TRACE_SAMPLE_RATE=SAMPLE_RATE)
 def test_block_health_sampler_returns_default_for_non_heath_check_transaction():
     # Given
-    settings.SENTRY_TRACE_SAMPLE_RATE = 10
     ctx = {"wsgi_env": {"PATH_INFO": "/home"}}
 
     # When
