05b3975e01579e635dcc1db8128fa8d0cb83ce3d
Ben Rometsch <ben.rometsch@flagsmith.com>
2021-08-03 12:43:07 +0100

    improvement/fixup healthcheck pr

--- api/integrations/sentry/tests/test_sampler.py
+++ api/integrations/sentry/tests/test_sampler.py
@@ -11,7 +11,7 @@ def test_block_health_sampler_empty_context_returns_defaut_sample_rate():
     sample_rate = block_health_sampler({})
 
     # Then
-    assert sample_rate == 10
+    assert sample_rate == SAMPLE_RATE
 
 
 @override_settings(SENTRY_TRACE_SAMPLE_RATE=SAMPLE_RATE)
@@ -35,4 +35,4 @@ def test_block_health_sampler_returns_default_for_non_heath_check_transaction():
     sample_rate = block_health_sampler(ctx)
 
     # Then
-    assert sample_rate == 10
+    assert sample_rate == SAMPLE_RATE
