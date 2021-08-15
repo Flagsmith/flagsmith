61887b7b6911b2a752fa5907559a09e2790af630
kyle-ssg <kyle@solidstategroup.com>
2021-08-15 16:30:52 +0100

    PAYMENTS_ENABLED ENV VAR

--- frontend/bin/yaml-env.js
+++ frontend/bin/yaml-env.js
@@ -14,6 +14,7 @@ env_variables:
   EVENTS_SLACK_CHANNEL: ${process.env.EVENTS_SLACK_CHANNEL}
   FLAGSMITH: ${process.env.FLAGSMITH}
   FLAGSMITH_ANALYTICS: ${process.env.FLAGSMITH_ANALYTICS}
+  PAYMENTS_ENABLED: ${process.env.PAYMENTS_ENABLED}
   GA: ${process.env.GA}
   CRISP_CHAT: ${process.env.CRISP_CHAT}
   LINKEDIN: ${process.env.LINKEDIN}
--- frontend/server/index.js
+++ frontend/server/index.js
@@ -58,6 +58,7 @@ app.get('/static/project-overrides.js', (req, res) => {
         { name: 'maintenance', value: process.env.MAINTENANCE },
         { name: 'assetURL', value: process.env.ASSET_URL },
         { name: 'flagsmithClientAPI', value: process.env.FLAGSMITH_CLIENT_API },
+        { name: 'paymentsEnabled', value: process.env.PAYMENTS_ENABLED },
         { name: 'disableInflux', value: process.env.DISABLE_INFLUXDB_FEATURES },
         { name: 'flagsmithAnalytics', value: !!process.env.FLAGSMITH_ANALYTICS },
         { name: 'amplitude', value: process.env.AMPLITUDE },
