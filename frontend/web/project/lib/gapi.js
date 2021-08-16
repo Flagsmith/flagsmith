71bec1275dc8b06e5b2132d2ae8560253290411c
Kyle Johnson <kyle@solidstategroup.com>
2021-08-16 19:25:46 +0100

    Upgrade gapi (#269)

--- frontend/web/project/lib/gapi.js
+++ frontend/web/project/lib/gapi.js
@@ -1,18 +1,31 @@
-if (typeof gapi == "undefined") {
-  function async (u, c) {
-    var d = document, t = 'script',
-      o = d.createElement(t),
-      s = d.getElementsByTagName(t)[0];
-    o.src = '//' + u;
+function async(u, c) {
+    const d = document; const t = 'script';
+
+
+    const o = d.createElement(t);
+
+
+    const s = d.getElementsByTagName(t)[0];
+    o.src = `${u}`;
     if (c) {
-      o.addEventListener('load', function (e) {
-        c(null, e);
-      }, false);
+        o.addEventListener('load', (e) => {
+            c(null, e);
+        }, false);
     }
     s.parentNode.insertBefore(o, s);
-  }
+}
 
-  async('apis.google.com/js/api.js', function () {
-    gapi.load("client");
-  });
+export default function (clientId, cb) {
+    const el = document.createElement('meta');
+    el.name = 'google-signin-client_id';
+    el.content = `${clientId}.apps.googleusercontent.com`;
+    const instance = document.getElementsByTagName('meta')[0];
+    instance.parentNode.insertBefore(el, instance);
+    async('https://apis.google.com/js/platform.js', () => {
+        setTimeout(() => {
+            gapi.load('auth2', () => {
+              cb();
+            });
+        }, 0);
+    });
 }
