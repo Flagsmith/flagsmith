81f807a174efc5ad080c25cbe1f5dad852da4746
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-08-16 13:19:23 +0530

    update the serializer to join channel on update/create

--- api/integrations/slack/serializers.py
+++ api/integrations/slack/serializers.py
@@ -1,8 +1,11 @@
 from copy import deepcopy
 
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
 from integrations.slack.models import SlackConfiguration, SlackEnvironment
+
+from .slack import join_channel
 
 
 class SlackEnvironmentSerializer(serializers.ModelSerializer):
@@ -12,9 +15,22 @@ class SlackEnvironmentSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         data = deepcopy(validated_data)
-        config = SlackConfiguration.objects.get(project=data["environment"].project)
+        try:
+            config = SlackConfiguration.objects.get(project=data["environment"].project)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError(
+                "Slack api token not found. Please generate the token using oauth"
+            )
+
         data.update({"slack_configuration": config})
+        join_channel(config.api_token, validated_data["channel_id"])
         return SlackEnvironment.objects.create(**data)
+
+    def update(self, instance, validated_data):
+        join_channel(
+            instance.slack_configuration.api_token, validated_data["channel_id"]
+        )
+        return super().update(instance, validated_data)
 
 
 class SlackChannelListSerializer(serializers.Serializer):
