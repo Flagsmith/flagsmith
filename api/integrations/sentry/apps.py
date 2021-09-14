bb51d98869019dbd27d8f266762338f471063a63
Ben Rometsch <ben.rometsch@flagsmith.com>
2021-09-14 17:56:57 +0100

    chore/dont-track-homepage-with-sentry (#309)

--- api/integrations/sentry/apps.py
+++ api/integrations/sentry/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 from django.conf import settings
 from sentry_sdk.integrations.django import DjangoIntegration
 
-from .samplers import block_health_sampler
+from .samplers import block_non_functional_endpoints_sampler
 
 
 class SentryConfig(AppConfig):
@@ -15,7 +15,7 @@ class SentryConfig(AppConfig):
                 dsn=settings.SENTRY_SDK_DSN,
                 integrations=[DjangoIntegration()],
                 environment=settings.ENV,
-                traces_sampler=block_health_sampler,
+                traces_sampler=block_non_functional_endpoints_sampler,
                 # If you wish to associate users to errors (assuming you are using
                 # django.contrib.auth) you may enable sending PII data.
                 send_default_pii=True,
