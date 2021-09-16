fbb4fce57dafc0cc1f9fad0653d9043457e07f5b
Ben Rometsch <ben.rometsch@gmail.com>
2021-09-16 09:17:40 +0100

    Allow faster login attempts during test CI

--- api/app/settings/test.py
+++ api/app/settings/test.py
@@ -2,3 +2,19 @@ from app.settings.common import *  # noqa
 
 # We dont want to track tests
 ENABLE_TELEMETRY = False
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "PAGE_SIZE": 10,
+    "UNICODE_JSON": False,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_THROTTLE_RATES": {
+        "login": "100/min",
+        "mfa_code": "5/min",
+        "invite": "10/min",
+    },
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
