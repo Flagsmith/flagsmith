70c911b6c313466a0ed7cf00f8305484ba60d7d7
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-09-20 14:03:31 +0530

    use sentry to capture error raised by influx

--- api/app_analytics/influxdb_wrapper.py
+++ api/app_analytics/influxdb_wrapper.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 from django.conf import settings
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from sentry_sdk import capture_exception
 from urllib3 import Retry
 
 logger = logging.getLogger(__name__)
@@ -43,7 +44,7 @@ class InfluxDBWrapper:
         try:
             self.write_api.write(bucket=settings.INFLUXDB_BUCKET, record=self.records)
         except Exception as e:
-            logger.error(f"Write to influx DB failed with {e}")
+            capture_exception(e)
 
     @staticmethod
     def influx_query_manager(
@@ -68,7 +69,7 @@ class InfluxDBWrapper:
             result = query_api.query(org=influx_org, query=query)
             return result
         except Exception as e:
-            logger.error(f"Influx read query failed failed with {e}")
+            capture_exception(e)
             return []
 
 
