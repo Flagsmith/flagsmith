3a7b8dfa7e41e3e27f48f98bdf300fd171ae0073
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-09-20 15:28:23 +0530

    Remove redundant  pytest fixtures

--- api/features/tests/conftest.py
+++ api/features/tests/conftest.py
@@ -1,33 +1,9 @@
 import pytest
 
-from environments.identities.models import Identity
-from environments.models import Environment
 from features.feature_types import MULTIVARIATE
 from features.models import Feature
 from features.multivariate.models import MultivariateFeatureOption
-from features.value_types import STRING, INTEGER
-from organisations.models import Organisation
-from projects.models import Project
-
-
-@pytest.fixture()
-def organisation(db):
-    return Organisation.objects.create(name="Test Org")
-
-
-@pytest.fixture()
-def project(organisation):
-    return Project.objects.create(name="Test Project", organisation=organisation)
-
-
-@pytest.fixture()
-def environment(project):
-    return Environment.objects.create(name="Test Environment", project=project)
-
-
-@pytest.fixture()
-def identity(environment):
-    return Identity.objects.create(identifier="test_identity", environment=environment)
+from features.value_types import STRING
 
 
 @pytest.fixture()
