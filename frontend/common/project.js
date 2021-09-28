f65c56d68019d3254a103084c5e283e26e003282
Kyle Johnson <kyle@solidstategroup.com>
2021-09-28 20:10:18 +0100

    archived flags (#356)

--- frontend/common/project.js
+++ frontend/common/project.js
@@ -1,11 +1,10 @@
 module.exports = global.Project = {
-    api: 'https://api-dev.flagsmith.com/api/v1/',
+    api: 'https://api-staging.flagsmith.com/api/v1/',
     flagsmithClientAPI: 'https://api.flagsmith.com/api/v1/',
-    flagsmith: '8KzETdDeMY7xkqkSkY3Gsg', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
-    debug: false,
-    delighted: true, // determines whether to shw delighted feedback widget
-    env: 'dev', // This is used for Sentry tracking
+    flagsmith: 'ENktaJnfLVbLifybz34JmX', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
+    env: 'staging', // This is used for Sentry tracking
     maintenance: false, // trigger maintenance mode
+    delighted: true, // determines whether to shw delighted feedback widget
     demoAccount: {
         email: 'kyle+bullet-train@solidstategroup.com',
         password: 'demo_account',
