fc484829ac5ecbee3cd4bc5d4009017bec780e25
gagandeep trivedi <gagandeeptrivedi47@gmail.com>
2021-09-29 19:20:42 +0530

    Hotfix/trait (#374)
    * revert trait/models
    
    * fix tests
    
    * fixup! remove mock args

--- api/environments/identities/traits/tests/test_models.py
+++ api/environments/identities/traits/tests/test_models.py
@@ -64,41 +64,9 @@ def test_trait_bulk_create_create_objects(identity):
     assert Trait.objects.filter(identity=identity).count() == 2
 
 
-def test_trait_bulk_create_calls_put_item(identity):
-    # Given
-    with mock.patch(
-        "environments.identities.models.dynamo_identity_table"
-    ) as dynamo_identity_table:
-        traits = [
-            Trait(identity=identity, trait_key="key1"),
-            Trait(identity=identity, trait_key="key2"),
-        ]
-
-        # When
-        Trait.objects.bulk_create(traits)
-        # Then
-        identity_dict = build_identity_dict(identity)
-        dynamo_identity_table.put_item.assert_called_with(Item=identity_dict)
-
-
 def test_trait_bulk_delete_deletes_objects(trait):
     # When
     Trait.objects.filter(identity=trait.identity).delete()
 
     # Then
     Trait.objects.filter(identity=trait.identity).count() == 0
-
-
-def test_trait_bulk_delete_calls_put_item(trait):
-    # Given
-    with mock.patch(
-        "environments.identities.models.dynamo_identity_table"
-    ) as dynamo_identity_table:
-        # When
-        Trait.objects.filter(identity=trait.identity).delete()
-
-        # Then
-        identity_dict = build_identity_dict(trait.identity)
-        dynamo_identity_table.batch_writer.return_value.__enter__.return_value.put_item.assert_called_with(
-            Item=identity_dict
-        )
