b3caa203f6afa63ce227b08e59f4251fd034423c
Ben Rometsch <ben.rometsch@gmail.com>
2021-10-11 12:06:10 +0100

    Implemented another test

--- api/benchmarks/benchmarks.py
+++ api/benchmarks/benchmarks.py
@@ -1,24 +1,27 @@
 # Needs to be first to set up django environment
 from .helpers import *  # isort:skip
+import json
 
 from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APIClient
 
 from environments.identities.models import Identity
 from environments.models import Environment
-from features.models import Feature, FeatureSegment, FeatureState
+from features.models import Feature
 from organisations.models import Organisation
 from projects.models import Project
-from segments.models import Segment
 
 
 class PerformanceSuite:
+    test_data_count = 3
+    test_iteration_count = 400
+
     def setup(self):
         self.client = APIClient()
 
-        for test_data_count in range(3):
+        for test_data_count in range(self.test_data_count):
             self.organisation = Organisation.objects.create(
                 name="Performance Test Organisation " + str(test_data_count)
             )
@@ -30,12 +33,12 @@ class PerformanceSuite:
                 name="Performance Test Environment " + str(test_data_count),
                 project=self.project,
             )
-            for test_feature_count in range(10):
+            for test_feature_count in range(self.test_data_count):
                 self.feature = Feature.objects.create(
                     name="Test Feature " + str(test_feature_count), project=self.project
                 )
 
-        for test_identity_count in range(5):
+        for test_identity_count in range(self.test_data_count):
             self.identity = Identity.objects.create(
                 identifier="test-identity-" + str(test_identity_count),
                 environment=self.environment,
@@ -44,32 +47,57 @@ class PerformanceSuite:
     def time_get_environment_flags(self):
         # Given
         url = reverse("api-v1:flags")
-
-        # When
         self.client.credentials(
             HTTP_X_ENVIRONMENT_KEY=self.identity.environment.api_key
         )
 
-        # Then
-        for test_flags_count in range(400):
+        # When
+        for test_flags_count in range(self.test_iteration_count):
             response = self.client.get(url)
             assert response.status_code == status.HTTP_200_OK
 
+        # Then
         pass
 
     def time_get_identity_flags(self):
         # Given
         base_url = reverse("api-v1:sdk-identities")
         url = base_url + "?identifier=" + self.identity.identifier
-
-        # When
         self.client.credentials(
             HTTP_X_ENVIRONMENT_KEY=self.identity.environment.api_key
         )
 
-        # Then
-        for test_identity_count in range(400):
+        # When
+        for test_identity_count in range(self.test_iteration_count):
             response = self.client.get(url)
             assert response.status_code == status.HTTP_200_OK
 
+        # Then
+        pass
+
+    def time_set_identity_traits_flags(self):
+        # Given
+        url = reverse("api-v1:sdk-identities")
+
+        # a payload for an identity with 2 traits
+        data = {
+            "identifier": self.identity.identifier,
+            "traits": [
+                {"trait_key": "my_trait", "trait_value": 123},
+                {"trait_key": "my_other_trait", "trait_value": "a value"},
+            ],
+        }
+
+        # When
+        # we identify that user by posting the above payload
+        self.client.credentials(HTTP_X_ENVIRONMENT_KEY=self.environment.api_key)
+        for test_identity_count in range(self.test_iteration_count):
+            response = self.client.post(
+                url, data=json.dumps(data), content_type="application/json"
+            )
+            print(response.status_code)
+            print(response.data)
+            assert response.status_code == status.HTTP_200_OK
+
+        # Then
         pass
