bedcfece7cc0991f28e17651d96f409f38c2f66a
Ben Rometsch <ben.rometsch@gmail.com>
2021-10-14 14:39:41 +0100

    RE flake8

--- api/custom_auth/tests/end_to_end/test_custom_auth_integration.py
+++ api/custom_auth/tests/end_to_end/test_custom_auth_integration.py
@@ -66,7 +66,7 @@ class AuthIntegrationTestCase(APITestCase):
         # verify that the user has been emailed with their reset code
         assert len(mail.outbox) == 1
         # get the url and grab the uid and token
-        url = re.findall("http\:\/\/.*", mail.outbox[0].body)[0]
+        url = re.findall(r"http\:\/\/.*", mail.outbox[0].body)[0]
         split_url = url.split("/")
         uid = split_url[-2]
         token = split_url[-1]
