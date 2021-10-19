570b061849586ddfdf82aa9f1edd9dc7d93c4c09
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-10-19 20:47:25 +0530

    fix(auth/serializer): Add method to do case insensitive lookup
    Add validate_email that does a case insensitive  lookup
    using the email to see a user exists with that email, and returns
    the email with all lower case letters

---
+++ api/custom_auth/tests/test_serializer.py
@@ -0,0 +1,32 @@
+from custom_auth.serializers import CustomUserCreateSerializer
+from users.models import FFAdminUser
+
+user_dict = {
+    "email": "TestUser@mail.com",
+    "password": "pass@word123",
+    "first_name": "test",
+    "last_name": "user",
+}
+
+
+def test_CustomUserCreateSerializer_converts_email_to_lower_case(db):
+    # Given
+    serializer = CustomUserCreateSerializer(data=user_dict)
+    # When
+    serializer.is_valid(raise_exception=True)
+    # Then
+    assert serializer.validated_data["email"] == "testuser@mail.com"
+
+
+def test_CustomUserCreateSerializer_does_case_insensitive_lookup_with_email(db):
+
+    # Given
+    FFAdminUser.objects.create(email="testuser@mail.com")
+    serializer = CustomUserCreateSerializer(data=user_dict)
+
+    # When
+    assert serializer.is_valid() == False
+    assert (
+        serializer.errors["email"][0].title()
+        == "Feature Flag Admin User With This Email Already Exists."
+    )
