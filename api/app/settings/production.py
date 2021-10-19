567c710679e6c75c817492276ec9c6599186ff27
Ben Rometsch <ben.rometsch@flagsmith.com>
2021-10-19 11:44:12 +0100

    Corrected ALB/AWS header (#418)

--- api/app/settings/production.py
+++ api/app/settings/production.py
@@ -1,3 +1,7 @@
 from app.settings.common import *
 
 REST_FRAMEWORK["PAGE_SIZE"] = 999
+
+# Needed by Elastic Beanstalk to correctly identify incoming protocol
+# https://docs.aws.amazon.com/elasticloadbalancing/latest/application/x-forwarded-headers.html#x-forwarded-proto
+SECURE_PROXY_SSL_HEADER = ("X-Forwarded-Proto", "https")
