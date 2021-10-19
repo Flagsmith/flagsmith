dbdc256ea299fcd838eb0932051f30f77a920214
Ben Rometsch <ben.rometsch@gmail.com>
2021-10-19 12:16:40 +0100

    Moved flake8 around

--- api/app/settings/local.py
+++ api/app/settings/local.py
@@ -3,16 +3,16 @@ from app.settings.common import *  # noqa
 ENABLE_AXES = False
 
 
-ALLOWED_HOSTS.extend([".ngrok.io", "127.0.0.1", "localhost"])  # noqa
+ALLOWED_HOSTS.extend([".ngrok.io", "127.0.0.1", "localhost"])
 
-INSTALLED_APPS.extend(["debug_toolbar"])  # noqa
+INSTALLED_APPS.extend(["debug_toolbar"])
 
-MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])  # noqa
+MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-SWAGGER_SETTINGS["USE_SESSION_AUTH"] = False  # noqa
+SWAGGER_SETTINGS["USE_SESSION_AUTH"] = False
 
 # Allow admin login with username and password
 ENABLE_ADMIN_ACCESS_USER_PASS = True
