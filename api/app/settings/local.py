b9c404664e9a5c9d92a637c7ef1c9e286b823d6a
Ben Rometsch <ben.rometsch@gmail.com>
2021-10-19 12:07:45 +0100

    Ignore * imports for flake8

--- api/app/settings/local.py
+++ api/app/settings/local.py
@@ -1,23 +1,18 @@
-from app.settings.common import (
-    ALLOWED_HOSTS,
-    INSTALLED_APPS,
-    MIDDLEWARE,
-    SWAGGER_SETTINGS,
-)
+from app.settings.common import *  # noqa
 
 ENABLE_AXES = False
 
 
-ALLOWED_HOSTS.extend([".ngrok.io", "127.0.0.1", "localhost"])
+ALLOWED_HOSTS.extend([".ngrok.io", "127.0.0.1", "localhost"])  # noqa
 
-INSTALLED_APPS.extend(["debug_toolbar"])
+INSTALLED_APPS.extend(["debug_toolbar"])  # noqa
 
-MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])
+MIDDLEWARE.extend(["debug_toolbar.middleware.DebugToolbarMiddleware"])  # noqa
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-SWAGGER_SETTINGS["USE_SESSION_AUTH"] = False
+SWAGGER_SETTINGS["USE_SESSION_AUTH"] = False  # noqa
 
 # Allow admin login with username and password
 ENABLE_ADMIN_ACCESS_USER_PASS = True
