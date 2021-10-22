6df8ddc9c4dfdd0d488c2ccb0a040c116ca853b2
kyle-ssg <kyle@solidstategroup.com>
2021-10-22 08:10:41 +0100

    tag list in dark mode

--- frontend/web/components/pages/FeaturesPage.js
+++ frontend/web/components/pages/FeaturesPage.js
@@ -236,7 +236,7 @@ const FeaturesPage = class extends Component {
                                                               ]}
                                                               items={this.filter(projectFlags, this.state.tags)}
                                                               header={(
-                                                                  <Row style={{ backgroundColor: '#f7f7f7' }}>
+                                                                  <Row className="panel--grey px-0 pt-0 pb-2">
                                                                       <TagSelect
                                                                         showUntagged
                                                                         showClearAll
