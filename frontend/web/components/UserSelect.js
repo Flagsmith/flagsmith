17798ad8937348d9da69f908e9c0ecdb9ffb4ea2
kyle-ssg <kyle@solidstategroup.com>
2021-10-23 10:29:53 +0100

    Flag owners

---
+++ frontend/web/components/UserSelect.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import InlineModal from './InlineModal';
+
+
+class TheComponent extends Component {
+    state = {
+        filter: '',
+    };
+
+    render() {
+        const users = this.props.users && this.props.users.filter((v) => {
+            const search = this.state.filter.toLowerCase();
+            if (!search) return true;
+            return `${v.first_name} ${v.last_name}`.toLowerCase().includes(search);
+        });
+        const value = this.props.value || [];
+        return (
+            <InlineModal
+              title="Users"
+              isOpen={this.props.isOpen}
+              onClose={this.props.onToggle}
+              className="inline-modal--tags"
+            >
+                <Input
+                  value={this.state.filter} onChange={e => this.setState({ filter: Utils.safeParseEventValue(e) })} className="full-width mb-2"
+                  placeholder="Type or choose a user"
+                />
+                {users && users.map(v => (
+                    <div className="list-item clickable" key={v.id}>
+                        <Row
+                          onClick={() => {
+                              const isRemove = value.includes(v.id);
+                              if (isRemove && this.props.onRemove) {
+                                  this.props.onRemove(v.id);
+                              } else if (!isRemove && this.props.onAdd) {
+                                  this.props.onAdd(v.id);
+                              }
+                              this.props.onChange && this.props.onChange(
+                                  isRemove ? value.filter(f => f !== v.id)
+                                      : value.concat([v.id]),
+                              );
+                          }} space
+                        >
+                            <Flex className={value.includes(v.id) ? 'font-weight-bold' : ''}>
+                                {v.first_name} {v.last_name}
+                            </Flex>
+                            <span style={{ fontSize: 24 }} className="ion ion-ios-checkmark"/>
+                        </Row>
+                    </div>
+                ))
+            }
+            </InlineModal>
+        );
+    }
+}
+
+module.exports = hot(module)(TheComponent);
