88e9ae7c425f7b178b707f6719e07764763e2f03
Ben Rometsch <ben.rometsch@gmail.com>
2021-11-01 14:52:04 +0000

    Making iOS Install help consistent with the documentation (#455)
    * Update install-ios.js
    
    * Updating initialisation code for iOS
    
    * Update create user documentation code for iOS
    
    * Formatting
    
    Co-authored-by: Rudrank Riyam <rudrankriyam@gmail.com>

--- frontend/common/code-help/create-user/create-user-ios.js
+++ frontend/common/code-help/create-user/create-user-ios.js
@@ -1,23 +1,22 @@
 import Utils from '../../utils/utils';
 
-module.exports = (envId, { FEATURE_NAME,USER_ID, FEATURE_FUNCTION, FEATURE_NAME_ALT }) => `import BulletTrainClient
+module.exports = (envId, { FEATURE_NAME,USER_ID, FEATURE_FUNCTION, FEATURE_NAME_ALT }) => `import FlagsmithClient
 
 func application(_ application: UIApplication,
  didFinishLaunchingWithOptions launchOptions:
   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-  BulletTrain.shared.apiKey = "${envId}"
-  
+  Flagsmith.shared.apiKey = "${envId}"
+
   // This will create a user in the dashboard if they don't already exist
-  
   // Check for a feature
-  BulletTrain.shared
+  Flagsmith.shared
   .hasFeatureFlag(withID: "${FEATURE_NAME}", forIdentity: "${USER_ID}") { (result) in
       print(result)
   }
 
   // Or, use the value of a feature
-  BulletTrain.shared
+  Flagsmith.shared
   .getFeatureValue(withID: "${FEATURE_NAME_ALT}", forIdentity: "${USER_ID}") { (result) in
       switch result {
       case .success(let value):
