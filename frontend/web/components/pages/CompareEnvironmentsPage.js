10828c346aa15fcade506cc6a7aa4c0ce439a16f
kyle-ssg <kyle@solidstategroup.com>
2021-12-04 11:53:46 +0000

    Compare Features

--- frontend/web/components/pages/CompareEnvironmentsPage.js
+++ frontend/web/components/pages/CompareEnvironmentsPage.js
@@ -1,17 +1,24 @@
 // import propTypes from 'prop-types';
 import React, { Component } from 'react';
 import EnvironmentSelect from '../EnvironmentSelect';
+import data from '../../../common/data/base/_data';
+import ProjectStore from '../../../common/stores/project-store';
 
 class TheComponent extends Component {
     static displayName = 'TheComponent';
 
     static propTypes = {};
 
+    static contextTypes = {
+        router: propTypes.object.isRequired,
+    };
+
     constructor(props) {
         super();
         this.state = {
             environmentLeft: props.match.params.environmentId,
             environmentRight: '',
+            projectFlags: null,
             isLoading: true,
         };
     }
@@ -19,10 +26,37 @@ class TheComponent extends Component {
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.state.environmentLeft !== prevState.environmentLeft || this.state.environmentRight !== prevState.environmentRight) {
             this.setState({ isLoading: true });
+            return Promise.all([
+                this.state.projectFlags ? Promise.resolve(this.state.projectFlags) : data.get(`${Project.api}projects/${this.props.match.params.projectId}/features/?page_size=999`),
+                data.get(`${Project.api}environments/${this.state.environmentLeft}/featurestates/?page_size=999`),
+                data.get(`${Project.api}environments/${this.state.environmentRight}/featurestates/?page_size=999`),
+            ]).then(([projectFlags, environmentLeftFlags, environmentRightFlags]) => {
+                const changes = [];
+                _.each(projectFlags.results, (projectFlag) => {
+                    const leftSide = environmentLeftFlags.results.find(v => v.feature === projectFlag.id);
+                    const rightSide = environmentRightFlags.results.find(v => v.feature === projectFlag.id);
+                    const change = {
+                        projectFlag,
+                        leftEnabled: leftSide.enabled,
+                        rightEnabled: rightSide.enabled,
+                        leftValue: leftSide.feature_state_value,
+                        rightValue: rightSide.feature_state_value,
+                    };
+                    change.enabledChanged = change.rightEnabled !== change.leftEnabled;
+                    change.valueChanged = change.rightValue !== change.leftValue;
+                    if (change.enabledChanged || change.valueChanged) {
+                        changes.push(change);
+                    }
+                });
+                this.setState({ changes, isLoading: false });
+            }).catch((e) => {
+                debugger;
+            });
         }
     }
 
     render() {
+        const featureNameWidth = 300;
         return (
             <div className="app-container container">
                 <h3>
@@ -33,7 +67,7 @@ class TheComponent extends Component {
                 </p>
                 <Row>
                     <Row>
-                        <div style={{ width: 200 }}>
+                        <div style={{ width: featureNameWidth }}>
                             <EnvironmentSelect onChange={environmentLeft => this.setState({ environmentLeft })} value={this.state.environmentLeft}/>
                         </div>
 
@@ -41,7 +75,7 @@ class TheComponent extends Component {
                             <ion className="icon ios ion-md-arrow-back mx-2"/>
                         </div>
 
-                        <div style={{ width: 200 }}>
+                        <div style={{ width: featureNameWidth }}>
                             <EnvironmentSelect ignoreAPIKey={this.state.environmentLeft} onChange={environmentRight => this.setState({ environmentRight })} value={this.state.environmentRight}/>
                         </div>
                     </Row>
@@ -50,9 +84,91 @@ class TheComponent extends Component {
 
                 {
                     this.state.environmentLeft && this.state.environmentRight ? (
-                        <div className="centered-container">
+                        <div>
                             {this.state.isLoading && (
-                                <Loader/>
+                                <div className="text-center">
+                                    <Loader/>
+                                </div>
+                            )}
+                            {!this.state.isLoading && (!this.state.changes || !this.state.changes.length) && (
+                                <div className="text-center">
+                                  These environments have no flag differences
+                                </div>
+                            )}
+                            {!this.state.isLoading && (this.state.changes && !!this.state.changes.length) && (
+                                <div style={{ minWidth: 800 }}>
+                                    <PanelSearch
+                                      title={
+                                      (
+                                          <Row>
+                                              <span style={{ width: featureNameWidth }}>
+                                                Feature
+                                              </span>
+                                          </Row>
+)
+                                  }
+                                      header={(
+                                      (
+                                          <Row className="mt-2" style={{ marginLeft: '0.75rem', marginRight: '0.75rem' }}>
+                                              <span style={{ width: featureNameWidth }} />
+                                              <Flex>
+                                                  <strong>
+                                                      {ProjectStore.getEnvironment(this.state.environmentLeft).name}
+
+                                                  </strong>
+                                              </Flex>
+                                              <Flex>
+                                                  <strong>
+                                                      {ProjectStore.getEnvironment(this.state.environmentRight).name}
+                                                  </strong>
+                                              </Flex>
+                                          </Row>
+)
+                                  )}
+                                      className="no-pad mt-2"
+                                      items={this.state.changes}
+                                      renderRow={p => (
+                                          <div className="list-item">
+                                              <Row>
+                                                  <div style={{ width: featureNameWidth }}>
+                                                      <Tooltip title={(
+                                                          <strong>
+                                                              {p.projectFlag.name}
+                                                          </strong>
+                                                  )}
+                                                      >
+                                                          {p.projectFlag.description}
+                                                      </Tooltip>
+                                                  </div>
+                                                  <Flex>
+                                                      <Row>
+                                                          <div className={`mr-2 ${!p.enabledChanged && 'faded'}`}>
+                                                              <Switch checked={p.leftEnabled}/>
+                                                          </div>
+                                                          <div className={`mr-2 ${!p.valueChanged && 'faded'}`}>
+                                                              <FeatureValue value={p.leftValue}/>
+                                                          </div>
+                                                      </Row>
+
+                                                  </Flex>
+                                                  <Flex>
+                                                      <Row>
+                                                          <div className={`mr-2 ${!p.enabledChanged && 'faded'}`}>
+                                                              <Switch checked={p.rightEnabled}/>
+                                                          </div>
+                                                          <div className={`mr-2 ${!p.valueChanged && 'faded'}`}>
+                                                              <FeatureValue value={p.rightValue}/>
+                                                          </div>
+                                                      </Row>
+
+                                                  </Flex>
+                                              </Row>
+
+
+                                          </div>
+                                      )}
+                                    />
+                                </div>
                             )}
                         </div>
                     ) : ''
@@ -63,4 +179,4 @@ class TheComponent extends Component {
     }
 }
 
-export default ConfigProvider(TheComponent);
+module.exports = hot(module)(ConfigProvider(TheComponent));
