066713d67681b57d40e372ae339bfe97f9d1a88e
kyle-ssg <kyle@solidstategroup.com>
2021-12-05 09:49:03 +0000

    re-add segment age test

--- frontend/e2e/initialise.js
+++ frontend/e2e/initialise.js
@@ -7,6 +7,7 @@ const email = 'nightwatch@solidstategroup.com';
 const password = 'str0ngp4ssw0rd!';
 const url = `http://localhost:${process.env.PORT || 8080}/signup`;
 const expect = require('chai').expect;
+
 const cacheWait = 1000;
 const byId = helpers.byTestID;
 
@@ -173,5 +174,73 @@ module.exports = {
     '[Initialise Tests] - Clear down features': function (browser) {
         testHelpers.deleteFeature(browser, 1, 'header_size');
         testHelpers.deleteFeature(browser, 0, 'header_enabled');
+    },
+    '[Initialise Tests] - Create Segment': function (browser) {
+        testHelpers.gotoSegments(browser);
+
+        // (=== 18 || === 19) && (> 17 || < 19) && (!=20) && (<=18) && (>=18)
+        // Rule 1- Age === 18 || Age === 19
+
+        testHelpers.createSegment(browser, 0, '18_or_19', [
+            // rule 2 =18 || =17
+            {
+                name: 'age',
+                operator: 'EQUAL',
+                value: 18,
+                ors: [
+                    {
+                        name: 'age',
+                        operator: 'EQUAL',
+                        value: 17,
+                    },
+                ],
+            },
+            // rule 2 >17 or <10
+            {
+                name: 'age',
+                operator: 'GREATER_THAN',
+                value: 17,
+                ors: [
+                    {
+                        name: 'age',
+                        operator: 'LESS_THAN',
+                        value: 10,
+                    },
+                ],
+            },
+            // rule 3 !=20
+            {
+                name: 'age',
+                operator: 'NOT_EQUAL',
+                value: 20,
+            },
+            // Rule 4 <= 18
+            {
+                name: 'age',
+                operator: 'LESS_THAN_INCLUSIVE',
+                value: 18,
+            },
+            // Rule 5 >= 18
+            {
+                name: 'age',
+                operator: 'GREATER_THAN_INCLUSIVE',
+                value: 18,
+            },
+        ]);
+    },
+    '[Initialise Tests] - Add segment trait for user': function (browser) {
+        testHelpers.gotoTraits(browser);
+        testHelpers.createTrait(browser, 0, 'age', 18);
+    },
+    '[Initialise Tests] - Check user now belongs to segment': function (browser) {
+        browser.waitForElementVisible(byId('segment-0-name'));
+        browser.expect.element(byId('segment-0-name')).text.to.equal('18_or_19');
+    },
+    '[Initialise Tests] - Delete segment trait for user': function (browser) {
+        testHelpers.deleteTrait(browser, 0);
+    },
+    '[Initialise Tests] - Delete segment': function (browser) {
+        testHelpers.gotoSegments(browser);
+        testHelpers.deleteSegment(browser, 0, '18_or_19');
     },
 };
