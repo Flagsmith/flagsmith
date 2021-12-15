3bee434953ecd724c9e545c27a3e299819d69d3a
kyle-ssg <kyle@solidstategroup.com>
2021-12-15 11:24:22 +0000

    Remove oauth buttons if disableSignup is true

--- frontend/web/components/pages/HomePage.js
+++ frontend/web/components/pages/HomePage.js
@@ -94,7 +94,7 @@ const HomePage = class extends React.Component {
         const oauths = [];
         const disableOauthRegister = this.props.hasFeature('disable_oauth_registration');
 
-        if (!isSignup || !disableOauthRegister) {
+        if ((!isSignup || !disableOauthRegister) && !disableSignup) {
             if (this.props.getValue('oauth_github')) {
                 oauths.push((
                     <a key="github" className="btn btn__oauth btn__oauth--github" href={JSON.parse(this.props.getValue('oauth_github')).url}>
@@ -107,19 +107,19 @@ const HomePage = class extends React.Component {
             if (this.props.getValue('oauth_google')) {
                 oauths.push((
                     <a
-                        key="google" className="btn btn__oauth btn__oauth--google" onClick={() => {
-                        Google.login().then((res) => {
-                            if (res) {
-                                document.location = `${document.location.origin}/oauth/google?code=${res}`;
-                            }
-                        });
-                    }}
+                      key="google" className="btn btn__oauth btn__oauth--google" onClick={() => {
+                          Google.login().then((res) => {
+                              if (res) {
+                                  document.location = `${document.location.origin}/oauth/google?code=${res}`;
+                              }
+                          });
+                      }}
                     >
                         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Google icon</title>
                             <path
-                                fill="#fff"
-                                d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"
+                              fill="#fff"
+                              d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"
                             />
                         </svg> Google
                     </a>
@@ -129,9 +129,9 @@ const HomePage = class extends React.Component {
             if (this.props.hasFeature('saml')) {
                 oauths.push((
                     <a
-                        onClick={() => {
-                            openModal('Single Sign-On', <SamlForm/>);
-                        }
+                      onClick={() => {
+                          openModal('Single Sign-On', <SamlForm/>);
+                      }
                         } key="single-sign-on" className="btn btn__oauth btn__oauth--saml"
                     >
                         Single Sign-On
