a0a239a81af9da26949abf378308af46d2e49987
kyle-ssg <kyle@solidstategroup.com>
2021-12-17 10:02:04 +0000

    revert main changes

--- frontend/web/components/pages/HomePage.js
+++ frontend/web/components/pages/HomePage.js
@@ -45,13 +45,13 @@ const HomePage = class extends React.Component {
                 });
             }
         }
-        setTimeout(()=>{
+        setTimeout(() => {
             const emailField = document.querySelector('input[name="firstName"]') || document.querySelector('input[name="email"]');
             if (emailField) {
                 emailField.focus();
                 emailField.value = emailField.value;
             }
-        },1000)
+        }, 1000);
 
 
         if (document.location.href.includes('saml')) {
@@ -92,50 +92,54 @@ const HomePage = class extends React.Component {
         const isSignup = !projectOverrides.preventSignup && ((isInvite && document.location.href.indexOf('login') === -1) || document.location.href.indexOf('signup') != -1);
         const disableSignup = Project.preventSignup && !isInvite && isSignup;
         const oauths = [];
-        if (this.props.getValue('oauth_github')) {
-            oauths.push((
-                <a key="github" className="btn btn__oauth btn__oauth--github" href={JSON.parse(this.props.getValue('oauth_github')).url}>
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                    </svg> GitHub
-                </a>
-            ));
+        const disableOauthRegister = this.props.hasFeature('disable_oauth_registration');
+
+        if ((!isSignup || !disableOauthRegister) && !disableSignup) {
+            if (this.props.getValue('oauth_github')) {
+                oauths.push((
+                    <a key="github" className="btn btn__oauth btn__oauth--github" href={JSON.parse(this.props.getValue('oauth_github')).url}>
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                        </svg> GitHub
+                    </a>
+                ));
+            }
+            if (this.props.getValue('oauth_google')) {
+                oauths.push((
+                    <a
+                      key="google" className="btn btn__oauth btn__oauth--google" onClick={() => {
+                          Google.login().then((res) => {
+                              if (res) {
+                                  document.location = `${document.location.origin}/oauth/google?code=${res}`;
+                              }
+                          });
+                      }}
+                    >
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <title>Google icon</title>
+                            <path
+                              fill="#fff"
+                              d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"
+                            />
+                        </svg> Google
+                    </a>
+                ));
+            }
+
+            if (this.props.hasFeature('saml')) {
+                oauths.push((
+                    <a
+                      onClick={() => {
+                          openModal('Single Sign-On', <SamlForm/>);
+                      }
+                        } key="single-sign-on" className="btn btn__oauth btn__oauth--saml"
+                    >
+                        Single Sign-On
+                    </a>
+                ));
+            }
         }
 
-        if (this.props.getValue('oauth_google')) {
-            oauths.push((
-                <a
-                  key="google" className="btn btn__oauth btn__oauth--google" onClick={() => {
-                      Google.login().then((res) => {
-                          if (res) {
-                              document.location = `${document.location.origin}/oauth/google?code=${res}`;
-                          }
-                      });
-                  }}
-                >
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <title>Google icon</title>
-                        <path
-                          fill="#fff"
-                          d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"
-                        />
-                    </svg> Google
-                </a>
-            ));
-        }
-
-        if (this.props.hasFeature('saml')) {
-            oauths.push((
-                <a
-                  onClick={() => {
-                      openModal('Single Sign-On', <SamlForm/>);
-                  }
-                } key="single-sign-on" className="btn btn__oauth btn__oauth--saml"
-                >
-                   Single Sign-On
-                </a>
-            ));
-        }
         return (
           <>
               <AccountProvider onLogout={this.onLogout} onLogin={this.onLogin}>
@@ -308,7 +312,15 @@ const HomePage = class extends React.Component {
                                                   </Link>
                                               </Row>
                                               <div className="mt-5 text-center text-small text-muted">
-                                                  By signing up you agree to our <a style={{opacity:0.8}} target="_blank" className="text-small" href="https://flagsmith.com/terms-of-service/">Terms of Service</a> and <a style={{opacity:0.8}} target="_blank" className="text-small" href="https://flagsmith.com/privacy-policy/">Privacy Policy</a>
+                                                  By signing up you agree to our <a
+                                                    style={{ opacity: 0.8 }} target="_blank" className="text-small"
+                                                    href="https://flagsmith.com/terms-of-service/"
+                                                  >Terms of Service
+                                                                                 </a> and <a
+                                                    style={{ opacity: 0.8 }} target="_blank" className="text-small"
+                                                    href="https://flagsmith.com/privacy-policy/"
+                                                  >Privacy Policy
+                                                           </a>
                                               </div>
                                           </div>
 
