01fbe8f27bc60e67399061f94736f4476cc0a9aa
Matthew Elwell <matthewe@solidstategroup.com>
2021-12-21 11:36:05 +0000

    Add SDK endpoint for getting environment document

---
+++ api/environments/sdk/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpRequest
+from flag_engine.django_transform.document_builders import (
+    build_environment_document,
+)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from environments.authentication import EnvironmentKeyAuthentication
+from environments.models import Environment
+from environments.permissions.permissions import EnvironmentKeyPermissions
+
+
+class SDKEnvironmentAPIView(APIView):
+    permission_classes = (EnvironmentKeyPermissions,)
+    authentication_classes = (EnvironmentKeyAuthentication,)
+
+    def get(self, request: HttpRequest) -> Response:
+        environment = Environment.objects.select_related(
+            "project", "project__organisation"
+        ).get(api_key=request.environment.api_key)
+        return Response(build_environment_document(environment))
---
+++ api/tests/unit/environments/test_environments_views_sdk_environment.py
@@ -0,0 +1,37 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from environments.models import Environment
+from features.models import Feature
+from segments.models import EQUAL, Condition, Segment, SegmentRule
+
+
+def test_get_environment_document(organisation_one, organisation_one_project_one):
+    # Given
+    project = organisation_one_project_one
+
+    # an environment
+    environment = Environment.objects.create(name="Test Environment", project=project)
+    client = APIClient()
+    client.credentials(HTTP_X_ENVIRONMENT_KEY=environment.api_key)
+
+    # and some other sample data to make sure we're testing all of the document
+    Feature.objects.create(name="test_feature", project=project)
+    segment = Segment.objects.create(project=project)
+    segment_rule = SegmentRule.objects.create(
+        segment=segment, type=SegmentRule.ALL_RULE
+    )
+    Condition.objects.create(
+        operator=EQUAL, property="property", value="value", rule=segment_rule
+    )
+
+    # and the relevant URL to get an environment document
+    url = reverse("api-v1:environment-document")
+
+    # When
+    response = client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()
