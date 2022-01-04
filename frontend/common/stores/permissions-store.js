0cf69cf4e7f45e8bb3030eadf646a7cde0e0d4f2
kyle-ssg <kyle@solidstategroup.com>
2022-01-04 09:20:28 +0000

    add permissions request under flag organisation_permissions

--- frontend/common/stores/permissions-store.js
+++ frontend/common/stores/permissions-store.js
@@ -27,7 +27,11 @@ const controller = {
         if (store.model.availablePermissions.projects) {
             return;
         }
-        Promise.all(['project', 'environment'].map(v => data.get(`${Project.api}${v}s/permissions/`)
+        const permissions = ['project', 'environment'];
+        if (flagsmith.hasFeature('organisation_permissions')) {
+            permissions.push('organisation');
+        }
+        Promise.all(permissions.map(v => data.get(`${Project.api}${v}s/permissions/`)
             .then((res) => {
                 store.model.availablePermissions[v] = res;
             }))).then(() => {
