974cc0a0e7704c004ebdf4505719e7c96dcae1b9
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-01-05 10:02:52 +0530

    tests(slack/permission/authentication): Add unit tests

--- api/integrations/slack/permissions.py
+++ api/integrations/slack/permissions.py
@@ -1,10 +1,11 @@
 from rest_framework.permissions import BasePermission
 
+from environments.models import Environment
+
 
 class OauthInitPermission(BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.user.get_permitted_environments("VIEW_ENVIRONMENT")
-            .filter(api_key=view.kwargs.get("environment_api_key"))
-            .exists()
+        environment = Environment.objects.get(
+            api_key=view.kwargs.get("environment_api_key")
         )
+        return request.user.is_environment_admin(environment)
---
+++ api/integrations/slack/tests/test_authentication.py
@@ -0,0 +1,23 @@
+import pytest
+from django.core.signing import TimestampSigner
+from rest_framework.exceptions import AuthenticationFailed
+
+from integrations.slack.authentication import OauthInitAuthentication
+
+oauth_init_authentication = OauthInitAuthentication()
+
+
+def test_oauth_init_authentication_failes_if_invalid_signature_is_passed(rf):
+    # Given
+    request = rf.get("/url", {"signature": "invalid_signature"})
+    with pytest.raises(AuthenticationFailed):
+        oauth_init_authentication.authenticate(request)
+
+
+def test_oauth_init_authentication_with_valid_signature(django_user_model, rf):
+    # Given
+
+    signer = TimestampSigner()
+    user = django_user_model.objects.create(username="test_user")
+    request = rf.get("/url", {"signature": signer.sign(user.id)})
+    oauth_init_authentication.authenticate(request)
