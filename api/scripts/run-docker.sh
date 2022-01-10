8bd265c31b1f0cb22a1d0f667651786d06a6551e
Ben Rometsch <ben.rometsch@gmail.com>
2022-01-10 13:04:57 +0000

    Fix gunicorn temp location

--- api/scripts/run-docker.sh
+++ api/scripts/run-docker.sh
@@ -2,4 +2,4 @@
 set -e
 
 python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 --workers ${GUNICORN_WORKERS:-3} --threads ${GUNICORN_THREADS:-2} app.wsgi --access-logfile $ACCESS_LOG_LOCATION
+gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers ${GUNICORN_WORKERS:-3} --threads ${GUNICORN_THREADS:-2} app.wsgi --access-logfile $ACCESS_LOG_LOCATION
