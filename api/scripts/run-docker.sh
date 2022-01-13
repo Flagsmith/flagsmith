1e44b9e3f61622229a75afa991e7dcb79e42d839
Ben Rometsch <ben.rometsch@gmail.com>
2022-01-13 13:02:47 +0000

    Added docker run options

--- api/scripts/run-docker.sh
+++ api/scripts/run-docker.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -e
 
-python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers ${GUNICORN_WORKERS:-3} --threads ${GUNICORN_THREADS:-2} --access-logfile $ACCESS_LOG_LOCATION app.wsgi
+function migrate () {
+    python manage.py migrate
+}
+
+function serve() {
+    gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers ${GUNICORN_WORKERS:-3} --threads ${GUNICORN_THREADS:-2} --access-logfile $ACCESS_LOG_LOCATION app.wsgi
+}
+
+if [ "$1" == "migrate" ]; then
+    migrate
+elif [ "$1" == "serve" ]; then
+    serve
+elif [ "$1" == "migrate-and-serve" ]; then
+    migrate
+    serve
+fi
