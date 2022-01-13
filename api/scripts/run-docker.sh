346d7bb0e3001f440294fbe6debc17f41a01b898
Ben Rometsch <ben.rometsch@gmail.com>
2022-01-13 21:26:39 +0000

    improvement/fix-render-builds (#678)

--- api/scripts/run-docker.sh
+++ api/scripts/run-docker.sh
@@ -2,4 +2,10 @@
 set -e
 
 python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 --worker-tmp-dir /dev/shm --workers ${GUNICORN_WORKERS:-3} --threads ${GUNICORN_THREADS:-2} --access-logfile $ACCESS_LOG_LOCATION app.wsgi
+gunicorn --bind 0.0.0.0:8000 \
+         --worker-tmp-dir /dev/shm \
+         --timeout ${GUNICORN_TIMEOUT:-30} \
+         --workers ${GUNICORN_WORKERS:-3} \
+         --threads ${GUNICORN_THREADS:-2} \
+         --access-logfile $ACCESS_LOG_LOCATION \
+         app.wsgi
