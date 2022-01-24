3fef650c6695d380d4de83632bd4a9a96606242f
Matthew Elwell <matthewe@solidstategroup.com>
2022-01-24 14:39:12 +0000

    Make secure proxy SSL header configurable (#705)
    * Make secure proxy SSL header configurable to allow for differences between EE and SaaS
    
    * Fix pipeline

--- api/app/settings/production.py
+++ api/app/settings/production.py
@@ -1,8 +1,3 @@
 from app.settings.common import *
 
 REST_FRAMEWORK["PAGE_SIZE"] = 999
-
-# Needed by Elastic Beanstalk to correctly identify incoming protocol
-# https://docs.aws.amazon.com/elasticloadbalancing/latest/application/x-forwarded-headers.html#x-forwarded-proto
-# Although in our case SSL termination happens in Cloudfront so the header is named as below.
-SECURE_PROXY_SSL_HEADER = ("HTTP_CLOUDFRONT_FORWARDED_PROTO", "https")
