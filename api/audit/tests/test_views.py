eb97eba94dec199a7acca62c3ea2945bc5cb9637
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-01-31 21:03:48 +0530

    fix/improve tests

--- api/audit/tests/test_views.py
+++ api/audit/tests/test_views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 
 from audit.models import AuditLog
 from environments.models import Environment
+from projects.models import Project
 
 
 def test_audit_log_can_be_filtered_by_environments(admin_client, project, environment):
@@ -25,9 +26,7 @@ def test_audit_log_can_be_filtered_by_environments(admin_client, project, enviro
     assert response.json()["results"][1]["environment"] is None
 
 
-def test_audit_log_can_be_filtered_by_related_object_type(
-    admin_client, project, environment
-):
+def test_audit_log_can_be_filtered_by_log_text(admin_client, project, environment):
     # Given
     flag_state_updated_log = "Flag state updated"
     flag_state_deleted_log = "flag state deleted"
@@ -46,3 +45,26 @@ def test_audit_log_can_be_filtered_by_related_object_type(
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["results"][0]["log"] == flag_state_deleted_log
     assert response.json()["results"][1]["log"] == flag_state_updated_log
+
+
+def test_audit_log_can_be_filtered_by_project(
+    admin_client, project, environment, organisation
+):
+    # Given
+    another_project = Project.objects.create(
+        name="another_project", organisation=organisation
+    )
+    AuditLog.objects.create(project=project)
+    AuditLog.objects.create(project=project, environment=environment)
+    AuditLog.objects.create(project=another_project, environment=environment)
+
+    url = reverse("api-v1:audit-list")
+
+    # When
+    response = admin_client.get(url, {"project": project.id})
+
+    # Then
+    assert response.json()["count"] == 2
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["results"][0]["project"]["id"] == project.id
+    assert response.json()["results"][1]["project"]["id"] == project.id
