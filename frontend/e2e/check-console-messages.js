0e8bae0627f36e238a2cfd71c5e9253126cd8558
kyle-ssg <kyle@solidstategroup.com>
2022-02-08 20:36:03 +0000

    console error request failures

--- frontend/e2e/check-console-messages.js
+++ frontend/e2e/check-console-messages.js
@@ -4,5 +4,6 @@ import { t } from 'testcafe';
 export default async function () {
     t.addRequestHooks()
     const { error, log } = await t.getBrowserConsoleMessages();
-    console.log(`\n\n Test Logs: \n${log}\n\n`)
+
+    console.log(`\n\n Test Logs: \n${log.join("\n\n")}\n\n`)
 }
