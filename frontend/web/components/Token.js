9dac419edb107e6b4173fdcfff11109891feef04
kyle-ssg <kyle@solidstategroup.com>
2022-02-09 18:03:52 +0000

    Adjust styles

--- frontend/web/components/Token.js
+++ frontend/web/components/Token.js
@@ -15,7 +15,7 @@ class Token extends Component {
             <div style={this.props.style} className={`${this.state.showToken?"font-weight-bold":""}`}>
                 {this.state.showToken?this.props.token : this.props.token.split("").map((v)=>"*").join("").trim()}
             </div>
-            <Button className={"ml-2 mr-4"} onClick={()=>this.setState({showToken:!this.state.showToken})}>{this.state.showToken?"Hide":"Show"}</Button>
+            <Button style={{width:80}} className={"ml-2 mr-4"} onClick={()=>this.setState({showToken:!this.state.showToken})}>{this.state.showToken?"Hide":"Show"}</Button>
         </Row>
     }
 }
