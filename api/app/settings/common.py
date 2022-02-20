c49a59e578cd286720ebb87bb881ec42b717fa23
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-02-20 16:30:11 +0530

    Add comment

--- api/app/settings/common.py
+++ api/app/settings/common.py
@@ -559,5 +559,6 @@ if SAML_INSTALLED:
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+# Used to keep edge identities in sync by forwarding the http requests
 EDGE_API_URL = env.str("EDGE_API_URL", None)
-EDGE_API_URL = env.str("EDGE_API_URL", "")
