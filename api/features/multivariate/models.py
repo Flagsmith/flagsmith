4c9041c5921bb5a7665ae897aad25bfc5d841713
Matthew Elwell <matthewe@solidstategroup.com>
2022-02-22 09:53:13 +0000

    Fix exception when updating multiple MV values for a FeatureState (#808)
    * Fix exception when updating multiple MV values for a FeatureState
    
    * Remove unnecessary tests and add comment
    
    * Add test for serializer validation

--- api/features/multivariate/models.py
+++ api/features/multivariate/models.py
@@ -1,7 +1,5 @@
-from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import QuerySet, Sum
 from django_lifecycle import (
     AFTER_CREATE,
     BEFORE_SAVE,
@@ -57,50 +55,8 @@ class MultivariateFeatureStateValue(LifecycleModelMixin, models.Model):
         unique_together = ("feature_state", "multivariate_feature_option")
 
     @hook(BEFORE_SAVE)
-    def validate_percentage_allocations(self):
-        total_sibling_percentage_allocation = (
-            self.get_siblings().aggregate(
-                total_percentage_allocation=Sum("percentage_allocation")
-            )["total_percentage_allocation"]
-            or 0
-        )
-        total_percentage_allocation = (
-            total_sibling_percentage_allocation + self.percentage_allocation
-        )
-        if total_percentage_allocation > 100:
-            raise ValidationError(
-                self._get_invalid_percentage_allocation_error_message()
-            )
-
-    @hook(BEFORE_SAVE)
     def validate_unique(self, exclude=None):
         """
         Override validate_unique method, so we can add the BEFORE_SAVE hook.
         """
         super(MultivariateFeatureStateValue, self).validate_unique(exclude=exclude)
-
-    def get_siblings(self) -> QuerySet:
-        # TODO: add tests
-        siblings = self.feature_state.multivariate_feature_state_values.all()
-        if self.id:
-            siblings = siblings.exclude(id=self.id)
-        return siblings
-
-    def _get_invalid_percentage_allocation_error_message(self) -> str:
-        # TODO: add tests
-        kwargs = {
-            "feature_name": self.feature_state.feature.name,
-            "environment_name": self.feature_state.environment.name,
-        }
-        message = (
-            "Total percentage allocation for feature {feature_name} "
-            "in environment {environment_name}"
-        )
-        if self.feature_state.identity:
-            message += " for identity {identity_identifier}"
-            kwargs["identity_identifier"] = self.feature_state.identity.identifier
-        elif self.feature_state.feature_segment:
-            message += " for segment {segment_name}"
-            kwargs["segment_name"] = self.feature_state.feature_segment.segment.name
-        message += " is greater than 100%."
-        return message.format(**kwargs)
