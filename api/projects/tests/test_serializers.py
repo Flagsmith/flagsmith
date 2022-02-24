e665a589f0cb6b2bb10beba987ce792eb74db926
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-02-24 10:22:49 +0530

    tidy up test

--- api/projects/tests/test_serializers.py
+++ api/projects/tests/test_serializers.py
@@ -6,19 +6,18 @@ def test_ProjectSerializer_get_is_identity_migration_done_calls_dynamo_wrapper_w
 ):
     # Given
     is_migration_done = True
-    mocked_dynamo_wrapper = mocker.patch("projects.serializers.DynamoIdentityWrapper")
-    mocked_is_migration_done = mocker.MagicMock(return_value=is_migration_done)
-    mocked_dynamo_wrapper.return_value.is_migration_done = mocked_is_migration_done
+    mock_dynamo_wrapper = mocker.MagicMock()
+    mock_dynamo_wrapper.is_migration_done.return_value = is_migration_done
+    mocker.patch(
+        "projects.serializers.DynamoIdentityWrapper", return_value=mock_dynamo_wrapper
+    )
 
     # When
     serializer = ProjectSerializer()
 
     # Then
     serializer.get_is_identity_migration_done(project) is is_migration_done
-
-    # and
-    mocked_dynamo_wrapper.assert_called_with()
-    mocked_is_migration_done.assert_called_with(project.id)
+    mock_dynamo_wrapper.is_migration_done.assert_called_with(project.id)
 
 
 def test_ProjectSerializer_get_is_identity_migration_done_returns_false_if_dynamo_not_enabled(
