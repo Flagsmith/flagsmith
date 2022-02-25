56eace6837c34f4bcce3227b182839cb095a12cf
Ben Rometsch <ben.rometsch@gmail.com>
2022-02-25 13:11:51 +0000

    Fixed E2E API
    Updated node version

--- frontend/env/project_local.js
+++ frontend/env/project_local.js
@@ -1,4 +1,7 @@
 module.exports = global.Project = {
+    api: 'http://localhost:8000/api/v1/',
+    flagsmithClientAPI: 'https://api.bullet-train.io/api/v1/',
+    flagsmith: '8KzETdDeMY7xkqkSkY3Gsg',
     debug: false,
     env: 'dev', // This is used for Sentry tracking
     maintenance: false, // trigger maintenance mode
