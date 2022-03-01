ca692226c03b662e9a90054ba91e7988209ca822
Kyle Johnson <kyle@solidstategroup.com>
2022-03-01 10:48:02 +0000

    fix staging env file (#821)

--- frontend/env/project_staging.js
+++ frontend/env/project_staging.js
@@ -1,5 +1,9 @@
 module.exports = global.Project = {
-    cookieDomain: 'staging.flagsmith.com',
+    api: 'https://api-staging.flagsmith.com/api/v1/',
+    flagsmithClientAPI: 'https://api.flagsmith.com/api/v1/',
+    flagsmith: 'ENktaJnfLVbLifybz34JmX', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
+    env: 'staging', // This is used for Sentry tracking
+    maintenance: false, // trigger maintenance mode
     demoAccount: {
         email: 'kyle+bullet-train@solidstategroup.com',
         password: 'demo_account',
