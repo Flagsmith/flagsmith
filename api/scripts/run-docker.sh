7db384d8bb953fdc0c53da6be2363809f50565ff
Ben Rometsch <ben.rometsch@gmail.com>
2022-03-03 18:22:35 +0000

    Feature/deploy-to-ecs (#686)
    * Push image to ECR staging
    
    * Renamed repo
    
    * secerts
    
    * name correction
    
    * Test deploy
    
    * removed extra image
    
    * cluster
    
    * Secret corrected
    
    * Insert docker image
    
    * Test deploy
    
    * Added migration Task def
    
    * revert test
    
    * Started migration
    
    * corrected migration cluster
    
    * renamed task def
    
    * added migration network info
    
    * renamed taskdef
    
    * added env vars
    
    * ENV
    
    * wip: Add put-target for migrate_identity rule
    
    * update service name
    
    * make put target less hard coded
    
    * fix json typo
    
    * parse migrate task arn and use in put_target
    
    * add missing colons
    
    * remove jq test whole output
    
    * add outputs to parse arn
    
    * Add jq back
    
    * remove extra double quotes
    
    * Add event-bus-name
    
    * fix event bus and rule name
    
    * temporary: update memory to see if pulumi reverts this
    
    * revert!
    
    * Add migrate project trigger to docker entrypoint
    
    * rename bash function
    
    * Add env vars to container definition
    
    * Added prod deploy ecs and renamed VPC to ECS
    
    * Removed ECR from secret
    
    * Updated ECS defs
    
    * Added prod task defs
    
    * corrected file path
    
    * UPdated prod migrate task role
    
    * Updated task role
    
    * updated correct pulumi event rule name
    
    * better env vars
    
    * Fixed up PR comments
    
    * Tweaked
    
    * Updated name of action
    
    * rationalised semi colons
    
    * Added env vars for staging
    
    * Fixed value
    
    * More env vars
    
    * Added prod secrets
    
    * Finalised triggers
    
    Co-authored-by: Gagan Trivedi <gagandeeptrivedi47@gmail.com>

--- api/scripts/run-docker.sh
+++ api/scripts/run-docker.sh
@@ -6,19 +6,23 @@ function migrate () {
 }
 
 function serve() {
-  gunicorn --bind 0.0.0.0:8000 \
-         --worker-tmp-dir /dev/shm \
-         --timeout ${GUNICORN_TIMEOUT:-30} \
-         --workers ${GUNICORN_WORKERS:-3} \
-         --threads ${GUNICORN_THREADS:-2} \
-         --access-logfile $ACCESS_LOG_LOCATION \
-         app.wsgi
+    gunicorn --bind 0.0.0.0:8000 \
+             --worker-tmp-dir /dev/shm \
+             --timeout ${GUNICORN_TIMEOUT:-30} \
+             --workers ${GUNICORN_WORKERS:-3} \
+             --threads ${GUNICORN_THREADS:-2} \
+             --access-logfile $ACCESS_LOG_LOCATION \
+             app.wsgi
 }
-
+function migrate_identities(){
+    python manage.py migrate_to_edge $1
+}
 if [ "$1" == "migrate" ]; then
     migrate
 elif [ "$1" == "serve" ]; then
     serve
+elif [ "$1" == "migrate_identities" ]; then
+    migrate_identities $2
 elif [ "$1" == "migrate-and-serve" ]; then
     migrate
     serve
