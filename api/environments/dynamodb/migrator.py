d2c90edcb6d19e870cfaac6ff8483899222d750c
gagandeep trivedi <gagandeeptrivedi47@gmail.com>
2022-03-07 09:05:14 +0530

    Feat(dynamodb/migrator): Add Identity Migrator (#818)
    * refac(project-metadata): Add migration status
    
    * refac: update dynamo identity wrapper api
    
    * fix(sales/org/html): Add migration status column
    
    * use identity migrator

---
+++ api/environments/dynamodb/migrator.py
@@ -0,0 +1,51 @@
+from django.db.models import Prefetch
+
+from environments.identities.models import Identity
+from features.models import FeatureState
+from features.multivariate.models import MultivariateFeatureStateValue
+
+from .dynamodb_wrapper import DynamoIdentityWrapper
+from .types import DynamoProjectMetadata, ProjectIdentityMigrationStatus
+
+
+class IdentityMigrator:
+    def __init__(self, project_id):
+        self.project_metadata = DynamoProjectMetadata.get_or_new(project_id)
+
+    @property
+    def migration_status(self) -> ProjectIdentityMigrationStatus:
+        return self.project_metadata.identity_migration_status
+
+    @property
+    def is_migration_done(self) -> bool:
+        migration_status = self.migration_status
+        return migration_status == ProjectIdentityMigrationStatus.MIGRATION_COMPLETED
+
+    @property
+    def can_migrate(self) -> bool:
+        migration_status = self.migration_status
+        return migration_status == ProjectIdentityMigrationStatus.MIGRATION_NOT_STARTED
+
+    def migrate(self):
+        self.project_metadata.start_identity_migration()
+        project_id = self.project_metadata.id
+        identity_wrapper = DynamoIdentityWrapper()
+        identities = Identity.objects.filter(
+            environment__project__id=project_id
+        ).prefetch_related(
+            "identity_traits",
+            Prefetch(
+                "identity_features",
+                queryset=FeatureState.objects.select_related(
+                    "feature", "feature_state_value"
+                ),
+            ),
+            Prefetch(
+                "identity_features__multivariate_feature_state_values",
+                queryset=MultivariateFeatureStateValue.objects.select_related(
+                    "multivariate_feature_option"
+                ),
+            ),
+        )
+        identity_wrapper.write_identities(identities)
+        self.project_metadata.finish_identity_migration()
