e75a239459ac789ecd8e353e9c5af2395add398b
Ben Rometsch <ben.rometsch@gmail.com>
2022-03-14 11:17:49 +0000

    Made MV values across environments clearer (#851)

--- frontend/web/components/mv/AddVariationButton.js
+++ frontend/web/components/mv/AddVariationButton.js
@@ -4,16 +4,9 @@ import Constants from '../../../common/constants';
 export default function AddVariationButton({ onClick }) {
     return (
         <div className="text-center">
-            <Tooltip
-              title={(
-                  <button type="button" onClick={onClick} className="btn btn--outline ">
-                        Add Variation
-                  </button>
-                )}
-              place="right"
-            >
-                {Constants.strings.MULTIVARIATE_DESCRIPTION}
-            </Tooltip>
+            <button type="button" onClick={onClick} className="btn btn--outline ">
+                Add Variation
+            </button>
         </div>
     );
 }
