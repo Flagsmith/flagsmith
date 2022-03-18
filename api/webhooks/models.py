d8b05336ab3ce45c49f0c729b17e7fb47b609133
Ben Rometsch <ben.rometsch@gmail.com>
2022-03-18 12:44:38 +0000

    Test ECS migrate staging

--- api/webhooks/models.py
+++ api/webhooks/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class AbstractBaseWebhookModel(models.Model):
     url = models.URLField()
     secret = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
 
     class Meta:
         abstract = True
