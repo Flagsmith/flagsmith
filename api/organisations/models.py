b5e58f400ece9ecaddf1417b764ab1a528e1f43d
Matthew Elwell <matthewe@solidstategroup.com>
2022-03-23 14:58:50 +0000

    Tidy up OrganisationRole logic

--- api/organisations/models.py
+++ api/organisations/models.py
@@ -17,12 +17,9 @@ from users.utils.mailer_lite import MailerLite
 from webhooks.models import AbstractBaseWebhookModel
 
 
-class OrganisationRole(enum.Enum):
-    ADMIN = "ADMIN"
-    USER = "USER"
-
-
-organisation_roles = ((tag.name, tag.value) for tag in OrganisationRole)
+class OrganisationRole(models.TextChoices):
+    ADMIN = ("ADMIN", "Admin")
+    USER = ("USER", "User")
 
 
 class Organisation(models.Model):
@@ -89,7 +86,7 @@ class UserOrganisation(models.Model):
     user = models.ForeignKey("users.FFAdminUser", on_delete=models.CASCADE)
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     date_joined = models.DateTimeField(auto_now_add=True)
-    role = models.CharField(max_length=50, choices=organisation_roles)
+    role = models.CharField(max_length=50, choices=OrganisationRole.choices)
 
     class Meta:
         unique_together = (
