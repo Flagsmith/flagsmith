ea46c9ba1a96e38bdb660b318c183762f67311e0
Ben Rometsch <ben.rometsch@gmail.com>
2022-03-23 11:36:56 +0000

    Updated endpoint

--- api/integrations/dynatrace/dynatrace.py
+++ api/integrations/dynatrace/dynatrace.py
@@ -7,14 +7,14 @@ from integrations.common.wrapper import AbstractBaseEventIntegrationWrapper
 
 logger = logging.getLogger(__name__)
 
-EVENTS_API_URI = "api/v1/events"
+EVENTS_API_URI = "api/v2/events/ingest"
 
 
 class DynatraceWrapper(AbstractBaseEventIntegrationWrapper):
     def __init__(self, base_url: str, api_key: str):
         self.base_url = base_url
         self.api_key = api_key
-        self.url = f"{self.base_url}{EVENTS_API_URI}?api_key={self.api_key}"
+        self.url = f"{self.base_url}{EVENTS_API_URI}?api-token={self.api_key}"
 
     def _track_event(self, event: dict) -> None:
         response = requests.post(self.url, data=json.dumps(event))
