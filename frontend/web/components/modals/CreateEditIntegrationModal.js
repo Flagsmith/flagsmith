b60dfa79b326f6416433fe747abbc341d4826183
kyle-ssg <kyle@solidstategroup.com>
2022-03-31 10:26:19 +0100

    Add slack channel id input

--- frontend/web/components/modals/CreateEditIntegrationModal.js
+++ frontend/web/components/modals/CreateEditIntegrationModal.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
 import EnvironmentSelect from '../EnvironmentSelect';
 import _data from '../../../common/data/base/_data';
 import ErrorMessage from '../ErrorMessage';
-import Select from "react-select";
 
 const CreateEditIntegration = class extends Component {
     static displayName = 'CreateEditIntegration'
 
     constructor(props, context) {
         super(props, context);
-        const fields = _.cloneDeep(this.props.integration.fields)
-        this.state = { fields, data: this.props.data ? { ...this.props.data } : {fields} };
-        if (this.props.id === "slack" && this.state.data.flagsmithEnvironment) {
+        const fields = _.cloneDeep(this.props.integration.fields);
+        this.state = { fields, data: this.props.data ? { ...this.props.data } : { fields } };
+        if (this.props.id === 'slack' && this.state.data.flagsmithEnvironment) {
             _data.get(`${Project.api}environments/${this.state.data.flagsmithEnvironment}/integrations/${this.props.id}-channels?limit=1000`)
-                .then((res)=>{
-                    this.state.data.enabled = true
-                    this.state.fields = this.state.fields || []
+                .then((res) => {
+                    this.state.data.enabled = true;
+                    this.state.fields = this.state.fields || [];
                     this.state.fields.push({
-                        key:"channel_id",
-                        label: "Channel",
-                        options:((res && res.channels)||[]).map((v)=>(
-                            {label:v.channel_name,value:v.channel_id}
-                        ))
-                    })
-                    this.setState({authorised:true})
-                })
+                        key: 'channel_id',
+                        label: 'Channel',
+                        options: ((res && res.channels) || []).map(v => (
+                            { label: v.channel_name, value: v.channel_id }
+                        )),
+                    });
+                    this.setState({ authorised: true });
+                });
         }
     }
 
@@ -50,22 +50,20 @@ const CreateEditIntegration = class extends Component {
             return;
         }
         this.setState({ isLoading: true });
-        const handleOauthSignature = (res, isProject)=> {
+        const handleOauthSignature = (res, isProject) => {
             const signature = res && res.signature;
             if (signature) {
-                const postfix = `?redirect_url=${encodeURIComponent(`${document.location.href}?environment=${this.state.data.flagsmithEnvironment}&configure=${this.props.id}`)}&signature=${signature}`
-                document.location = isProject?
-                    `${Project.api}projects/${this.props.projectId}/integrations/${this.props.id}/oauth/${postfix}`
-                    :
-                    `${Project.api}environments/${this.state.data.flagsmithEnvironment}/integrations/${this.props.id}/oauth/${postfix}`
+                const postfix = `?redirect_url=${encodeURIComponent(`${document.location.href}?environment=${this.state.data.flagsmithEnvironment}&configure=${this.props.id}`)}&signature=${signature}`;
+                document.location = isProject
+                    ? `${Project.api}projects/${this.props.projectId}/integrations/${this.props.id}/oauth/${postfix}`
+                    : `${Project.api}environments/${this.state.data.flagsmithEnvironment}/integrations/${this.props.id}/oauth/${postfix}`;
             }
-
-        }
+        };
         if (this.props.integration.perEnvironment) {
             if (isOauth) {
                 return _data.get(`${Project.api}environments/${this.state.data.flagsmithEnvironment}/integrations/${this.props.id}/signature/`, {
                     redirect_url: document.location.href,
-                }).then((res)=>handleOauthSignature(res, false))
+                }).then(res => handleOauthSignature(res, false));
             }
             if (isEdit) {
                 _data.put(`${Project.api}environments/${this.state.data.flagsmithEnvironment}/integrations/${this.props.id}/${this.props.data.id}/`, this.state.data)
@@ -74,10 +72,10 @@ const CreateEditIntegration = class extends Component {
                 _data.post(`${Project.api}environments/${this.state.data.flagsmithEnvironment}/integrations/${this.props.id}/`, this.state.data)
                     .then(this.onComplete).catch(this.onError);
             }
-        } else if (isOauth){
+        } else if (isOauth) {
             return _data.get(`${Project.api}projects/${this.props.projectId}/integrations/${this.props.id}/signature/`, {
                 redirect_url: document.location.href,
-            }).then((res)=>handleOauthSignature(res, true))
+            }).then(res => handleOauthSignature(res, true));
         } else if (isEdit) {
             _data.put(`${Project.api}projects/${this.props.projectId}/integrations/${this.props.id}/${this.props.data.id}/`, this.state.data)
                 .then(this.onComplete).catch(this.onError);
@@ -133,19 +131,19 @@ const CreateEditIntegration = class extends Component {
                           <div className="mb-2">
                               {this.state.data[field.key]}
                           </div>
-                      ) : field.options? (
-                          <div                             className="full-width mb-2">
+                      ) : field.options ? (
+                          <div className="full-width mb-2">
                               <Select
-                                  onChange={(v) => {
-                                      this.update(field.key, v.value)
-                                  }}
-                                  options={field.options}
-                                  value={this.state.data[field.key] && field.options.find((v)=>v.value===this.state.data[field.key]) ? {
-                                      label: field.options.find((v)=>v.value===this.state.data[field.key]).label,
-                                      value: this.state.data[field.key],
-                                  } : {
-                                      label: 'Please select',
-                                  }}
+                                onChange={(v) => {
+                                    this.update(field.key, v.value);
+                                }}
+                                options={field.options}
+                                value={this.state.data[field.key] && field.options.find(v => v.value === this.state.data[field.key]) ? {
+                                    label: field.options.find(v => v.value === this.state.data[field.key]).label,
+                                    value: this.state.data[field.key],
+                                } : {
+                                    label: 'Please select',
+                                }}
                               />
                           </div>
 
@@ -163,6 +161,19 @@ const CreateEditIntegration = class extends Component {
 
                   </>
                 ))}
+                {this.state.authorised && this.props.id === 'slack' && (
+                    <div>
+                        Can't see your channel? Enter your channel ID here (C0xxxxxx)
+                        <Input
+                          ref={e => this.input = e}
+                          value={this.state.data.channel_id}
+                          onChange={e => this.update('channel_id', e)}
+                          isValid={!!this.state.data.channel_id}
+                          type="text"
+                          className="full-width mt-2"
+                        />
+                    </div>
+                )}
                 <ErrorMessage error={this.state.error}/>
                 {!this.props.readOnly && (
                     <div className="text-right">
