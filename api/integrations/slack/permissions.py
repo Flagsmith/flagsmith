88dc6e230eb50291dd37ac2e671978e0d3544041
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-01 13:09:56 +0530

    remove logging

--- api/integrations/slack/permissions.py
+++ api/integrations/slack/permissions.py
@@ -1,19 +1,13 @@
-import logging
-
 from rest_framework.permissions import IsAuthenticated
 
 from environments.models import Environment
-
-logger = logging.getLogger(__name__)
 
 
 class OauthInitPermission(IsAuthenticated):
     def has_permission(self, request, view):
         if not super().has_permission(request, view):
             return False
-        logger.debug(
-            "OauthInitPermission called with user: %s and view: %s", request.user, view
-        )
+
         environment = Environment.objects.get(
             api_key=view.kwargs.get("environment_api_key")
         )
