d8963ad64c4be9863b0ea8b65c6907c181129ae9
Kyle Johnson <kyle@solidstategroup.com>
2022-04-19 11:09:46 +0100

    Update SDK / other endpoints for edge identities (#978)
    * Update SDK / other endpoints for edge identities

--- frontend/env/project_e2e.js
+++ frontend/env/project_e2e.js
@@ -1,6 +1,7 @@
 module.exports = global.Project = {
     api: 'http://flagsmith-api:8000/api/v1/',
     flagsmithClientAPI: 'https://api.bullet-train.io/api/v1/',
+    flagsmithClientEdgeAPI: 'https://edge.api.flagsmith.com/api/v1/',
     flagsmith: '8KzETdDeMY7xkqkSkY3Gsg', // This is our Bullet Train API key - Bullet Train runs on Bullet Train!
     debug: false,
     env: 'dev', // This is used for Sentry tracking
