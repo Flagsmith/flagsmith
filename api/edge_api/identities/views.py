7757f09429833d501a1effb8bb398a56ef0c3789
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-26 13:02:12 +0530

    wip

--- api/edge_api/identities/views.py
+++ api/edge_api/identities/views.py
@@ -2,13 +2,16 @@ import base64
 import json
 import typing
 
+import marshmallow
 from boto3.dynamodb.conditions import Key
-from flag_engine.identities.builders import (
-    build_identity_dict,
-    build_identity_model,
-)
+
+from django.core.exceptions import ObjectDoesNotExist
+from drf_yasg2.utils import swagger_auto_schema
+from flag_engine.api.schemas import APITraitSchema
+from flag_engine.identities.builders import build_identity_dict, build_identity_model
 from rest_framework import status, viewsets
-from rest_framework.exceptions import NotFound
+from rest_framework.decorators import action
+from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -16,12 +19,17 @@ from app.pagination import EdgeIdentityPagination
 from edge_api.identities.serializers import (
     EdgeIdentityFeatureStateSerializer,
     EdgeIdentitySerializer,
+    EdgeIdentityTraitsSerializer,
 )
 from environments.identities.models import Identity
 from environments.models import Environment
 from environments.permissions.permissions import NestedEnvironmentPermissions
 from features.permissions import IdentityFeatureStatePermissions
 from projects.exceptions import DynamoNotEnabledError
+
+from .views_mixins import GetIdentityMixin
+
+trait_schema = APITraitSchema()
 
 
 class EdgeIdentityViewSet(viewsets.ModelViewSet):
@@ -89,8 +97,36 @@ class EdgeIdentityViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         Identity.dynamo_wrapper.delete_item(instance["composite_key"])
 
+    @swagger_auto_schema(
+        responses={200: EdgeIdentityTraitsSerializer(many=True)},
+    )
+    def get_traits(self, request, *args, **kwargs):
+        data = trait_schema.dump(self.identity.identity_traits, many=True)
+        return Response(data=data, status=status.HTTP_200_OK)
 
-from .views_mixins import GetIdentityMixin
+    @swagger_auto_schema(
+        method="put",
+        request_body=EdgeIdentityTraitsSerializer,
+        responses={200: EdgeIdentityTraitsSerializer(many=True)},
+    )
+    @swagger_auto_schema(
+        method="post",
+        request_body=EdgeIdentityTraitsSerializer,
+        responses={200: EdgeIdentityTraitsSerializer(many=True)},
+    )
+    @action(detail=False, methods=["post", "put"], url_path="create-or-update")
+    def update_traits(self, request, *args, **kwargs):
+        data = self._create_or_update(request)
+        return Response(data, status=status.HTTP_200_OK)
+
+    def _create_or_update(self, request):
+        try:
+            trait = trait_schema.load(request.data)
+        except marshmallow.ValidationError as validation_error:
+            raise ValidationError(validation_error) from validation_error
+        self.identity.update_traits([trait])
+        self._save()
+        return trait_schema.dump(trait)
 
 
 class EdgeIdentityFeatureStateViewSet(viewsets.ModelViewSet, GetIdentityMixin):
