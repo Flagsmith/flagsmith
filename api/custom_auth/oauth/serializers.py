7a5e185dd8be79f88efcf8b7ac331b5c222e20e2
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-05-05 16:32:52 +0530

    fix lint

--- api/custom_auth/oauth/serializers.py
+++ api/custom_auth/oauth/serializers.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import get_user_model
 from django.contrib.auth.signals import user_logged_in
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
