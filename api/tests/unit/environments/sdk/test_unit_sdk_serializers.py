80f6e5d55350cc836a722623c47c9e69eeb85f71
Matthew Elwell <matthewe@solidstategroup.com>
2022-05-12 11:34:21 +0100

    Ensure identity integrations are called on post requests to identities endpoint

---
+++ api/tests/unit/environments/sdk/test_unit_sdk_serializers.py
@@ -0,0 +1,46 @@
+import pytest
+
+from environments.identities.models import Identity
+from environments.sdk.serializers import IdentifyWithTraitsSerializer
+
+
+@pytest.mark.parametrize(
+    "create_identity_before, persist_traits",
+    ((True, True), (True, False), (False, True), (False, False)),
+)
+def test_identify_with_traits_serializer_runs_identity_integrations_on_create(
+    mocker, environment, feature, create_identity_before, persist_traits
+):
+    # Given
+    identifier = "johnnybravo"
+    trait_key = "foo"
+    trait_value = "bar"
+
+    if create_identity_before:
+        Identity.objects.create(identifier=identifier, environment=environment)
+
+    environment.project.organisation.persist_trait_data = persist_traits
+    environment.project.organisation.save()
+
+    data = {
+        "identifier": identifier,
+        "traits": [{"trait_key": trait_key, "trait_value": trait_value}],
+    }
+    serializer = IdentifyWithTraitsSerializer(
+        data=data, context={"environment": environment}
+    )
+
+    mock_identify_integrations = mocker.patch(
+        "environments.sdk.serializers.identify_integrations"
+    )
+
+    # When
+    assert serializer.is_valid()
+    serializer.save()
+
+    # Then
+    mock_identify_integrations.assert_called_once()
+    call_args = mock_identify_integrations.call_args[0]
+
+    identity = call_args[0]
+    assert identity.identifier == identifier
