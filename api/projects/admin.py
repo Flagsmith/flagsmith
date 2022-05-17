ede39df08e3addd3c8af6eadf7bc01db76536ea9
Matthew Elwell <matthewe@solidstategroup.com>
2022-05-17 16:07:00 +0100

    Fix project admin timeouts (#1126)
    * Optimise project admin page load
    
    * Explicitly define fields on all project admin classes

--- api/projects/admin.py
+++ api/projects/admin.py
@@ -14,24 +14,35 @@ class EnvironmentInline(admin.StackedInline):
     model = Environment
     extra = 0
     show_change_link = True
+    fields = ("name", "api_key", "minimum_change_request_approvals")
 
 
 class FeatureInline(admin.StackedInline):
     model = Feature
     extra = 0
     show_change_link = True
+    fields = (
+        "name",
+        "description",
+        "initial_value",
+        "default_enabled",
+        "type",
+        "is_archived",
+    )
 
 
 class SegmentInline(admin.StackedInline):
     model = Segment
     extra = 0
     show_change_link = True
+    fields = ("name", "description")
 
 
 class TagInline(admin.StackedInline):
     model = Tag
     extra = 0
     show_change_link = True
+    fields = ("label", "description", "color")
 
 
 @admin.register(Project)
@@ -46,3 +57,4 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = ("created_date", "enable_dynamo_db")
     list_select_related = ("organisation",)
     search_fields = ("organisation__name",)
+    fields = ("name", "organisation", "hide_disabled_flags", "enable_dynamo_db")
