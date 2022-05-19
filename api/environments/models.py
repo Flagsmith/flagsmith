94f38e939335694bbf20da247dbc19ecea68aef2
Matthew Elwell <matthewe@solidstategroup.com>
2022-05-19 18:52:32 +0100

    Add cache for bad environments

--- api/environments/models.py
+++ api/environments/models.py
@@ -30,6 +30,7 @@ from webhooks.models import AbstractBaseWebhookModel
 
 logger = logging.getLogger(__name__)
 environment_cache = caches[settings.ENVIRONMENT_CACHE_LOCATION]
+bad_environments_cache = caches[settings.BAD_ENVIRONMENTS_CACHE_LOCATION]
 
 
 class Environment(LifecycleModel):
@@ -115,6 +116,9 @@ class Environment(LifecycleModel):
                 logger.warning("Requested environment with null api_key.")
                 return None
 
+            if cls.is_bad_key(api_key):
+                return None
+
             environment = environment_cache.get(api_key)
             if not environment:
                 select_related_args = (
@@ -135,6 +139,7 @@ class Environment(LifecycleModel):
                 environment_cache.set(environment.api_key, environment, timeout=60)
             return environment
         except cls.DoesNotExist:
+            cls.set_bad_key(api_key)
             logger.info("Environment with api_key %s does not exist" % api_key)
 
     def get_feature_state(
@@ -155,6 +160,24 @@ class Environment(LifecycleModel):
                 self.feature_states.filter(**filter_kwargs),
             )
         )
+
+    @staticmethod
+    def is_bad_key(environment_key: str) -> bool:
+        return (
+            settings.CACHE_BAD_ENVIRONMENTS_SECONDS > 0
+            and bad_environments_cache.get(environment_key, 0)
+            >= settings.CACHE_BAD_ENVIRONMENTS_AFTER_FAILURES
+        )
+
+    @staticmethod
+    def set_bad_key(environment_key: str) -> None:
+        if settings.CACHE_BAD_ENVIRONMENTS_SECONDS:
+            current_count = bad_environments_cache.get(environment_key, 0)
+            bad_environments_cache.set(
+                environment_key,
+                current_count + 1,
+                timeout=settings.CACHE_BAD_ENVIRONMENTS_SECONDS,
+            )
 
 
 class Webhook(AbstractBaseWebhookModel):
