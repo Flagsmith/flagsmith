feab68f3257b6a49dfb28adcd165d2e612d2167e
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-07 14:37:01 +0100

    Fixed install instructions for v2 sdks

--- frontend/common/code-help/install/install-rust.js
+++ frontend/common/code-help/install/install-rust.js
@@ -1,3 +1,3 @@
 import Utils from '../../utils/utils';
 
-module.exports = ({  }) => `The package can be found at <a href="https://crates.io/crates/bullettrain">https://crates.io/crates/bullettrain</a>;`;
+module.exports = ({  }) => `The package can be found at <a href="https://crates.io/crates/flagsmith">https://crates.io/crates/flagsmith</a>;`;
