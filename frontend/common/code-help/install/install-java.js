14722dc48717979d1bdcd2888c456ee6bb3c5154
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-08 16:43:47 +0100

    improvement/sdk-v2-in-app-docs (#1179)
    SDK Snippets

--- frontend/common/code-help/install/install-java.js
+++ frontend/common/code-help/install/install-java.js
@@ -4,9 +4,9 @@ module.exports = ({ NPM_NODE_CLIENT, URL_CLIENT }) => `// Maven
 ${Utils.escapeHtml('<dependency>')}
 ${Utils.escapeHtml('<groupId>com.flagsmith</groupId>')}
 ${Utils.escapeHtml('<artifactId>flagsmith-java-client</artifactId>')}
-${Utils.escapeHtml('<version>2.8</version>')}
+${Utils.escapeHtml('<version>5.0.0/version>')}
 </dependency>
 
 // Gradle
-implementation 'com.flagsmith:flagsmith-java-client:2.6'
+implementation 'com.flagsmith:flagsmith-java-client:5.0.0'
 `;
