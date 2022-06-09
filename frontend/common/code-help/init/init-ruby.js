1523291fadf977bc4dfcdba8b1cd110c7cd0c612
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-09 15:14:35 +0100

    improvement/more-in-app-sdk-docs

--- frontend/common/code-help/init/init-ruby.js
+++ frontend/common/code-help/init/init-ruby.js
@@ -2,12 +2,13 @@ import Utils from '../../utils/utils';
 
 module.exports = (envId, { FEATURE_NAME, FEATURE_FUNCTION, FEATURE_NAME_ALT }) => `require "flagsmith"
 
-flagsmith = Flagsmith.new("${envId}")
+$flagsmith = Flagsmith::Client.new(
+    environment_key: '${envId}'
+)
 
 // Check for a feature
-if flagsmith.feature_enabled?("${FEATURE_NAME}")
-end
+$show_button = $flags.is_feature_enabled('${FEATURE_NAME}')
 
-// Or, use the value of a feature
-if flagsmith.get_value("${FEATURE_NAME_ALT}")
-end`;
+// Or, use the value of a feature]
+$button_data = $flags.get_feature_value('${FEATURE_NAME_ALT}')
+`;
