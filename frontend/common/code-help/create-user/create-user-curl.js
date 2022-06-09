b2113663798dd2f72c70fe7cbfcc5e3893eb1e8e
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-09 15:23:29 +0100

    improvement/more-in-app-sdk-docs (#1190)

--- frontend/common/code-help/create-user/create-user-curl.js
+++ frontend/common/code-help/create-user/create-user-curl.js
@@ -1,4 +1,3 @@
-module.exports = (envId, { LIB_NAME, NPM_RN_CLIENT, USER_ID, USER_FEATURE_FUNCTION, USER_FEATURE_NAME }, userId) => `
-// Identify / create user
+module.exports = (envId, { LIB_NAME, NPM_RN_CLIENT, USER_ID, USER_FEATURE_FUNCTION, USER_FEATURE_NAME }, userId) => `// Identify / create user
 curl 'https://edge.api.flagsmith.com/api/v1/identities/?identifier=${userId}' -H 'authority: edge.api.flagsmith.com' -H 'x-environment-key: ${envId}' --compressed
 `;
