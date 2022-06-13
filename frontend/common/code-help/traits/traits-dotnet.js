666120adc3f8f8478ae92e7ed78253eceb9df2c4
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-13 14:32:15 +0100

    Removed increment/decrement trait

--- frontend/common/code-help/traits/traits-dotnet.js
+++ frontend/common/code-help/traits/traits-dotnet.js
@@ -2,9 +2,6 @@ module.exports = (envId, { LIB_NAME, USER_ID, TRAIT_NAME, LIB_NAME_JAVA, FEATURE
 // Set a user trait
 Trait userTrait = await BulletTrainClient.instance.SetTrait("${USER_ID}", "${TRAIT_NAME}", "blue");
 
-// Increment a trait
-Trait userTrait = await BulletTrainClient.instance.IncrementTrait("${USER_ID}", "${TRAIT_NAME}", 1);
-
 Identity userIdentity = await BulletTrainClient.instance.GetUserIdentity("${USER_ID}");
 if (userIdentity != null) {
   // Run the code to use user identity i.e. userIdentity.flags or userIdentity.traits
