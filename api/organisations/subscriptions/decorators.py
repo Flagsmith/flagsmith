d9936209ca9c1895e492c52d895fca816d378909
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-15 16:38:20 +0100

    Update decorator

--- api/organisations/subscriptions/decorators.py
+++ api/organisations/subscriptions/decorators.py
@@ -1,6 +1,6 @@
 import typing
 
-from django.views import View
+from rest_framework.request import Request
 
 from organisations.models import Subscription
 from organisations.subscriptions.exceptions import InvalidSubscriptionPlanError
@@ -8,16 +8,29 @@ from organisations.subscriptions.exceptions import InvalidSubscriptionPlanError
 
 def require_plan(
     valid_plan_ids: typing.Iterable[str],
-    view: View,
-    subscription_retriever: typing.Callable[[View], Subscription],
+    subscription_retriever: typing.Callable[[Request], Subscription],
 ):
+    """
+    Decorator to be used on view functions / methods that require a specific plan.
+
+    Will result in 403 if resource requested is not part of an organisation that has
+    the correct subscription.
+    """
+
     def decorator(func):
         def wrapper(*args, **kwargs):
-            sub = subscription_retriever(view)
-            if sub.plan not in valid_plan_ids:
+            if not (args and isinstance(args[0], Request)):
+                raise ValueError(
+                    "require_plan decorator must be used on methods / functions whose "
+                    "first argument is a Request object."
+                )
+
+            sub = subscription_retriever(args[0])
+            if not sub or sub.plan not in valid_plan_ids:
                 raise InvalidSubscriptionPlanError(
                     f"Resource not available on plan {sub.plan}"
                 )
+
             return func(*args, **kwargs)
 
         return wrapper
--- api/tests/unit/organisations/subscriptions/test_unit_subscriptions_decorators.py
+++ api/tests/unit/organisations/subscriptions/test_unit_subscriptions_decorators.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 
 import pytest
+from rest_framework.request import Request
 
 from organisations.subscriptions.decorators import require_plan
 from organisations.subscriptions.exceptions import InvalidSubscriptionPlanError
@@ -11,34 +12,34 @@ def test_require_plan_raises_exception_if_plan_invalid():
     valid_plan_id = "plan-id"
     invalid_plan_id = "invalid-plan-id"
 
-    mock_view = MagicMock(kwargs={"foo": "bar"})
+    mock_request = MagicMock(spec=Request)
     mock_subscription = MagicMock(plan=invalid_plan_id)
 
-    @require_plan([valid_plan_id], mock_view, lambda v: mock_subscription)
-    def test_function():
+    @require_plan([valid_plan_id], lambda v: mock_subscription)
+    def test_function(request: Request):
         return "foo"
 
     # When
     with pytest.raises(InvalidSubscriptionPlanError):
-        test_function()
+        test_function(mock_request)
 
     # Then
     # Exception is raised
 
 
-def test_require_plan_does_not_raise_exception_if_plan_valid():
+def test_require_plan_does_not_raise_exception_if_plan_valid(rf):
     # Given
     valid_plan_id = "plan-id"
 
-    mock_view = MagicMock()
+    mock_request = MagicMock(spec=Request)
     mock_subscription = MagicMock(plan=valid_plan_id)
 
-    @require_plan([valid_plan_id], mock_view, lambda v: mock_subscription)
-    def test_function():
+    @require_plan([valid_plan_id], lambda v: mock_subscription)
+    def test_function(request: Request):
         return "foo"
 
     # When
-    res = test_function()
+    res = test_function(mock_request)
 
     # Then
     assert res == "foo"
