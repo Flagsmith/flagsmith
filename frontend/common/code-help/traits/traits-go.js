44492d80b1dc0f502c370f68c8eb24477a419f76
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-17 13:21:46 +0100

    added go traits example

--- frontend/common/code-help/traits/traits-go.js
+++ frontend/common/code-help/traits/traits-go.js
@@ -1,9 +1,13 @@
 module.exports = (envId, { LIB_NAME, USER_ID, LIB_NAME_JAVA, FEATURE_NAME, FEATURE_FUNCTION, FEATURE_NAME_ALT, FEATURE_NAME_ALT_VALUE, NPM_CLIENT }, userId) => `var testUser = flagsmith.User{Identifier: "${USER_ID}"}
 
-c := flagsmith.DefaultClient("${envId}")
+client := flagsmith.DefaultClient("${envId}")
 
-enabled, err := c.HasUserFeature(testUser, "${FEATURE_NAME}")
+trait := flagsmith.Trait{TraitKey: "trait", TraitValue: "trait_value"}
+traits = []*flagsmith.Trait{&trait}
 
-val, err := c.GetUserValue(testUser, ${FEATURE_NAME_ALT})
+// The method below triggers a network request
+flags, _ := client.GetIdentityFlags(identifier, traits)
 
+showButton, _ := flags.IsFeatureEnabled("${FEATURE_NAME}")
+buttonData, _ := flags.GetFeatureValue("${FEATURE_NAME_ALT}")
 `;
