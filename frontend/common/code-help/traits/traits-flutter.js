71d2928b3c46546793ea93d8df0bec0dd2fa86bf
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-17 13:06:23 +0100

    Fixed flutter user creation

--- frontend/common/code-help/traits/traits-flutter.js
+++ frontend/common/code-help/traits/traits-flutter.js
@@ -1,6 +1,6 @@
 module.exports = (envId, { LIB_NAME, USER_ID, LIB_NAME_JAVA, TRAIT_NAME, FEATURE_NAME, FEATURE_FUNCTION, FEATURE_NAME_ALT, FEATURE_NAME_ALT_VALUE, NPM_CLIENT }, userId) => `final flagsmithClient = FlagsmithClient(
-    apiKey: '${envId}' 
-    config: config, 
+    apiKey: '${envId}'
+    config: config,
     seeds: <Flag>[
         Flag.seed('feature', enabled: true),
     ],
@@ -9,20 +9,16 @@ module.exports = (envId, { LIB_NAME, USER_ID, LIB_NAME_JAVA, TRAIT_NAME, FEATURE
 //if you prefer async initialization then you should use
 //final flagsmithClient = await FlagsmithClient.init(
 //        apiKey: 'YOUR_ENV_API_KEY',
-//        config: config, 
+//        config: config,
 //        seeds: <Flag>[
 //            Flag.seed('feature', enabled: true),
-//        ], 
+//        ],
 //        update: false,
 //    );
 
-// This will create a user in the dashboard if they don't already exist
 final user = FeatureUser(identifier: '${USER_ID}');
+// This will create a user in the dashboard if they don't already exist
+bool featureEnabled = await flagsmithClient.hasFeatureFlag('${FEATURE_NAME}', user: user);
 
-bool featureEnabled = await flagsmithClient
-.hasFeatureFlag('${FEATURE_NAME}', user: user);
-
-final myRemoteConfig = await flagsmithClient
-.getFeatureFlagValue('${FEATURE_NAME_ALT}', user: user);
-
+final myRemoteConfig = await flagsmithClient.getFeatureFlagValue('${FEATURE_NAME_ALT}', user: user);
 `;
