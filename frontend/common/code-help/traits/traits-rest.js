c38b1fb05f542bfb979e7dfbf2ef3e143c53cef2
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-17 13:04:07 +0100

    remove redundant query param

--- frontend/common/code-help/traits/traits-rest.js
+++ frontend/common/code-help/traits/traits-rest.js
@@ -1,6 +1,6 @@
 module.exports = (envId, { FEATURE_NAME, FEATURE_FUNCTION, USER_ID, FEATURE_NAME_ALT }) => `# This will create a user in the dashboard if they don't already exist
 
-curl -X "POST" "https://edge.api.flagsmith.com/api/v1/identities/?identifier=winning2"
+curl -X "POST" "https://edge.api.flagsmith.com/api/v1/identities/"
      -H 'X-Environment-Key: ${envId}'
      -H 'Content-Type: application/json; charset=utf-8'
      -d $'{
