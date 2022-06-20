7de00ebc3b1d3abb691736961b474c8b8d5080c4
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-06-20 15:16:37 +0530

    tidy up create-user-rust.js

--- frontend/common/code-help/create-user/create-user-rust.js
+++ frontend/common/code-help/create-user/create-user-rust.js
@@ -1,20 +1,17 @@
-module.exports = (envId, { FEATURE_NAME, FEATURE_NAME_ALT }, userId) => `use std::env;
+module.exports = (envId, { FEATURE_NAME, FEATURE_NAME_ALT }, userId) => `
 use flagsmith::{Flag, Flagsmith, FlagsmithOptions};
 
 let options = FlagsmithOptions {..Default::default()};
 let flagsmith = Flagsmith::new(
-    env::var("${envId}")
-        .expect("FLAGSMITH_ENVIRONMENT_KEY not found in environment"),
+    "${envId}".to_string(),
     options,
 );
 
 // Identify the user
-let identity_flags = flagsmith.get_identity_flags("${userId}").unwrap();
+let identity_flags = flagsmith.get_identity_flags("${userId}", None).unwrap();
 
-// get the state / value of the user's flags 
+// get the state / value of the user's flags
 let is_enabled = identity_flags.is_feature_enabled("${FEATURE_NAME}").unwrap();
 let feature_value = identity_flags.get_feature_value_as_string("${FEATURE_NAME_ALT}").unwrap();
 `;
-
-// TODO: verify that get_identity_flags works without traits
 
