921213f7e3995a5599e0889dd33da80695b0b66f
Ben Rometsch <ben.rometsch@gmail.com>
2022-06-20 14:33:50 +0100

    Added flutter example

--- frontend/common/code-help/traits/traits-flutter.js
+++ frontend/common/code-help/traits/traits-flutter.js
@@ -16,9 +16,29 @@ module.exports = (envId, { LIB_NAME, USER_ID, LIB_NAME_JAVA, TRAIT_NAME, FEATURE
 //        update: false,
 //    );
 
-final user = FeatureUser(identifier: '${USER_ID}');
-// This will create a user in the dashboard if they don't already exist
-bool featureEnabled = await flagsmithClient.hasFeatureFlag('${FEATURE_NAME}', user: user);
+// An identifier is created when we get the feature flags for said identifier for the first time
+flagsmithClient.getFeatureFlags(user: const Identity(identifier: '${USER_ID}'));
+// Create a new user trait based on the above identifier
+flagsmithClient.createTrait(
+    value: TraitWithIdentity(
+    identity: const Identity(identifier: '${USER_ID}'),
+    key: 'age',
+    value: '21',
+    ),
+);
 
-final myRemoteConfig = await flagsmithClient.getFeatureFlagValue('${FEATURE_NAME_ALT}', user: user);
+// Update the previously created trait with a new value
+flagsmithClient.updateTraits(value: [
+    TraitWithIdentity(
+    identity: const Identity(identifier: '${USER_ID}'),
+    key: 'age',
+    value: '20',
+    ),
+]);
+
+final user = FeatureUser(identifier: '');
+// This will create a user in the dashboard if they don't already exist
+bool featureEnabled = await flagsmithClient.hasFeatureFlag('${FEATURE_NAME}', user: const Identity(identifier: '${USER_ID}'));
+
+final myRemoteConfig = await flagsmithClient.getFeatureFlagValue('${FEATURE_NAME_ALT}', user: const Identity(identifier: '${USER_ID}'));
 `;
