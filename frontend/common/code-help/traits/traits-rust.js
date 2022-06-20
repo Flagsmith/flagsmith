965a3e432682fb3723789026877e1fe908d86392
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-06-20 15:19:25 +0530

    Add traits to traits-rust.js

--- frontend/common/code-help/traits/traits-rust.js
+++ frontend/common/code-help/traits/traits-rust.js
@@ -1,14 +1,25 @@
-module.exports = (envId, { USER_ID }) => `use std::env;
+module.exports = (envId, { USER_ID }) => `
 use flagsmith::{Flag, Flagsmith, FlagsmithOptions};
+use flagsmith_flag_engine::types::{FlagsmithValue, FlagsmithValueType};
+use flagsmith_flag_engine::identities::Trait;
 
 let options = FlagsmithOptions {..Default::default()};
 let flagsmith = Flagsmith::new(
-    env::var("${envId}")
-        .expect("FLAGSMITH_ENVIRONMENT_KEY not found in environment"),
+    "${envId}".to_string(),
     options,
 );
+
+let trait_key = "trait_key1";
+let trait_value = "trai_value1";
+
+let traits = vec![Trait {
+    trait_key: trait_key.to_string(),
+    trait_value: FlagsmithValue {
+        value: trait_value.to_string(),
+        value_type: FlagsmithValueType::String,
+    },
+}];
 // The method below triggers a network request
 let identity_flags = flagsmith.get_identity_flags("${USER_ID}", Some(traits)).unwrap();
 `;
 
-// TODO: deal with the `Some(traits)` line above - this is invalid (I think)
