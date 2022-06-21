ffe0e126dedcafa1aa37006eb50db4c8f292891a
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-21 16:38:48 +0100

    Add base class and add uuid to amplitude configuration model for testing

--- api/core/migration_helpers.py
+++ api/core/migration_helpers.py
@@ -1,4 +1,7 @@
-from django.db import migrations
+import typing
+import uuid
+
+from django.db import migrations, models
 
 
 class PostgresOnlyRunSQL(migrations.RunSQL):
@@ -11,3 +14,11 @@ class PostgresOnlyRunSQL(migrations.RunSQL):
         if schema_editor.connection.vendor != "postgresql":
             return
         super().database_backwards(app_label, schema_editor, from_state, to_state)
+
+
+def add_default_uuids(model_class: typing.Type[models.Model]) -> None:
+    to_update = []
+    for model in model_class.objects.all():
+        model.uuid = uuid.uuid4()
+        to_update.append(model)
+    model_class.objects.bulk_update(to_update, fields=["uuid"])
---
+++ api/core/models.py
@@ -0,0 +1,21 @@
+import uuid
+
+from django.db import models
+from django.db.models import Manager
+
+
+class AbstractBaseExportableModelManager(Manager):
+    def get_by_natural_key(self, uuid_: str):
+        return self.get(uuid=uuid_)
+
+
+class AbstractBaseExportableModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    objects = AbstractBaseExportableModelManager()
+
+    class Meta:
+        abstract = True
+
+    def natural_key(self):
+        return self.uuid
