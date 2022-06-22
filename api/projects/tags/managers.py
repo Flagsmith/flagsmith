fda24b0a960db961bf544f83f066440936298e16
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-22 17:13:51 +0100

    add uuid field to tag

--- api/projects/tags/managers.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import Manager
-
-
-class TagManager(Manager):
-    def get_by_natural_key(self, label, project):
-        return self.get(project=project, label=label)
--- api/projects/tags/models.py
+++ api/projects/tags/models.py
@@ -1,10 +1,10 @@
+from core.models import AbstractBaseExportableModel
 from django.db import models
 
 from projects.models import Project
-from projects.tags.managers import TagManager
 
 
-class Tag(models.Model):
+class Tag(AbstractBaseExportableModel):
     label = models.CharField(max_length=100)
     color = models.CharField(
         max_length=10, help_text="Hexadecimal value of the tag color"
@@ -12,15 +12,9 @@ class Tag(models.Model):
     description = models.CharField(max_length=512, blank=True, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tags")
 
-    objects = TagManager()
-
     class Meta:
         ordering = ("id",)  # explicit ordering to prevent pagination warnings
         unique_together = (("project", "label"),)
 
     def __str__(self):
         return "Tag %s" % self.label
-
-    # TODO
-    def natural_key(self):
-        return self.label, self.project_id
