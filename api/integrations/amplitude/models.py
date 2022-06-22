c70a371e914d3993ccd01dad4884d1c7265e32e8
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-22 10:14:23 +0100

    Add base classes for adding UUID attribute (#1223)
    * Add base class and add uuid to amplitude configuration model for testing
    
    * Refactor to use a class to run the migration function
    
    * Add test and stringify uuid for natural key

--- api/integrations/amplitude/models.py
+++ api/integrations/amplitude/models.py
@@ -1,10 +1,11 @@
+from core.models import AbstractBaseExportableModel
 from django.db import models
 
 from environments.models import Environment
 from integrations.common.models import IntegrationsModel
 
 
-class AmplitudeConfiguration(IntegrationsModel):
+class AmplitudeConfiguration(IntegrationsModel, AbstractBaseExportableModel):
     environment = models.OneToOneField(
         Environment, related_name="amplitude_config", on_delete=models.CASCADE
     )
