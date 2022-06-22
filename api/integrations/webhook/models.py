b013e4572e60bd3285dcd8b72e5c7b59b1a4e14b
Matthew Elwell <matthewe@solidstategroup.com>
2022-06-22 16:29:58 +0100

    Add UUID fields to integration models

--- api/integrations/webhook/models.py
+++ api/integrations/webhook/models.py
@@ -1,13 +1,11 @@
+from core.models import AbstractBaseExportableModel
 from django.db import models
 
 from environments.models import Environment
 from webhooks.models import AbstractBaseWebhookModel
 
 
-class WebhookConfiguration(AbstractBaseWebhookModel):
+class WebhookConfiguration(AbstractBaseWebhookModel, AbstractBaseExportableModel):
     environment = models.OneToOneField(
         Environment, related_name="webhook_config", on_delete=models.CASCADE
     )
-
-    def natural_key(self):
-        return self.environment_id, self.url, self.secret
