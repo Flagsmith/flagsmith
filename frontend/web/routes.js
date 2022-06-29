5453c4a2e8fa5d0b2418dcf6ba3056be6e2a0dff
Kyle Johnson <kyle@solidstategroup.com>
2022-06-29 19:04:44 +0100

    Link to user page by friendly id (#1253)

--- frontend/web/routes.js
+++ frontend/web/routes.js
@@ -11,6 +11,7 @@ import CreateOrganisationPage from './components/pages/CreateOrganisationPage';
 import CreateEnvironmentPage from './components/pages/CreateEnvironmentPage';
 import UsersPage from './components/pages/UsersPage';
 import UserPage from './components/pages/UserPage';
+import UserIdPage from './components/pages/UserIdPage';
 import IntegrationsPage from './components/pages/IntegrationsPage';
 import FlagsPage from './components/pages/FeaturesPage';
 import SegmentsPage from './components/pages/SegmentsPage';
@@ -55,6 +56,7 @@ export default (
             <Route path="/project/:projectId/environment/:environmentId/settings" exact component={EnvironmentSettingsPage}/>
             <Route path="/project/:projectId/integrations" exact component={IntegrationsPage}/>
             <Route path="/project/:projectId/environment/:environmentId/users" exact component={UsersPage}/>
+            <Route path="/project/:projectId/environment/:environmentId/users/:identity" exact component={UserIdPage}/>
             <Route path="/project/:projectId/environment/:environmentId/users/:identity/:id" exact component={UserPage}/>
             <Route path="/project/:projectId/environment/create" exact component={CreateEnvironmentPage}/>
             <Route path="/project/:projectId/environment/:environmentId/project-settings" exact component={ProjectSettingsPage}/>
