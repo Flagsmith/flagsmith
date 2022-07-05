ba3c5ebc786886532a45d1d04ad1faa94de742c0
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-05 14:36:42 +0100

    Add management command for importing organisation

---
+++ api/import_export/management/commands/importorganisation.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand, CommandParser
+
+from import_export.import_ import import_organisation
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument(
+            "bucket-name",
+            type=int,
+            help="Name of the S3 bucket to get the organisation data from.",
+        )
+        parser.add_argument(
+            "key",
+            type=str,
+            help="S3 location key to retrieve the organisation data from.",
+        )
+
+    def handle(self, *args, **options):
+        bucket_name = options["bucket-name"]
+        key = options["key"]
+
+        import_organisation(bucket_name, key)
