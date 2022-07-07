83bf2a7d045752034a23107e53ac8d7d83487b42
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-07 15:09:10 +0100

    Add docstring

--- api/import_export/import_.py
+++ api/import_export/import_.py
@@ -10,6 +10,20 @@ class OrganisationImporter:
         self._s3_client = s3_client or boto3.client("s3")
 
     def import_organisation(self, s3_bucket: str, s3_key: str) -> None:
+        """
+        Import an organisation from a json file containing the django fixtures
+        as exported by the `export` module in this package.
+
+        This code essentially acts as a wrapper to the django loaddata management
+        command to grab a file from S3 and read from that.
+
+        Since loaddata only accepts the name of a fixture or the path to a fixture
+        file, we have to store the data in a local file before passing it to the
+        call_command function. We store it in /tmp/ with a unique uuid and remove it
+        after, regardless of the success of the task to ensure we're not clogging up
+        the task's storage.
+        """
+
         obj = self._s3_client.get_object(Bucket=s3_bucket, Key=s3_key)
         file_path = f"/tmp/{uuid.uuid4()}.json"
 
