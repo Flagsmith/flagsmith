9c62009681f9ad9e536d560937dbb2dd5709ea1d
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-07 15:31:37 +0100

    Fix node js client init (#1283)

--- frontend/common/code-help/init/init-node.js
+++ frontend/common/code-help/init/init-node.js
@@ -1,8 +1,8 @@
 module.exports = (envId, { LIB_NAME, FEATURE_NAME, FEATURE_NAME_ALT, NPM_NODE_CLIENT }, customFeature) => `import ${LIB_NAME} from "${NPM_NODE_CLIENT}"; // Add this line if you're using ${LIB_NAME} via npm
 
-const ${LIB_NAME} = new Flagsmith(
+const ${LIB_NAME} = new Flagsmith({
     environmentKey: '${envId}'
-);
+});
 
 const flags = await flagsmith.getEnvironmentFlags();
 
