d9083f065b4d470363361b83628e9f0c35f6fe9e
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-07 16:38:33 +0100

    ensure no null values before migration

--- api/features/migrations/0047_prevent_null_feature_type.py
+++ api/features/migrations/0047_prevent_null_feature_type.py
@@ -2,22 +2,32 @@
 
 from django.db import migrations, models
 
+from features.feature_types import STANDARD
+
+
+def set_default_feature_type(apps, schema_editor):
+    feature_model_class = apps.get_model("features", "feature")
+    feature_model_class.objects.filter(type__isnull=True).update(type=STANDARD)
+
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('features', '0046_add_uuid_field_to_feature_segment'),
+        ("features", "0046_add_uuid_field_to_feature_segment"),
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='feature',
-            name='type',
-            field=models.CharField(blank=True, default='STANDARD', max_length=50),
+        migrations.RunPython(
+            set_default_feature_type, reverse_code=migrations.RunPython.noop
         ),
         migrations.AlterField(
-            model_name='historicalfeature',
-            name='type',
-            field=models.CharField(blank=True, default='STANDARD', max_length=50),
+            model_name="feature",
+            name="type",
+            field=models.CharField(blank=True, default="STANDARD", max_length=50),
+        ),
+        migrations.AlterField(
+            model_name="historicalfeature",
+            name="type",
+            field=models.CharField(blank=True, default="STANDARD", max_length=50),
         ),
     ]
