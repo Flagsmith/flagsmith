e1e215ac0e4a8a5f0edc3fcc68ac007bbb02b400
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-08 14:46:46 +0100

    Add import-organisation command to run-docker.sh

--- api/scripts/run-docker.sh
+++ api/scripts/run-docker.sh
@@ -4,7 +4,6 @@ set -e
 function migrate () {
     python manage.py migrate
 }
-
 function serve() {
     gunicorn --bind 0.0.0.0:8000 \
              --worker-tmp-dir /dev/shm \
@@ -16,14 +15,20 @@ function serve() {
              app.wsgi
 }
 function migrate_identities(){
+    python manage.py importorganisation $1 $2
+}
+function import_organisation(){
     python manage.py migrate_to_edge $1
 }
+
 if [ "$1" == "migrate" ]; then
     migrate
 elif [ "$1" == "serve" ]; then
     serve
 elif [ "$1" == "migrate_identities" ]; then
     migrate_identities $2
+elif [ "$1" == "import-organisation" ]; then
+    import_organisation $2 $3
 elif [ "$1" == "migrate-and-serve" ]; then
     migrate
     serve
