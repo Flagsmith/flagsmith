a8e956d6e8bf83f4afc5cb444a9876b257e32355
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-07-18 19:07:41 +0530

    Add docs for pagination

--- api/app/pagination.py
+++ api/app/pagination.py
@@ -2,6 +2,8 @@ import base64
 import json
 from collections import OrderedDict
 
+from drf_yasg2 import openapi
+from drf_yasg2.inspectors import PaginatorInspector
 from flag_engine.identities.builders import build_identity_model
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -12,6 +14,63 @@ class CustomPagination(PageNumberPagination):
     page_size = 999
     page_size_query_param = "page_size"
     max_page_size = 999
+
+
+class EdgeIdentityPaginationInspector(PaginatorInspector):
+    def get_paginator_parameters(self, paginator):
+        """
+        :param BasePagination paginator: the paginator
+        :rtype: list[openapi.Parameter]
+        """
+        return [
+            openapi.Parameter(
+                "page_size",
+                openapi.IN_QUERY,
+                "Number of results to return per page.",
+                required=False,
+                type=openapi.TYPE_INTEGER,
+            ),
+            openapi.Parameter(
+                "last_evaluated_key",
+                openapi.IN_QUERY,
+                "last evaluated key that's part of next/previous uri",
+                required=False,
+                type=openapi.TYPE_INTEGER,
+            ),
+        ]
+
+    def get_paginated_response(self, paginator, response_schema):
+        """
+        :param BasePagination paginator: the paginator
+        :param openapi.Schema response_schema: the response schema that must be paged.
+        :rtype: openapi.Schema
+        """
+
+        return openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties=OrderedDict(
+                (
+                    (
+                        "next",
+                        openapi.Schema(
+                            type=openapi.TYPE_STRING,
+                            format=openapi.FORMAT_URI,
+                            x_nullable=True,
+                        ),
+                    ),
+                    (
+                        "previous",
+                        openapi.Schema(
+                            type=openapi.TYPE_STRING,
+                            format=openapi.FORMAT_URI,
+                            x_nullable=True,
+                        ),
+                    ),
+                    ("results", response_schema),
+                )
+            ),
+            required=["results"],
+        )
 
 
 class EdgeIdentityPagination(CustomPagination):
@@ -50,6 +109,14 @@ class EdgeIdentityPagination(CustomPagination):
         return previous_url
 
     def get_paginated_response(self, data) -> Response:
+        """
+        Note: "If the size of the Query result set is larger than 1 MB, ScannedCount
+                and Count represent only a partial count of the total items"
+        ref: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Query.html#Query.Count
+
+        Tl;dr: there is no straightforward way to get the total number of items from dynamodb, that's why we don't
+                include count in the response
+        """
         return Response(
             OrderedDict(
                 [
@@ -59,3 +126,27 @@ class EdgeIdentityPagination(CustomPagination):
                 ]
             )
         )
+
+    def get_paginated_response_schema(self, schema):
+        return {
+            "type": "object",
+            "properties": {
+                "next": {
+                    "type": "string",
+                    "nullable": True,
+                    "format": "uri",
+                    "example": "http://api.example.org/accounts/?{page_query_param}=4".format(
+                        page_query_param=self.page_query_param
+                    ),
+                },
+                "previous": {
+                    "type": "string",
+                    "nullable": True,
+                    "format": "uri",
+                    "example": "http://api.example.org/accounts/?{page_query_param}=2".format(
+                        page_query_param=self.page_query_param
+                    ),
+                },
+                "results": schema,
+            },
+        }
