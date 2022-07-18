fb3d27969ae84f4166b5e23e120fae75273ad7b6
kyle-ssg <kyle@solidstategroup.com>
2022-07-18 11:57:13 +0100

    Create additional project permissions

--- frontend/web/components/modals/CreateProject.js
+++ frontend/web/components/modals/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import InfoMessage from "../InfoMessage";
+import PaymentModal from "./Payment";
 
 const CreateProject = class extends Component {
     static displayName = 'CreateProject'
@@ -36,29 +38,52 @@ const CreateProject = class extends Component {
                     const disableCreate = !canCreate && hasProject;
 
                     return (
-                        <form
-                            data-test="create-project-modal"
-                            id="create-project-modal" onSubmit={(e) => {
-                            e.preventDefault();
-                            !isSaving && name && createProject(name);
-                        }}
-                        >
-                            <InputGroup
-                                ref={e => this.input = e}
-                                data-test="projectName"
-                                inputProps={{ name: 'projectName', className: 'full-width' }}
-                                onChange={e => this.setState({ name: Utils.safeParseEventValue(e) })}
-                                isValid={name && name.length}
-                                type="text" title="Project Name*"
-                                placeholder="My Product Name"
-                            />
-                            {error && <Error error={error}/>}
-                            <div className="text-right">
-                                <Button data-test="create-project-btn" className="mt-3" id="create-project-btn" disabled={isSaving || !name}>
-                                    {isSaving ? 'Creating' : 'Create Project'}
-                                </Button>
-                            </div>
-                        </form>
+                        <div>
+
+                            {disableCreate && (
+                                <InfoMessage>
+                                    View and manage multiple projects in your organisation with the <a
+                                    href="#" onClick={() => {
+                                    openModal('Payment plans', <PaymentModal
+                                        viewOnly={false}
+                                    />, null, { large: true });
+                                }}
+                                >
+                                    Startup plan
+                                </a>
+                                </InfoMessage>
+                            )}
+                            <form
+                                style={{opacity:disableCreate?0.5:1}}
+                                data-test="create-project-modal"
+                                id="create-project-modal" onSubmit={(e) => {
+                                if(disableCreate) {
+                                    return
+                                }
+                                e.preventDefault();
+                                !isSaving && name && createProject(name);
+                            }}
+                            >
+
+                                <InputGroup
+                                    ref={e => this.input = e}
+                                    data-test="projectName"
+                                    disabled={disableCreate}
+                                    inputProps={{ name: 'projectName', className: 'full-width' }}
+                                    onChange={e => this.setState({ name: Utils.safeParseEventValue(e) })}
+                                    isValid={name && name.length}
+                                    type="text" title="Project Name*"
+                                    placeholder="My Product Name"
+                                />
+                                {error && <Error error={error}/>}
+                                <div className="text-right">
+                                    <Button data-test="create-project-btn" className="mt-3" id="create-project-btn" disabled={isSaving || !name}>
+                                        {isSaving ? 'Creating' : 'Create Project'}
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
+
                     )
                 }}
 
