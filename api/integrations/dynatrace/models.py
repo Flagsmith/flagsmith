57e5fb77e0ce5f6bbf32ff24e63abe4b85fea5fa
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-25 14:56:15 +0100

    Trigger environment document builds when updating environment integrations (#1299)
    * Refactor logic for writing environments to dynamo
    
    * Write environment to dynamodb when integration models are created / updated
    
    * Add test for delete

--- api/integrations/dynatrace/models.py
+++ api/integrations/dynatrace/models.py
@@ -3,12 +3,12 @@ import logging
 from django.db import models
 
 from environments.models import Environment
-from integrations.common.models import IntegrationsModel
+from integrations.common.models import EnvironmentIntegrationModel
 
 logger = logging.getLogger(__name__)
 
 
-class DynatraceConfiguration(IntegrationsModel):
+class DynatraceConfiguration(EnvironmentIntegrationModel):
     environment = models.OneToOneField(
         Environment, related_name="dynatrace_config", on_delete=models.CASCADE
     )
