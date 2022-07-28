349161ac1459ecddfbf6985e3af5b3e670a0b0c6
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-28 15:23:35 +0100

    Refactor

--- api/edge_api/identities/views.py
+++ api/edge_api/identities/views.py
@@ -4,18 +4,20 @@ import typing
 
 import marshmallow
 from boto3.dynamodb.conditions import Key
-from django.db.models import Q
 from django.utils.decorators import method_decorator
 from drf_yasg2.utils import swagger_auto_schema
 from flag_engine.api.schemas import APITraitSchema
-from flag_engine.features.models import FeatureStateModel
 from flag_engine.identities.builders import (
     build_identity_dict,
     build_identity_model,
 )
-from rest_framework import serializers, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import NotFound, ValidationError
+from rest_framework.exceptions import (
+    NotFound,
+    PermissionDenied,
+    ValidationError,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -24,6 +26,7 @@ from app.pagination import (
     EdgeIdentityPaginationInspector,
 )
 from edge_api.identities.serializers import (
+    EdgeIdentityAllFeatureStatesSerializer,
     EdgeIdentityFeatureStateSerializer,
     EdgeIdentityFsQueryparamSerializer,
     EdgeIdentitySerializer,
@@ -33,10 +36,10 @@ from environments.identities.models import Identity
 from environments.models import Environment
 from environments.permissions.constants import MANAGE_IDENTITIES
 from environments.permissions.permissions import NestedEnvironmentPermissions
-from features.models import FeatureState
 from features.permissions import IdentityFeatureStatePermissions
 from projects.exceptions import DynamoNotEnabledError
 
+from .edge_identity_service import get_all_feature_states_for_edge_identity
 from .exceptions import TraitPersistenceError
 
 trait_schema = APITraitSchema()
@@ -177,6 +180,13 @@ class EdgeIdentityFeatureStateViewSet(viewsets.ModelViewSet):
         identity_document = Identity.dynamo_wrapper.get_item_from_uuid_or_404(
             self.kwargs["edge_identity_identity_uuid"]
         )
+
+        if (
+            identity_document["environment_api_key"]
+            != self.kwargs["environment_api_key"]
+        ):
+            raise PermissionDenied("Identity does not belong to this environment.")
+
         self.identity = build_identity_model(identity_document)
 
     def get_object(self):
@@ -214,41 +224,18 @@ class EdgeIdentityFeatureStateViewSet(viewsets.ModelViewSet):
         self.identity.identity_features.remove(instance)
         Identity.dynamo_wrapper.put_item(build_identity_dict(self.identity))
 
+    @swagger_auto_schema(
+        responses={200: EdgeIdentityAllFeatureStatesSerializer(many=True)}
+    )
     @action(detail=False, methods=["GET"])
     def all(self, request, *args, **kwargs):
-        django_environment = Environment.objects.get(
-            api_key=kwargs["environment_api_key"]
-        )
-
-        segment_ids = Identity.dynamo_wrapper.get_segment_ids(self.identity)
-
-        q = Q(identity__isnull=True) & (
-            Q(feature_segment__segment__id__in=segment_ids)
-            | Q(feature_segment__isnull=True)
-        )
-        environment_and_segment_feature_states = (
-            django_environment.feature_states.select_related(
-                "feature", "feature_segment", "feature_segment__segment"
-            ).filter(q)
-        )
-
-        feature_states = {}
-        for feature_state in environment_and_segment_feature_states:
-            if (
-                feature_state.feature.name not in feature_states
-                or feature_state.feature_segment_id is not None
-            ):
-                feature_states[feature_state.feature.name] = feature_state
-
-        identity_feature_states = self.identity.identity_features
-        identity_feature_names = set()
-        for identity_feature_state in identity_feature_states:
-            feature_name = identity_feature_state.feature.name
-            feature_states[feature_name] = identity_feature_state
-            identity_feature_names.add(feature_name)
+        (
+            feature_states,
+            identity_feature_names,
+        ) = get_all_feature_states_for_edge_identity(self.identity)
 
         serializer = EdgeIdentityAllFeatureStatesSerializer(
-            instance=feature_states.values(),
+            instance=feature_states,
             many=True,
             context={
                 "request": request,
@@ -258,68 +245,3 @@ class EdgeIdentityFeatureStateViewSet(viewsets.ModelViewSet):
         )
 
         return Response(serializer.data)
-
-
-class EdgeIdentityAllFeatureStatesFeatureSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    type = serializers.CharField()
-
-
-class EdgeIdentityAllFeatureStatesSegmentSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-
-
-class EdgeIdentityAllFeatureStatesMVFeatureOptionSerializer(serializers.Serializer):
-    value = serializers.SerializerMethodField()
-
-    def get_value(self, instance) -> typing.Union[int, bool, str]:
-        return instance.value
-
-
-class EdgeIdentityAllFeatureStatesMVFeatureStateValueSerializer(serializers.Serializer):
-    multivariate_feature_option = (
-        EdgeIdentityAllFeatureStatesMVFeatureOptionSerializer()
-    )
-    percentage_allocation = serializers.FloatField()
-
-
-class EdgeIdentityAllFeatureStatesSerializer(serializers.Serializer):
-    feature = EdgeIdentityAllFeatureStatesFeatureSerializer()
-    enabled = serializers.BooleanField()
-    feature_state_value = serializers.SerializerMethodField()
-    overridden_by = serializers.SerializerMethodField()
-    segment = serializers.SerializerMethodField()
-    multivariate_feature_state_values = (
-        EdgeIdentityAllFeatureStatesMVFeatureStateValueSerializer(many=True)
-    )
-
-    def get_feature_state_value(
-        self, instance: typing.Union[FeatureState, FeatureStateModel]
-    ) -> typing.Union[str, int, bool]:
-        identity = self.context["identity"]
-        identity_id = getattr(identity, "id", None) or getattr(
-            identity, "django_id", identity.identity_uuid
-        )
-
-        if type(instance) == FeatureState:
-            return instance.get_feature_state_value_by_id(identity_id)
-
-        return instance.get_value(identity_id)
-
-    def get_overridden_by(self, instance) -> typing.Optional[str]:
-        if getattr(instance, "feature_segment_id", None) is not None:
-            return "SEGMENT"
-        elif instance.feature.name in self.context.get("identity_feature_names", []):
-            return "IDENTITY"
-        return None
-
-    def get_segment(
-        self, instance
-    ) -> typing.Optional[EdgeIdentityAllFeatureStatesSegmentSerializer]:
-        if getattr(instance, "feature_segment_id", None) is not None:
-            return EdgeIdentityAllFeatureStatesSegmentSerializer(
-                instance=instance.feature_segment.segment
-            ).data
-        return None
