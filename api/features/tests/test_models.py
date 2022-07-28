28c752493179cda47a607f0e7f8b61dc1e2ade30
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-28 14:25:40 +0100

    Fix tests

--- api/features/tests/test_models.py
+++ api/features/tests/test_models.py
@@ -572,7 +572,7 @@ def test_get_multivariate_value_returns_correct_value_when_we_pass_identity(
 
     # When
     multivariate_value = feature_state.get_multivariate_feature_state_value(
-        identity=identity.id
+        identity_id=identity.id
     )
 
     # Then
@@ -607,4 +607,4 @@ def test_get_feature_state_value_for_multivariate_features(
     # the correct value is returned
     assert feature_state_value == value
     # and the correct call is made to get the multivariate feature state value
-    mock_get_mv_feature_state_value.assert_called_once_with(identity)
+    mock_get_mv_feature_state_value.assert_called_once_with(identity.id)
