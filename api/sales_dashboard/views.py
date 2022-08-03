9581aa01f889b8a6c06db31c56496e7fd1634b9b
Matthew Elwell <matthewe@solidstategroup.com>
2022-08-03 16:26:18 +0100

    Refactor / display improvements

--- api/sales_dashboard/views.py
+++ api/sales_dashboard/views.py
@@ -33,11 +33,6 @@ from .forms import EmailUsageForm, MaxAPICallsForm, MaxSeatsForm
 
 OBJECTS_PER_PAGE = 50
 
-DEFAULT_API_CALLS = 50000
-DEFAULT_SEATS = 1
-DEFAULT_PROJECTS = 1
-SUBSCRIPTION_DEFAULT_LIMITS = (DEFAULT_API_CALLS, DEFAULT_SEATS, DEFAULT_PROJECTS)
-
 
 class OrganisationList(ListView):
     model = Organisation
@@ -115,15 +110,15 @@ def organisation_info(request, organisation_id):
     )
     template = loader.get_template("sales_dashboard/organisation.html")
 
-    max_api_calls, max_seats, max_projects = SUBSCRIPTION_DEFAULT_LIMITS
+    max_api_calls, max_seats, max_projects = Subscription.SUBSCRIPTION_DEFAULT_LIMITS
     if getattr(organisation, "subscription", None) is not None:
         if organisation.subscription.payment_method == Subscription.CHARGEBEE:
             subscription_metadata = get_subscription_metadata(
                 organisation.subscription.subscription_id
             )
-            max_api_calls = getattr(subscription_metadata, "api_calls", 50000)
-            max_seats = getattr(subscription_metadata, "seats", 0)
-            max_projects = getattr(subscription_metadata, "projects", 1)
+            max_api_calls = getattr(subscription_metadata, "api_calls", max_api_calls)
+            max_seats = getattr(subscription_metadata, "seats", max_seats)
+            max_projects = getattr(subscription_metadata, "projects", max_projects)
         else:
             max_api_calls = organisation.subscription.max_api_calls
             max_seats = organisation.subscription.max_seats
