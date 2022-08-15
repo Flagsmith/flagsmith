4106704932c7c8be070f7c01efdd3734ddf4f9f4
Jethro Muller <JethroMuller@gmail.com>
2022-08-15 09:35:50 +0200

    Fix NodeJS client docs - update import & add missing assignment (#1382)
    * Fix NodeJS client docs - add missing assignment

--- frontend/common/code-help/init/init-node.js
+++ frontend/common/code-help/init/init-node.js
@@ -1,4 +1,4 @@
-module.exports = (envId, { LIB_NAME, FEATURE_NAME, FEATURE_NAME_ALT, NPM_NODE_CLIENT }, customFeature) => `import ${LIB_NAME} from "${NPM_NODE_CLIENT}"; // Add this line if you're using ${LIB_NAME} via npm
+module.exports = (envId, { LIB_NAME, FEATURE_NAME, FEATURE_NAME_ALT, NPM_NODE_CLIENT }, customFeature) => `import Flagsmith from "${NPM_NODE_CLIENT}"; // Add this line if you're using ${LIB_NAME} via npm
 
 const ${LIB_NAME} = new Flagsmith({
     environmentKey: '${envId}'
@@ -7,7 +7,7 @@ const ${LIB_NAME} = new Flagsmith({
 const flags = await flagsmith.getEnvironmentFlags();
 
 // Check for a feature
-var isEnabled flags.isFeatureEnabled("${customFeature || FEATURE_NAME}")
+var isEnabled = flags.isFeatureEnabled("${customFeature || FEATURE_NAME}")
 
 // Or, use the value of a feature
 var featureValue = flags.getFeatureValue('${customFeature || FEATURE_NAME_ALT}');
