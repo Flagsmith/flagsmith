66456b8cfe40392f5ee32c4043335f5ba05c2218
Matthew Elwell <matthew.elwell@flagsmith.com>
2022-09-16 11:38:39 +0100

    Add second test to verify valid data still works

--- api/tests/unit/app_analytics/test_unit_app_analytics_views.py
+++ api/tests/unit/app_analytics/test_unit_app_analytics_views.py
@@ -21,3 +21,24 @@ def test_sdk_analytics_does_not_allow_bad_data(mocker, settings, environment):
     # Then
     assert response.status_code == status.HTTP_200_OK
     mocked_track_feature_eval.assert_not_called()
+
+
+def test_sdk_analytics_allows_valid_data(mocker, settings, environment, feature):
+    # Given
+    settings.INFLUXDB_TOKEN = "some-token"
+
+    data = {feature.name: 12}
+    request = mocker.MagicMock(data=data, environment=environment)
+
+    view = SDKAnalyticsFlags(request=request)
+
+    mocked_track_feature_eval = mocker.patch(
+        "app_analytics.views.track_feature_evaluation_influxdb"
+    )
+
+    # When
+    response = view.post(request)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    mocked_track_feature_eval.assert_called_once_with(environment.id, data)
