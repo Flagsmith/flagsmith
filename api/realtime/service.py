bd4c3fd3bb176b970558a22bd7ce8f789b5b2015
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-09-21 17:29:09 +0530

    Add test cases and fixes

--- api/realtime/service.py
+++ api/realtime/service.py
@@ -7,20 +7,22 @@ from django.conf import settings
 def send_environment_update_messages(environment_keys: List[str]):
     if not settings.SSE_SERVER_BASE_URL:
         return
-    header = {"Authorization": f"Token {settings.SSE_SERVER_TOKEN}"}
 
     for environment_key in environment_keys:
-        url = f"https://{settings.SSE_SERVER_BASE_URL}/sse/environments/{environment_key}/queue-change"
-        response = requests.post(url, headers=header)
+        url = f"{settings.SSE_SERVER_BASE_URL}/sse/environments/{environment_key}/queue-change"
+        response = requests.post(url, headers=get_auth_header())
         response.raise_for_status()
 
 
 def send_identity_update_message(environment_key: str, identifier: str):
     if not settings.SSE_SERVER_BASE_URL:
         return
-    url = f"https://{settings.SSE_SERVER_BASE_URL}/sse/environments/{environment_key}/identities/queue-change"
+    url = f"{settings.SSE_SERVER_BASE_URL}/sse/environments/{environment_key}/identities/queue-change"
 
     payload = {"identifier": identifier}
-    header = {"Authorization": f"Token {settings.SSE_SERVER_TOKEN}"}
-    response = requests.post(url, headers=header, payload=payload)
+    response = requests.post(url, headers=get_auth_header(), json=payload)
     response.raise_for_status()
+
+
+def get_auth_header():
+    return {"Authorization": f"Token {settings.SSE_AUTHENTICATION_TOKEN}"}
--- api/tests/unit/realtime/test_service.py
+++ api/tests/unit/realtime/test_service.py
@@ -28,3 +28,51 @@ def test_identity_update_message_returns_without_request_if_not_configured(
 
     # Then
     mocked_requests.post.assert_not_called()
+
+
+def test_send_environment_update_messages_make_correct_request(mocker, settings):
+    # Given
+    base_url = "http://localhost:8000"
+    token = "token"
+    environment_keys = ["test_environment_1", "test_environment_2"]
+
+    settings.SSE_SERVER_BASE_URL = base_url
+    settings.SSE_AUTHENTICATION_TOKEN = token
+    mocked_requests = mocker.patch("realtime.service.requests")
+
+    # When
+    send_environment_update_messages(environment_keys)
+
+    # Then
+    mocked_requests.post.has_calls(
+        mocker.call(
+            f"{base_url}/sse/environments/{environment_keys[0]}/queue-change",
+            headers={"Authorization": f"Token {token}"},
+        ),
+        mocker.call(
+            f"{base_url}/sse/environments/{environment_keys[1]}/queue-change",
+            headers={"Authorization": f"Token {token}"},
+        ),
+    )
+
+
+def test_send_identity_update_message_make_correct_request(mocker, settings):
+    # Given
+    base_url = "http://localhost:8000"
+    token = "token"
+    identifier = "test_identity"
+    environment_key = "test_environment"
+
+    settings.SSE_SERVER_BASE_URL = base_url
+    settings.SSE_AUTHENTICATION_TOKEN = token
+    mocked_requests = mocker.patch("realtime.service.requests")
+
+    # When
+    send_identity_update_message(environment_key, identifier)
+
+    # Then
+    mocked_requests.post.assert_called_once_with(
+        f"{base_url}/sse/environments/{environment_key}/identities/queue-change",
+        headers={"Authorization": f"Token {token}"},
+        json={"identifier": identifier},
+    )
