ed8a02b0f07d78f64fe93976f9883636d940ea3b
Gagan <gagandeeptrivedi47@gmail.com>
2022-10-04 08:21:34 +0530

    Fix/1434/use cb for sales dashboard overview  (#1494)
    *  fetch data for overview from cb
    
    * refac: move get_subscription_metadata to subscription service
    
    * create a custom tag to pass subscription metadata
    
    * update overview html to use cb data
    
    * Add test case for subscription service
    
    * rename node
    
    * rename subscription service

---
+++ api/tests/unit/organisations/subscriptions/test_unit_subscription_service.py
@@ -0,0 +1,59 @@
+from organisations.subscriptions.constants import (
+    MAX_API_CALLS_IN_FREE_PLAN,
+    MAX_PROJECTS_IN_FREE_PLAN,
+    MAX_SEATS_IN_FREE_PLAN,
+)
+from organisations.subscriptions.dataclasses import BaseSubscriptionMetadata
+from organisations.subscriptions.subscription_service import (
+    get_subscription_metadata,
+)
+
+
+def test_get_subscription_metadata_returns_default_values_if_org_does_not_have_subscription(
+    organisation,
+):
+    # When
+    subscription_metadata = get_subscription_metadata(organisation)
+
+    # Then
+    assert subscription_metadata.api_calls == MAX_API_CALLS_IN_FREE_PLAN
+    assert subscription_metadata.seats == MAX_SEATS_IN_FREE_PLAN
+    assert subscription_metadata.projects == MAX_PROJECTS_IN_FREE_PLAN
+
+
+def test_get_subscription_metadata_uses_chargebee_data_if_chargebee_subscription_exists(
+    organisation, chargebee_subscription, mocker
+):
+    # Given
+    seats = 10
+    projects = 20
+    api_calls = 30
+    mocked_get_chargebee_subscription_metadata = mocker.patch(
+        "organisations.subscriptions.subscription_service.get_chargebee_subscription_metadata",
+        autospec=True,
+        return_value=BaseSubscriptionMetadata(
+            seats=seats, projects=projects, api_calls=api_calls
+        ),
+    )
+    # When
+    subscription_metadata = get_subscription_metadata(organisation)
+
+    # Then
+    assert subscription_metadata.api_calls == api_calls
+    assert subscription_metadata.seats == seats
+    assert subscription_metadata.projects == projects
+    mocked_get_chargebee_subscription_metadata.assert_called_once_with(
+        chargebee_subscription.subscription_id
+    )
+
+
+def test_get_subscription_metadata_uses_metadata_from_subscription_for_non_chargebee_subscription(
+    organisation, xero_subscription
+):
+    # When
+    subscription_metadata = get_subscription_metadata(organisation)
+
+    # Then
+    assert subscription_metadata.api_calls == xero_subscription.max_api_calls
+    assert subscription_metadata.seats == xero_subscription.max_seats
+    assert subscription_metadata.projects is None
