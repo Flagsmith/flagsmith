9dfcd978ff24ca427320c1a2122795ef4f1df38f
Matthew Elwell <matthew.elwell@flagsmith.com>
2022-10-07 12:43:08 +0100

    Reduce complexity of monitoring endpoint

--- api/task_processor/views.py
+++ api/task_processor/views.py
@@ -1,11 +1,9 @@
-from django.db.models import Count, Q
-from django.utils import timezone
 from drf_yasg2.utils import swagger_auto_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
-from task_processor.models import Task, TaskResult
+from task_processor.models import Task
 from task_processor.serializers import MonitoringSerializer
 
 
@@ -13,16 +11,7 @@ from task_processor.serializers import MonitoringSerializer
 @permission_classes([IsAuthenticated, IsAdminUser])
 @swagger_auto_schema(responses={200: MonitoringSerializer()})
 def monitoring(request, **kwargs):
-    qs = Task.objects.annotate(num_task_runs=Count("task_runs")).aggregate(
-        waiting=Count(
-            "id", filter=Q(num_task_runs=0, scheduled_for__lte=timezone.now())
-        ),
-        scheduled=Count("id", filter=Q(scheduled_for__gt=timezone.now())),
-        completed=Count(
-            "id",
-            filter=Q(task_runs__result=TaskResult.SUCCESS),
-        ),
-        failed=Count("id", filter=Q(num_failures__gte=3)),
+    waiting_tasks = Task.objects.filter(num_failures__lt=3, completed=False).count()
+    return Response(
+        data={"waiting": waiting_tasks}, headers={"Content-Type": "application/json"}
     )
-    serializer = MonitoringSerializer(instance=qs)
-    return Response(data=serializer.data, headers={"Content-Type": "application/json"})
