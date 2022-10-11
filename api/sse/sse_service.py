bee7b95dc9b86765a526a08265ca12230e7402b4
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-10-11 15:16:48 +0530

    update service: use decorator

--- api/sse/sse_service.py
+++ api/sse/sse_service.py
@@ -1,77 +1,54 @@
-# from functools import wraps
+from functools import wraps
 from typing import List
 
 from django.conf import settings
 
 from . import tasks
 
-# def call_function_if(condition: callable[[Any], bool]):
-#     def decorator(f):
-#         @wraps(f)
-#         def wrapper(*args, **kwargs):
-#             if condition(*args, **kwargs):
-#                 return f(*args, **kwargs)
-#             return f(*args, **kwargs)
 
-#         return wrapper
+def call_service_method_if(get_project_from_first_arg=lambda obj: obj.project):
+    def decorator(service_func):
+        @wraps(service_func)
+        def wrapper(*args, **kwargs):
+            project = get_project_from_first_arg(args[0])
+            if all(
+                [
+                    settings.SSE_SERVER_BASE_URL,
+                    settings.SSE_AUTHENTICATION_TOKEN,
+                    project.enable_realtime_updates,
+                ]
+            ):
+                return service_func(*args, **kwargs)
+            return None
 
-#     return decorator
+        return wrapper
 
-
-# def sse_enabled_required(f):
-#     @wraps(f)
-#     def wrapper(*args, **kwargs):
-#         if all([settings.SSE_SERVER_BASE_URL, settings.SSE_AUTHENTICATION_TOKEN]):
-#             return
-#         return f(*args, **kwargs)
-
-#     return wrapper
+    return decorator
 
 
-# @sse_enabled_required
+@call_service_method_if(get_project_from_first_arg=lambda obj: obj)
 def send_environment_update_message_using_project(project):
-    if not should_send_message_to_sse(project):
-        return
-
     environment_keys = list(
         project.environments.all().values_list("api_key", flat=True)
     )
     tasks.send_environment_update_messages.delay(args=(environment_keys,))
 
 
-# @sse_enabled_required
+@call_service_method_if()
 def send_environment_update_message_using_environment(environment):
-    if not should_send_message_to_sse(environment.project):
-        return
-
     tasks.send_environment_update_message.delay(args=(environment.api_key,))
 
 
-# @sse_enabled_required
+@call_service_method_if()
 def send_identity_update_message(environment, identifier: str):
-    if not should_send_message_to_sse(environment.project):
-        return
     tasks.send_identity_update_message.delay(args=(environment.api_key, identifier))
 
 
-# @sse_enabled_required
+@call_service_method_if()
 def send_identity_update_messages(environment, identifiers: List[str]):
-    if not should_send_message_to_sse(environment.project):
-        return
-
     tasks.send_identity_update_messages.delay(
         args=(
             environment.api_key,
             identifiers,
         )
-    )
-
-
-def should_send_message_to_sse(project):
-    return all(
-        [
-            settings.SSE_SERVER_BASE_URL,
-            settings.SSE_AUTHENTICATION_TOKEN,
-            project.enable_realtime_updates,
-        ]
     )
