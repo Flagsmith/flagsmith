311e0b8e8de97baa04a632909ace39d89077e5b2
Matthew Elwell <matthew.elwell@flagsmith.com>
2022-10-11 15:08:36 +0100

    update traces sampler

--- api/integrations/sentry/samplers.py
+++ api/integrations/sentry/samplers.py
@@ -1,11 +1,24 @@
+import re
+
 from django.conf import settings
 
+NON_FUNCTIONAL_ENDPOINTS = {"/health", "/"}
+sdk_endpoint_matcher = re.compile(
+    r"/api/v1/(traits|identities|flags|environment-document)/*.*"
+)
 
-def block_non_functional_endpoints_sampler(ctx):
-    non_functional_endpoints = ["/health", "/"]
 
-    sample_rate = settings.SENTRY_TRACE_SAMPLE_RATE
+def traces_sampler(ctx):
     wsgi_env = ctx.get("wsgi_environ")
-    if wsgi_env and wsgi_env.get("PATH_INFO") in non_functional_endpoints:
-        sample_rate = 0
-    return sample_rate
+
+    if wsgi_env:
+        path_info = wsgi_env.get("PATH_INFO", "")
+
+        if path_info in NON_FUNCTIONAL_ENDPOINTS:
+            return 0
+        elif not sdk_endpoint_matcher.match(path_info):
+            # request must be a call to the 'dashboard' API. Note: we check prefixes, so we can ignore any redirected
+            # requests for trailing slash appending.
+            return 1
+
+    return settings.SENTRY_TRACE_SAMPLE_RATE
