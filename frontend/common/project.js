b529624088eb1a11649641ad2a1927cd1d551885
Kyle Johnson <kyle@solidstategroup.com>
2022-11-04 13:06:20 +0000

    Chores/zendesk (#1603)
    * Add zendesk snippet
    
    * move snippet
    
    * zendesk
    
    * revert project js
    
    * Tidy
    
    * zendesk
    
    * zendesk
    
    * ZENDESK_WIDGET_ID

--- frontend/common/project.js
+++ frontend/common/project.js
@@ -11,5 +11,5 @@ module.exports = global.Project = {
     },
     chargebee: {
         site: 'flagsmith-test',
-    },
+    }
 };
--- frontend/web/main.js
+++ frontend/web/main.js
@@ -103,3 +103,12 @@ if (!E2E && Project.crispChat) {
         d.getElementsByTagName('head')[0].appendChild(s);
     }());
 }
+
+if (!E2E && Project.zendesk) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.id = 'ze-snippet';
+    script.async = true;
+    script.src = 'https://static.zdassets.com/ekr/snippet.js?key='+Project.zendesk;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
