ec5e2e315086f051c5f656e6c6d94f5bb58c8298
Kyle Johnson <kyle@solidstategroup.com>
2022-11-04 13:06:20 +0000

    Chores/zendesk (#1603)
    * Add zendesk snippet
    
    * move snippet
    
    * zendesk
    
    * revert project js
    
    * Tidy
    
    * zendesk
    
    * zendesk
    
    * ZENDESK_WIDGET_ID

--- frontend/common/utils/utils.js
+++ frontend/common/utils/utils.js
@@ -56,6 +56,14 @@ module.exports = Object.assign({}, require('./base/_utils'), {
         }
         return false;
     },
+    openChat() {
+        if (typeof $crisp !== 'undefined') {
+            $crisp.push(['do', 'chat:open'])
+        }
+        if (window.zE) {
+            zE('messenger', 'open');
+        }
+    },
     isMigrating() {
         if (Utils.getFlagsmithHasFeature('edge_migrator') && ProjectStore.model && (ProjectStore.model.migration_status === 'MIGRATION_IN_PROGRESS' || ProjectStore.model.migration_status === 'MIGRATION_SCHEDULED')) {
             return true;
