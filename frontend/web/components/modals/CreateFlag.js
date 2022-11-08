d11d2a19a4113569e4fca3c2f901e8774a1d7449
Kyle Johnson <kyle@solidstategroup.com>
2022-11-08 10:10:24 +0000

    Chores/silently fail influx endpoint (#1623)
    * Silently fail influx enpoint

--- frontend/web/components/modals/CreateFlag.js
+++ frontend/web/components/modals/CreateFlag.js
@@ -920,6 +920,7 @@ const CreateFlag = class extends Component {
                                                                             <Panel
                                                                               title={!!influxData && <h6 className="mb-0">Flag events for last {influxData.timespan} days</h6>}
                                                                             >
+                                                                                {!influxData && <div className="text-center"><Loader/></div> }
                                                                                 {this.drawChart(influxData)}
                                                                             </Panel>
                                                                         </FormGroup>
