e3ca7941394e5764138e20868db7a0d72903f11c
kyle-ssg <kyle@solidstategroup.com>
2022-11-08 09:50:11 +0000

    handle null data

--- frontend/web/components/modals/CreateFlag.js
+++ frontend/web/components/modals/CreateFlag.js
@@ -921,7 +921,6 @@ const CreateFlag = class extends Component {
                                                                               title={!!influxData && <h6 className="mb-0">Flag events for last {influxData.timespan} days</h6>}
                                                                             >
                                                                                 {!influxData && <div className="text-center"><Loader/></div> }
-
                                                                                 {this.drawChart(influxData)}
                                                                             </Panel>
                                                                         </FormGroup>
