62ab815c27b5b602d205cedad3afc2b4d34700e1
Matthew Elwell <matthew.elwell@flagsmith.com>
2022-11-08 11:39:06 +0000

    Sales dashboard improvements (#1626)
    * Remove all references to chargebee from sales dashboard list
    
    * Fix sorting
    
    * Add default order by
    
    * Remove unnecessary context data
    
    * Reduce number of influx calls from sales dashboard organisation page

--- api/sales_dashboard/templatetags/sales_dashboard_extras.py
+++ api/sales_dashboard/templatetags/sales_dashboard_extras.py
@@ -1,37 +1,11 @@
 import typing
 
-from django import template
 from django.template.defaulttags import register
-
-from organisations.subscriptions.subscription_service import (
-    get_subscription_metadata,
-)
 
 
 @register.filter
 def get_item(dictionary: dict, key: typing.Any) -> typing.Any:
     return isinstance(dictionary, dict) and dictionary.get(key)
-
-
-class LoadSubcriptionMetadataNode(template.Node):
-    def __init__(self, org):
-        self.org = template.Variable(org)
-
-    def render(self, context):
-        org = self.org.resolve(context)
-        context["subscription_metadata"] = get_subscription_metadata(org)
-        return ""
-
-
-@register.tag
-def load_subcription_metadata(parser, token) -> typing.Any:
-    try:
-        _, org = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError(
-            "%r tag requires a single argument" % token.contents.split()[0]
-        )
-    return LoadSubcriptionMetadataNode(org)
 
 
 @register.simple_tag
