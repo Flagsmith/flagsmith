73790371515d6c76b61b897996938a4380e6bdb2
Gagan <gagandeeptrivedi47@gmail.com>
2022-11-25 09:16:30 +0530

    feat(token/rotation): add endpoint to delete token (#1682)

--- api/custom_auth/views.py
+++ api/custom_auth/views.py
@@ -1,4 +1,10 @@
+from django.contrib.auth import user_logged_out
 from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.throttling import ScopedRateThrottle
 from trench.views.authtoken import (
     AuthTokenLoginOrRequestMFACode,
@@ -35,3 +41,13 @@ class ThrottledUserViewSet(UserViewSet):
         if self.action == "create":
             throttles = [ScopedRateThrottle()]
         return throttles
+
+
+@api_view(["DELETE"])
+@permission_classes([IsAuthenticated])
+def delete_token(request):
+    Token.objects.filter(user=request.user).delete()
+    user_logged_out.send(
+        sender=request.user.__class__, request=request, user=request.user
+    )
+    return Response(status=status.HTTP_204_NO_CONTENT)
