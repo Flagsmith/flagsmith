543cdc6debd650cd6b44164a9c22dfa227f12f82
kyle-ssg <kyle@solidstategroup.com>
2022-11-28 21:47:41 +0000

    Use functional components for button

--- frontend/web/components/base/forms/Button.tsx
+++ frontend/web/components/base/forms/Button.tsx
@@ -1,68 +1,48 @@
-import React, {HTMLAttributeAnchorTarget, PureComponent, ReactNode} from 'react';
-export type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+import React, {FC, HTMLAttributeAnchorTarget, PureComponent, ReactNode} from 'react';
+export type ButtonType = React.ButtonHTMLAttributes<HTMLButtonElement>
+export type ButtonLinkType = ButtonType & {
     href?: string
     target?: HTMLAttributeAnchorTarget
 }
-const Button = class extends PureComponent<ButtonType> {
-    static displayName = 'Button'
 
-    button: HTMLButtonElement|null = null
-
-    onMouseUp = () => {
-        this.button?.blur();
-    }
-
-    render() {
-        return (
-            <button
-              ref={(ref)=>this.button = ref} {...this.props} onMouseUp={this.onMouseUp}
-              className={`btn ${this.props.className || ''}`}
-            >
-                {this.props.children}
-            </button>
-        );
-    }
-};
-
-
-export default class extends Button {
-    static displayName = 'Button';
-
-    render() {
-        return <Button {...this.props} className={`${this.props.className || ''}`}/>;
-    }
+const Button: FC<ButtonType> = (props) => {
+    return (
+        <button
+             {...props}
+            className={`btn ${props.className || ''}`}
+        >
+            {props.children}
+        </button>
+    )
 }
 
-export const ButtonOutline = class extends Button {
-    static displayName = 'ButtonOutline';
 
-    render() {
-        return <Button {...this.props} className={`btn--outline ${this.props.className || ''}`}/>;
-    }
-};
+export default Button;
 
-export const ButtonLink = class extends Button {
-    static displayName = 'ButtonLink';
 
-    render() {
-        const { ...rest } = this.props;
-        return (
-            <Button {...rest} className={`btn--link ${this.props.className || ''}`}>
-                {this.props.href ? (
-                    <a className="btn--link" target={this.props.target} href={this.props.href}>{this.props.children}</a>
-                ) : (
 
-                    <span className="btn--link">{this.props.children}</span>
-                )}
-            </Button>
-        );
-    }
-};
+const ButtonOutline: FC<ButtonType> = (props) => {
+    return (
+        <Button {...props} className={`btn--outline ${props.className || ''}`}/>
+    )
+}
 
-export const ButtonProject = class extends Button {
-    static displayName = 'ButtonProject';
+export const ButtonProject: FC<ButtonType> = (props) => {
+    return (
+        <Button {...props} className={`btn--project ${props.className || ''}`}/>
+    )
+}
 
-    render() {
-        return <Button {...this.props} className={`btn--project ${this.props.className || ''}`}/>;
-    }
-};
+export const ButtonLink: FC<ButtonLinkType> = ({href, className, target, children,...rest}) => {
+    return (
+        <Button {...rest} className={`btn--link ${className || ''}`}>
+            {href ? (
+                <a className="btn--link" target={target} href={href}>{children}</a>
+            ) : (
+
+                <span className="btn--link">{children}</span>
+            )}
+        </Button>
+    )
+}
+
