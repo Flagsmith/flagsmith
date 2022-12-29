515b1e20c175f5a706d93b77249440fe94e7aea4
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-12-29 14:26:59 +0530

    Add delete model metadata field

--- api/metadata/base_views.py
+++ api/metadata/base_views.py
@@ -1,5 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
+from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -25,8 +27,8 @@ class BaseMetadataView(GenericViewSet):
         serializer = MetaDataModelFieldSerializer(metadata_fields, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=["POST", "PUT"], url_path="metadata-fields")
-    def metadata_fields(self, request):
+    @action(detail=False, methods=["POST", "PUT"], url_path="metadata-field")
+    def create_or_update_metadata_field(self, request):
         serializer = MetaDataModelFieldSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
@@ -36,3 +38,15 @@ class BaseMetadataView(GenericViewSet):
         serializer.save(content_type=content_type)
 
         return Response(serializer.data)
+
+    @action(
+        detail=False,
+        methods=["DELETE"],
+        url_path=r"metadata-field/(?P<metadata_pk>\d+)",
+    )
+    def delete_metadata_field(self, request, metadata_pk):
+        model_metadata_field = get_object_or_404(MetadataModelField, pk=metadata_pk)
+
+        model_metadata_field.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
