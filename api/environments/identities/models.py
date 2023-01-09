41441e451be27a61df79896580de5b56d6a62c57
Gagan <gagandeeptrivedi47@gmail.com>
2023-01-09 08:22:05 +0530

    Add hide disabled flags to environments (#1824)
    Add hide disabled flags to environments
    
    * improve test to include enabled flags

--- api/environments/identities/models.py
+++ api/environments/identities/models.py
@@ -108,9 +108,8 @@ class Identity(models.Model):
                 if flag > current_flag:
                     identity_flags[flag.feature_id] = flag
 
-        if self.environment.project.hide_disabled_flags:
-            # filter out any flags that are disabled if configured on the project
-            # Note: done here instead of the DB because of CH1245
+        if self.environment.get_hide_disabled_flags() is True:
+            # filter out any flags that are disabled
             return [value for value in identity_flags.values() if value.enabled]
 
         return list(identity_flags.values())
