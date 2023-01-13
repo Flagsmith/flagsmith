550d7f98b7b82435ae1db7895f1a6f7f75cd7bdb
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-01-13 16:18:29 +0530

    update datamodel

--- api/app_analytics/models.py
+++ api/app_analytics/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from environments.models import Environment
+from features.models import Feature
 from organisations.models import Organisation
 from projects.models import Project
 
@@ -12,10 +13,16 @@ class Resource(models.IntegerChoices):
     ENVIRONMENT_DOCUMENT = 4
 
 
-class Analytics(models.Model):
+class APIUsage(models.Model):
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     host = models.CharField(max_length=255)
     resource = models.IntegerField(choices=Resource.choices)
+
+
+class FeatureEvaluation(models.Model):
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    evaluation_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
