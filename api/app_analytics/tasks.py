f75e410a82bd4ffcf65d6d6f4e0833735aa20b22
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-01-16 10:52:39 +0530

    Add middleware for storing analytics locally

--- api/app_analytics/tasks.py
+++ api/app_analytics/tasks.py
@@ -1,9 +1,7 @@
-from environment.models import Environment
-
+from environments.models import Environment
 from task_processor.decorators import register_task_handler
 
 from .models import APIUsage, FeatureEvaluation, Resource
-from .tracker import TRACKED_RESOURCE_ACTIONS
 
 
 @register_task_handler()
@@ -15,7 +13,7 @@ def track_feature_evaluation(environment_id, feature_evaluations):
 
 # TODO: improve this
 def get_resource_enum(resource):
-    {
+    return {
         "flags": Resource.FLAGS,
         "identities": Resource.IDENTITIES,
         "traits": Resource.TRAITS,
@@ -25,12 +23,11 @@ def get_resource_enum(resource):
 
 @register_task_handler()
 def track_request(resource: str, host: str, environment_key: str):
-    if resource and resource in TRACKED_RESOURCE_ACTIONS:
-        environment = Environment.get_from_cache(environment_key)
-        if environment is None:
-            return
-        APIUsage.objects.create(
-            enviroment=environment,
-            resource=get_resource_enum(resource),
-            host=host,
-        )
+    environment = Environment.get_from_cache(environment_key)
+    if environment is None:
+        return
+    APIUsage.objects.create(
+        environment=environment,
+        resource=get_resource_enum(resource),
+        host=host,
+    )
