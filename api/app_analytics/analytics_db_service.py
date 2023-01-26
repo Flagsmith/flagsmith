c1d5d05b968322bb9630b3fb6e7ef2a3c59db227
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-01-26 14:34:56 +0530

    fix service

--- api/app_analytics/analytics_db_service.py
+++ api/app_analytics/analytics_db_service.py
@@ -8,8 +8,12 @@ from app_analytics.influxdb_wrapper import (
 from django.conf import settings
 from django.db.models import Sum
 
-from .models import APIUsageByDay, Resource
+from environments.models import Environment
+
+from .models import APIUsageBucket, Resource
 from .schemas import UsageData, UsageDataSchema
+
+USAGE_READ_BUCKET_SIZE = 15
 
 
 def get_usage_data(
@@ -36,43 +40,50 @@ def get_usage_data_from_influxdb(
 def get_usage_data_from_local_db(
     organisation, environment_id=None, project_id=None
 ) -> List[UsageData]:
-    if settings.USE_CUSTOM_ANALYTICS:
-        qs = APIUsageByDay.objects.filter(
-            environment_id__in=organisation.project.all().values_list(
-                "environment_id", flat=True
-            ),
-            created_date=date.today() - timedelta(days=30),
-        )
-        if project_id:
-            qs = qs.filter(project_id=project_id)
-        if environment_id:
-            qs = qs.filter(environment_id=environment_id)
+    environment_ids = Environment.objects.filter(
+        project_id__in=organisation.projects.all().values_list("id", flat=True)
+    ).values_list("id", flat=True)
 
-        qs = qs.values("resource", "date", "total_count")
+    environment_ids = [id for id in environment_ids]
 
-        def get_count_from_qs(qs, date, resource) -> int:
-            return next(
-                filter(
-                    lambda obj: obj["date"] == date and obj["resource"] == resource, qs
-                ),
-                {},
-            ).get_total_count(0)
+    qs = APIUsageBucket.objects.filter(
+        environment_id__in=environment_ids, bucket_size=USAGE_READ_BUCKET_SIZE
+    )
+    if project_id:
+        qs = qs.filter(project_id=project_id)
+    if environment_id:
+        qs = qs.filter(environment_id=environment_id)
 
-        usage_list = []
-        dates = set(obj["date"] for obj in qs)
-        for day in dates:
-            usage_list.append(
-                UsageData(
-                    flags=get_count_from_qs(qs, date, Resource.FLAGS),
-                    day=day,
-                    identities=get_count_from_qs(qs, date, Resource.IDENTITIES),
-                    traits=get_count_from_qs(qs, date, Resource.TRAITS),
-                    environment_document=get_count_from_qs(
-                        qs, date, Resource.ENVIRONMENT_DOCUMENT
-                    ),
-                )
+    def get_count_from_qs(qs, resource) -> int:
+        return next(
+            filter(lambda obj: obj["resource"] == resource, qs),
+            {},
+        ).get("count", 0)
+
+    usage_list = []
+    today = date.today()
+    for i in range(30):
+        process_from = today - timedelta(days=i)
+        process_till = process_from + timedelta(days=1)
+        events = (
+            qs.filter(
+                created_at__date__gte=process_from, created_at__date__lt=process_till
             )
-        return usage_list
+            .values("created_at__date", "resource")
+            .annotate(count=Sum("total_count"))
+        )
+        usage_list.append(
+            UsageData(
+                day=process_from,
+                flags=get_count_from_qs(events, Resource.FLAGS),
+                identities=get_count_from_qs(events, Resource.IDENTITIES),
+                traits=get_count_from_qs(events, Resource.TRAITS),
+                environment_document=get_count_from_qs(
+                    events, Resource.ENVIRONMENT_DOCUMENT
+                ),
+            )
+        )
+    return usage_list
 
 
 def get_total_events_count(organisation) -> int:
@@ -80,12 +91,16 @@ def get_total_events_count(organisation) -> int:
     Return total number of events for an organisation in the last 30 days
     """
     if settings.USE_CUSTOM_ANALYTICS:
-        events = APIUsageByDay.objects.filter(
-            environment_id__in=organisation.project.all().values_list(
-                "environment_id", flat=True
-            ),
-            date__lte=date.today(),
-            date__gte=date.today() - timedelta(days=30),
+        environment_ids = Environment.objects.filter(
+            project_id__in=organisation.projects.all().values_list("id", flat=True)
+        ).values_list("id", flat=True)
+
+        environment_ids = [id for id in environment_ids]
+        events = APIUsageBucket.objects.filter(
+            environment_id__in=environment_ids,
+            created_at__date__lte=date.today(),
+            created_at__date__gt=date.today() - timedelta(days=30),
+            bucket_size=USAGE_READ_BUCKET_SIZE,
         ).aggregate(total_count=Sum("total_count"))["total_count"]
     else:
         events = get_events_for_organisation(organisation.id)
