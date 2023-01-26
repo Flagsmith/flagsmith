46e983fecd5cdb3e078cf66cd53ee5f220d86915
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-01-26 14:25:54 +0530

    clean up models

--- api/app_analytics/models.py
+++ api/app_analytics/models.py
@@ -1,5 +1,3 @@
-from datetime import date
-
 from django.db import models
 
 
@@ -20,11 +18,6 @@ class APIUsageRaw(models.Model):
         index_together = (("environment_id", "created_at"),)
 
 
-class APIUsageAggBucketWindow(models.IntegerChoices):
-    HOUR = 1
-    DAY = 2
-
-
 class APIUsageBucket(models.Model):
     environment_id = models.PositiveIntegerField()
     resource = models.IntegerField(choices=Resource.choices)
@@ -33,36 +26,7 @@ class APIUsageBucket(models.Model):
     bucket_size = models.PositiveIntegerField(help_text="Bucket size in minutes")
 
 
-class APIUsageByDay(APIUsageBucket):
-    class Meta:
-        proxy = True
-
-
 class FeatureEvaluation(models.Model):
     feature_id = models.PositiveIntegerField()
     evaluation_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-def aggregate_usage_by_day():
-    # let's process for the last bucket first(we might have missed some data)
-    last_procssed_bucket = APIUsageByDay.objects.order_by("-date").first()
-    if last_procssed_bucket:
-        aggregate_for_a_day(last_procssed_bucket.date)
-    # now let's process for today
-    aggregate_for_a_day(date.today)
-
-
-def aggregate_for_a_day(day: date):
-    data = (
-        APIUsageRaw.objects.filter(created_at__gt=day)
-        .values("environment_id", "resource")
-        .annotate(count=models.Count("id"))
-    )
-    for row in data:
-        APIUsageByDay.objects.update_or_create(
-            environment_id=row["environment_id"],
-            resource=row["resource"],
-            date=day,
-            defaults={"total_count": row["count"]},
-        )
