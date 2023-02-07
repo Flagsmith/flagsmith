11dfeabe040c44f700879389ee7e98e7ce177716
kyle-ssg <kyle@solidstategroup.com>
2023-02-07 16:11:32 +0000

    Remove flagsmith dev env

--- frontend/env/project_local.js
+++ frontend/env/project_local.js
@@ -3,7 +3,7 @@ module.exports = global.Project = {
     api: 'http://localhost:8000/api/v1/',
     flagsmithClientAPI: 'https://api.flagsmith.com/api/v1/',
     flagsmithClientEdgeAPI: 'https://edge.api.flagsmith.com/api/v1/',
-    flagsmith: '8KzETdDeMY7xkqkSkY3Gsg',
+    flagsmith: 'ENktaJnfLVbLifybz34JmX',
     debug: false,
     env: 'dev', // This is used for Sentry tracking
     maintenance: false, // trigger maintenance mode
