4d21d76bf335bc76ae20ef62645d9c93b6b1a588
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-02-14 13:59:47 +0000

    Fix/segment audit (#1919)
    * Add audit logs to segment conditions
    
    * Refactor condition model test
    
    * Add tests

--- api/segments/tests/test_models.py
+++ api/segments/tests/test_models.py
@@ -1,4 +1,4 @@
-from unittest import TestCase, mock
+from unittest import TestCase
 
 import pytest
 
@@ -63,38 +63,3 @@ class SegmentRuleTest(TestCase):
 
         # Then
         assert segment == self.segment
-
-
-@pytest.mark.django_db
-class ConditionTest(TestCase):
-    def setUp(self) -> None:
-        self.organisation = Organisation.objects.create(name="Test Org")
-        self.project = Project.objects.create(
-            name="Test Project", organisation=self.organisation
-        )
-        self.environment = Environment.objects.create(
-            name="Test Environment", project=self.project
-        )
-        self.identity = Identity.objects.create(
-            environment=self.environment, identifier="test_identity"
-        )
-        self.segment = Segment.objects.create(project=self.project, name="test_segment")
-        self.rule = SegmentRule.objects.create(
-            segment=self.segment, type=SegmentRule.ALL_RULE
-        )
-
-    @mock.patch("segments.models.get_hashed_percentage_for_object_ids")
-    def test_percentage_split_calculation_divides_value_by_100_before_comparison(
-        self, mock_get_hashed_percentage_for_object_ids
-    ):
-        # Given
-        condition = Condition.objects.create(
-            rule=self.rule, operator=PERCENTAGE_SPLIT, value=10
-        )
-        mock_get_hashed_percentage_for_object_ids.return_value = 0.2
-
-        # When
-        res = condition.does_identity_match(self.identity)
-
-        # Then
-        assert not res
