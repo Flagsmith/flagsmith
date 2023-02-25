27075de1ec54eaabbe8d6f12684d97a4900a6e00
Micah <micahhauge@gmail.com>
2023-02-25 17:26:44 -0600

    fix typo in docs

--- frontend/common/code-help/create-user/create-user-react.js
+++ frontend/common/code-help/create-user/create-user-react.js
@@ -4,7 +4,7 @@ import flagsmith from '${LIB_NAME}';
 import { useFlags, useFlagsmith } from 'flagsmith/react';
 
 export default function HomePage() {
-  const flags = useFlags(['${FEATURE_NAME}','${FEATURE_NAME_ALT}']]); // only causes re-render if specified flag values / traits change
+  const flags = useFlags(['${FEATURE_NAME}','${FEATURE_NAME_ALT}']); // only causes re-render if specified flag values / traits change
   const ${FEATURE_NAME} = flags.${FEATURE_NAME}.enabled
   const ${FEATURE_NAME_ALT} = flags.${FEATURE_NAME_ALT}.value
   
