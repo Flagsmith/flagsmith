541e041e191201a52758068740c1a1ece0489138
kyle-ssg <kyle@solidstategroup.com>
2023-02-28 14:07:56 +0000

    Add identity overrides

---
+++ frontend/web/components/IdentitySelect.tsx
@@ -0,0 +1,81 @@
+import React, {FC, useMemo, useState} from 'react'
+import {FeatureState, Identity, Res} from "common/types/responses"; // we need this to make JSX compile
+import {filter, find} from "lodash";
+import {useGetIdentitiesQuery} from "../../common/services/useIdentity";
+import useInfiniteScroll from "../../common/useInfiniteScroll";
+import {Req} from "../../common/types/requests";
+import {components} from "react-select";
+import {ButtonOutline} from "./base/forms/Button";
+import Utils from "common/utils/utils";
+type IdentitySelectType = {
+    value: Identity['id'] | null | undefined
+    overrides?: (Omit<FeatureState,"identity">&{
+        identity: {id:number, identifier:string}
+    })[]
+    isEdge: boolean
+    onChange: (v:Identity)=> void
+    environmentId: string
+}
+
+const IdentitySelect: FC<IdentitySelectType> = ({value, isEdge, environmentId, overrides,onChange}) => {
+
+    const {data, searchItems, loadMore, isLoading} = useInfiniteScroll<Req['getIdentities'], Res['identities']>(
+        useGetIdentitiesQuery, {
+        environmentId,
+        isEdge,
+        page_size:10
+    })
+    const identityOptions = useMemo(() => {
+        return filter(
+            data?.results, identity => !overrides?.length || !find(overrides, v => v.identity?.identifier === identity.identifier),
+        ).map(({ identifier: label, id: value }) => ({ value, label })).slice(0, 10);
+    },[
+        overrides,
+        data
+    ])
+    return (
+        <Flex className="text-left">
+            <Select
+                onInputChange={(e:InputEvent)=>{
+                    searchItems(Utils.safeParseEventValue(e))
+                }}
+                data-test="select-identity"
+                placeholder="Create an Identity Override..."
+                value={value}
+                components={{
+                    Menu: ({...props}: any) => {
+                        return (
+                            <components.Menu {...props}>
+                                <React.Fragment>
+                                    {props.children}
+                                    {!!data?.next && (
+                                        <div className="text-center mb-4">
+                                            <ButtonOutline onClick={() => {
+                                                loadMore()
+                                            }} disabled={isLoading}
+                                            >
+                                                Load More
+                                            </ButtonOutline>
+                                        </div>
+                                    )
+                                    }
+                                </React.Fragment>
+                            </components.Menu>
+                        )
+                    }
+                }}
+                onChange={onChange}
+                options={identityOptions}
+                styles={{
+                    control: (base:any) => ({
+                        ...base,
+                        '&:hover': { borderColor: '$bt-brand-secondary' },
+                        border: '1px solid $bt-brand-secondary',
+                    }),
+                }}
+            />
+        </Flex>
+    )
+}
+
+export default IdentitySelect
