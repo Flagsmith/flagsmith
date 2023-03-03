9ccbad39c467f26cd3db2a52b6f6ff8021b4c2b3
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-03-03 15:39:40 +0000

    Fix audit log messages not written for overrides (#1992)
    * Ensure override audit logs are written
    
    * Add unit test

--- api/features/models.py
+++ api/features/models.py
@@ -809,15 +809,15 @@ class FeatureState(
             # feature states
             return
 
-        if self.environment.created_date > self.feature.created_date:
-            # Don't create an audit log record for feature states created when
-            # creating an environment
-            return
-
         if self.identity_id:
             return audit_helpers.get_identity_override_created_audit_message(self)
         elif self.feature_segment_id:
             return audit_helpers.get_segment_override_created_audit_message(self)
+
+        if self.environment.created_date > self.feature.created_date:
+            # Don't create an audit log record for feature states created when
+            # creating an environment
+            return
 
         return audit_helpers.get_environment_feature_state_created_audit_message(self)
 
