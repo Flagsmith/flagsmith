020e227205f056829dd6bf51fe51580939f2e3b7
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-03-03 09:29:21 +0000

    Add final audit log related object type

--- frontend/common/types/responses.ts
+++ frontend/common/types/responses.ts
@@ -89,7 +89,7 @@ export type AuditLogItem = {
   environment?: Environment;
   project: ProjectSummary;
   related_object_id: number;
-  related_object_type: "FEATURE" | "FEATURE_STATE" | "ENVIRONMENT" | "CHANGE_REQUEST" | "SEGMENT";
+  related_object_type: "FEATURE" | "FEATURE_STATE" | "ENVIRONMENT" | "CHANGE_REQUEST" | "SEGMENT" | "EDGE_IDENTITY";
   is_system_event: boolean;
 }
 
