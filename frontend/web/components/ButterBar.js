80fe3a093ec9dadcdec801e06c0e9414222c2af6
Kyle Johnson <kyle@solidstategroup.com>
2023-03-14 10:48:16 +0000

    Add markdown support for butter_bar (#2019)

--- frontend/web/components/ButterBar.js
+++ frontend/web/components/ButterBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Component } from 'react';
 import ConfigProvider from 'common/providers/ConfigProvider';
 import ProjectStore from 'common/stores/project-store'
+import ReactMarkdown from 'react-markdown'
+
 class ButterBar extends Component {
     static contextTypes = {
         router: propTypes.object.isRequired,
@@ -31,10 +33,7 @@ class ButterBar extends Component {
         }
         return <>
             {Utils.getFlagsmithValue('butter_bar') && !Utils.getFlagsmithHasFeature('read_only_mode') && (
-                <div
-                  dangerouslySetInnerHTML={{ __html: Utils.getFlagsmithValue('butter_bar') }}
-                  className="butter-bar"
-                />
+               <ReactMarkdown className="butter-bar">{Utils.getFlagsmithValue('butter_bar')}</ReactMarkdown>
             )}
             {Utils.getFlagsmithHasFeature('read_only_mode') && (
                 <div
