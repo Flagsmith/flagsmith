61d23b7777ad466eb44badb5631b1adc1ed2337d
kyle-ssg <kyle@solidstategroup.com>
2023-03-16 08:01:09 +0000

    Use new analytics dto

--- frontend/common/stores/feature-list-store.js
+++ frontend/common/stores/feature-list-store.js
@@ -199,29 +199,26 @@ const controller = {
         });
     },
     getFeatureUsage(projectId, environmentId, flag, period) {
-        data.get(`${Project.api}projects/${projectId}/features/${flag}/${Project.disableInflux? "usage" : "influx"}-data/?period=${period}&environment_id=${environmentId}`)
+        data.get(`${Project.api}projects/${projectId}/features/${flag}/evaluation-data/?period=${period}&environment_id=${environmentId}`)
             .then((result) => {
-                const firstResult = result.events_list[0];
-                const lastResult = firstResult && result.events_list[result.events_list.length - 1];
-                const diff = firstResult ? moment(lastResult.datetime, 'YYYY-MM-DD').diff(moment(firstResult.datetime, 'YYYY-MM-DD'), 'days') : 0;
+                const firstResult = result[0];
+                const lastResult = firstResult && result[result.length - 1];
+                const diff = firstResult ? moment(lastResult.day, 'YYYY-MM-DD').diff(moment(firstResult.day, 'YYYY-MM-DD'), 'days') : 0;
                 if (firstResult && diff) {
-                    const key = Object.keys(firstResult).find(v => v !== 'datetime');
                     _.range(0, diff).map((v) => {
-                        const datetime = moment(firstResult.datetime).add(v, 'days').format('YYYY-MM-DD');
-                        if (!result.events_list.find(v => v.datetime === datetime)) {
-                            result.events_list.push({
-                                [key]: 0,
-                                datetime,
+                        const day = moment(firstResult.day).add(v, 'days').format('YYYY-MM-DD');
+                        if (!result.find(v => v.day === day)) {
+                            result.push({
+                                "count": 0,
+                                day,
                             });
                         }
                     });
                 }
-                result.events_list = _.sortBy(result.events_list, v => moment(v.datetime, 'YYYY-MM-DD').valueOf()).map(v => ({
+                store.model.usageData = _.sortBy(result, v => moment(v.day, 'YYYY-MM-DD').valueOf()).map(v => ({
                     ...v,
-                    datetime: moment(v.datetime, 'YYYY-MM-DD').format('Do MMM'),
+                    day: moment(v.day, 'YYYY-MM-DD').format('Do MMM'),
                 }));
-                result.timespan = diff;
-                store.model.influxData = result;
                 store.changed();
             });
     },
@@ -466,7 +463,7 @@ const store = Object.assign({}, BaseStore, {
         return store.model && store.model.lastSaved;
     },
     getFeatureUsage() {
-        return store.model && store.model.influxData;
+        return store.model && store.model.usageData;
     },
 
 });
--- frontend/web/components/modals/CreateFlag.js
+++ frontend/web/components/modals/CreateFlag.js
@@ -21,7 +21,7 @@ import JSONReference from "../JSONReference";
 import ErrorMessage from '../ErrorMessage';
 import Permission from "common/providers/Permission";
 import IdentitySelect from '../IdentitySelect';
-
+import moment from 'moment';
 const CreateFlag = class extends Component {
     static displayName = 'CreateFlag'
 
@@ -50,7 +50,7 @@ const CreateFlag = class extends Component {
             tab: Utils.fromParam().tab || 0,
             enabledSegment: false,
             is_archived,
-            period: '28d',
+            period: 30,
         };
     }
 
@@ -99,7 +99,7 @@ const CreateFlag = class extends Component {
             }, 500);
         }
         if ((!Project.disableInflux || !Project.disableAnalytics) && this.props.projectFlag && this.props.environmentFlag) {
-            this.getInfluxData();
+            this.getFeatureUsage();
         }
     };
 
@@ -236,29 +236,25 @@ const CreateFlag = class extends Component {
     }
 
     drawChart = (data) => {
-        const { name } = this.state;
-        if (data && data.events_list) { // protect against influx setup incorrectly
-            return data.events_list.length ? (
-                <ResponsiveContainer height={400} width="100%">
-                    <BarChart data={data.events_list}>
-                        <CartesianGrid strokeDasharray="3 5"/>
-                        <XAxis
-                          interval={0}
-                          height={100} angle={-90}
-                          textAnchor="end" allowDataOverflow={false} dataKey="datetime"
-                        />
-                        <YAxis allowDataOverflow={false}/>
-                        <RechartsTooltip/>
-                        <Bar dataKey={name} stackId="a" fill="#6633ff" />
-                    </BarChart>
-                </ResponsiveContainer>
-            ) : (
-                <div className="text-center">
+        return data?.length ? (
+            <ResponsiveContainer height={400} width="100%">
+                <BarChart data={data}>
+                    <CartesianGrid strokeDasharray="3 5"/>
+                    <XAxis
+                      interval={0}
+                      height={100} angle={-90}
+                      textAnchor="end" allowDataOverflow={false} dataKey="day"
+                    />
+                    <YAxis allowDataOverflow={false}/>
+                    <RechartsTooltip/>
+                    <Bar dataKey={"count"} stackId="a" fill="#6633ff" />
+                </BarChart>
+            </ResponsiveContainer>
+        ) : (
+            <div className="text-center">
                 There has been no activity for this flag within the past month. Find out about Flag Analytics <a target="_blank" href="https://docs.flagsmith.com/advanced-use/flag-analytics">here</a>.
-                </div>
-            );
-        }
-        return null;
+            </div>
+        );
     }
 
     addItem = () => {
@@ -539,7 +535,7 @@ const CreateFlag = class extends Component {
                         AppActions.refreshFeatures(this.props.projectId, this.props.environmentId);
                     }}
                     >
-                        {({ isLoading, isSaving, error, influxData }, { createFlag, editFeatureSettings, editFeatureValue, editFeatureSegments, createChangeRequest }) => {
+                        {({ isLoading, isSaving, error, usageData }, { createFlag, editFeatureSettings, editFeatureValue, editFeatureSegments, createChangeRequest }) => {
                             const saveFeatureValue = (schedule) => {
                                 this.setState({ valueChanged: false });
                                 if ((is4Eyes || schedule) && !identity) {
@@ -898,11 +894,11 @@ const CreateFlag = class extends Component {
                                                                     <TabItem data-test="analytics" tabLabel="Analytics">
                                                                         <FormGroup className="mb-4 mr-3 ml-3">
                                                                             <Panel
-                                                                              title={!!influxData && <h6 className="mb-0">Flag events for last {influxData.timespan} days</h6>}
+                                                                              title={!!usageData && <h6 className="mb-0">Flag events for last 30 days</h6>}
                                                                             >
-                                                                                {!influxData && <div className="text-center"><Loader/></div> }
+                                                                                {!usageData && <div className="text-center"><Loader/></div> }
 
-                                                                                {this.drawChart(influxData)}
+                                                                                {this.drawChart(usageData)}
                                                                             </Panel>
                                                                         </FormGroup>
                                                                     </TabItem>
