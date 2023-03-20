6e65c961b12889e90f7a4f820d7d3687d1ecf81b
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-03-20 11:50:02 +0000

    Fix issue committing scheduled change requests in the wrong order (#2036)
    * Fix environment logic which ends up in scheduled flags being returned in wrong order
    
    * Add comment
    
    * Fix TypeError
    
    * Apply to identity logic and refactor

--- api/tests/unit/features/conftest.py
+++ api/tests/unit/features/conftest.py
@@ -18,3 +18,20 @@ def feature_state_version_generator(environment, feature, request):
         ),
         expected_result,
     )
+
+
+@pytest.fixture()
+def feature_state_live_from_generator(environment, feature, request):
+    live_from_one = request.param[0]
+    live_from_two = request.param[1]
+    expected_result = request.param[2]
+
+    return (
+        FeatureState.objects.create(
+            feature=feature, environment=environment, live_from=live_from_one, version=2
+        ),
+        FeatureState.objects.create(
+            feature=feature, environment=environment, live_from=live_from_two, version=3
+        ),
+        expected_result,
+    )
