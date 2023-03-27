883583ad216fdd6a6b58dc4ed43970e3f2bef107
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-03-27 18:11:47 +0100

    WIP: add task for cleaning up old tasks

--- api/app/settings/common.py
+++ api/app/settings/common.py
@@ -830,6 +830,7 @@ TASK_RUN_METHOD = env.enum(
 ENABLE_TASK_PROCESSOR_HEALTH_CHECK = env.bool(
     "ENABLE_TASK_PROCESSOR_HEALTH_CHECK", default=False
 )
+TASK_RETENTION_DAYS = env.int("TASK_RETENTION_DAYS", default=30)
 
 # Real time(server sent events) settings
 SSE_SERVER_BASE_URL = env.str("SSE_SERVER_BASE_URL", None)
--- api/task_processor/tasks.py
+++ api/task_processor/tasks.py
@@ -1,7 +1,15 @@
 import logging
+from datetime import timedelta
 
-from task_processor.decorators import register_task_handler
-from task_processor.models import HealthCheckModel
+from django.conf import settings
+from django.db.models import Q
+from django.utils import timezone
+
+from task_processor.decorators import (
+    register_recurring_task,
+    register_task_handler,
+)
+from task_processor.models import HealthCheckModel, Task
 
 logger = logging.getLogger(__name__)
 
@@ -10,3 +18,20 @@ logger = logging.getLogger(__name__)
 def create_health_check_model(health_check_model_uuid: str):
     logger.info("Creating health check model.")
     HealthCheckModel.objects.create(uuid=health_check_model_uuid)
+
+
+@register_recurring_task(
+    run_every=timedelta(days=1),
+    kwargs={"task_retention_days": settings.TASK_RETENTION_DAYS},
+)
+def clean_up_old_tasks(task_retention_days: int):
+    now = timezone.now()
+    delete_before = now - timedelta(days=task_retention_days)
+
+    query = Q(scheduled_for__lt=delete_before) & (
+        Q(completed=True) | Q(num_failures__gte=3)
+    )
+    queryset = Task.objects.filter(query)
+    while queryset.exists():
+        # delete in batches of 2000
+        queryset[0:2000].delete()
