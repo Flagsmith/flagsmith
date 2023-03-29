c455781684b752f5146fa01e3901d68c4f94dadb
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-03-29 15:28:36 +0530

    fix(audit/signal/task): fix race condition on feature creation  (#2081)
    * fix(audit/signal/task): fix race condition
    
    The environment document in dynamodb is updated based on the post_save signal from the audit log
    When creating a new feature, the feature states are created after the feature has been created.
    i.e: the below task gets created/scheduled before feature states are created
    Usually, there is enough time for the main thread to create the feature states
    before the task is executed, but not always.
    In those cases, we send the environment document to dynamodb without any feature states for the new feature.
    In order to avoid this, either we need to update environment
    document when creating feature states
    or delay the execution of this task
    We prefer to delay the execution of the task because of it's low surface area
    
    * skip delay task processor is not running

--- api/core/signals.py
+++ api/core/signals.py
@@ -1,7 +1,10 @@
 from core.models import _AbstractBaseAuditableModel
+from django.conf import settings
+from django.utils import timezone
 from simple_history.models import HistoricalRecords
 
 from audit import tasks
+from task_processor.task_run_method import TaskRunMethod
 from users.models import FFAdminUser
 
 
@@ -11,12 +14,29 @@ def create_audit_log_from_historical_record(
     history_instance,
     **kwargs,
 ):
+    # The environment document in dynamodb is updated based on the post_save signal from the audit log
+    # When creating a new feature, the feature states are created after the feature has been created.
+    # i.e: the below task gets created/scheduled before feature states are created
+    # Usually, there is enough time for the main thread to create the feature states
+    # before the task is executed, but not always.
+    # In those cases, we send the environment document to dynamodb without any feature states for the new feature.
+    # In order to avoid this, either we need to update environment
+    # document when creating feature states
+    # or delay the execution of this task
+    # We prefer to delay the execution of the task because of it's low surface area
+    delay_until = (
+        timezone.now() + timezone.timedelta(seconds=1)
+        if settings.TASK_RUN_METHOD == TaskRunMethod.TASK_PROCESSOR
+        else None
+    )
+
     tasks.create_audit_log_from_historical_record.delay(
         kwargs={
             "history_instance_id": history_instance.history_id,
             "history_user_id": getattr(history_user, "id", None),
             "history_record_class_path": instance.history_record_class_path,
-        }
+        },
+        delay_until=delay_until,
     )
 
 
