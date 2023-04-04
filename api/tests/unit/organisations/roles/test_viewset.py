e5d87b5c9a15407c38e22832e4497b5756345f71
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-04-04 13:30:04 +0530

    Add tests for group and user views

--- api/tests/unit/organisations/roles/test_viewset.py
+++ api/tests/unit/organisations/roles/test_viewset.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 
 
-def test_create_role(organisation, admin_client, view_environment_permission):
+def test_create_role(organisation, admin_client):
     # Given
     url = reverse(
         "api-v1:organisations:organisation-roles-list",
@@ -13,7 +13,6 @@ def test_create_role(organisation, admin_client, view_environment_permission):
     data = {
         "name": "a role",
         "organisation": organisation.id,
-        "permissions": [view_environment_permission.key],
     }
 
     # When
@@ -25,7 +24,7 @@ def test_create_role(organisation, admin_client, view_environment_permission):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["name"] == "a role"
     assert response.json()["organisation"] == organisation.id
-    assert response.json()["permissions"] == [view_environment_permission.key]
+    assert response.json()["id"]
 
 
 def test_list_roles(organisation, admin_client, role):
@@ -105,6 +104,70 @@ def test_create_user_role(organisation, admin_client, role, test_user):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["user"] == test_user.id
     assert response.json()["role"] == role.id
+    assert response.json()["id"]
+
+
+def test_retrieve_user_role(organisation, admin_client, role, user_role, test_user):
+    # Given
+    url = reverse(
+        "api-v1:organisations:user-roles-detail",
+        args=[organisation.id, role.id, user_role.id],
+    )
+
+    # When
+    response = admin_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["user"] == test_user.id
+    assert response.json()["role"] == role.id
+    assert response.json()["id"] == user_role.id
+
+
+def test_delete_user_role(organisation, admin_client, role, user_role):
+    # Given
+    url = reverse(
+        "api-v1:organisations:user-roles-detail",
+        args=[organisation.id, role.id, user_role.id],
+    )
+
+    # When
+    response = admin_client.delete(url)
+
+    # Then
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_list_user_role(organisation, admin_client, role, user_role):
+    # Given
+    url = reverse(
+        "api-v1:organisations:user-roles-list",
+        args=[organisation.id, role.id],
+    )
+
+    # When
+    response = admin_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["results"][0]["user"] == user_role.user.id
+    assert response.json()["results"][0]["role"] == user_role.role.id
+
+
+def test_update_user_role(organisation, admin_client, role, user_role, admin_user):
+    # Given
+    url = reverse(
+        "api-v1:organisations:user-roles-detail",
+        args=[organisation.id, role.id, user_role.id],
+    )
+    data = {"role": role.id, "user": admin_user.id}
+
+    # When
+    response = admin_client.put(url, data=data)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["user"] == admin_user.id
 
 
 def test_create_group_role(
@@ -126,3 +189,83 @@ def test_create_group_role(
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["group"] == default_user_permission_group.id
     assert response.json()["role"] == role.id
+    assert response.json()["id"]
+
+
+def test_delete_group_role(organisation, admin_client, role, group_role):
+    # Given
+    url = reverse(
+        "api-v1:organisations:group-roles-detail",
+        args=[organisation.id, role.id, group_role.id],
+    )
+
+    # When
+    response = admin_client.delete(url)
+
+    # Then
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_retrieve_group_role(
+    organisation, admin_client, role, user_permission_group, group_role
+):
+    # Given
+    url = reverse(
+        "api-v1:organisations:group-roles-detail",
+        args=[organisation.id, role.id, group_role.id],
+    )
+
+    # When
+    response = admin_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["group"] == user_permission_group.id
+    assert response.json()["role"] == role.id
+    assert response.json()["id"] == group_role.id
+
+
+def test_list_group_role(
+    organisation, admin_client, role, group_role, user_permission_group
+):
+    # Given
+    url = reverse(
+        "api-v1:organisations:group-roles-list",
+        args=[organisation.id, role.id],
+    )
+
+    # When
+    response = admin_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["results"][0]["group"] == user_permission_group.id
+    assert response.json()["results"][0]["role"] == role.id
+    assert response.json()["results"][0]["id"] == group_role.id
+
+
+def test_update_group_role(
+    organisation,
+    admin_client,
+    role,
+    default_user_permission_group,
+    group_role,
+):
+    # Given
+    url = reverse(
+        "api-v1:organisations:group-roles-detail",
+        args=[organisation.id, role.id, group_role.id],
+    )
+
+    data = {"group": default_user_permission_group.id, "role": role.id}
+
+    # When
+    response = admin_client.patch(
+        url, data=json.dumps(data), content_type="application/json"
+    )
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["group"] == default_user_permission_group.id
+    assert response.json()["role"] == role.id
+    assert response.json()["id"] == group_role.id
