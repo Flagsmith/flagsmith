da5194a90e467cadc6cb8911eac229852c412338
kyle-ssg <kyle@solidstategroup.com>
2023-04-10 10:21:27 +0100

    Revert env changes

--- frontend/bin/env.js
+++ frontend/bin/env.js
@@ -2,12 +2,14 @@
  * Created by kylejohnson on 02/08/2016.
  */
 require('colors')
-const fs1 = require('fs')
 const fs = require('fs-extra')
 const path = require('path')
-const { execSync } = require('child_process')
+
 const env = process.env.ENV || 'dev'
 const src = path.resolve(__dirname, `../env/project_${env}.js`)
 const target = path.resolve(__dirname, '../common/project.js')
-fs.copySync(src, target)
 
+// eslint-disable-next-line
+console.log(`Using project_${env}.js`.green);
+
+fs.copySync(src, target)
