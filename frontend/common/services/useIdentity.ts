77a59d5bf49266613b3c00accad9e500b62d9a6e
Kyle Johnson <kyle@solidstategroup.com>
2023-04-21 09:34:01 +0100

    support core page numbers for identities (#2143)
    * support core page numbers for identities
    
    * Create core paging util

--- frontend/common/services/useIdentity.ts
+++ frontend/common/services/useIdentity.ts
@@ -1,6 +1,7 @@
 import { Res } from 'common/types/responses'
 import { Req } from 'common/types/requests'
 import { service } from 'common/service'
+import transformCorePaging from 'common/transformCorePaging'
 
 const getIdentityEndpoint = (environmentId: string, isEdge: boolean) => {
   const identityPart = isEdge ? 'edge-identities' : 'identities'
@@ -82,16 +83,18 @@ export const identityService = service
             url,
           }
         },
-        transformResponse(
-          baseQueryReturnValue: Res['identities'],
-          meta,
-          { isEdge, page_size = 10, pageType, pages: _pages },
-        ) {
+        transformResponse(baseQueryReturnValue: Res['identities'], meta, req) {
+          const {
+            isEdge,
+            page = 1,
+            page_size = 10,
+            pageType,
+            pages: _pages,
+          } = req
           if (isEdge) {
             // For edge, we create our own paging
             let pages = _pages ? _pages.concat([]) : []
             const next_evaluated_key = baseQueryReturnValue.last_evaluated_key
-
             if (pageType === 'NEXT') {
               pages.push(next_evaluated_key)
             } else if (pageType === 'PREVIOUS') {
@@ -120,7 +123,7 @@ export const identityService = service
               }), //
             }
           }
-          return baseQueryReturnValue
+          return transformCorePaging(req, baseQueryReturnValue)
         },
       }),
       // END OF ENDPOINTS
