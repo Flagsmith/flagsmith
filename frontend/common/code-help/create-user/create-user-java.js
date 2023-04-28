4d4705ecacd6fc008ab1443648c1c480094df38c
Kyle Johnson <kyle@solidstategroup.com>
2023-04-28 15:47:19 +0100

    Chores/fix identify code examples (#2167)
    * Edit segment persistence
    
    * Fix identify examples
    
    * Sort code help select
    
    * Remove CRLF
    
    ---------
    
    Co-authored-by: Ben Rometsch <ben.rometsch@gmail.com>

--- frontend/common/code-help/create-user/create-user-java.js
+++ frontend/common/code-help/create-user/create-user-java.js
@@ -1,6 +1,6 @@
 module.exports = (
   envId,
-  { FEATURE_NAME, FEATURE_NAME_ALT, LIB_NAME, LIB_NAME_JAVA },
+  { FEATURE_NAME, FEATURE_NAME_ALT, LIB_NAME, LIB_NAME_JAVA, USER_ID },
   userId,
 ) => `${LIB_NAME_JAVA} ${LIB_NAME} = ${LIB_NAME_JAVA}
     .newBuilder()
@@ -8,7 +8,7 @@ module.exports = (
     .build();
 
 // Identify the user
-Flags flags = flagsmith.getIdentityFlags("${userId}");
+Flags flags = flagsmith.getIdentityFlags("${userId || USER_ID}");
 
 // get the state / value of the user's flags
 Boolean isEnabled = flags.isFeatureEnabled("${FEATURE_NAME}");
