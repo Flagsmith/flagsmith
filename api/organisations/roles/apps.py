3d1dba4bd08a59e3fa14abb68e632f39285736f7
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-05-01 10:24:53 +0530

    remove roles app

--- api/organisations/roles/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class RolesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "organisations.roles"
-    label = "organisation_roles"
