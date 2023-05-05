b72021aadcbdb18a0ea5e9dbdc31a3bbe64829da
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-05-05 17:37:26 +0100

    Add new VIEW_AUDIT_LOG permission at the organisation level

---
+++ api/audit/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework.permissions import BasePermission
+
+from organisations.models import Organisation
+from organisations.permissions.permissions import VIEW_AUDIT_LOG
+
+
+class AuditLogPermissions(BasePermission):
+    def has_permission(self, request, view):
+        try:
+            organisation_id = view.kwargs["organisation"]
+            organisation = Organisation.objects.get(id=organisation_id)
+        except (Organisation.DoesNotExist, KeyError):
+            return False
+
+        return request.user.has_organisation_permission(organisation, VIEW_AUDIT_LOG)
