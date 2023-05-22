4ee9468d52f27c0e9466f2e9443ac8e830f326c9
kyle-ssg <kyle@solidstategroup.com>
2023-05-22 18:27:12 +0100

    Fix 404 page

--- frontend/web/components/pages/NotFoundPage.tsx
+++ frontend/web/components/pages/NotFoundPage.tsx
@@ -6,10 +6,7 @@ const NotFoundPage: FC<NotFoundPageType> = ({}) => {
   return (
     <div className='app-container container'>
       <h3 className='pt-5'>Oops, we can't seem to find this page!</h3>
-      <p>
-        It looks like you have found a URL that does not exist, please check the
-        URL you are trying to visit and try again.
-      </p>
+      <p>Please check the URL you are trying to visit and try again.</p>
     </div>
   )
 }
