d86ea176d717bf15f7bf002abedf03d2e76db96c
Novak Zaballa <zaballa.novak@gmail.com>
2023-05-23 21:33:14 +0200

    fix e2e test

--- frontend/web/components/pages/AccountSettingsPage.js
+++ frontend/web/components/pages/AccountSettingsPage.js
@@ -197,6 +197,7 @@ class TheComponent extends Component {
                                   )
                                 }
                                 id='change-email-button'
+                                data-test='change-email-button'
                                 type='button'
                                 class='input-group-addon'
                               >
@@ -277,7 +278,8 @@ class TheComponent extends Component {
                             </p>
                           </div>
                           <Button
-                            id='delete-org-btn'
+                            id='delete-user-btn'
+                            data-test='delete-user-btn'
                             onClick={() =>
                               this.confirmDeleteAccount(
                                 lastUserOrganisations,
