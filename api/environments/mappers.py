7b691f46b39b9e8d05318a0ca9e3ae54e8a97daf
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-05-25 13:11:31 +0100

    feat(docs): document `map_environment_to_document`

--- api/environments/mappers.py
+++ api/environments/mappers.py
@@ -7,6 +7,16 @@ if TYPE_CHECKING:
 
 
 def map_environment_to_document(environment: "Environment") -> dict[str, Any]:
+    """
+    Maps Core API's `environments.models.Environment` mode instance to the
+    flag_engine environment document.
+    Before building the document, takes care of resolving relationships and
+    feature versions.
+
+    :param Environment environment: the environment to map
+    :rtype dict[str, Any]
+    """
+    # TODO @khvn26 Migrate flag_engine logic to here https://github.com/Flagsmith/flagsmith/issues/2079
     environment.project.server_key_only_feature_ids: list[int] = [
         feature.id
         for feature_state in environment.feature_states.all()
