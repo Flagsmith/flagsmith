d50fcbd355d3d633d709078e4d855870fd19957c
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-06-15 11:28:10 +0100

    Prevent deleted API keys from being returned (#2182)
    * Prevent deleted API keys from being returned
    
    * typing
    
    * Better typing
    
    * Add return type to test function

--- api/api_keys/models.py
+++ api/api_keys/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-from rest_framework_api_key.models import AbstractAPIKey
-from softdelete.models import SoftDeleteObject
+from rest_framework_api_key.models import AbstractAPIKey, APIKeyManager
+from softdelete.models import SoftDeleteManager, SoftDeleteObject
 
 from organisations.models import Organisation
+
+
+class MasterAPIKeyManager(APIKeyManager, SoftDeleteManager):
+    pass
 
 
 class MasterAPIKey(AbstractAPIKey, SoftDeleteObject):
@@ -11,3 +15,5 @@ class MasterAPIKey(AbstractAPIKey, SoftDeleteObject):
         on_delete=models.CASCADE,
         related_name="master_api_keys",
     )
+
+    objects = MasterAPIKeyManager()
