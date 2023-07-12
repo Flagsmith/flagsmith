45c198ddf0c5572700aef36b9c121cc2862bf959
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-07-12 11:46:07 +0100

    formatting

--- api/tests/unit/app/test_unit_app_views.py
+++ api/tests/unit/app/test_unit_app_views.py
@@ -15,5 +15,5 @@ def test_get_version_info(api_client: APIClient) -> None:
     assert response.json() == {
         "ci_commit_sha": "unknown",
         "image_tag": "unknown",
-        "is_enterprise": False
+        "is_enterprise": False,
     }
