6f67869ac27f7c85aefc37ebb2ba89b85caaf5bd
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-07-12 11:40:56 +0100

    Bake enterprise version info into private cloud image

---
+++ api/tests/unit/app/test_unit_app_views.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+def test_get_version_info(api_client: APIClient) -> None:
+    # Given
+    url = reverse("version-info")
+
+    # When
+    response = api_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "ci_commit_sha": "unknown",
+        "image_tag": "unknown",
+        "is_enterprise": False
+    }
