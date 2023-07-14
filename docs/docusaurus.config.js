5b9fe58d8307bdac8a2456429d10e3c4d132ab14
Ben Rometsch <ben.rometsch@gmail.com>
2023-07-14 18:02:32 +0100

    ci/lint all the things

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,20 +1,57 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-    title: 'Flagsmith Docs',
-    tagline: 'Open Source Feature Flags',
-    url: 'https://docs.flagsmith.com',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    organizationName: 'Flagsmith',
-    projectName: 'flagsmith',
-    plugins: ['@ionic-internal/docusaurus-plugin-tag-manager'],
-    themeConfig: {
-        prism: {
-            additionalLanguages: ['java', 'csharp', 'ruby', 'rust', 'swift', 'dart', 'php', 'kotlin', 'groovy', 'hcl'],
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  i18n: {
+    defaultLocale: 'en',
+    localeConfigs: {
+      en: {
+        direction: 'ltr',
+        label: 'English',
+      },
+    },
+    locales: ['en'],
+  },
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'Flagsmith',
+  plugins: ['@ionic-internal/docusaurus-plugin-tag-manager'],
+  presets: [
+    [
+      'docusaurus-preset-openapi',
+      {
+        api: {
+          path: 'api_spec',
         },
-        /*
+        docs: {
+          // Please change this to your repo.
+          editUrl: 'https://github.com/flagsmith/flagsmith/tree/main/docs/',
+
+          lastVersion: 'current',
+
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          versions: {
+            current: {
+              badge: false,
+              label: 'v2.0',
+            },
+            'v1.0': {
+              badge: false,
+              label: 'v1.0',
+            },
+          },
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  projectName: 'flagsmith',
+  tagline: 'Open Source Feature Flags',
+  themeConfig: {
+    /*
         typesense: {
             typesenseCollectionName: 'flagsmith-docs',
             typesenseServerConfig: {
@@ -29,146 +66,124 @@ module.exports = {
             },
         },
         */
-        algolia: {
-            // The application ID provided by Algolia
-            appId: 'GZAPCBLIEE',
-            // Public API key: it is safe to commit it
-            apiKey: '00987f2d774d7787dcae25294463294c',
-            indexName: 'flagsmith',
-        },
-        tagManager: {
-            trackingID: 'GTM-5ZV5K5G',
-        },
-        navbar: {
-            title: 'Flagsmith',
-            logo: {
-                alt: 'Flagsmith Logo',
-                src: 'img/logo.svg',
-            },
-            items: [
-                {
-                    type: 'doc',
-                    docId: 'intro',
-                    position: 'left',
-                    label: 'Docs',
-                },
-                {
-                    to: '/api',
-                    label: 'API',
-                    position: 'left',
-                },
-                {
-                    href: 'https://flagsmith.com',
-                    label: 'Flagsmith.com',
-                    position: 'left',
-                },
-                {
-                    href: 'https://github.com/flagsmith/flagsmith',
-                    label: 'GitHub',
-                    position: 'left',
-                },
-                {
-                    label: 'Discord',
-                    href: 'https://discord.gg/hFhxNtXzgm',
-                    position: 'left',
-                },
-                {
-                    type: 'docsVersionDropdown',
-                    position: 'right',
-                },
-            ],
-        },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Flagsmith',
-                    items: [
-                        {
-                            label: 'Flagsmith.com',
-                            to: 'https://flagsmith.com/',
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/getflagsmith',
-                        },
-                    ],
-                },
-                {
-                    title: 'Open Source',
-                    items: [
-                        {
-                            label: 'Github',
-                            href: 'https://github.com/Flagsmith',
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discord.gg/hFhxNtXzgm',
-                        },
-                    ],
-                },
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'Blog',
-                            to: 'https://flagsmith.com/blog/',
-                        },
-                        {
-                            label: 'Podcast',
-                            href: 'https://flagsmith.com/podcast/',
-                        },
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Bullet Train Ltd. Built with Docusaurus.`,
-        },
-        announcementBar: {
-            id: 'support_us',
-            content: `If you like Flagsmith, give us a star 🌟 on <a target="_blank" rel="noopener noreferrer" href="https://github.com/Flagsmith/flagsmith">GitHub</a> and follow us ❤️ on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/getflagsmith">Twitter</a>`,
-            backgroundColor: '#5d60cc',
-            textColor: '#ffffff',
-            isCloseable: true,
-        },
+    algolia: {
+      // Public API key: it is safe to commit it
+      apiKey: '00987f2d774d7787dcae25294463294c',
+
+      // The application ID provided by Algolia
+      appId: 'GZAPCBLIEE',
+      indexName: 'flagsmith',
     },
-    presets: [
-        [
-            'docusaurus-preset-openapi',
+
+    announcementBar: {
+      backgroundColor: '#5d60cc',
+      content: `If you like Flagsmith, give us a star 🌟 on <a target="_blank" rel="noopener noreferrer" href="https://github.com/Flagsmith/flagsmith">GitHub</a> and follow us ❤️ on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/getflagsmith">Twitter</a>`,
+      id: 'support_us',
+      isCloseable: true,
+      textColor: '#ffffff',
+    },
+    footer: {
+      copyright: `Copyright © ${new Date().getFullYear()} Bullet Train Ltd. Built with Docusaurus.`,
+      links: [
+        {
+          items: [
             {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    routeBasePath: '/',
-                    // Please change this to your repo.
-                    editUrl: 'https://github.com/flagsmith/flagsmith/tree/main/docs/',
-                    lastVersion: 'current',
-                    versions: {
-                        current: {
-                            label: 'v2.0',
-                            badge: false,
-                        },
-                        'v1.0': {
-                            label: 'v1.0',
-                            badge: false,
-                        },
-                    },
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-                api: {
-                    path: 'api_spec',
-                },
+              label: 'Flagsmith.com',
+              to: 'https://flagsmith.com/',
             },
-        ],
-    ],
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-        localeConfigs: {
-            en: {
-                label: 'English',
-                direction: 'ltr',
+            {
+              href: 'https://twitter.com/getflagsmith',
+              label: 'Twitter',
             },
+          ],
+          title: 'Flagsmith',
         },
+        {
+          items: [
+            {
+              href: 'https://github.com/Flagsmith',
+              label: 'Github',
+            },
+            {
+              href: 'https://discord.gg/hFhxNtXzgm',
+              label: 'Discord',
+            },
+          ],
+          title: 'Open Source',
+        },
+        {
+          items: [
+            {
+              label: 'Blog',
+              to: 'https://flagsmith.com/blog/',
+            },
+            {
+              href: 'https://flagsmith.com/podcast/',
+              label: 'Podcast',
+            },
+          ],
+          title: 'More',
+        },
+      ],
+      style: 'dark',
     },
-};
+    navbar: {
+      items: [
+        {
+          docId: 'intro',
+          label: 'Docs',
+          position: 'left',
+          type: 'doc',
+        },
+        {
+          label: 'API',
+          position: 'left',
+          to: '/api',
+        },
+        {
+          href: 'https://flagsmith.com',
+          label: 'Flagsmith.com',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/flagsmith/flagsmith',
+          label: 'GitHub',
+          position: 'left',
+        },
+        {
+          href: 'https://discord.gg/hFhxNtXzgm',
+          label: 'Discord',
+          position: 'left',
+        },
+        {
+          position: 'right',
+          type: 'docsVersionDropdown',
+        },
+      ],
+      logo: {
+        alt: 'Flagsmith Logo',
+        src: 'img/logo.svg',
+      },
+      title: 'Flagsmith',
+    },
+    prism: {
+      additionalLanguages: [
+        'java',
+        'csharp',
+        'ruby',
+        'rust',
+        'swift',
+        'dart',
+        'php',
+        'kotlin',
+        'groovy',
+        'hcl',
+      ],
+    },
+    tagManager: {
+      trackingID: 'GTM-5ZV5K5G',
+    },
+  },
+  title: 'Flagsmith Docs',
+  url: 'https://docs.flagsmith.com',
+}
--- docs/src/theme/prism-include-languages.js
+++ docs/src/theme/prism-include-languages.js
@@ -4,22 +4,22 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-import siteConfig from '@generated/docusaurus.config';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
+import siteConfig from '@generated/docusaurus.config'
 
 const prismIncludeLanguages = (PrismObject) => {
-    if (ExecutionEnvironment.canUseDOM) {
-        const {
-            themeConfig: { prism: { additionalLanguages = [] } = {} },
-        } = siteConfig;
-        window.Prism = PrismObject;
-        additionalLanguages.forEach((lang) => {
-            require(`prismjs/components/prism-${lang}`); // eslint-disable-line
-        });
-        require('./prism/prism-java');
+  if (ExecutionEnvironment.canUseDOM) {
+    const {
+      themeConfig: { prism: { additionalLanguages = [] } = {} },
+    } = siteConfig
+    window.Prism = PrismObject
+    additionalLanguages.forEach((lang) => {
+      require(`prismjs/components/prism-${lang}`) // eslint-disable-line
+    })
+    require('./prism/prism-java')
 
-        delete window.Prism;
-    }
-};
+    delete window.Prism
+  }
+}
 
-export default prismIncludeLanguages;
+export default prismIncludeLanguages
