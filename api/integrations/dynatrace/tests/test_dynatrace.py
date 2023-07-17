9034a8e5a35753f76cca46a9100dcf53e6b2a351
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-07-17 17:10:35 +0100

    Update test to cover all related object types

--- api/integrations/dynatrace/tests/test_dynatrace.py
+++ api/integrations/dynatrace/tests/test_dynatrace.py
@@ -1,3 +1,6 @@
+import pytest
+from pytest_lazyfixture import lazy_fixture
+
 from audit.models import AuditLog
 from audit.related_object_type import RelatedObjectType
 from environments.models import Environment
@@ -20,8 +23,28 @@ def test_dynatrace_initialized_correctly():
     assert dynatrace.url == expected_url
 
 
+@pytest.mark.parametrize(
+    "related_object_type, related_object, expected_deployment_name",
+    (
+        (
+            RelatedObjectType.FEATURE.name,
+            lazy_fixture("feature"),
+            "Flagsmith Deployment - Flag Changed: Test Feature1",
+        ),
+        (
+            RelatedObjectType.FEATURE_STATE.name,
+            lazy_fixture("feature_state"),
+            "Flagsmith Deployment - Flag Changed: Test Feature1",
+        ),
+        (
+            RelatedObjectType.SEGMENT.name,
+            lazy_fixture("segment"),
+            "Flagsmith Deployment - Segment Changed: segment",
+        ),
+    ),
+)
 def test_dynatrace_when_generate_event_data_with_correct_values_then_success(
-    django_user_model,
+    django_user_model, related_object_type, related_object, expected_deployment_name
 ):
     # Given
     log = "some log data"
@@ -29,7 +52,13 @@ def test_dynatrace_when_generate_event_data_with_correct_values_then_success(
     author = django_user_model(email="test@email.com")
     environment = Environment(name="test")
 
-    audit_log_record = AuditLog(log=log, author=author, environment=environment)
+    audit_log_record = AuditLog(
+        log=log,
+        author=author,
+        environment=environment,
+        related_object_type=related_object_type,
+        related_object_id=related_object.id,
+    )
 
     dynatrace = DynatraceWrapper(
         base_url="http://test.com",
@@ -45,6 +74,9 @@ def test_dynatrace_when_generate_event_data_with_correct_values_then_success(
 
     assert event_data["properties"]["event"] == expected_event_text
     assert event_data["properties"]["environment"] == environment.name
+    assert (
+        event_data["properties"]["dt.event.deployment.name"] == expected_deployment_name
+    )
 
 
 def test_dynatrace_when_generate_event_data_with_missing_author_then_success():
@@ -98,7 +130,3 @@ def test_dynatrace_when_generate_event_data_with_missing_environment_then_succes
     expected_event_text = f"{log} by user {author.email}"
     assert event_data["properties"]["event"] == expected_event_text
     assert event_data["properties"]["environment"] == "unknown"
-    assert (
-        event_data["properties"]["dt.event.deployment.name"]
-        == f"Flagsmith Deployment - Flag Changed: {feature.name}"
-    )
