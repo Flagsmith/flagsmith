0ede9155da6ce0b3e85f7d635a4f0169f08d413b
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-07-25 10:51:25 +0100

    fix: rendering recurring task admin times out (#2514)

--- api/task_processor/admin.py
+++ api/task_processor/admin.py
@@ -1,30 +1,18 @@
 from django.contrib import admin
 
-from task_processor.models import (
-    RecurringTask,
-    RecurringTaskRun,
-    Task,
-    TaskRun,
-)
-
-
-class TaskRunInline(admin.StackedInline):
-    model = TaskRun
-    extra = 0
-    show_change_link = False
-
-
-class RecurringTaskRunInline(admin.StackedInline):
-    model = RecurringTaskRun
-    extra = 0
-    show_change_link = False
+from task_processor.models import RecurringTask
 
 
 @admin.register(RecurringTask)
 class RecurringTaskAdmin(admin.ModelAdmin):
-    inlines = (RecurringTaskRunInline,)
-    list_display = ("uuid", "task_identifier", "run_every", "last_run_status")
+    list_display = (
+        "uuid",
+        "task_identifier",
+        "run_every",
+        "last_run_status",
+        "is_locked",
+    )
     readonly_fields = ("args", "kwargs")
 
-    def last_run_status(self, instance: Task) -> str:
+    def last_run_status(self, instance: RecurringTask) -> str:
         return instance.task_runs.order_by("-started_at").first().result
