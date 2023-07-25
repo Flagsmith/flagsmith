a111976f230e2c75d7ff3076efb8bbac0905283a
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-07-25 14:21:59 +0100

    chore: clean up old tasks (#2095)

--- api/task_processor/models.py
+++ api/task_processor/models.py
@@ -128,6 +128,7 @@ class Task(AbstractBaseTask):
 
 class RecurringTask(AbstractBaseTask):
     run_every = models.DurationField()
+    first_run_time = models.TimeField(blank=True, null=True)
 
     objects = RecurringTaskManager()
 
@@ -141,10 +142,15 @@ class RecurringTask(AbstractBaseTask):
 
     @property
     def should_execute(self) -> bool:
+        now = timezone.now()
         last_task_run = self.task_runs.order_by("-started_at").first()
-        # if we have never run this task, then we should execute it
+
         if not last_task_run:
-            return True
+            # If we have never run this task, then we should execute it only if
+            # the time has passed after which we want to ensure this task runs.
+            # This allows us to control when intensive tasks should be run.
+            return not (self.first_run_time and self.first_run_time > now.time())
+
         # if the last run was at t- run_every, then we should execute it
         if (timezone.now() - last_task_run.started_at) >= self.run_every:
             return True
@@ -153,9 +159,7 @@ class RecurringTask(AbstractBaseTask):
         # more than 3 failures in t- run_every, then we should execute it
         if (
             last_task_run.result != TaskResult.SUCCESS.name
-            and self.task_runs.filter(
-                started_at__gte=(timezone.now() - self.run_every)
-            ).count()
+            and self.task_runs.filter(started_at__gte=(now - self.run_every)).count()
             <= 3
         ):
             return True
