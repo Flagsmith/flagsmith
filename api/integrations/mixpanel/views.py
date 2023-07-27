34308293a2b3a5dfa8f4b764e847e6cd297279ed
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-07-27 13:55:51 +0100

    fix: allow creating integration configurations where deleted versions exist (#2531)

--- api/integrations/mixpanel/views.py
+++ api/integrations/mixpanel/views.py
@@ -1,8 +1,8 @@
-from integrations.common.views import IntegrationCommonViewSet
+from integrations.common.views import EnvironmentIntegrationCommonViewSet
 from integrations.mixpanel.models import MixpanelConfiguration
 from integrations.mixpanel.serializers import MixpanelConfigurationSerializer
 
 
-class MixpanelConfigurationViewSet(IntegrationCommonViewSet):
+class MixpanelConfigurationViewSet(EnvironmentIntegrationCommonViewSet):
     serializer_class = MixpanelConfigurationSerializer
     model_class = MixpanelConfiguration
