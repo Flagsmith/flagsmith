b518e6db3a313d3654fba86d53075892e8684713
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-08-01 15:18:37 +0100

    Mask environment json

--- api/integrations/flagsmith/flagsmith_service.py
+++ api/integrations/flagsmith/flagsmith_service.py
@@ -10,11 +10,21 @@ ENVIRONMENT_JSON_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "data/environment.json"
 )
 
+KEEP_ENVIRONMENT_FIELDS = (
+    "name",
+    "feature_states",
+    "use_identity_composite_key_for_hashing",
+)
+KEEP_PROJECT_FIELDS = ("name", "organisation", "hide_disabled_flags")
+KEEP_ORGANISATION_FIELDS = (
+    "name",
+    "feature_analytics",
+    "stop_serving_flags",
+    "persist_trait_data",
+)
 
-def update_environment_json(
-    environment_key: str = None,
-    api_url: str = None,
-) -> None:
+
+def update_environment_json(environment_key: str = None, api_url: str = None) -> None:
     environment_key = environment_key or settings.FLAGSMITH_ON_FLAGSMITH_SERVER_KEY
     api_url = api_url or settings.FLAGSMITH_ON_FLAGSMITH_SERVER_API_URL
 
@@ -27,5 +37,41 @@ def update_environment_json(
             f"Couldn't get defaults from Flagsmith. Got {response.status_code} response."
         )
 
+    environment_json = _get_masked_environment_data(response.json())
+    sorted_environment_json = dict(sorted(environment_json.items()))
+
     with open(ENVIRONMENT_JSON_PATH, "w+") as defaults:
-        defaults.write(json.dumps(dict(sorted(response.json().items()))))
+        defaults.write(json.dumps(sorted_environment_json, indent=2))
+
+
+def _get_masked_environment_data(environment_document: dict) -> dict:
+    """
+    Return a cut down / masked version of the environment
+    document which can be committed to VCS.
+    """
+
+    project_json = environment_document.pop("project")
+    organisation_json = project_json.pop("organisation")
+
+    return {
+        "id": 0,
+        "api_key": "masked",
+        **{
+            k: v
+            for k, v in environment_document.items()
+            if k in KEEP_ENVIRONMENT_FIELDS
+        },
+        "project": {
+            "id": 0,
+            **{k: v for k, v in project_json.items() if k in KEEP_PROJECT_FIELDS},
+            "organisation": {
+                "id": 0,
+                **{
+                    k: v
+                    for k, v in organisation_json.items()
+                    if k in KEEP_ORGANISATION_FIELDS
+                },
+            },
+            "segments": [],
+        },
+    }
--- api/tests/unit/integrations/flagsmith/test_unit_flagsmith_service.py
+++ api/tests/unit/integrations/flagsmith/test_unit_flagsmith_service.py
@@ -8,19 +8,70 @@ from integrations.flagsmith.exceptions import FlagsmithIntegrationError
 from integrations.flagsmith.flagsmith_service import update_environment_json
 
 
+@pytest.fixture()
+def environment_document():
+    """
+    An example environment document as returned from the API.
+    """
+    return {
+        "id": 1,
+        "api_key": "some-key",
+        "project": {
+            "id": 1,
+            "name": "Test Project",
+            "organisation": {
+                "id": 1,
+                "name": "Test Organisation",
+                "feature_analytics": False,
+                "stop_serving_flags": False,
+                "persist_trait_data": True,
+            },
+            "hide_disabled_flags": False,
+            "segments": [],
+            "enable_realtime_updates": False,
+            "server_key_only_feature_ids": [],
+        },
+        "feature_states": [
+            {
+                "feature": {"id": 1, "name": "test", "type": "STANDARD"},
+                "enabled": False,
+                "django_id": 1,
+                "feature_segment": None,
+                "featurestate_uuid": "ec33a926-0b7e-4eb7-b02b-bf9df2ffa53e",
+                "feature_state_value": None,
+                "multivariate_feature_state_values": [],
+            }
+        ],
+        "name": "Test",
+        "allow_client_traits": True,
+        "updated_at": "2023-08-01T14:00:36.347565+00:00",
+        "hide_sensitive_data": False,
+        "hide_disabled_flags": None,
+        "use_identity_composite_key_for_hashing": True,
+        "amplitude_config": None,
+        "dynatrace_config": None,
+        "heap_config": None,
+        "mixpanel_config": None,
+        "rudderstack_config": None,
+        "segment_config": None,
+        "webhook_config": None,
+    }
+
+
 @responses.activate
-def test_update_environment_json(settings):
+def test_update_environment_json(settings, environment_document):
+    """
+    Test to verify that, when we call update_environment_json, the response is written
+    to the correct file and that the sensitive data from the response is masked.
+    """
     # Given
     api_url = "https://api.flagsmith.com/api/v1"
     settings.FLAGSMITH_ON_FLAGSMITH_SERVER_API_URL = api_url
 
-    environment_document = {"api_key": "some-key", "name": "test"}
-    environment_json = json.dumps(environment_document)
-
     responses.add(
         method="GET",
         url=f"{api_url}/environment-document",
-        body=environment_json,
+        body=json.dumps(environment_document),
         status=200,
     )
 
@@ -29,7 +80,13 @@ def test_update_environment_json(settings):
         update_environment_json()
 
     # Then
-    mocked_open.return_value.write.assert_called_once_with(environment_json)
+    written_json = json.loads(mocked_open.return_value.write.call_args[0][0])
+    assert written_json["id"] == 0
+    assert written_json["api_key"] == "masked"
+    assert written_json["feature_states"] == environment_document["feature_states"]
+    assert written_json["project"]["id"] == 0
+    assert written_json["project"]["segments"] == []
+    assert written_json["project"]["organisation"]["id"] == 0
 
 
 @responses.activate
