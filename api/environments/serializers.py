bd1101198449688032d4108fa21a937245a518d3
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-08-09 17:41:48 +0100

    Fix metadata validation for patch requests

--- api/environments/serializers.py
+++ api/environments/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 from environments.models import Environment, EnvironmentAPIKey, Webhook
 from features.serializers import FeatureStateSerializerFull
 from metadata.serializers import MetadataSerializer, SerializerWithMetadata
-from organisations.models import Organisation, Subscription
+from organisations.models import Subscription
 from organisations.subscriptions.serializers.mixins import (
     ReadOnlyIfNotValidPlanMixin,
 )
@@ -75,11 +75,15 @@ class EnvironmentSerializerWithMetadata(
     class Meta(EnvironmentSerializerLight.Meta):
         fields = EnvironmentSerializerLight.Meta.fields + ("metadata",)
 
-    def get_organisation_from_validated_data(self, validated_data) -> Organisation:
-        return validated_data.get("project").organisation
+    def get_project(self, validated_data: dict = None) -> Project:
+        if self.instance:
+            return self.instance.project
+        elif "project" in validated_data:
+            return validated_data["project"]
 
-    def get_project_from_validated_data(self, validated_data) -> Project:
-        return validated_data.get("project")
+        raise serializers.ValidationError(
+            "Unable to retrieve project for metadata validation."
+        )
 
 
 class CreateUpdateEnvironmentSerializer(
