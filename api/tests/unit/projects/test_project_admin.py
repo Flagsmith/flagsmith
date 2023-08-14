1e9a2114aadbb5c5b04665126aa11776facaeba9
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-08-14 12:09:13 +0100

    Remove unncessary comment

--- api/tests/unit/projects/test_project_admin.py
+++ api/tests/unit/projects/test_project_admin.py
@@ -1,19 +1,3 @@
-"""
-def test_environment_admin_rebuild_environments(environment, mocker):
-    # GIVEN
-    mocked_rebuild_environment_document = mocker.patch(
-        "environments.admin.rebuild_environment_document"
-    )
-    environment_admin = EnvironmentAdmin(Environment, AdminSite())
-    # WHEN
-    environment_admin.rebuild_environments(
-        request=mocker.MagicMock(), queryset=Environment.objects.all()
-    )
-    # THEN
-    mocked_rebuild_environment_document.delay.assert_called_once_with(
-        args=(environment.id,)
-    )
-"""
 import typing
 from unittest.mock import MagicMock
 
