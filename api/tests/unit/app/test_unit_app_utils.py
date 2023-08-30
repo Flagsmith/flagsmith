f38076c7169a4c31a6cb9710b1c2d6f6a1171700
Ben Rometsch <ben.rometsch@gmail.com>
2023-08-30 14:46:01 +0100

    refactor/remove tag hack for version number

--- api/tests/unit/app/test_unit_app_utils.py
+++ api/tests/unit/app/test_unit_app_utils.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+from unittest import mock
 
 from pytest_mock import MockerFixture
 
@@ -26,8 +27,6 @@ def test_get_version_info(mocker: MockerFixture) -> None:
         "api": "2.66.2",
         "docs": "2.66.2",
     }
-    mock_is_file = mocker.patch("os.path.isfile")
-    mock_is_file.side_effect = (True,)
     mock_get_file_contents = mocker.patch("app.utils._get_file_contents")
     mock_get_file_contents.side_effect = (json.dumps(manifest_mocked_file), "some_sha")
 
@@ -45,4 +44,30 @@ def test_get_version_info(mocker: MockerFixture) -> None:
             "docs": "2.66.2",
             "frontend": "2.66.2",
         },
+    }
+
+
+def test_get_version_info_with_missing_files(mocker: MockerFixture) -> None:
+    # Given
+    mocked_pathlib = mocker.patch("app.utils.pathlib")
+
+    def path_side_effect(file_path: str) -> mocker.MagicMock:
+        mocked_path_object = mocker.MagicMock(spec=pathlib.Path)
+
+        if file_path == "./ENTERPRISE_VERSION":
+            mocked_path_object.exists.return_value = True
+
+        return mocked_path_object
+
+    mocked_pathlib.Path.side_effect = path_side_effect
+    mock.mock_open.side_effect = IOError
+
+    # When
+    result = get_version_info()
+
+    # Then
+    assert result == {
+        "ci_commit_sha": "unknown",
+        "image_tag": "unknown",
+        "is_enterprise": True,
     }
