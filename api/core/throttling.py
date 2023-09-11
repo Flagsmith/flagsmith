9f9fb78c34b5f857710fd67eab805ec1275016d8
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-09-11 13:25:50 +0530

    cache: remove db backend

--- api/core/throttling.py
+++
@@ -1,7 +0,0 @@
-from django.core.cache import caches
-from rest_framework.throttling import UserRateThrottle
-
-
-class DBBackedUserRateThrottle(UserRateThrottle):
-    cache = caches["throttle-cache"]
-    scope = "user"
