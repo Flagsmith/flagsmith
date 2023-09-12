3b32d2fea6c2589084143023664406a0d7f1ac5f
Novak Zaballa <zaballa.novak@gmail.com>
2023-09-12 12:02:16 -0400

    fix: Refactored create logic

--- api/features/multivariate/views.py
+++ api/features/multivariate/views.py
@@ -1,5 +1,5 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -36,12 +36,7 @@ class MultivariateFeatureOptionViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         feature_pk = self.kwargs.get("feature_pk")
         get_object_or_404(Feature, pk=feature_pk)
-
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
 
     def get_queryset(self):
         if getattr(self, "swagger_fake_view", False):
