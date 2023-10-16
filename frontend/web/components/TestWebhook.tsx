af464aaf8b691adcc3c570358a026bcc96e70225
Novak Zaballa <41410593+novakzaballa@users.noreply.github.com>
2023-10-16 09:56:51 -0400

    fix: Logged out of Flagsmith when testing Webhook (#2842)

--- frontend/web/components/TestWebhook.tsx
+++ frontend/web/components/TestWebhook.tsx
@@ -19,7 +19,7 @@ const TestWebhook: FC<TestWebhookType> = ({ json, webhook }) => {
     setLoading(true)
     setSuccess(false)
     data
-      .post(webhook, JSON.parse(json))
+      .post(webhook, JSON.parse(json), null, true)
       .then(() => {
         setLoading(false)
         setSuccess(true)
