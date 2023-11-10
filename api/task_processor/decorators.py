7583e41a1f0c1de618acf77a7225864a5ac085f7
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-11-10 22:50:22 +0000

    fix: run threaded tasks on transaction commit by default  - Wrap threaded task runs in `transaction.on_commit` by default, make it configurable  - Add docs, functioning typing

--- api/task_processor/decorators.py
+++ api/task_processor/decorators.py
@@ -6,6 +6,7 @@ from inspect import getmodule
 from threading import Thread
 
 from django.conf import settings
+from django.db.transaction import on_commit
 from django.utils import timezone
 
 from task_processor.exceptions import InvalidArgumentsError, TaskQueueFullError
@@ -15,14 +16,63 @@ from task_processor.task_run_method import TaskRunMethod
 
 logger = logging.getLogger(__name__)
 
+W = typing.TypeVar("W")
+P = typing.ParamSpec("P")
+WrappedFunction = typing.Callable[P, None]
 
-def register_task_handler(
-    task_name: str = None,
-    queue_size: int = None,
+
+class TaskHandler(typing.Generic[W]):
+    task_identifier: str
+    unwrapped: W
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> None:
+        ...
+
+    # TODO @khvn26 Consider typing `delay` and `run_in_thread` using `ParamSpec`.
+    # This will require a change to methods' signatures.
+
+    def delay(
+        self,
+        *,
+        delay_until: datetime | None = None,
+        args: tuple[typing.Any] = (),
+        kwargs: dict[str, typing.Any] | None = None,
+    ) -> Task | None:
+        ...
+
+    def run_in_thread(
+        *,
+        args: tuple[typing.Any] = (),
+        kwargs: dict[str, typing.Any] | None = None,
+    ) -> None:
+        ...
+
+    def foo(*args: P.args, **kwargs: P.kwargs) -> None:
+        ...
+
+
+def register_task_handler(  # noqa: C901
+    *,
+    task_name: str | None = None,
+    queue_size: int | None = None,
     priority: TaskPriority = TaskPriority.NORMAL,
-):
-    def decorator(f: typing.Callable):
-        nonlocal task_name
+    transaction_on_commit: bool = True,
+) -> typing.Callable[[W], TaskHandler[W]]:
+    """
+    Turn a function into an asynchronous task.
+
+    :param str task_name: task name. Defaults to function name.
+    :param int queue_size: (`TASK_PROCESSOR` task run method only)
+        max queue size for the task. Task runs exceeding the max size get dropped by
+        the task processor Defaults to `None` (infinite).
+    :param TaskPriority priority: task priority.
+    :param bool transaction_on_commit: (`SEPARATE_THREAD` task run method only)
+        Whether to wrap the task call in `transanction.on_commit`. Defaults to `True`.
+    :rtype: TaskProtocol
+    """
+
+    def decorator(f: W) -> TaskHandler[W]:
+        nonlocal task_name, transaction_on_commit
 
         task_name = task_name or f.__name__
         task_module = getmodule(f).__name__.rsplit(".")[-1]
@@ -31,13 +81,13 @@ def register_task_handler(
 
         def delay(
             *,
-            delay_until: datetime = None,
-            args: typing.Tuple = (),
-            kwargs: typing.Dict = None,
-        ) -> typing.Optional[Task]:
+            delay_until: datetime | None = None,
+            args: tuple[typing.Any] = (),
+            kwargs: dict[str, typing.Any] | None = None,
+        ) -> Task | None:
             logger.debug("Request to run task '%s' asynchronously.", task_identifier)
 
-            kwargs = kwargs or dict()
+            kwargs = kwargs or {}
 
             if delay_until and settings.TASK_RUN_METHOD != TaskRunMethod.TASK_PROCESSOR:
                 logger.warning(
@@ -69,12 +119,19 @@ def register_task_handler(
                 task.save()
                 return task
 
-        def run_in_thread(*, args: typing.Tuple = (), kwargs: typing.Dict = None):
+        def run_in_thread(
+            *,
+            args: tuple[typing.Any] = (),
+            kwargs: dict[str, typing.Any] | None = None,
+        ) -> None:
             logger.info("Running function %s in unmanaged thread.", f.__name__)
             _validate_inputs(*args, **kwargs)
-            Thread(target=f, args=args, kwargs=kwargs, daemon=True).start()
+            thread = Thread(target=f, args=args, kwargs=kwargs, daemon=True)
+            if transaction_on_commit:
+                return on_commit(thread.start)
+            return thread.start()
 
-        def _wrapper(*args, **kwargs):
+        def _wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
             """
             Execute the function after validating the arguments. Ensures that, in unit testing,
             the arguments are validated to prevent issues with serialization in an environment
@@ -97,11 +154,11 @@ def register_task_handler(
 
 def register_recurring_task(
     run_every: timedelta,
-    task_name: str = None,
-    args: typing.Tuple = (),
-    kwargs: typing.Dict = None,
-    first_run_time: time = None,
-):
+    task_name: str | None = None,
+    args: tuple[typing.Any] = (),
+    kwargs: dict[str, typing.Any] | None = None,
+    first_run_time: time | None = None,
+) -> typing.Callable[[WrappedFunction], RecurringTask]:
     if not os.environ.get("RUN_BY_PROCESSOR"):
         # Do not register recurring tasks if not invoked by task processor
         return lambda f: f
@@ -118,8 +175,8 @@ def register_recurring_task(
         task, _ = RecurringTask.objects.update_or_create(
             task_identifier=task_identifier,
             defaults={
-                "serialized_args": RecurringTask.serialize_data(args or tuple()),
-                "serialized_kwargs": RecurringTask.serialize_data(kwargs or dict()),
+                "serialized_args": RecurringTask.serialize_data(args or ()),
+                "serialized_kwargs": RecurringTask.serialize_data(kwargs or {}),
                 "run_every": run_every,
                 "first_run_time": first_run_time,
             },
@@ -129,9 +186,9 @@ def register_recurring_task(
     return decorator
 
 
-def _validate_inputs(*args, **kwargs):
+def _validate_inputs(*args: typing.Any, **kwargs: typing.Any) -> None:
     try:
-        Task.serialize_data(args or tuple())
-        Task.serialize_data(kwargs or dict())
+        Task.serialize_data(args or ())
+        Task.serialize_data(kwargs or {})
     except TypeError as e:
         raise InvalidArgumentsError("Inputs are not serializable.") from e
