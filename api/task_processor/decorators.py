76a8eb61ddb67b65615161c9ef2b88c0e732bf3b
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-11-13 00:59:57 +0000

    chore: improve typing

--- api/task_processor/decorators.py
+++ api/task_processor/decorators.py
@@ -14,38 +14,106 @@ from task_processor.models import RecurringTask, Task, TaskPriority
 from task_processor.task_registry import register_task
 from task_processor.task_run_method import TaskRunMethod
 
+P = typing.ParamSpec("P")
+
 logger = logging.getLogger(__name__)
 
-W = typing.TypeVar("W")
-P = typing.ParamSpec("P")
-WrappedFunction = typing.Callable[P, None]
 
+class TaskHandler(typing.Generic[P]):
+    __slots__ = (
+        "unwrapped",
+        "queue_size",
+        "priority",
+        "transaction_on_commit",
+        "task_identifier",
+    )
 
-class TaskHandler(typing.Generic[W]):
-    task_identifier: str
-    unwrapped: W
+    unwrapped: typing.Callable[P, None]
+
+    def __init__(
+        self,
+        f: typing.Callable[P, None],
+        *,
+        task_name: str | None = None,
+        queue_size: int | None = None,
+        priority: TaskPriority = TaskPriority.NORMAL,
+        transaction_on_commit: bool = True,
+    ) -> None:
+        self.unwrapped = f
+        self.queue_size = queue_size
+        self.priority = priority
+        self.transaction_on_commit = transaction_on_commit
+
+        task_name = task_name or f.__name__
+        task_module = getmodule(f).__name__.rsplit(".")[-1]
+        self.task_identifier = task_identifier = f"{task_module}.{task_name}"
+        register_task(task_identifier, f)
 
     def __call__(self, *args: P.args, **kwargs: P.kwargs) -> None:
-        ...
-
-    # TODO @khvn26 Consider typing `delay` and `run_in_thread` using `ParamSpec`.
-    # This will require a change to methods' signatures.
+        _validate_inputs(*args, **kwargs)
+        return self.unwrapped(*args, **kwargs)
 
     def delay(
         self,
         *,
         delay_until: datetime | None = None,
+        # TODO @khvn26 consider typing `args` and `kwargs` with `ParamSpec`
+        # (will require a change to the signature)
         args: tuple[typing.Any] = (),
         kwargs: dict[str, typing.Any] | None = None,
     ) -> Task | None:
-        ...
+        logger.debug("Request to run task '%s' asynchronously.", self.task_identifier)
+
+        kwargs = kwargs or {}
+
+        if delay_until and settings.TASK_RUN_METHOD != TaskRunMethod.TASK_PROCESSOR:
+            logger.warning(
+                "Cannot schedule tasks to run in the future without task processor."
+            )
+            return
+
+        if settings.TASK_RUN_METHOD == TaskRunMethod.SYNCHRONOUSLY:
+            _validate_inputs(*args, **kwargs)
+            self.unwrapped(*args, **kwargs)
+        elif settings.TASK_RUN_METHOD == TaskRunMethod.SEPARATE_THREAD:
+            logger.debug("Running task '%s' in separate thread", self.task_identifier)
+            self.run_in_thread(args=args, kwargs=kwargs)
+        else:
+            logger.debug("Creating task for function '%s'...", self.task_identifier)
+            try:
+                task = Task.create(
+                    task_identifier=self.task_identifier,
+                    scheduled_for=delay_until or timezone.now(),
+                    priority=self.priority,
+                    queue_size=self.queue_size,
+                    args=args,
+                    kwargs=kwargs,
+                )
+            except TaskQueueFullError as e:
+                logger.warning(e)
+                return
+
+            task.save()
+            return task
 
     def run_in_thread(
+        self,
         *,
         args: tuple[typing.Any] = (),
         kwargs: dict[str, typing.Any] | None = None,
     ) -> None:
-        ...
+        _validate_inputs(*args, **kwargs)
+        thread = Thread(target=self.unwrapped, args=args, kwargs=kwargs, daemon=True)
+
+        def _start() -> None:
+            logger.info(
+                "Running function %s in unmanaged thread.", self.unwrapped.__name__
+            )
+            thread.start()
+
+        if self.transaction_on_commit:
+            return on_commit(_start)
+        return _start()
 
 
 def register_task_handler(  # noqa: C901
@@ -54,7 +122,7 @@ def register_task_handler(  # noqa: C901
     queue_size: int | None = None,
     priority: TaskPriority = TaskPriority.NORMAL,
     transaction_on_commit: bool = True,
-) -> typing.Callable[[W], TaskHandler[W]]:
+) -> typing.Callable[[typing.Callable[P, None]], TaskHandler[P]]:
     """
     Turn a function into an asynchronous task.
 
@@ -68,85 +136,16 @@ def register_task_handler(  # noqa: C901
     :rtype: TaskProtocol
     """
 
-    def decorator(f: W) -> TaskHandler[W]:
-        nonlocal task_name, transaction_on_commit
+    def wrapper(f: typing.Callable[P, None]) -> TaskHandler[P]:
+        return TaskHandler(
+            f,
+            task_name=task_name,
+            queue_size=queue_size,
+            priority=priority,
+            transaction_on_commit=transaction_on_commit,
+        )
 
-        task_name = task_name or f.__name__
-        task_module = getmodule(f).__name__.rsplit(".")[-1]
-        task_identifier = f"{task_module}.{task_name}"
-        register_task(task_identifier, f)
-
-        def delay(
-            *,
-            delay_until: datetime | None = None,
-            args: tuple[typing.Any] = (),
-            kwargs: dict[str, typing.Any] | None = None,
-        ) -> Task | None:
-            logger.debug("Request to run task '%s' asynchronously.", task_identifier)
-
-            kwargs = kwargs or {}
-
-            if delay_until and settings.TASK_RUN_METHOD != TaskRunMethod.TASK_PROCESSOR:
-                logger.warning(
-                    "Cannot schedule tasks to run in the future without task processor."
-                )
-                return
-
-            if settings.TASK_RUN_METHOD == TaskRunMethod.SYNCHRONOUSLY:
-                _validate_inputs(*args, **kwargs)
-                f(*args, **kwargs)
-            elif settings.TASK_RUN_METHOD == TaskRunMethod.SEPARATE_THREAD:
-                logger.debug("Running task '%s' in separate thread", task_identifier)
-                run_in_thread(args=args, kwargs=kwargs)
-            else:
-                logger.debug("Creating task for function '%s'...", task_identifier)
-                try:
-                    task = Task.create(
-                        task_identifier=task_identifier,
-                        scheduled_for=delay_until or timezone.now(),
-                        priority=priority,
-                        queue_size=queue_size,
-                        args=args,
-                        kwargs=kwargs,
-                    )
-                except TaskQueueFullError as e:
-                    logger.warning(e)
-                    return
-
-                task.save()
-                return task
-
-        def run_in_thread(
-            *,
-            args: tuple[typing.Any] = (),
-            kwargs: dict[str, typing.Any] | None = None,
-        ) -> None:
-            logger.info("Running function %s in unmanaged thread.", f.__name__)
-            _validate_inputs(*args, **kwargs)
-            thread = Thread(target=f, args=args, kwargs=kwargs, daemon=True)
-            if transaction_on_commit:
-                return on_commit(thread.start)
-            return thread.start()
-
-        def _wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
-            """
-            Execute the function after validating the arguments. Ensures that, in unit testing,
-            the arguments are validated to prevent issues with serialization in an environment
-            that utilises the task processor.
-            """
-            _validate_inputs(*args, **kwargs)
-            return f(*args, **kwargs)
-
-        _wrapper.delay = delay
-        _wrapper.run_in_thread = run_in_thread
-        _wrapper.task_identifier = task_identifier
-
-        # patch the original unwrapped function onto the wrapped version for testing
-        _wrapper.unwrapped = f
-
-        return _wrapper
-
-    return decorator
+    return wrapper
 
 
 def register_recurring_task(
@@ -155,12 +154,12 @@ def register_recurring_task(
     args: tuple[typing.Any] = (),
     kwargs: dict[str, typing.Any] | None = None,
     first_run_time: time | None = None,
-) -> typing.Callable[[WrappedFunction], RecurringTask]:
+) -> typing.Callable[[typing.Callable[..., None]], RecurringTask]:
     if not os.environ.get("RUN_BY_PROCESSOR"):
         # Do not register recurring tasks if not invoked by task processor
         return lambda f: f
 
-    def decorator(f: typing.Callable):
+    def decorator(f: typing.Callable[..., None]) -> RecurringTask:
         nonlocal task_name
 
         task_name = task_name or f.__name__
