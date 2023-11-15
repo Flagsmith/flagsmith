cc2badae9ec124007feb6a8d78e4a6da4b92e7fc
Novak Zaballa <zaballa.novak@gmail.com>
2023-11-15 13:46:21 -0400

    fix: Delete moved tests

--- api/features/tests/test_views.py
+++ api/features/tests/test_views.py
@@ -787,44 +787,6 @@ def test_get_feature_evaluation_data(project, feature, environment, mocker, clie
     )
 
 
-@pytest.mark.parametrize(
-    "client",
-    [lazy_fixture("admin_master_api_key_client"), lazy_fixture("admin_client")],
-)
-def test_create_feature_with_required_metadata_returns_201(
-    project,
-    client,
-    required_a_feature_metadata_field,
-    optional_b_environment_metadata_field,
-):
-    # Given
-    url = reverse("api-v1:environments:environment-list")
-    description = "This is the description"
-    field_value = 10
-    data = {
-        "name": "Test environment",
-        "project": project.id,
-        "description": description,
-        "metadata": [
-            {
-                "model_field": required_a_feature_metadata_field.id,
-                "field_value": field_value,
-            },
-        ],
-    }
-
-    # When
-    response = client.post(url, data=json.dumps(data), content_type="application/json")
-
-    # Then
-    assert response.status_code == status.HTTP_201_CREATED
-    assert (
-        response.json()["metadata"][0]["model_field"]
-        == required_a_feature_metadata_field.field.id
-    )
-    assert response.json()["metadata"][0]["field_value"] == str(field_value)
-
-
 def test_create_segment_override(admin_client, feature, segment, environment):
     # Given
     url = reverse(
