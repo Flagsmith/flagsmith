596731840676b305f2656ef0ec02cb213bd5e2c9
Zach Aysan <zachaysan@gmail.com>
2023-11-16 14:46:30 -0500

    fix: Excessive 404s on subscription metadata (#2985)

--- api/organisations/exceptions.py
+++ api/organisations/exceptions.py
@@ -1,11 +1,6 @@
 from rest_framework.exceptions import APIException
 
 
-class OrganisationHasNoSubscription(APIException):
+class OrganisationHasNoPaidSubscription(APIException):
     status_code = 400
     default_detail = "Organisation has no subscription"
-
-
-class SubscriptionNotFound(APIException):
-    status_code = 404
-    default_detail = "Subscription Not found"
