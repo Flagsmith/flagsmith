2fa5b5c16a23514518c02b71bd49c2f12ffbc715
Zach Aysan <zachaysan@gmail.com>
2023-11-16 16:15:01 +0000

    Remove exception

--- api/organisations/exceptions.py
+++ api/organisations/exceptions.py
@@ -4,8 +4,3 @@ from rest_framework.exceptions import APIException
 class OrganisationHasNoPaidSubscription(APIException):
     status_code = 400
     default_detail = "Organisation has no subscription"
-
-
-class SubscriptionNotFound(APIException):
-    status_code = 404
-    default_detail = "Subscription Not found"
