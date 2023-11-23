7c5c9459bbe5032ed214125f45100954cb6dde8d
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-11-23 13:35:57 +0000

    Add self hosted defaults environment

--- frontend/bin/env.js
+++ frontend/bin/env.js
@@ -5,7 +5,7 @@ require('colors')
 const fs = require('fs-extra')
 const path = require('path')
 
-const env = process.env.ENV || 'dev'
+const env = process.env.ENV || 'selfhosted'
 const src = path.resolve(__dirname, `../env/project_${env}.js`)
 const target = path.resolve(__dirname, '../common/project.js')
 
