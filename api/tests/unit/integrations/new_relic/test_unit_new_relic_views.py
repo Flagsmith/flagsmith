0dc4e14aa10fa4f0401c2cac200e91a390700e28
Zach Aysan <zachaysan@gmail.com>
2023-11-27 15:40:52 -0500

    fix: Move projects and integrations to tests (#3044)

--- api/tests/unit/integrations/new_relic/test_unit_new_relic_views.py
+++ api/tests/unit/integrations/new_relic/test_unit_new_relic_views.py
@@ -143,3 +143,27 @@ class NewRelicConfigurationTestCase(TestCase):
         assert res.status_code == status.HTTP_204_NO_CONTENT
         #  and
         assert not NewRelicConfiguration.objects.filter(project=self.project).exists()
+
+
+def test_create_newrelic_configuration_in_project_with_deleted_configuration(
+    admin_client, project, deleted_newrelic_configuration
+):
+    # Given
+    url = reverse("api-v1:projects:integrations-new-relic-list", args=[project.id])
+
+    api_key, base_url, app_id = "some-key", "https://api.newrelic.com/", "1"
+
+    # When
+    response = admin_client.post(
+        path=url,
+        data=json.dumps({"api_key": api_key, "base_url": base_url, "app_id": app_id}),
+        content_type="application/json",
+    )
+
+    # Then
+    assert response.status_code == status.HTTP_201_CREATED
+
+    response_json = response.json()
+    assert response_json["api_key"] == api_key
+    assert response_json["base_url"] == base_url
+    assert response_json["app_id"] == app_id
