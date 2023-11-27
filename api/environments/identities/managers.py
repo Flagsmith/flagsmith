2a2c6c21bf782c1aafda1bb3f7b8bfd523b9a2c0
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-11-27 12:03:12 +0000

    fix: Avoid N+1 queries in deprecated identities SDK endpoint

--- api/environments/identities/managers.py
+++ api/environments/identities/managers.py
@@ -1,6 +1,32 @@
+from typing import TYPE_CHECKING
+
 from django.db.models import Manager
+
+from integrations.integration import IDENTITY_INTEGRATIONS
+
+if TYPE_CHECKING:
+    from environments.identities.models import Identity
+    from environments.models import Environment
 
 
 class IdentityManager(Manager):
     def get_by_natural_key(self, identifier, environment_api_key):
         return self.get(identifier=identifier, environment__api_key=environment_api_key)
+
+    def get_or_create_for_sdk(
+        self,
+        identifier: str,
+        environment: "Environment",
+    ) -> tuple["Identity", bool]:
+        return (
+            self.select_related(
+                "environment",
+                "environment__project",
+                *[
+                    f"environment__{integration['relation_name']}"
+                    for integration in IDENTITY_INTEGRATIONS
+                ],
+            )
+            .prefetch_related("identity_traits")
+            .get_or_create(identifier=identifier, environment=environment)
+        )
--- api/environments/identities/views.py
+++ api/environments/identities/views.py
@@ -31,10 +31,7 @@ from environments.sdk.serializers import (
     IdentitySerializerWithTraitsAndSegments,
 )
 from features.serializers import SDKFeatureStateSerializer
-from integrations.integration import (
-    IDENTITY_INTEGRATIONS,
-    identify_integrations,
-)
+from integrations.integration import identify_integrations
 from util.views import SDKAPIView
 
 
@@ -112,11 +109,10 @@ class SDKIdentitiesDeprecated(SDKAPIView):
     def get(self, request, identifier, *args, **kwargs):
         # if we have identifier fetch, or create if does not exist
         if identifier:
-            identity, _ = Identity.objects.get_or_create(
+            identity, _ = Identity.objects.get_or_create_for_sdk(
                 identifier=identifier,
                 environment=request.environment,
             )
-
         else:
             return Response(
                 {"detail": "Missing identifier"}, status=status.HTTP_400_BAD_REQUEST
@@ -172,17 +168,9 @@ class SDKIdentities(SDKAPIView):
                 {"detail": "Missing identifier"}
             )  # TODO: add 400 status - will this break the clients?
 
-        identity, _ = (
-            Identity.objects.select_related(
-                "environment",
-                "environment__project",
-                *[
-                    f"environment__{integration['relation_name']}"
-                    for integration in IDENTITY_INTEGRATIONS
-                ],
-            )
-            .prefetch_related("identity_traits")
-            .get_or_create(identifier=identifier, environment=request.environment)
+        identity, _ = Identity.objects.get_or_create_for_sdk(
+            identifier=identifier,
+            environment=request.environment,
         )
         self.identity = identity
 
