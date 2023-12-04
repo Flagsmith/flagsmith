503a501864b88d72ed5a45682195a8d436758fe3
Ben Rometsch <ben.rometsch@gmail.com>
2023-12-04 13:30:13 +0000

    docs/upgrade docusaurus to v3

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,27 +13,14 @@ const config = {
     url: 'https://docs.flagsmith.com',
     baseUrl: '/',
     favicon: 'img/favicon.ico',
-
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
     organizationName: 'Flagsmith',
     projectName: 'flagsmith',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
-
+    plugins: ['@ionic-internal/docusaurus-plugin-tag-manager'],
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            algolia: {
-                // The application ID provided by Algolia
-                appId: 'GZAPCBLIEE',
-                // Public API key: it is safe to commit it
-                apiKey: '00987f2d774d7787dcae25294463294c',
-                indexName: 'flagsmith',
-            },
-            tagManager: {
-                trackingID: 'GTM-5ZV5K5G',
-            },
             prism: {
                 theme: prismThemes.github,
                 darkTheme: prismThemes.dracula,
@@ -51,12 +38,15 @@ const config = {
                     'hcl',
                 ],
             },
-            announcementBar: {
-                id: 'support_us',
-                content: `If you like Flagsmith, give us a star 🌟 on <a target="_blank" rel="noopener noreferrer" href="https://github.com/Flagsmith/flagsmith">GitHub</a> and follow us ❤️ on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/getflagsmith">Twitter</a>`,
-                backgroundColor: '#5d60cc',
-                textColor: '#ffffff',
-                isCloseable: true,
+            algolia: {
+                // The application ID provided by Algolia
+                appId: 'GZAPCBLIEE',
+                // Public API key: it is safe to commit it
+                apiKey: '00987f2d774d7787dcae25294463294c',
+                indexName: 'flagsmith',
+            },
+            tagManager: {
+                trackingID: 'GTM-5ZV5K5G',
             },
             navbar: {
                 title: 'Flagsmith',
@@ -137,6 +127,13 @@ const config = {
                     },
                 ],
                 copyright: `Copyright © ${new Date().getFullYear()} Bullet Train Ltd. Built with Docusaurus.`,
+            },
+            announcementBar: {
+                id: 'support_us',
+                content: `If you like Flagsmith, give us a star 🌟 on <a target="_blank" rel="noopener noreferrer" href="https://github.com/Flagsmith/flagsmith">GitHub</a> and follow us ❤️ on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/getflagsmith">Twitter</a>`,
+                backgroundColor: '#5d60cc',
+                textColor: '#ffffff',
+                isCloseable: true,
             },
         }),
     // Even if you don't use internationalization, you can use this field to set
