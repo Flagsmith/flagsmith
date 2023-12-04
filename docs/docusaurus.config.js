a4781c8d52927df5e5f9b5d844d27020b44483a3
Ben Rometsch <ben.rometsch@gmail.com>
2023-12-04 13:27:38 +0000

    docs/upgrade docusaurus to v3

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -39,18 +39,10 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    sidebarPath: './sidebars.js',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    routeBasePath: '/',
                     // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/flagsmith/flagsmith/tree/main/docs/',
                 },
                 theme: {
                     customCss: './src/css/custom.css',
