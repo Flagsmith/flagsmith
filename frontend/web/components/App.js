ebce10d6e2e251da3f777b746b149d8d6aff7f00
Novak Zaballa <zaballa.novak@gmail.com>
2023-12-07 17:03:57 -0400

    feat: Add dunning banner

--- frontend/web/components/App.js
+++ frontend/web/components/App.js
@@ -478,7 +478,12 @@ const App = class extends Component {
                   )}
                   {isMobile && pageHasAside && asideIsVisible ? null : (
                     <div>
-                      <ButterBar />
+                      <ButterBar
+                        billingStatus={
+                          AccountStore.getOrganisation()?.subscription
+                            .billing_status
+                        }
+                      />
                       {projectNotLoaded ? (
                         <div className='text-center'>
                           <Loader />
