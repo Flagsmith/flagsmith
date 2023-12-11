c77554f83fe183dd0760ebe13aa0562e061cf3be
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2023-12-11 18:10:37 +0530

    refac: initialize boto in function scope

--- api/app/settings/test.py
+++ api/app/settings/test.py
@@ -1,5 +1,3 @@
-import os
-
 from app.settings.common import *  # noqa
 from app.settings.common import REST_FRAMEWORK
 
@@ -15,10 +13,3 @@ REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
 }
 
 AWS_SSE_LOGS_BUCKET_NAME = "test_bucket"
-
-# used by moto # ref https://github.com/getmoto/moto/issues/5941
-os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-os.environ["AWS_SECURITY_TOKEN"] = "testing"
-os.environ["AWS_SESSION_TOKEN"] = "testing"
-os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
