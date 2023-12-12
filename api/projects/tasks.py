a69766fae2cca351e1fe04b44d82e53b0cd4a721
Kim Gustyr <kim.gustyr@flagsmith.com>
2023-12-12 14:10:41 +0000

    feat: Write migrated environments to v2 table

--- api/projects/tasks.py
+++ api/projects/tasks.py
@@ -4,14 +4,14 @@ from task_processor.decorators import register_task_handler
 
 
 @register_task_handler()
-def write_environments_to_dynamodb(project_id: int):
+def write_environments_to_dynamodb(project_id: int) -> None:
     from environments.models import Environment
 
     Environment.write_environments_to_dynamodb(project_id=project_id)
 
 
 @register_task_handler()
-def migrate_project_environments_to_v2(project_id: int):
+def migrate_project_environments_to_v2(project_id: int) -> None:
     from environments.dynamodb.services import migrate_environments_to_v2
     from projects.models import IdentityOverridesV2MigrationStatus, Project
 
