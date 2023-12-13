7a20e16ce77928c8e37702f4af561d71a89878f4
Kyle Johnson <kyle@solidstategroup.com>
2023-12-13 19:08:47 +0000

    chore: Improve admin create project settings flow (#3160)

--- frontend/web/components/pages/ProjectSelectPage.js
+++ frontend/web/components/pages/ProjectSelectPage.js
@@ -143,14 +143,18 @@ const ProjectSelectPage = class extends Component {
                               <div className='col-md-6 col-xl-3'>
                                 <Permission
                                   level='organisation'
-                                  permission='CREATE_PROJECT'
+                                  permission={Utils.getCreateProjectPermission(
+                                    AccountStore.getOrganisation(),
+                                  )}
                                   id={AccountStore.getOrganisation().id}
                                 >
                                   {({ permission }) => {
                                     return Utils.renderWithPermission(
                                       permission,
-                                      Constants.environmentPermissions(
-                                        'Create Project',
+                                      Constants.organisationPermissions(
+                                        Utils.getCreateProjectPermissionDescription(
+                                          AccountStore.getOrganisation(),
+                                        ),
                                       ),
                                       <Button
                                         disabled={!permission}
@@ -210,14 +214,18 @@ const ProjectSelectPage = class extends Component {
                         <div>
                           <Permission
                             level='organisation'
-                            permission='CREATE_PROJECT'
+                            permission={Utils.getCreateProjectPermission(
+                              AccountStore.getOrganisation(),
+                            )}
                             id={AccountStore.getOrganisation().id}
                           >
                             {({ permission }) => {
                               return Utils.renderWithPermission(
                                 permission,
-                                Constants.environmentPermissions(
-                                  'Create Project',
+                                Constants.organisationPermissions(
+                                  Utils.getCreateProjectPermissionDescription(
+                                    AccountStore.getOrganisation(),
+                                  ),
                                 ),
                                 <div className='col-md-6 col-xl-3'>
                                   <Button
