08f41fe6ba82aae1a1d601dca776550ac16a8098
Zach Aysan <zachaysan@gmail.com>
2024-01-08 15:40:50 +0000

    Switch to mixins and use query serializer

--- api/app_analytics/split_testing/views.py
+++ api/app_analytics/split_testing/views.py
@@ -1,37 +1,40 @@
 from django.db.models import QuerySet
-from django.http import Http404
-from rest_framework import viewsets
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import mixins, viewsets
 
 from environments.authentication import EnvironmentKeyAuthentication
 from environments.permissions.permissions import EnvironmentKeyPermissions
 
 from .models import SplitTest
 from .permissions import SplitTestPermissions
-from .serializers import ConversionEventSerializer, SplitTestSerializer
+from .serializers import (
+    ConversionEventSerializer,
+    SplitTestQuerySerializer,
+    SplitTestSerializer,
+)
 
 
-class ConversionEventViewSet(viewsets.ModelViewSet):
+class ConversionEventViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     permission_classes = (EnvironmentKeyPermissions,)
     authentication_classes = (EnvironmentKeyAuthentication,)
     serializer_class = ConversionEventSerializer
-    http_method_names = ["post"]
 
 
-class SplitTestViewSet(viewsets.ModelViewSet):
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(query_serializer=SplitTestQuerySerializer()),
+)
+class SplitTestViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     permission_classes = (SplitTestPermissions,)
     serializer_class = SplitTestSerializer
-    http_method_names = ["get"]
 
     def get_queryset(self) -> QuerySet[SplitTest]:
-        environment_id = self.request.query_params.get("environment_id")
-        if not environment_id:
-            raise Http404(
-                "Unable to find split test without environment_id query param set"
-            )
-
-        qs = SplitTest.objects.filter(environment_id=environment_id).select_related(
-            "feature", "multivariate_feature_option"
-        )
+        query_serializer = SplitTestQuerySerializer(data=self.request.query_params)
+        query_serializer.is_valid(raise_exception=True)
+        qs = SplitTest.objects.filter(
+            environment_id=query_serializer.validated_data["environment_id"]
+        ).select_related("feature", "multivariate_feature_option")
 
         # In order to keep split test results in the same order
         # between refreshes, order first by the feature name then
