b7ecd75810d0d98221b775a7f87ba1e73b98647a
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2024-01-08 17:40:22 +0530

    feat(dynamo_documents): propagate delete to dynamo  (#3220)

--- api/tests/unit/environments/dynamodb/wrappers/test_unit_dynamo_environment_wrapper.py
+++ api/tests/unit/environments/dynamodb/wrappers/test_unit_dynamo_environment_wrapper.py
@@ -1,5 +1,6 @@
 import pytest
 from django.core.exceptions import ObjectDoesNotExist
+from mypy_boto3_dynamodb.service_resource import Table
 
 from environments.dynamodb import DynamoEnvironmentWrapper
 from environments.models import Environment
@@ -59,3 +60,19 @@ def test_get_item_raises_object_does_not_exists_if_get_item_does_not_return_any_
     # Then
     with pytest.raises(ObjectDoesNotExist):
         dynamo_environment_wrapper.get_item(api_key)
+
+
+def test_delete_environment__removes_environment_document_from_dynamodb(
+    dynamo_enabled_project_environment_one_document: dict,
+    dynamo_environment_wrapper: DynamoEnvironmentWrapper,
+    flagsmith_environment_table: Table,
+):
+    # Given
+    api_key = dynamo_enabled_project_environment_one_document["api_key"]
+    assert flagsmith_environment_table.scan()["Count"] == 1
+
+    # When
+    dynamo_environment_wrapper.delete_environment(api_key)
+
+    # Then
+    assert flagsmith_environment_table.scan()["Count"] == 0
