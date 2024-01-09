830f6d822217cb6d7b252a2f1768152f5d0d3ef4
Zach Aysan <zachaysan@gmail.com>
2024-01-09 16:40:29 +0000

    Add split test creation and update tasks and add feature control for multivariate

--- api/tests/unit/app_analytics/split_testing/test_unit_split_testing_tasks.py
+++ api/tests/unit/app_analytics/split_testing/test_unit_split_testing_tasks.py
@@ -11,7 +11,7 @@ from features.value_types import STRING
 from projects.models import Project
 
 
-def test_update_split_tests(
+def test_update_split_tests_with_new_split_tests(
     settings: SettingsWrapper,
     project: Project,
     environment: Environment,
@@ -41,19 +41,19 @@ def test_update_split_tests(
     )
     MultivariateFeatureOption.objects.create(
         feature=feature1,
-        default_percentage_allocation=70,
+        default_percentage_allocation=30,
         type=STRING,
         string_value="mv_feature_option2",
     )
     MultivariateFeatureOption.objects.create(
         feature=feature2,
-        default_percentage_allocation=50,
+        default_percentage_allocation=20,
         type=STRING,
         string_value="mv_feature_option3",
     )
     MultivariateFeatureOption.objects.create(
         feature=feature2,
-        default_percentage_allocation=50,
+        default_percentage_allocation=15,
         type=STRING,
         string_value="mv_feature_option4",
     )
@@ -131,8 +131,112 @@ def test_update_split_tests(
 
     # Then
     split_tests = SplitTest.objects.all()
-    assert len(split_tests) == 4
+
+    assert len(split_tests) == 6
     assert sum([st.evaluation_count for st in split_tests]) == 5
     assert sum([st.conversion_count for st in split_tests]) == 2
     for st in split_tests:
-        assert st.pvalue == 1.0
+        assert st.pvalue > 0.9
+
+
+def test_update_split_tests_with_existing_split_tests(
+    settings: SettingsWrapper,
+    project: Project,
+    environment: Environment,
+) -> None:
+    # Given
+    settings.USE_POSTGRES_FOR_ANALYTICS = True
+
+    feature1 = Feature.objects.create(
+        name="1",
+        project=project,
+        initial_value="200",
+        is_server_key_only=True,
+        default_enabled=True,
+    )
+
+    mvfo1 = MultivariateFeatureOption.objects.create(
+        feature=feature1,
+        default_percentage_allocation=30,
+        type=STRING,
+        string_value="mv_feature_option1",
+    )
+    mvfo2 = MultivariateFeatureOption.objects.create(
+        feature=feature1,
+        default_percentage_allocation=30,
+        type=STRING,
+        string_value="mv_feature_option2",
+    )
+
+    identity1 = Identity.objects.create(
+        identifier="test-identity-1", environment=environment
+    )
+    identity2 = Identity.objects.create(
+        identifier="test-identity-2", environment=environment
+    )
+
+    FeatureEvaluationRaw.objects.create(
+        feature_name=feature1.name,
+        environment_id=environment.id,
+        evaluation_count=2,
+        identity_identifier=identity1.identifier,
+    )
+
+    FeatureEvaluationRaw.objects.create(
+        feature_name=feature1.name,
+        environment_id=environment.id,
+        evaluation_count=1,
+        identity_identifier=identity2.identifier,
+    )
+
+    ConversionEvent.objects.create(
+        environment=environment,
+        identity=identity1,
+    )
+
+    # Create some split test objects to replace.
+    split_test1 = SplitTest.objects.create(
+        environment=environment,
+        feature=feature1,
+        multivariate_feature_option_id=None,
+        evaluation_count=0,
+        conversion_count=0,
+        pvalue=1.0,
+    )
+    split_test2 = SplitTest.objects.create(
+        environment=environment,
+        feature=feature1,
+        multivariate_feature_option_id=mvfo1.id,
+        evaluation_count=0,
+        conversion_count=0,
+        pvalue=1.0,
+    )
+    split_test3 = SplitTest.objects.create(
+        environment=environment,
+        feature=feature1,
+        multivariate_feature_option_id=mvfo2.id,
+        evaluation_count=0,
+        conversion_count=0,
+        pvalue=1.0,
+    )
+
+    former_split_test_updated_at = split_test1.updated_at
+
+    # When
+    _update_split_tests()
+
+    # Then
+    split_tests = SplitTest.objects.all()
+    assert len(split_tests) == 3
+    assert split_test1 in split_tests
+    assert split_test2 in split_tests
+    assert split_test3 in split_tests
+
+    split_test1.refresh_from_db()
+
+    assert split_test1.updated_at > former_split_test_updated_at
+
+    assert sum([st.evaluation_count for st in split_tests]) == 2
+    assert sum([st.conversion_count for st in split_tests]) == 1
+    for st in split_tests:
+        assert st.pvalue > 0.9
