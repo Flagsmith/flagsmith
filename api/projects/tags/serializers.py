f7ee61ad6065db9b5e855ef0bc782d9d511e0cb5
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-01-10 11:26:31 +0000

    Add new fields to tag serializer

--- api/projects/tags/serializers.py
+++ api/projects/tags/serializers.py
@@ -6,5 +6,13 @@ from projects.tags.models import Tag
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ("id", "label", "color", "description", "project")
-        read_only_fields = ("project",)
+        fields = (
+            "id",
+            "label",
+            "color",
+            "description",
+            "project",
+            "is_permanent",
+            "is_system_tag",
+        )
+        read_only_fields = ("project", "is_system_tag")
