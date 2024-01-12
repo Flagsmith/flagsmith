fc6cb7e4ab7633a06c35a61da189cc682611635d
Zach Aysan <zachaysan@gmail.com>
2024-01-12 14:11:41 +0000

    Update split testing views tests

--- api/tests/unit/app_analytics/split_testing/test_unit_split_testing_views.py
+++ api/tests/unit/app_analytics/split_testing/test_unit_split_testing_views.py
@@ -1,7 +1,11 @@
 import json
 from typing import Callable
 
-from app_analytics.split_testing.models import SplitTest
+from app_analytics.split_testing.models import (
+    ConversionEvent,
+    ConversionEventType,
+    SplitTest,
+)
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -23,7 +27,8 @@ def test_conversion_event(
     # Given
     url = reverse("api-v1:conversion-events-list")
     api_client.credentials(HTTP_X_ENVIRONMENT_KEY=environment.api_key)
-    data = {"identity_identifier": identity.identifier}
+    _type = "paid_plan"
+    data = {"identity_identifier": identity.identifier, "type": _type}
 
     # When
     response = api_client.post(
@@ -33,6 +38,16 @@ def test_conversion_event(
     # Then
     assert response.status_code == status.HTTP_201_CREATED
     assert response.data == data
+
+    assert ConversionEvent.objects.count() == 1
+    assert ConversionEventType.objects.count() == 1
+
+    conversion_event = ConversionEvent.objects.first()
+    assert conversion_event.identity == identity
+    assert conversion_event.environment == environment
+
+    assert conversion_event.type.name == _type
+    assert conversion_event.type.environment == environment
 
 
 def test_conversion_event_unauthorized(
@@ -54,13 +69,72 @@ def test_conversion_event_unauthorized(
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
+def test_conversion_event_type_list_forbidden(
+    staff_client: APIClient,
+    environment: Environment,
+) -> None:
+    url = reverse("api-v1:conversion-event-types")
+    url += f"?environment_id={environment.id}"
+
+    # When
+    # Staff is missing VIEW_ENVIRONMENT permission
+    response = staff_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_conversion_event_type_list(
+    staff_client: APIClient,
+    environment: Environment,
+    with_environment_permissions: Callable[[list[str], int], None],
+    project: Project,
+) -> None:
+    # Create two target CETs.
+    ConversionEventType.objects.create(
+        environment=environment,
+        name="free_plan",
+    )
+    ConversionEventType.objects.create(
+        environment=environment,
+        name="paid_plan",
+    )
+
+    unrelated_environment = Environment.objects.create(
+        project=project, name="unrelated environment"
+    )
+
+    # Create an unrelated CET.
+    ConversionEventType.objects.create(
+        environment=unrelated_environment,
+        name="free_plan",
+    )
+
+    with_environment_permissions([VIEW_ENVIRONMENT])
+    url = reverse("api-v1:conversion-event-types")
+    url += f"?environment_id={environment.id}"
+
+    # When
+    response = staff_client.get(url)
+
+    # Then
+    assert response.data["count"] == 2
+    assert response.data["results"][0]["name"] == "free_plan"
+    assert response.data["results"][1]["name"] == "paid_plan"
+
+
 def test_split_test_list_forbidden(
     staff_client: APIClient,
     environment: Environment,
 ) -> None:
     # Given
+    cet = ConversionEventType.objects.create(
+        environment=environment,
+        name="free_plan",
+    )
+
     url = reverse("api-v1:split-tests-list")
-    url = f"{url}?environment_id={environment.id}"
+    url = f"{url}?conversion_event_type_id={cet.id}"
 
     # When
     response = staff_client.get(url)
@@ -80,7 +154,7 @@ def test_split_test_list(
     feature = Feature.objects.create(
         name="single_feature_for_test",
         project=project,
-        initial_value="200",
+        initial_value="feature",
         is_server_key_only=True,
         default_enabled=True,
     )
@@ -104,7 +178,13 @@ def test_split_test_list(
         string_value="mv_feature_option2",
     )
 
+    cet1 = ConversionEventType.objects.create(
+        environment=environment,
+        name="free_plan",
+    )
+
     SplitTest.objects.create(
+        conversion_event_type=cet1,
         environment=environment,
         feature=feature,
         multivariate_feature_option=None,
@@ -114,6 +194,7 @@ def test_split_test_list(
     )
 
     SplitTest.objects.create(
+        conversion_event_type=cet1,
         environment=environment,
         feature=feature,
         multivariate_feature_option=multivariate_feature_option1,
@@ -123,6 +204,7 @@ def test_split_test_list(
     )
 
     SplitTest.objects.create(
+        conversion_event_type=cet1,
         environment=environment,
         feature=feature,
         multivariate_feature_option=multivariate_feature_option2,
@@ -132,7 +214,12 @@ def test_split_test_list(
     )
 
     # Create unrelated environment split test for filtering.
+    cet2 = ConversionEventType.objects.create(
+        environment=environment2,
+        name="free_plan",
+    )
     SplitTest.objects.create(
+        conversion_event_type=cet2,
         environment=environment2,
         feature=feature,
         multivariate_feature_option=multivariate_feature_option1,
@@ -143,8 +230,8 @@ def test_split_test_list(
 
     with_environment_permissions([VIEW_ENVIRONMENT])
     url = reverse("api-v1:split-tests-list")
-    url = f"{url}?environment_id={environment.id}"
-    expected_query_count = 6
+    url = f"{url}?conversion_event_type_id={cet1.id}"
+    expected_query_count = 9
 
     # When
     with django_assert_num_queries(expected_query_count):
@@ -158,30 +245,33 @@ def test_split_test_list(
     assert result1["evaluation_count"] == 123
     assert result1["feature"]["id"] == feature.id
     assert result1["feature"]["name"] == feature.name
-    assert result1["multivariate_feature_option"] is None
+    assert result1["value_data"] == {
+        "boolean_value": None,
+        "integer_value": None,
+        "string_value": "feature",
+        "type": "unicode",
+    }
 
     result2 = response.data["results"][1]
     assert result2["conversion_count"] == 10
     assert result2["evaluation_count"] == 100
     assert result2["feature"]["id"] == feature.id
     assert result2["feature"]["name"] == feature.name
-    assert (
-        result2["multivariate_feature_option"]["id"] == multivariate_feature_option1.id
-    )
-    assert result2["multivariate_feature_option"]["type"] == "unicode"
-    assert (
-        result2["multivariate_feature_option"]["string_value"] == "mv_feature_option1"
-    )
+    assert result2["value_data"] == {
+        "boolean_value": None,
+        "integer_value": None,
+        "string_value": "mv_feature_option1",
+        "type": "unicode",
+    }
 
     result3 = response.data["results"][2]
     assert result3["conversion_count"] == 20
     assert result3["evaluation_count"] == 111
     assert result3["feature"]["id"] == feature.id
     assert result3["feature"]["name"] == feature.name
-    assert (
-        result3["multivariate_feature_option"]["id"] == multivariate_feature_option2.id
-    )
-    assert result3["multivariate_feature_option"]["type"] == "unicode"
-    assert (
-        result3["multivariate_feature_option"]["string_value"] == "mv_feature_option2"
-    )
+    assert result3["value_data"] == {
+        "boolean_value": None,
+        "integer_value": None,
+        "string_value": "mv_feature_option2",
+        "type": "unicode",
+    }
