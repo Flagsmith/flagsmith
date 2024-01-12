b5d8eccc10b4d3d2277579c515ae6094fc902ddd
Zach Aysan <zachaysan@gmail.com>
2024-01-12 14:37:12 +0000

    Change split test permission check and add ConversionEventTypePermissions

--- api/app_analytics/split_testing/permissions.py
+++ api/app_analytics/split_testing/permissions.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.viewsets import ModelViewSet
@@ -5,7 +7,10 @@ from rest_framework.viewsets import ModelViewSet
 from environments.models import Environment
 from environments.permissions.constants import VIEW_ENVIRONMENT
 
-from .serializers import SplitTestQuerySerializer
+from .serializers import (
+    ConversionEventTypeQuerySerializer,
+    SplitTestQuerySerializer,
+)
 
 
 class SplitTestPermissions(IsAuthenticated):
@@ -16,12 +21,29 @@ class SplitTestPermissions(IsAuthenticated):
         query_serializer = SplitTestQuerySerializer(data=request.query_params)
         query_serializer.is_valid(raise_exception=True)
 
-        environment = Environment.objects.filter(
-            id=query_serializer.validated_data["environment_id"]
-        ).first()
+        environment = get_object_or_404(
+            Environment,
+            conversion_event_types__id=query_serializer.validated_data[
+                "conversion_event_type_id"
+            ],
+        )
 
-        if environment is None:
+        return request.user.has_environment_permission(
+            permission=VIEW_ENVIRONMENT, environment=environment
+        )
+
+
+class ConversionEventTypePermissions(IsAuthenticated):
+    def has_permission(self, request: Request, view: ListAPIView) -> bool:
+        if not super().has_permission(request, view):
             return False
+
+        query_serializer = ConversionEventTypeQuerySerializer(data=request.query_params)
+        query_serializer.is_valid(raise_exception=True)
+
+        environment = get_object_or_404(
+            Environment, id=query_serializer.validated_data["environment_id"]
+        )
 
         return request.user.has_environment_permission(
             permission=VIEW_ENVIRONMENT, environment=environment
