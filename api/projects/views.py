e9b35ec75b82fbd3770dd253a5367593b986e1a8
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-02-01 14:10:34 +0000

    Revert minor change

--- api/projects/views.py
+++ api/projects/views.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.conf import settings
 from django.utils.decorators import method_decorator
 from drf_yasg import openapi
