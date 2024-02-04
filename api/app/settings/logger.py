16a7a8e64891a1c0dbcd64f7a217e807716a7eea
Alek Jouharyan <alek.jouharyan@delinea.com>
2024-02-04 15:12:33 -0800

    pull in latest and run pre-commit

--- api/app/settings/logger.py
+++ api/app/settings/logger.py
@@ -1,21 +1,22 @@
-import logging
 import json
+import logging
 
 
 class JsonFormatter(logging.Formatter):
     """Custom formatter for json logs."""
+
     def format(self, record):
         try:
-            log_message = record.msg.replace('"', "'").replace('%s', '{}')
+            log_message = record.msg.replace('"', "'").replace("%s", "{}")
             formatted_message = log_message.format(*record.args)
             log_record = {
-                'levelname': record.levelname,
-                'message': formatted_message,
-                'timestamp': self.formatTime(record, self.datefmt),
-                'logger_name': record.name,
-                'process_id': record.process,
-                'thread_name': record.threadName,
+                "levelname": record.levelname,
+                "message": formatted_message,
+                "timestamp": self.formatTime(record, self.datefmt),
+                "logger_name": record.name,
+                "process_id": record.process,
+                "thread_name": record.threadName,
             }
             return json.dumps(log_record)
         except Exception as e:
-            return f'Error formatting log record: {str(e)}'
+            return f"Error formatting log record: {str(e)}"
