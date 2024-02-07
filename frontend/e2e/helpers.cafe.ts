e3bc6b01ee055687d70d4ef9b0111746611624aa
Novak Zaballa <zaballa.novak@gmail.com>
2024-02-07 17:54:18 -0400

    test: Solve flag and segments flaky tests

--- frontend/e2e/helpers.cafe.ts
+++ frontend/e2e/helpers.cafe.ts
@@ -295,6 +295,10 @@ export const createFeature = async (
   description = 'description',
 ) => {
   await gotoFeatures()
+  await t.eval(() => {
+    window.scrollBy(0, 15000)
+  })
+
   await click('#show-create-feature-btn')
   await setText(byId('featureID'), name)
   await setText(byId('featureDesc'), description)
