c60d1117532c7aabaaeed2c574bcc34e3a11169e
kyle-ssg <kyle@solidstategroup.com>
2024-02-07 11:11:31 +0000

    Fix incorrect id

--- frontend/web/components/base/forms/Checkbox.tsx
+++ frontend/web/components/base/forms/Checkbox.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ReactMarkdown from 'react-markdown'
 import Icon from 'components/Icon'
+import Utils from 'common/utils/utils'
 
 interface CheckboxProps {
   label: string
   onChange: (value: boolean) => void
   checked: boolean
+  id?: string
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ checked, label, onChange }) => {
+const Checkbox: React.FC<CheckboxProps> = ({
+  checked,
+  id,
+  label,
+  onChange,
+}) => {
+  const idRef = useRef(id || Utils.GUID())
   const handleChange = () => {
     onChange(!checked)
   }
 
   return (
     <>
-      <input type='checkbox' />
+      <input id={idRef.current} type='checkbox' />
       <label
         onClick={handleChange}
         className='mb-0'
-        htmlFor='mailinglist'
+        htmlFor={idRef.current}
         style={{ display: 'inline' }}
       >
         <span className='checkbox mr-2'>
