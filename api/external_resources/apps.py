bbe10661c5054cfa40780a93cb2a276891f92219
Novak Zaballa <zaballa.novak@gmail.com>
2024-03-05 12:29:36 -0400

    Change signals to hooks

--- api/external_resources/apps.py
+++ api/external_resources/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class ExternalResourcesConfig(AppConfig):
     name = "external_resources"
-
-    def ready(self):
-        # noinspection PyUnresolvedReferences
-        import external_resources.signals  # noqa
--- api/external_resources/models.py
+++ api/external_resources/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django_lifecycle import (
+    AFTER_SAVE,
+    BEFORE_DELETE,
+    LifecycleModelMixin,
+    hook,
+)
 
-from features.models import Feature
+from features.models import Feature, FeatureState
+from integrations.github.tasks import call_github_app_webhook_for_feature_state
+from webhooks.webhooks import WebhookEventType
 
 
-class ExternalResources(models.Model):
+class ExternalResources(LifecycleModelMixin, models.Model):
     url = models.URLField()
     type = models.TextField()
     status = models.TextField(null=True)
@@ -12,6 +20,82 @@ class ExternalResources(models.Model):
         related_name="features",
         on_delete=models.CASCADE,
     )
+
+    @hook(AFTER_SAVE)
+    def trigger_feature_external_resource_added_hook(self):
+        if hasattr(self.feature.project.organisation, "github_config"):
+            github_configuration = self.feature.project.organisation.github_config
+
+            feature_states = FeatureState.objects.filter(feature_id=self.feature_id)
+            feature_data = {
+                "id": self.feature_id,
+                "name": self.feature.name,
+                "feature_states": [],
+            }
+            feature_data["installation_id"] = github_configuration.installation_id
+            feature_data["organisation_id"] = github_configuration.organisation.id
+
+            for feature_state in feature_states:
+                feature_env_data = {}
+                if feature_state.feature_state_value.string_value is not None:
+                    feature_env_data[
+                        "string_value"
+                    ] = feature_state.feature_state_value.string_value
+                if feature_state.feature_state_value.boolean_value is not None:
+                    feature_env_data[
+                        "boolean_value"
+                    ] = feature_state.feature_state_value.boolean_value
+                if feature_state.feature_state_value.integer_value is not None:
+                    feature_env_data[
+                        "integer_value"
+                    ] = feature_state.feature_state_value.integer_value
+
+                feature_env_data["environment_name"] = feature_state.environment.name
+                feature_env_data["feature_value"] = feature_state.enabled
+                if (
+                    hasattr(feature_state, "feature_segment")
+                    and feature_state.feature_segment is not None
+                ):
+                    feature_env_data[
+                        "segment_name"
+                    ] = feature_state.feature_segment.segment.name
+                feature_data["feature_states"].append(feature_env_data)
+
+            call_github_app_webhook_for_feature_state.delay(
+                args=(
+                    feature_data,
+                    WebhookEventType.FEATURE_EXTERNAL_RESOURCE_ADDED.value,
+                ),
+            )
+        else:
+            organisation_id = self.feature.project.organisation.id
+            print(
+                f"No GitHub integration exists for organisation {organisation_id}. Not calling webhooks."
+            )
+
+    @hook(BEFORE_DELETE)
+    def trigger_feature_external_resource_removed_hook(self):
+        if hasattr(self.feature.project.organisation, "github_config"):
+            github_configuration = self.feature.project.organisation.github_config
+            feature_data = {
+                "id": self.feature_id,
+                "name": self.feature.name,
+                "url": self.url,
+            }
+            feature_data["installation_id"] = github_configuration.installation_id
+            feature_data["organisation_id"] = github_configuration.organisation.id
+
+            call_github_app_webhook_for_feature_state.delay(
+                args=(
+                    feature_data,
+                    WebhookEventType.FEATURE_EXTERNAL_RESOURCE_REMOVED.value,
+                ),
+            )
+        else:
+            organisation_id = self.feature.project.organisation.id
+            print(
+                f"No GitHub integration exists for organisation {organisation_id}. Not calling webhooks."
+            )
 
     class Meta:
         ordering = ("id",)
