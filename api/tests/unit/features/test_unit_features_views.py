39e81c46870914a6d30e8281aa830cc12de9f25f
Zach Aysan <zachaysan@gmail.com>
2024-03-05 19:41:04 +0000

    Create test for pulling list of features

--- api/tests/unit/features/test_unit_features_views.py
+++ api/tests/unit/features/test_unit_features_views.py
@@ -38,6 +38,7 @@ from features.models import (
     FeatureStateValue,
 )
 from features.multivariate.models import MultivariateFeatureOption
+from features.value_types import STRING
 from features.versioning.models import EnvironmentFeatureVersion
 from organisations.models import Organisation, OrganisationRole
 from permissions.models import PermissionModel
@@ -2508,6 +2509,88 @@ def test_list_features_with_intersection_tag(
 
     assert response.data["results"][0]["id"] == feature2.id
     assert response.data["results"][0]["tags"] == [tag1.id, tag2.id]
+
+
+def test_list_features_with_multivariate(
+    staff_client: APIClient,
+    project: Project,
+    feature: Feature,
+    with_project_permissions: WithProjectPermissionsCallable,
+    environment: Environment,
+) -> None:
+    # Given
+    with_project_permissions([VIEW_PROJECT])
+    feature2 = Feature.objects.create(
+        name="another_feature", project=project, initial_value="initial_value"
+    )
+    feature3 = Feature.objects.create(
+        name="missing_feature", project=project, initial_value="gone"
+    )
+
+    Environment.objects.create(
+        name="Out of test scope environment",
+        project=project,
+    )
+
+    feature_state1 = feature.feature_states.filter(environment=environment).first()
+    feature_state1.enabled = True
+    feature_state1.save()
+
+    feature_state_value1 = feature_state1.feature_state_value
+    feature_state_value1.string_value = None
+    feature_state_value1.integer_value = 1945
+    feature_state_value1.save()
+
+    feature_state2 = feature2.feature_states.filter(environment=environment).first()
+    feature_state2.enabled = True
+    feature_state2.save()
+
+    feature_state_value2 = feature_state2.feature_state_value
+    feature_state_value2.string_value = None
+    feature_state_value2.boolean_value = True
+    feature_state_value2.save()
+
+    feature_state_value3 = (
+        feature3.feature_states.filter(environment=environment)
+        .first()
+        .feature_state_value
+    )
+    feature_state_value3.string_value = "present"
+    feature_state_value3.save()
+
+    MultivariateFeatureOption.objects.create(
+        feature=feature2,
+        default_percentage_allocation=30,
+        type=STRING,
+        string_value="mv_feature_option1",
+    )
+    MultivariateFeatureOption.objects.create(
+        feature=feature2,
+        default_percentage_allocation=70,
+        type=STRING,
+        string_value="mv_feature_option2",
+    )
+
+    base_url = reverse("api-v1:projects:project-features-list", args=[project.id])
+    url = f"{base_url}?environment={environment.id}"
+
+    # When
+    response = staff_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_200_OK
+
+    assert len(response.data["results"]) == 3
+    results = response.data["results"]
+    assert results[0]["feature_state"]["enabled"] is True
+    assert results[0]["feature_state"]["multivariate_feature_state_values"] == []
+    assert results[0]["name"] == feature.name
+    assert results[1]["feature_state"]["enabled"] is True
+    assert len(results[1]["feature_state"]["multivariate_feature_state_values"]) == 2
+    assert results[1]["name"] == feature2.name
+    assert results[2]["feature_state"]["enabled"] is False
+    assert results[2]["feature_state"]["multivariate_feature_state_values"] == []
+    assert results[2]["name"] == feature3.name
 
 
 def test_simple_feature_state_returns_only_latest_versions(
