9d737ad456dbf07596cb0dd7b796a7e3175c1ab8
Kyle Johnson <kyle@solidstategroup.com>
2024-03-13 14:13:18 +0000

    fix: remove feature modal (#3608)

--- frontend/web/components/modals/ConfirmRemoveFeature.tsx
+++ frontend/web/components/modals/ConfirmRemoveFeature.tsx
@@ -62,7 +62,7 @@ const ConfirmRemoveFeature: FC<ConfirmRemoveFeatureType> = ({
 
           <ModalHR />
           <div className='modal-footer'>
-            <Button className='mr-2' theme='secondary' onClick={closeModal}>
+            <Button className='mr-2' theme='secondary' onClick={closeModal2}>
               Cancel
             </Button>
             <Button
