c2c7c7313603bbf8e6d1c617e75b7a48a1bd9795
Shubham Padia <shubhamapadia@gmail.com>
2024-03-25 23:41:53 +0530

    fix: Add Flagsmith signature header when testing webhook.
    Fixes #2786.
    We are trying to create the same signature as the webhook
    in the python code. This commit assumes that the python code
    will use the same approach to create signature for long term.

--- api/webhooks/webhooks.py
+++ api/webhooks/webhooks.py
@@ -153,6 +153,8 @@ def _call_webhook(
     headers = {"content-type": "application/json"}
     json_data = json.dumps(data, sort_keys=True, cls=DjangoJSONEncoder)
     if webhook.secret:
+        # signPayload of frontend/web/components/TestWebHook on the frontend replicates this
+        # exact function, change the function there if this changes.
         signature = sign_payload(json_data, key=webhook.secret)
         headers.update({FLAGSMITH_SIGNATURE_HEADER: signature})
 
@@ -199,6 +201,8 @@ def call_webhook_with_failure_mail_after_retries(
     headers = {"content-type": "application/json"}
     json_data = json.dumps(data, sort_keys=True, cls=DjangoJSONEncoder)
     if webhook.secret:
+        # signPayload of frontend/web/components/TestWebHook on the frontend replicates this
+        # exact function, change the function there if this changes.
         signature = sign_payload(json_data, key=webhook.secret)
         headers.update({FLAGSMITH_SIGNATURE_HEADER: signature})
 
