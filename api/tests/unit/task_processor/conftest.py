b872a6ca568b0541ca46c2f38135739d214b2f10
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-03-27 17:38:22 +0100

    fix: prevent tasks dying from temporary loss of db connection (#3674)

--- api/tests/unit/task_processor/conftest.py
+++ api/tests/unit/task_processor/conftest.py
@@ -1,6 +1,35 @@
+import logging
+import typing
+
 import pytest
 
 
 @pytest.fixture
 def run_by_processor(monkeypatch):
     monkeypatch.setenv("RUN_BY_PROCESSOR", "True")
+
+
+class GetTaskProcessorCaplog(typing.Protocol):
+    def __call__(
+        self, log_level: str | int = logging.INFO
+    ) -> pytest.LogCaptureFixture: ...
+
+
+@pytest.fixture
+def get_task_processor_caplog(
+    caplog: pytest.LogCaptureFixture,
+) -> GetTaskProcessorCaplog:
+    # caplog doesn't allow you to capture logging outputs from loggers that don't
+    # propagate to root. Quick hack here to get the task_processor logger to
+    # propagate.
+    # TODO: look into using loguru.
+
+    def _inner(log_level: str | int = logging.INFO) -> pytest.LogCaptureFixture:
+        task_processor_logger = logging.getLogger("task_processor")
+        task_processor_logger.propagate = True
+        # Assume required level for the logger.
+        task_processor_logger.setLevel(log_level)
+        caplog.set_level(log_level)
+        return caplog
+
+    return _inner
