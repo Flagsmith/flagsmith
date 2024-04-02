37509c6894b7741cca2461b30f569ab23a9373b9
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-04-02 09:54:07 +0100

    fix: Avoid using a Gunicorn config file (#3699)

--- api/scripts/run-docker.sh
+++ api/scripts/run-docker.sh
@@ -22,7 +22,8 @@ function serve() {
              --workers ${GUNICORN_WORKERS:-3} \
              --threads ${GUNICORN_THREADS:-2} \
              --access-logfile $ACCESS_LOG_LOCATION \
-             --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({origin}i)s %({access-control-allow-origin}o)s' \
+             --logger-class ${GUNICORN_LOGGER_CLASS:-'util.logging.GunicornJsonCapableLogger'} \
+             --access-logformat ${ACCESS_LOG_FORMAT:-'%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({origin}i)s %({access-control-allow-origin}o)s'} \
              --keep-alive ${GUNICORN_KEEP_ALIVE:-2} \
              ${STATSD_HOST:+--statsd-host $STATSD_HOST:$STATSD_PORT} \
              ${STATSD_HOST:+--statsd-prefix $STATSD_PREFIX} \
--- api/tests/unit/util/test_logging.py
+++ api/tests/unit/util/test_logging.py
@@ -4,7 +4,7 @@ import logging
 from util.logging import JsonFormatter
 
 
-def test_json_formatter():
+def test_json_formatter__outputs_expected():
     json_formatter = JsonFormatter()
 
     log_record = logging.LogRecord(
@@ -12,8 +12,8 @@ def test_json_formatter():
         level=logging.INFO,
         pathname="test.py",
         lineno=42,
-        msg="This is a test.",
-        args=(),
+        msg="This is a test message with args: %s and %s",
+        args=("arg1", "arg2"),
         exc_info=None,
     )
     formatted_message = json_formatter.format(log_record)
@@ -25,44 +25,4 @@ def test_json_formatter():
     assert "logger_name" in json_message
     assert "process_id" in json_message
     assert "thread_name" in json_message
-
-
-def test_json_formatter_with_old_style_placeholders():
-    json_formatter = JsonFormatter()
-
-    log_record = logging.LogRecord(
-        name="test_logger",
-        level=logging.INFO,
-        pathname="example.py",
-        lineno=42,
-        msg="This is a test with old-style placeholders: %s and %s",
-        args=("arg1", "arg2"),
-        exc_info=None,
-    )
-
-    formatted_message = json_formatter.format(log_record)
-    parsed_json = json.loads(formatted_message)
-    assert (
-        parsed_json["message"]
-        == "This is a test with old-style placeholders: arg1 and arg2"
-    )
-
-
-def test_json_formatter_arguments_with_new_style_placeholders():
-    json_formatter = JsonFormatter()
-    log_record = logging.LogRecord(
-        name="test_logger",
-        level=logging.INFO,
-        pathname="example.py",
-        lineno=42,
-        msg="This is a test with new-style placeholders: {} and {}",
-        args=("arg1", "arg2"),
-        exc_info=None,
-    )
-
-    formatted_message = json_formatter.format(log_record)
-    parsed_json = json.loads(formatted_message)
-    assert (
-        parsed_json["message"]
-        == "This is a test with new-style placeholders: arg1 and arg2"
-    )
+    assert json_message["message"] == "This is a test message with args: arg1 and arg2"
