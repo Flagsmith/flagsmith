0105bf2db4943eeba144ecc3b4944944a3e42a6f
kyle-ssg <kyle@solidstategroup.com>
2024-04-04 09:46:35 +0100

    Fix async feature versioning test

--- frontend/e2e/tests/versioning-tests.ts
+++ frontend/e2e/tests/versioning-tests.ts
@@ -31,7 +31,7 @@ export default async () => {
     await waitForElementVisible(byId('features-page'))
     await click('#env-settings-link')
     await click(byId('enable-versioning'))
-    // Feature versioning is async on the API, we can assume it'll be done quick for this environment.
+    // Feature versioning is async on the API, we can assume it will not take long here.
     await waitAndRefresh(5000)
     await click('#confirm-toggle-feature-btn')
     await waitAndRefresh()
