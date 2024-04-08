5ba3083f88ddd8965d65225105d515cfd1ab9bd0
Kyle Johnson <kyle@solidstategroup.com>
2024-04-08 13:01:49 +0100

    fix: Adjust permissions logic for view / manage groups (#3679)

--- frontend/web/components/pages/OrganisationSettingsPage.js
+++ frontend/web/components/pages/OrganisationSettingsPage.js
@@ -105,7 +105,7 @@ const OrganisationSettingsPage = class extends Component {
   onRemove = () => {
     toast('Your organisation has been removed')
     if (AccountStore.getOrganisation()) {
-      this.context.router.history.replace('/projects')
+      this.context.router.history.replace('/organisation-settings')
     } else {
       this.context.router.history.replace('/create')
     }
@@ -333,8 +333,6 @@ const OrganisationSettingsPage = class extends Component {
     const verifySeatsLimit = Utils.getFlagsmithHasFeature(
       'verify_seats_limit_for_invite_links',
     )
-    const canManageGroups =
-      this.state.permissions.includes('MANAGE_USER_GROUPS')
 
     return (
       <div className='app-container container'>
@@ -348,6 +346,7 @@ const OrganisationSettingsPage = class extends Component {
               <OrganisationProvider>
                 {({
                   error,
+                  groups,
                   invalidateInviteLink,
                   inviteLinks,
                   invites,
@@ -356,6 +355,7 @@ const OrganisationSettingsPage = class extends Component {
                   subscriptionMeta,
                   users,
                 }) => {
+                  const canManageGroups = !!groups?.length
                   const { max_seats } = subscriptionMeta ||
                     organisation.subscription || { max_seats: 1 }
                   const isAWS =
@@ -398,6 +398,7 @@ const OrganisationSettingsPage = class extends Component {
                       </div>
 
                       <Tabs
+                        hideNavOnSingleTab
                         value={this.state.tab || 0}
                         onChange={(tab) => this.setState({ tab })}
                         className='mt-0'
