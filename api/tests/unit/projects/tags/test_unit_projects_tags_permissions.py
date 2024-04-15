2ba2d3c7a533262523bf8fa58ea63d433506ddb6
Zach Aysan <zachaysan@gmail.com>
2024-04-15 06:51:54 -0400

    chore: Convert projects tags permissions TestCase to normal test function (#3743)

--- api/tests/unit/projects/tags/test_unit_projects_tags_permissions.py
+++ api/tests/unit/projects/tags/test_unit_projects_tags_permissions.py
@@ -1,12 +1,10 @@
-from unittest import TestCase, mock
+from unittest import mock
 
-import pytest
-
-from organisations.models import Organisation
-from projects.models import Project, UserProjectPermission
+from projects.models import Project
 from projects.permissions import VIEW_PROJECT
 from projects.tags.models import Tag
 from projects.tags.permissions import TagPermissions
+from tests.types import WithProjectPermissionsCallable
 from users.models import FFAdminUser
 
 mock_request = mock.MagicMock()
@@ -15,105 +13,132 @@ mock_view = mock.MagicMock()
 permissions = TagPermissions()
 
 
-@pytest.mark.django_db
-class TagPermissionsTestCase(TestCase):
-    def setUp(self):
-        organisation = Organisation.objects.create(name="Test org")
-        self.project = Project.objects.create(
-            name="Test project", organisation=organisation
-        )
-        self.tag = Tag.objects.create(label="test", project=self.project)
-        mock_view.kwargs = {"project_pk": self.project.id}
+def test_project_admin_has_permission(
+    staff_user: FFAdminUser,
+    with_project_permissions: WithProjectPermissionsCallable,
+    project: Project,
+) -> None:
+    # Given
+    with_project_permissions(admin=True)
+    mock_request = mock.MagicMock(user=staff_user)
+    mock_view = mock.MagicMock(kwargs={"project_pk": project.id})
+    permissions = TagPermissions()
 
-        self.project_admin = FFAdminUser.objects.create(
-            email="project_admin@example.com"
-        )
-        self.project_admin.add_organisation(organisation)
-        UserProjectPermission.objects.create(
-            user=self.project_admin, admin=True, project=self.project
-        )
+    # When
+    results = []
+    for action in ["list", "create"]:
+        mock_view.action = action
+        results.append(permissions.has_permission(mock_request, mock_view))
 
-        self.project_user = FFAdminUser.objects.create(email="project_user@example.com")
-        self.project_user.add_organisation(organisation)
-        user_project_permission = UserProjectPermission.objects.create(
-            user=self.project_user, project=self.project
-        )
-        user_project_permission.add_permission(VIEW_PROJECT)
+    # Then
+    assert all(results)
 
-    def test_project_admin_has_permission(self):
-        # Given
-        mock_request.user = self.project_admin
 
-        # When
-        results = []
-        for action in ["list", "create"]:
-            mock_view.action = action
-            results.append(permissions.has_permission(mock_request, mock_view))
+def test_project_admin_has_object_permission(
+    staff_user: FFAdminUser,
+    with_project_permissions: WithProjectPermissionsCallable,
+    project: Project,
+) -> None:
+    # Given
+    with_project_permissions(admin=True)
+    mock_request = mock.MagicMock(user=staff_user)
+    mock_view = mock.MagicMock(kwargs={"project_pk": project.id})
+    permissions = TagPermissions()
+    tag = Tag.objects.create(label="test", project=project)
+    # When
+    results = []
+    for action in ["update", "delete", "detail"]:
+        mock_view.action = action
+        results.append(permissions.has_object_permission(mock_request, mock_view, tag))
 
-        # Then
-        assert all(results)
+    # Then
+    assert all(results)
 
-    def test_project_admin_has_object_permission(self):
-        # Given
-        mock_request.user = self.project_admin
 
-        # When
-        results = []
-        for action in ["update", "delete", "detail"]:
-            mock_view.action = action
-            results.append(
-                permissions.has_object_permission(mock_request, mock_view, self.tag)
-            )
+def test_project_user_has_list_permission(
+    staff_user: FFAdminUser,
+    with_project_permissions: WithProjectPermissionsCallable,
+    project: Project,
+) -> None:
+    # Given
+    with_project_permissions(admin=True)
+    mock_request = mock.MagicMock(user=staff_user)
+    mock_view = mock.MagicMock(
+        detail=False,
+        action="list",
+        kwargs={"project_pk": project.id},
+    )
+    permissions = TagPermissions()
 
-        # Then
-        assert all(results)
+    # When
+    result = permissions.has_permission(mock_request, mock_view)
 
-    def test_project_user_has_list_permission(self):
-        # Given
-        mock_request.user = self.project_user
-        mock_view.detail = False
+    # Then
+    assert result is True
 
-        # When
-        mock_view.action = "list"
-        result = permissions.has_permission(mock_request, mock_view)
 
-        # Then
-        assert result
+def test_project_user_has_no_create_permission(
+    staff_user: FFAdminUser,
+    with_project_permissions: WithProjectPermissionsCallable,
+    project: Project,
+) -> None:
+    # Given
+    with_project_permissions([VIEW_PROJECT], admin=False)
+    mock_request = mock.MagicMock(user=staff_user)
+    mock_view = mock.MagicMock(
+        detail=False,
+        action="create",
+        kwargs={"project_pk": project.id},
+    )
+    permissions = TagPermissions()
 
-    def test_project_user_has_no_create_permission(self):
-        # Given
-        mock_request.user = self.project_user
-        mock_view.detail = False
+    # When
+    result = permissions.has_permission(mock_request, mock_view)
 
-        # When
-        mock_view.action = "create"
-        result = permissions.has_permission(mock_request, mock_view)
+    # Then
+    assert result is False
 
-        # Then
-        assert not result
 
-    def test_project_user_has_no_update_or_delete_permission(self):
-        # Given
-        mock_request.user = self.project_user
+def test_project_user_has_no_update_or_delete_permission(
+    staff_user: FFAdminUser,
+    project: Project,
+) -> None:
+    # Given
+    mock_request = mock.MagicMock(user=staff_user)
+    mock_view = mock.MagicMock(
+        detail=False,
+        kwargs={"project_pk": project.id},
+    )
+    permissions = TagPermissions()
+    tag = Tag.objects.create(label="test", project=project)
 
-        # When
-        results = []
-        for action in ["update", "delete"]:
-            mock_view.action = action
-            results.append(
-                permissions.has_object_permission(mock_request, mock_view, self.tag)
-            )
+    # When
+    results = []
+    for action in ["update", "delete"]:
+        mock_view.action = action
+        results.append(permissions.has_object_permission(mock_request, mock_view, tag))
 
-        # Then
-        assert all(result is False for result in results)
+    # Then
+    assert all(result is False for result in results)
 
-    def test_project_user_has_detail_permission(self):
-        # Given
-        mock_request.user = self.project_user
 
-        # When
-        mock_view.action = "detail"
-        result = permissions.has_object_permission(mock_request, mock_view, self.tag)
+def test_project_user_has_detail_permission(
+    staff_user: FFAdminUser,
+    project: Project,
+    with_project_permissions: WithProjectPermissionsCallable,
+) -> None:
+    # Given
+    with_project_permissions([VIEW_PROJECT])
+    mock_request = mock.MagicMock(user=staff_user)
+    mock_view = mock.MagicMock(
+        action="detail",
+        kwargs={"project_pk": project.id},
+    )
+    permissions = TagPermissions()
+    tag = Tag.objects.create(label="test", project=project)
 
-        # Then
-        assert result
+    # When
+    result = permissions.has_object_permission(mock_request, mock_view, tag)
+
+    # Then
+    assert result is True
