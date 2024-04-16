b5351b5a80c06db4263551eb587ea0419f2c4f06
Novak Zaballa <zaballa.novak@gmail.com>
2024-04-16 09:45:57 -0400

    Check if the project has a Github Config

--- api/features/feature_external_resources/views.py
+++ api/features/feature_external_resources/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+
+from features.models import Feature
 
 from .models import FeatureExternalResource
 from .serializers import FeatureExternalResourceSerializer
@@ -15,6 +19,17 @@ class FeatureExternalResourceViewSet(viewsets.ModelViewSet):
         else:
             features_pk = self.kwargs["feature_pk"]
             return FeatureExternalResource.objects.filter(feature=features_pk)
+
+    def create(self, request, *args, **kwargs):
+        feature = get_object_or_404(
+            Feature.objects.filter(
+                id=self.kwargs["feature_pk"],
+            ),
+        )
+        if hasattr(feature.project, "github_config"):
+            raise PermissionDenied("You don't have a GitHub config")
+
+        return super().create(request, *args, **kwargs)
 
     def perform_update(self, serializer):
         external_resource_id = int(self.kwargs["id"])
