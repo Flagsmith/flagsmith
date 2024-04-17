cd28783bf28a66fac4186dd77b557822c7571709
Novak Zaballa <zaballa.novak@gmail.com>
2024-04-17 10:57:53 -0400

    Update github permission

--- api/integrations/github/permissions.py
+++ api/integrations/github/permissions.py
@@ -8,8 +8,6 @@ class HasPermissionToGithubConfiguration(BasePermission):
     """
 
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
 
         organisation_id = view.kwargs.get("organisation_pk")
         return (
