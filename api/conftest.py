b4559e2dc1cc36d74c291cff9c112862b439d3ba
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-04-19 17:46:02 +0100

    Remove duplicate fixture

--- api/conftest.py
+++ api/conftest.py
@@ -292,6 +292,7 @@ def with_project_permissions(
 @pytest.fixture()
 def environment_v2_versioning(environment):
     enable_v2_versioning(environment.id)
+    environment.refresh_from_db()
     return environment
 
 
