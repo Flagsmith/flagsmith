4f8c0a327743cbd9c84d0f248ca84bb7c26f18b7
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-04-22 17:01:57 +0100

    Update warning log message

--- api/core/management/commands/waitfordb.py
+++ api/core/management/commands/waitfordb.py
@@ -63,11 +63,8 @@ class Command(BaseCommand):
                 logger.info("Successfully connected to the database.")
                 break
             except OperationalError as e:
-                logger.warning(
-                    "Database not yet ready for connections. Error was: %s",
-                    str(e),
-                    exc_info=e,
-                )
+                logger.warning("Database not yet ready for connections.")
+                logger.warning("Error was: %s: %s", e.__class__.__name__, e)
 
             time.sleep(wait_between_checks)
 
