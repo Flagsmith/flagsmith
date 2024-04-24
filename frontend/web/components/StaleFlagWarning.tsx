1f9793ef0ae40a5884e5718ef9226dff4eb51044
kyle-ssg <kyle@solidstategroup.com>
2024-04-24 12:46:37 +0100

    Stale flag warning

---
+++ frontend/web/components/StaleFlagWarning.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react'
+import Tag from './tags/Tag'
+import Constants from 'common/constants'
+import moment from 'moment'
+import { ProjectFlag } from 'common/types/responses'
+import { getProtectedTags } from 'common/utils/getProtectedTags'
+import { IonIcon } from '@ionic/react'
+import { close, warning } from 'ionicons/icons'
+import Tooltip from './Tooltip'
+
+type StaleFlagWarningType = {
+  projectFlag: ProjectFlag
+}
+
+const StaleFlagWarning: FC<StaleFlagWarningType> = ({ projectFlag }) => {
+  const protectedTags = getProtectedTags(projectFlag, `${projectFlag.project}`)
+  if (protectedTags?.length) {
+    return null
+  }
+  const created_date = projectFlag?.created_date
+  const daysAgo = created_date && moment().diff(moment(created_date), 'days')
+  const suggestStale = daysAgo >= 30
+  if (!suggestStale) {
+    return null
+  }
+  return (
+    <Tooltip
+      title={
+        <span
+          className='fs-caption d-flex align-items-center'
+          style={{ color: Constants.tagColors[2] }}
+        >
+          Created {daysAgo} days ago
+          <IonIcon className='ms-1' icon={warning} />{' '}
+        </span>
+      }
+    >
+      You may wish to consider removing this feature or tagging it as permanent.
+    </Tooltip>
+  )
+}
+
+export default StaleFlagWarning
