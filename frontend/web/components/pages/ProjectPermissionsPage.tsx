67860aae7e4f39bf52271099eee8d0d31f8b63a4
kyle-ssg <kyle@solidstategroup.com>
2024-05-03 13:30:39 +0100

    Remove unused page

--- frontend/web/components/pages/ProjectPermissionsPage.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-type ProjectPermissionsPageType = {}
-
-const ProjectPermissionsPage: FC<ProjectPermissionsPageType> = ({}) => {
-  return <></>
-}
-
-export default ProjectPermissionsPage
