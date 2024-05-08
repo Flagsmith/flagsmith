8dd0cc9710d98cc30ee9c1b2a4e500f5923b175e
novakzaballa <zaballa.novak@gmail.com>
2024-05-08 09:24:13 -0400

    fix: Move code to call GitHub integration tasks out from versioning V2 condition block

--- api/features/signals.py
+++ api/features/signals.py
@@ -21,35 +21,37 @@ logger = logging.getLogger(__name__)
 @receiver(post_save, sender=FeatureState)
 def trigger_feature_state_change_webhooks_signal(instance, **kwargs):
 
-    if instance.environment_feature_version_id:
-        if (
-            not instance.identity_id
-            and not instance.feature_segment
-            and instance.feature.external_resources.exists()
-            and instance.environment.project.github_project.exists()
-            and hasattr(instance.environment.project.organisation, "github_config")
-        ):
-            github_configuration = (
-                Organisation.objects.prefetch_related("github_config")
-                .get(id=instance.environment.project.organisation_id)
-                .github_config.first()
+    # Enqueue Github integration tasks if feature has GitHub external resources
+    if (
+        not instance.identity_id
+        and not instance.feature_segment
+        and instance.feature.external_resources.exists()
+        and instance.environment.project.github_project.exists()
+        and hasattr(instance.environment.project.organisation, "github_config")
+    ):
+        github_configuration = (
+            Organisation.objects.prefetch_related("github_config")
+            .get(id=instance.environment.project.organisation_id)
+            .github_config.first()
+        )
+        if check_if_installation_id_is_valid(github_configuration):
+            feature_states = []
+            feature_states.append(instance)
+
+            feature_data: GithubData = generate_data(
+                github_configuration=github_configuration,
+                feature_id=instance.feature.id,
+                feature_name=instance.feature.name,
+                type=WebhookEventType.FLAG_UPDATED.value,
+                feature_states=feature_states,
             )
-            if check_if_installation_id_is_valid(github_configuration):
-                feature_states = []
-                feature_states.append(instance)
 
-                feature_data: GithubData = generate_data(
-                    github_configuration=github_configuration,
-                    feature_id=instance.feature.id,
-                    feature_name=instance.feature.name,
-                    type=WebhookEventType.FLAG_UPDATED.value,
-                    feature_states=feature_states,
-                )
+            call_github_app_webhook_for_feature_state.delay(
+                args=(asdict(feature_data),),
+            )
+        else:
+            raise InvalidInstallation
 
-                call_github_app_webhook_for_feature_state.delay(
-                    args=(asdict(feature_data),),
-                )
-            else:
-                raise InvalidInstallation
+    if instance.environment_feature_version_id:
         return
     trigger_feature_state_change_webhooks(instance)
