e613eec2fce870fea17aa81427f61ec511c6e7c7
Gagan <gagandeeptrivedi47@gmail.com>
2024-05-18 10:05:43 +0530

    chore: remove deprecated functions url and ugettext_lazy (#3976)
    Co-authored-by: Matthew Elwell <matthew.elwell@flagsmith.com>

--- api/features/models.py
+++ api/features/models.py
@@ -21,7 +21,6 @@ from django.core.exceptions import (
 from django.db import models
 from django.db.models import Max, Q, QuerySet
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
 from django_lifecycle import (
     AFTER_CREATE,
     AFTER_SAVE,
@@ -99,7 +98,7 @@ class Feature(
     project = models.ForeignKey(
         Project,
         related_name="features",
-        help_text=_(
+        help_text=(
             "Changing the project selected will remove previous Feature States for the previously "
             "associated projects Environments that are related to this Feature. New default "
             "Feature States will be created for the new selected projects Environments for this "
