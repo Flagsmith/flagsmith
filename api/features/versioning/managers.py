27b61d3973e26cf0b0bf7dee8da7f562c03fa96f
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-05-21 15:17:15 +0100

    Simplify SQL

--- api/features/versioning/managers.py
+++ api/features/versioning/managers.py
@@ -3,16 +3,8 @@ from pathlib import Path
 from django.db.models.query import RawQuerySet
 from softdelete.models import SoftDeleteManager
 
-with open(
-    Path(__file__).parent.resolve() / "sql/get_latest_versions_by_environment_id.sql"
-) as f:
-    get_latest_versions_by_environment_id_sql = f.read()
-
-with open(
-    Path(__file__).parent.resolve()
-    / "sql/get_latest_versions_by_environment_api_key.sql"
-) as f:
-    get_latest_versions_by_environment_api_key_sql = f.read()
+with open(Path(__file__).parent.resolve() / "sql/get_latest_versions.sql") as f:
+    get_latest_versions_sql = f.read()
 
 
 class EnvironmentFeatureVersionManager(SoftDeleteManager):
@@ -20,10 +12,7 @@ class EnvironmentFeatureVersionManager(SoftDeleteManager):
         """
         Get the latest EnvironmentFeatureVersion objects for a given environment.
         """
-        return self.raw(
-            get_latest_versions_by_environment_id_sql,
-            params={"environment_id": environment_id},
-        )
+        return self._get_latest_versions(environment_id=environment_id)
 
     def get_latest_versions_by_environment_api_key(
         self, environment_api_key: str
@@ -31,7 +20,16 @@ class EnvironmentFeatureVersionManager(SoftDeleteManager):
         """
         Get the latest EnvironmentFeatureVersion objects for a given environment.
         """
+        return self._get_latest_versions(environment_api_key=environment_api_key)
+
+    def _get_latest_versions(
+        self, environment_id: int = None, environment_api_key: str = None
+    ) -> RawQuerySet:
+        assert (environment_id or environment_api_key) and not (
+            environment_id and environment_api_key
+        ), "Must provide exactly one of environment_id or environment_api_key"
+
         return self.raw(
-            get_latest_versions_by_environment_api_key_sql,
-            params={"api_key": environment_api_key},
+            get_latest_versions_sql,
+            params={"environment_id": environment_id, "api_key": environment_api_key},
         )
