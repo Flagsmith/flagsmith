9d5c058f8bf702e717d1b12ec3b8a0f72a03ef7e
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2024-05-23 13:39:46 +0530

    fix broken test

--- api/tests/unit/features/test_unit_feature_external_resources_views.py
+++ api/tests/unit/features/test_unit_feature_external_resources_views.py
@@ -277,8 +277,8 @@ def test_cannot_create_feature_external_resource_due_to_unique_constraint(
     # Then
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert (
-        "Duplication error. The feature already has this resource URI"
-        in response.json()[0]
+        response.json()["non_field_errors"][0]
+        == "The fields feature, url must make a unique set."
     )
 
 
