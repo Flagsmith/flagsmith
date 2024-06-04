e74e936bcf6a6a02f5f3594259bf4202dc84925b
kyle-ssg <kyle@solidstategroup.com>
2024-06-04 13:04:35 +0100

    Add autocomplete for login

--- frontend/web/components/pages/HomePage.js
+++ frontend/web/components/pages/HomePage.js
@@ -366,6 +366,7 @@ const HomePage = class extends React.Component {
                                           <InputGroup
                                             title='Password'
                                             inputProps={{
+                                              enableAutoComplete: true,
                                               className: 'full-width',
                                               error: error && error.password,
                                               name: 'password',
@@ -569,6 +570,7 @@ const HomePage = class extends React.Component {
                                       data-test='email'
                                       inputProps={{
                                         className: 'full-width',
+                                        enableAutoComplete: true,
                                         error: error && error.email,
                                         name: 'email',
                                       }}
@@ -586,6 +588,7 @@ const HomePage = class extends React.Component {
                                       title='Password'
                                       data-test='password'
                                       inputProps={{
+
                                         className: 'full-width',
                                         error: error && error.password,
                                         name: 'password',
