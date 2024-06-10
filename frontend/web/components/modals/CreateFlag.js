61299568e73c459870126fc0cf6568cb1a9f9168
Novak Zaballa <41410593+novakzaballa@users.noreply.github.com>
2024-06-10 03:09:11 -0400

    fix: Identity overrides tab (#4134)

--- frontend/web/components/modals/CreateFlag.js
+++ frontend/web/components/modals/CreateFlag.js
@@ -272,11 +272,11 @@ const CreateFlag = class extends Component {
   }
   save = (func, isSaving) => {
     const {
+      environmentFlag,
       environmentId,
       identity,
       identityFlag,
       projectFlag: _projectFlag,
-      environmentFlag,
       segmentOverrides,
     } = this.props
     const {
@@ -446,8 +446,7 @@ const CreateFlag = class extends Component {
   }
 
   addItem = () => {
-    const { environmentId, identity, projectFlag } = this.props
-    const { environmentFlag } = this.state
+    const { environmentFlag, environmentId, identity, projectFlag } = this.props
     this.setState({ isLoading: true })
     const selectedIdentity = this.state.selectedIdentity.value
     const identities = identity ? identity.identifier : []
