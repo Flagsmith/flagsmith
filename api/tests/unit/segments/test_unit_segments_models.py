17b2c9db50e801b768430fa916c126493d592d9a
Zach Aysan <zachaysan@gmail.com>
2024-06-17 15:24:23 +0000

    Update clone tests and add deep delete tests

--- api/tests/unit/segments/test_unit_segments_models.py
+++ api/tests/unit/segments/test_unit_segments_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.utils import timezone
 from flag_engine.segments.constants import EQUAL, PERCENTAGE_SPLIT
 
 from features.models import Feature
@@ -183,61 +184,41 @@ def test_deep_clone_of_segment(
         feature=feature,
     )
 
-    # Check that the versioning is correct, since we'll testing
+    # Check that the versioning is correct, since we'll be testing
     # against it later in the test.
     assert segment.version == 1
     assert segment.version_of == segment
 
-    parent_rule1 = SegmentRule.objects.create(
-        segment=segment, type=SegmentRule.ALL_RULE
-    )
-    parent_rule2 = SegmentRule.objects.create(
-        segment=segment, type=SegmentRule.ANY_RULE
-    )
+    parent_rule = SegmentRule.objects.create(segment=segment, type=SegmentRule.ALL_RULE)
 
     child_rule1 = SegmentRule.objects.create(
-        rule=parent_rule1, type=SegmentRule.ANY_RULE
+        rule=parent_rule, type=SegmentRule.ANY_RULE
     )
     child_rule2 = SegmentRule.objects.create(
-        rule=parent_rule1, type=SegmentRule.ALL_RULE
+        rule=parent_rule, type=SegmentRule.NONE_RULE
     )
     child_rule3 = SegmentRule.objects.create(
-        rule=parent_rule2, type=SegmentRule.NONE_RULE
+        rule=parent_rule, type=SegmentRule.NONE_RULE
     )
     child_rule4 = SegmentRule.objects.create(
-        rule=parent_rule2, type=SegmentRule.ALL_RULE
+        rule=parent_rule, type=SegmentRule.ANY_RULE
     )
 
     condition1 = Condition.objects.create(
-        rule=parent_rule1,
-        property="parent_rule1",
-        operator=EQUAL,
-        value="condition1",
-        created_with_segment=True,
-    )
-    condition2 = Condition.objects.create(
-        rule=parent_rule1,
-        property="parent_rule1",
-        operator=PERCENTAGE_SPLIT,
-        value="0.1",
-        created_with_segment=False,
-    )
-
-    condition3 = Condition.objects.create(
         rule=child_rule1,
         property="child_rule1",
         operator=EQUAL,
         value="condition3",
         created_with_segment=True,
     )
-    Condition.objects.create(
+    condition2 = Condition.objects.create(
         rule=child_rule2,
         property="child_rule2",
         operator=PERCENTAGE_SPLIT,
         value="0.2",
         created_with_segment=False,
     )
-    Condition.objects.create(
+    condition3 = Condition.objects.create(
         rule=child_rule2,
         property="child_rule2",
         operator=EQUAL,
@@ -245,7 +226,7 @@ def test_deep_clone_of_segment(
         created_with_segment=False,
     )
 
-    Condition.objects.create(
+    condition4 = Condition.objects.create(
         rule=child_rule3,
         property="child_rule3",
         operator=EQUAL,
@@ -253,7 +234,7 @@ def test_deep_clone_of_segment(
         created_with_segment=False,
     )
 
-    Condition.objects.create(
+    condition5 = Condition.objects.create(
         rule=child_rule4,
         property="child_rule4",
         operator=EQUAL,
@@ -262,77 +243,82 @@ def test_deep_clone_of_segment(
     )
 
     # When
-    new_segment = segment.deep_clone()
+    cloned_segment = segment.deep_clone()
 
     # Then
-    assert new_segment.name == segment.name
-    assert new_segment.description == segment.description
-    assert new_segment.project == project
-    assert new_segment.feature == feature
-    assert new_segment.version == 1
-    assert new_segment.version_of == segment
+    assert cloned_segment.name == segment.name
+    assert cloned_segment.description == segment.description
+    assert cloned_segment.project == project
+    assert cloned_segment.feature == feature
+    assert cloned_segment.version == 1
+    assert cloned_segment.version_of == segment
 
     assert segment.version == 2
 
-    assert len(new_segment.rules.all()) == len(segment.rules.all()) == 2
-    new_parent_rule1, new_parent_rule2 = list(new_segment.rules.all())
+    assert len(cloned_segment.rules.all()) == len(segment.rules.all()) == 1
+    new_parent_rule = cloned_segment.rules.first()
 
-    assert new_parent_rule1.segment == new_segment
-    assert new_parent_rule2.segment == new_segment
-    assert new_parent_rule1.type == parent_rule1.type
-    assert new_parent_rule2.type == parent_rule2.type
+    assert new_parent_rule.segment == cloned_segment
+    assert new_parent_rule.type == parent_rule.type
 
-    assert len(new_parent_rule1.rules.all()) == len(parent_rule1.rules.all()) == 2
-    assert len(new_parent_rule2.rules.all()) == len(parent_rule2.rules.all()) == 2
-    new_child_rule1, new_child_rule2 = list(new_parent_rule1.rules.all())
+    assert len(new_parent_rule.rules.all()) == len(parent_rule.rules.all()) == 4
+    new_child_rule1, new_child_rule2, new_child_rule3, new_child_rule4 = list(
+        new_parent_rule.rules.all()
+    )
 
     assert new_child_rule1.type == child_rule1.type
     assert new_child_rule2.type == child_rule2.type
-
-    new_child_rule3, new_child_rule4 = list(new_parent_rule2.rules.all())
-
     assert new_child_rule3.type == child_rule3.type
     assert new_child_rule4.type == child_rule4.type
 
     assert (
-        len(new_parent_rule1.conditions.all())
-        == len(parent_rule1.conditions.all())
-        == 2
+        len(new_parent_rule.conditions.all()) == len(parent_rule.conditions.all()) == 0
     )
 
-    new_condition1, new_condition2 = list(new_parent_rule1.conditions.all())
+    assert (
+        len(new_child_rule1.conditions.all()) == len(child_rule1.conditions.all()) == 1
+    )
+    new_condition1 = new_child_rule1.conditions.first()
 
     assert new_condition1.property == condition1.property
     assert new_condition1.operator == condition1.operator
     assert new_condition1.value == condition1.value
     assert new_condition1.created_with_segment is condition1.created_with_segment
 
+    assert (
+        len(new_child_rule2.conditions.all()) == len(child_rule2.conditions.all()) == 2
+    )
+    new_condition2, new_condition3 = list(new_child_rule2.conditions.all())
+
     assert new_condition2.property == condition2.property
     assert new_condition2.operator == condition2.operator
     assert new_condition2.value == condition2.value
     assert new_condition2.created_with_segment is condition2.created_with_segment
-
-    assert (
-        len(new_child_rule1.conditions.all()) == len(child_rule1.conditions.all()) == 1
-    )
-    new_condition3 = new_child_rule1.conditions.first()
 
     assert new_condition3.property == condition3.property
     assert new_condition3.operator == condition3.operator
     assert new_condition3.value == condition3.value
     assert new_condition3.created_with_segment is condition3.created_with_segment
 
-    # Since the conditions have been asserted at multiple levels,
-    # we leave the rest to basic size assertions.
-    assert (
-        len(new_child_rule2.conditions.all()) == len(child_rule2.conditions.all()) == 2
-    )
     assert (
         len(new_child_rule3.conditions.all()) == len(child_rule3.conditions.all()) == 1
     )
+    new_condition4 = new_child_rule3.conditions.first()
+
+    assert new_condition4.property == condition4.property
+    assert new_condition4.operator == condition4.operator
+    assert new_condition4.value == condition4.value
+    assert new_condition4.created_with_segment is condition4.created_with_segment
+
     assert (
         len(new_child_rule4.conditions.all()) == len(child_rule4.conditions.all()) == 1
     )
+    new_condition5 = new_child_rule4.conditions.first()
+
+    assert new_condition5.property == condition5.property
+    assert new_condition5.operator == condition5.operator
+    assert new_condition5.value == condition5.value
+    assert new_condition5.created_with_segment is condition5.created_with_segment
 
 
 def test_manager_returns_only_highest_version_of_segments(
@@ -343,24 +329,24 @@ def test_manager_returns_only_highest_version_of_segments(
     assert segment.version == 2
     assert segment.version_of == segment
 
-    new_segment = segment.deep_clone()
-    assert new_segment.version == 2
+    cloned_segment = segment.deep_clone()
+    assert cloned_segment.version == 2
     assert segment.version == 3
 
     # When
-    queryset1 = Segment.objects.filter(id=new_segment.id)
-    queryset2 = Segment.all_objects.filter(id=new_segment.id)
+    queryset1 = Segment.objects.filter(id=cloned_segment.id)
+    queryset2 = Segment.all_objects.filter(id=cloned_segment.id)
     queryset3 = Segment.objects.filter(id=segment.id)
     queryset4 = Segment.all_objects.filter(id=segment.id)
 
     # Then
     assert not queryset1.exists()
-    assert queryset2.first() == new_segment
+    assert queryset2.first() == cloned_segment
     assert queryset3.first() == segment
     assert queryset4.first() == segment
 
 
-def test_deep_clone_of_segment_with_sub_rule(
+def test_deep_clone_of_segment_with_improper_sub_rule(
     project: Project,
     feature: Feature,
 ) -> None:
@@ -372,12 +358,13 @@ def test_deep_clone_of_segment_with_sub_rule(
         feature=feature,
     )
 
-    sub_rule = SegmentRule.objects.create(type=SegmentRule.ALL_RULE)
-
-    # Parent rule with invalid sub rule.
-    SegmentRule.objects.create(
-        segment=segment, type=SegmentRule.ALL_RULE, rule=sub_rule
+    rule = SegmentRule.objects.create(
+        type=SegmentRule.ALL_RULE,
+        segment=segment,
     )
+
+    # Rule with invalid relation to both segment and rule.
+    SegmentRule.objects.create(segment=segment, type=SegmentRule.ALL_RULE, rule=rule)
 
     with pytest.raises(AssertionError) as exception:
         segment.deep_clone()
@@ -413,3 +400,60 @@ def test_deep_clone_of_segment_with_grandchild_rule(
     assert (
         "AssertionError: Expected two layers of rules, not more" == exception.exconly()
     )
+
+
+def test_deep_delete_hard(segment: Segment) -> None:
+    # Given
+    parent_rule = SegmentRule.objects.create(segment=segment, type=SegmentRule.ALL_RULE)
+
+    child_rule = SegmentRule.objects.create(rule=parent_rule, type=SegmentRule.ANY_RULE)
+
+    condition = Condition.objects.create(
+        rule=child_rule,
+        property="child_rule",
+        operator=EQUAL,
+        value="condition",
+        created_with_segment=True,
+    )
+
+    # When
+    segment.deep_delete(hard=True)
+
+    # Then
+    with pytest.raises(Segment.DoesNotExist):
+        segment.refresh_from_db()
+    with pytest.raises(SegmentRule.DoesNotExist):
+        parent_rule.refresh_from_db()
+    with pytest.raises(SegmentRule.DoesNotExist):
+        child_rule.refresh_from_db()
+    with pytest.raises(Condition.DoesNotExist):
+        condition.refresh_from_db()
+
+
+def test_deep_delete_soft(segment: Segment) -> None:
+    # Given
+    parent_rule = SegmentRule.objects.create(segment=segment, type=SegmentRule.ALL_RULE)
+
+    child_rule = SegmentRule.objects.create(rule=parent_rule, type=SegmentRule.ANY_RULE)
+
+    condition = Condition.objects.create(
+        rule=child_rule,
+        property="child_rule",
+        operator=EQUAL,
+        value="condition",
+        created_with_segment=True,
+    )
+    before_delete = timezone.now()
+
+    # When
+    segment.deep_delete(hard=False)
+
+    # Then
+    segment.refresh_from_db()
+    assert segment.deleted_at > before_delete
+    parent_rule.refresh_from_db()
+    assert parent_rule.deleted_at > before_delete
+    child_rule.refresh_from_db()
+    assert child_rule.deleted_at > before_delete
+    condition.refresh_from_db()
+    assert condition.deleted_at > before_delete
