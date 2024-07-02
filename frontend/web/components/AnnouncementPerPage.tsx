9b3c30f497efe942a06817a5bbd097d0f87bb1e0
Novak Zaballa <zaballa.novak@gmail.com>
2024-07-02 15:08:10 -0400

    Compare param values

--- frontend/web/components/AnnouncementPerPage.tsx
+++ frontend/web/components/AnnouncementPerPage.tsx
@@ -10,6 +10,7 @@ import { routes } from 'web/routes'
 type AnnouncementPerPageValueType = AnnouncementValueType & {
   pages: string[]
   projectId: number
+  params: { [key: string]: string }
 }
 
 type AnnouncementPerPageType = {
@@ -35,6 +36,18 @@ const AnnouncementPerPage: FC<AnnouncementPerPageType> = ({ pathname }) => {
     Utils.getFlagsmithHasFeature('announcement_per_page') &&
     announcementPerPageValue?.pages?.length > 0
 
+  const convertObjectValuesToStrings = (
+    obj: AnnouncementPerPageValueType['params'],
+  ) => {
+    const newObj = {}
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        newObj[key] = String(obj[key])
+      }
+    }
+    return newObj
+  }
+
   const announcementInPage = announcementPerPageValue?.pages?.some((page) => {
     if (Object.keys(routes).includes(page)) {
       const match = matchPath(pathname, {
@@ -42,18 +55,32 @@ const AnnouncementPerPage: FC<AnnouncementPerPageType> = ({ pathname }) => {
         path: routes[page],
         strict: false,
       })
+
       if (match) {
-        if (
-          parseInt(get(match, 'params.projectId')!) !==
-          announcementPerPageValue?.projectId
-        ) {
-          return false
+        const annParams =
+          convertObjectValuesToStrings(announcementPerPageValue?.params) || {}
+        const matchParams = convertObjectValuesToStrings(match?.params) || {}
+        const objectsMatch = (
+          obj1: AnnouncementPerPageValueType['params'],
+          obj2: AnnouncementPerPageValueType['params'],
+        ) => {
+          return Object.keys(obj1).every((key) => {
+            return obj2.hasOwnProperty(key) && obj1[key] === obj2[key]
+          })
         }
-        return true
+        const annParamsMatch = objectsMatch(annParams, matchParams)
+        const matchParamsMatch = objectsMatch(matchParams, annParams)
+        if (annParamsMatch || matchParamsMatch) {
+          return true
+        }
+
+        return false
       }
     }
+
     return false
   })
+
   return (
     <>
       {showAnnouncementPerPage && announcementInPage && (
