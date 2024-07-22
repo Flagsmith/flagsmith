b4af2f32c3b2663984bbc9dac89fac8488c787a2
Zach Aysan <zachaysan@gmail.com>
2024-07-22 13:40:57 +0000

    Add polymorphic managers

--- api/segments/managers.py
+++ api/segments/managers.py
@@ -1,8 +1,13 @@
 from core.models import SoftDeleteExportableManager
 from django.db.models import F
+from polymorphic.models import PolymorphicManager
 
 
-class SegmentManager(SoftDeleteExportableManager):
+class AllSegmentManager(SoftDeleteExportableManager, PolymorphicManager):
+    pass
+
+
+class SegmentManager(AllSegmentManager):
     def get_queryset(self):
         """
         Returns only the canonical segments, which will always be
