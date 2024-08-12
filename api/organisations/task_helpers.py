0dd3322158d303ba77a1aefe150f1ce5641808b6
Zach Aysan <zachaysan@gmail.com>
2024-08-12 18:50:20 +0000

    Remove pointless bool cast

--- api/organisations/task_helpers.py
+++ api/organisations/task_helpers.py
@@ -69,7 +69,7 @@ def _send_api_usage_notification(
     context = {
         "organisation": organisation,
         "matched_threshold": matched_threshold,
-        "grace_period": not bool(hasattr(organisation, "breached_grace_period")),
+        "grace_period": not hasattr(organisation, "breached_grace_period"),
     }
 
     send_mail(
