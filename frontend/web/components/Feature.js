1ba7a30d42c909d7136ae5c3d8252863e7406db2
kyle-ssg <kyle@solidstategroup.com>
2024-08-14 13:04:39 +0100

    Fix space

--- frontend/web/components/Feature.js
+++ frontend/web/components/Feature.js
@@ -138,7 +138,7 @@ export default class Feature extends PureComponent {
                 <strong>"{environmentFlag?.feature_state_value}"</strong> as a
                 string. As of version <strong>2.137.0</strong>, saving this
                 feature will convert its value from a string to a number. If you
-                wish to preserve this value as a string, please save it via our
+                wish to preserve this value as a string, please save it via our{' '}
                 <a href='https://api.flagsmith.com/api/v1/docs/#/api/api_v1_environments_featurestates_update'>
                   API
                 </a>
