4e037ae6d5732c80510762fc4eab1b7720326db8
Zach Aysan <zachaysan@gmail.com>
2024-08-15 14:43:57 +0000

    Remove stale all segment type

--- api/audit/related_object_type.py
+++ api/audit/related_object_type.py
@@ -5,7 +5,6 @@ class RelatedObjectType(enum.Enum):
     FEATURE = "Feature"
     FEATURE_STATE = "Feature state"
     SEGMENT = "Segment"
-    ALL_SEGMENT = "All segment"
     ENVIRONMENT = "Environment"
     CHANGE_REQUEST = "Change request"
     EDGE_IDENTITY = "Edge Identity"
