e7456a18eb74a894c3ffb317e1d367d61b31b087
kyle-ssg <kyle@solidstategroup.com>
2024-08-21 13:15:32 +0100

    Usage period

--- frontend/common/types/requests.ts
+++ frontend/common/types/requests.ts
@@ -22,6 +22,17 @@ export type PagedRequest<T> = T & {
 }
 export type OAuthType = 'github' | 'saml' | 'google'
 export type PermissionLevel = 'organisation' | 'project' | 'environment'
+export const billingPeriods = [
+  {
+    label: 'Current billing period',
+    value: 'current_billing_period',
+  },
+  {
+    label: 'Previous billing period',
+    value: 'previous_billing_period',
+  },
+  { label: 'Last 90 days', value: '90_day_period' },
+]
 export type CreateVersionFeatureState = {
   environmentId: number
   featureId: number
