06bde236b37307d7d4d9c7353c8e18994db27b9b
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-08-28 18:51:30 +0100

    add delete tests

--- api/tests/unit/integrations/grafana/test_views.py
+++ api/tests/unit/integrations/grafana/test_views.py
@@ -1,11 +1,23 @@
 import json
 
+import pytest
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
 from integrations.grafana.models import GrafanaOrganisationConfiguration
 from organisations.models import Organisation
+
+
+@pytest.fixture
+def grafana_organisation_configuration(
+    organisation: Organisation,
+) -> GrafanaOrganisationConfiguration:
+    return GrafanaOrganisationConfiguration.objects.create(
+        organisation=organisation,
+        base_url="http://test.com",
+        api_key="abc-123",
+    )
 
 
 def test_grafana_organisation_view__create_configuration__persist_expected(
@@ -47,13 +59,9 @@ def test_grafana_organisation_view__create_configuration__persist_expected(
 def test_grafana_organisation_view__create_configuration__existing__return_expected(
     admin_client_new: APIClient,
     organisation: Organisation,
+    grafana_organisation_configuration: GrafanaOrganisationConfiguration,
 ) -> None:
     # Given
-    GrafanaOrganisationConfiguration.objects.create(
-        organisation=organisation,
-        base_url="http://test.com",
-        api_key="abc-123",
-    )
     data = {
         "base_url": "http://test.com",
         "api_key": "abc-123",
@@ -76,20 +84,16 @@ def test_grafana_organisation_view__create_configuration__existing__return_expec
 def test_grafana_organisation_view__update_configuration__persist_expected(
     admin_client_new: APIClient,
     organisation: Organisation,
+    grafana_organisation_configuration: GrafanaOrganisationConfiguration,
 ) -> None:
     # Given
-    config = GrafanaOrganisationConfiguration.objects.create(
-        organisation=organisation,
-        base_url="http://test.com",
-        api_key="abc-123",
-    )
     data = {
         "base_url": "http://updated.test.com",
         "api_key": "updated-abc-123",
     }
     url = reverse(
         "api-v1:organisations:integrations-grafana-detail",
-        args=[organisation.id, config.id],
+        args=[organisation.id, grafana_organisation_configuration.id],
     )
 
     # When
@@ -108,9 +112,31 @@ def test_grafana_organisation_view__update_configuration__persist_expected(
     assert updated_config.api_key == data["api_key"]
 
 
+def test_grafana_organisation_view__delete_configuration__return_expected(
+    admin_client_new: APIClient,
+    organisation: Organisation,
+    grafana_organisation_configuration: GrafanaOrganisationConfiguration,
+) -> None:
+    # Given
+    url = reverse(
+        "api-v1:organisations:integrations-grafana-detail",
+        args=[organisation.id, grafana_organisation_configuration.id],
+    )
+
+    # When
+    response = admin_client_new.delete(url)
+
+    # Then
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert not GrafanaOrganisationConfiguration.objects.filter(
+        organisation=organisation
+    ).exists()
+
+
 def test_grafana_organisation_view__create_configuration__non_admin__return_expected(
     test_user_client: APIClient,
     organisation: Organisation,
+    grafana_organisation_configuration: GrafanaOrganisationConfiguration,
 ) -> None:
     # Given
     data = {
@@ -135,20 +161,34 @@ def test_grafana_organisation_view__create_configuration__non_admin__return_expe
 def test_grafana_organisation_view__get_configuration__non_admin__return_expected(
     test_user_client: APIClient,
     organisation: Organisation,
+    grafana_organisation_configuration: GrafanaOrganisationConfiguration,
 ) -> None:
     # Given
-    config = GrafanaOrganisationConfiguration.objects.create(
-        organisation=organisation,
-        base_url="http://test.com",
-        api_key="abc-123",
-    )
     url = reverse(
         "api-v1:organisations:integrations-grafana-detail",
-        args=[organisation.id, config.id],
+        args=[organisation.id, grafana_organisation_configuration.id],
     )
 
     # When
     response = test_user_client.get(url)
+
+    # Then
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_grafana_organisation_view__delete_configuration__non_admin__return_expected(
+    test_user_client: APIClient,
+    organisation: Organisation,
+    grafana_organisation_configuration: GrafanaOrganisationConfiguration,
+) -> None:
+    # Given
+    url = reverse(
+        "api-v1:organisations:integrations-grafana-detail",
+        args=[organisation.id, grafana_organisation_configuration.id],
+    )
+
+    # When
+    response = test_user_client.delete(url)
 
     # Then
     assert response.status_code == status.HTTP_403_FORBIDDEN
