63053828946452c1331d2c735f14e29705501a6c
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-09-06 20:03:03 +0530

    chore: updated maven dependency version to the latest available

--- frontend/common/code-help/install/install-java.js
+++ frontend/common/code-help/install/install-java.js
@@ -2,9 +2,9 @@ import Utils from 'common/utils/utils'
 
 module.exports = () => `// Maven
 ${Utils.escapeHtml('<dependency>')}
-${Utils.escapeHtml('<groupId>com.flagsmith</groupId>')}
-${Utils.escapeHtml('<artifactId>flagsmith-java-client</artifactId>')}
-${Utils.escapeHtml('<version>5.0.0</version>')}
+    ${Utils.escapeHtml('<groupId>com.flagsmith</groupId>')}
+    ${Utils.escapeHtml('<artifactId>flagsmith-java-client</artifactId>')}
+    ${Utils.escapeHtml('<version>7.4.1</version>')}
 ${Utils.escapeHtml('</dependency>')}
 
 // Gradle
