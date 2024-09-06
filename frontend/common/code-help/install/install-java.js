ce56fa6a67f5d720020b6f8fb76fae610c506170
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-09-06 19:51:20 +0530

    fix: Fixed the error in Java SDK usage code example

--- frontend/common/code-help/install/install-java.js
+++ frontend/common/code-help/install/install-java.js
@@ -5,7 +5,7 @@ ${Utils.escapeHtml('<dependency>')}
 ${Utils.escapeHtml('<groupId>com.flagsmith</groupId>')}
 ${Utils.escapeHtml('<artifactId>flagsmith-java-client</artifactId>')}
 ${Utils.escapeHtml('<version>5.0.0/version>')}
-</dependency>
+${Utils.escapeHtml('</dependency>')}
 
 // Gradle
 implementation 'com.flagsmith:flagsmith-java-client:5.0.0'
