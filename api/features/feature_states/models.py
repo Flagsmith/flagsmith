5f73cfffc6179ea6fb1e7a3787cbc2c85bfcc1f9
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-09-10 18:49:20 +0100

    Make feature value size configurable

--- api/features/feature_states/models.py
+++ api/features/feature_states/models.py
@@ -1,5 +1,6 @@
 import typing
 
+from django.conf import settings
 from django.db import models
 
 from features.value_types import (
@@ -23,7 +24,9 @@ class AbstractBaseFeatureValueModel(models.Model):
     )
     boolean_value = models.BooleanField(null=True, blank=True)
     integer_value = models.IntegerField(null=True, blank=True)
-    string_value = models.CharField(null=True, max_length=2_000_000, blank=True)
+    string_value = models.CharField(
+        null=True, max_length=settings.FEATURE_VALUE_LIMIT, blank=True
+    )
 
     @property
     def value(self) -> typing.Union[str, int, bool]:
