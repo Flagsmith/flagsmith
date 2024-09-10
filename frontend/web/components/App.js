6ef774be945304bf05ad43515eccef51db3a5063
Rodrigo López Dato <rodrigo.lopezdato@flagsmith.com>
2024-09-10 07:28:58 -0300

    fix: Allow switching organisations if current one is blocked (#4606)

--- frontend/web/components/App.js
+++ frontend/web/components/App.js
@@ -299,7 +299,8 @@ const App = class extends Component {
     }
     if (
       AccountStore.getOrganisation() &&
-      AccountStore.getOrganisation().block_access_to_admin
+      AccountStore.getOrganisation().block_access_to_admin &&
+      pathname !== '/organisations'
     ) {
       return <Blocked />
     }
