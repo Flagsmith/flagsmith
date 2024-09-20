9add2d613445bdb149ce9c815ebae5be49586988
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-09-20 22:53:53 +0100

    fix: Webhook integration not rebuilding environment (#4641)

---
+++ api/tests/unit/integrations/webhook/test_unit_webhook_models.py
@@ -0,0 +1,19 @@
+from pytest_mock import MockerFixture
+
+from environments.models import Environment
+from integrations.webhook.models import WebhookConfiguration
+
+
+def test_webhook_model__save__call_expected(
+    environment: Environment, mocker: MockerFixture
+) -> None:
+    # Given
+    environment_mock = mocker.patch("integrations.webhook.models.Environment")
+
+    # When
+    WebhookConfiguration.objects.create(environment=environment)
+
+    # Then
+    environment_mock.write_environments_to_dynamodb.assert_called_with(
+        environment_id=environment.id
+    )
