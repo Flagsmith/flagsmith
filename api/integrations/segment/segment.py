d8b13c46d4e1a8333827b06fae3806ac0eb1ef0f
Zach Aysan <zachaysan@gmail.com>
2024-10-02 13:49:31 +0000

    Set host for segment client

--- api/integrations/segment/segment.py
+++ api/integrations/segment/segment.py
@@ -14,12 +14,12 @@ logger = logging.getLogger(__name__)
 
 class SegmentWrapper(AbstractBaseIdentityIntegrationWrapper):
     def __init__(self, config: SegmentConfiguration):
-        api_key = config.api_key
-        self.analytics = SegmentClient(write_key=api_key, sync_mode=True)
+        self.analytics = SegmentClient(
+            write_key=config.api_key, sync_mode=True, host=config.base_url
+        )
 
     def _identify_user(self, data: dict) -> None:
         self.analytics.identify(**data)
-        logger.debug("Sent event to Segment.")
 
     def generate_user_data(
         self,
