c916aedd182b2da8e00cf620f88265dae95baa58
kyle-ssg <kyle@solidstategroup.com>
2024-10-02 14:44:54 +0100

    test build

--- frontend/webpack/webpack.config.local.js
+++ frontend/webpack/webpack.config.local.js
@@ -33,7 +33,6 @@ module.exports = {
         new webpack.NoEmitOnErrorsPlugin(),
     ]).concat(require('./pages').map((page) => {
         // eslint-disable-next-line no-console
-        console.log(page);
         return new HtmlWebpackPlugin({
             filename: `${page}.html`, // output
             template: `./web/${page}.html`, // template to use
