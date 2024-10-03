1a0dc8714c554c66e50f93a50ea129fffdd077fd
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-10-03 11:46:24 +0100

    restore token deletion

--- api/custom_auth/jwt_cookie/views.py
+++ api/custom_auth/jwt_cookie/views.py
@@ -1,21 +1,15 @@
+from djoser.views import TokenDestroyView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import SlidingToken
 
 from custom_auth.jwt_cookie.constants import JWT_SLIDING_COOKIE_KEY
 
 
-class JWTSlidingTokenLogoutView(APIView):
-    """
-    This view only invalidates the JWT cookie.
-    Currently, for clients which use token authentication, it's a no-op view.
-    """
-
+class JWTSlidingTokenLogoutView(TokenDestroyView):
     def post(self, request: Request) -> Response:
-        response = Response(status=HTTP_204_NO_CONTENT)
-        if isinstance(request.auth, SlidingToken):
-            request.auth.blacklist()
+        response = super().post(request)
+        if isinstance(jwt_token := request.auth, SlidingToken):
+            jwt_token.blacklist()
             response.delete_cookie(JWT_SLIDING_COOKIE_KEY)
         return response
