58e6478656634dd8ea00d3131c2c4eed7e55f10c
kyle-ssg <kyle@solidstategroup.com>
2024-10-08 15:01:41 +0100

    Merge

--- frontend/web/components/tags/AddEditTags.tsx
+++ frontend/web/components/tags/AddEditTags.tsx
@@ -214,25 +214,25 @@ const AddEditTags: FC<AddEditTagsType> = ({
                       {!readOnly &&
                         !!createEditTagPermission &&
                         !tag.is_system_tag && (
-                        <>
-                          <div
-                            onClick={() => editTag(tag)}
-                            className={
-                              !readOnly
-                                ? 'clickable'
-                                : 'opacity-0 pointer-events-none'
-                            }
-                          >
-                            <Icon name='setting' fill='#9DA4AE' />
-                          </div>
-                          <div
-                            onClick={() => confirmDeleteTag(tag)}
-                            className='ml-3 clickable'
-                          >
-                            <Icon name='trash-2' fill='#9DA4AE' />
-                          </div>
-                        </>
-                      )}
+                          <>
+                            <div
+                              onClick={() => editTag(tag)}
+                              className={
+                                !readOnly
+                                  ? 'clickable'
+                                  : 'opacity-0 pointer-events-none'
+                              }
+                            >
+                              <Icon name='setting' fill='#9DA4AE' />
+                            </div>
+                            <div
+                              onClick={() => confirmDeleteTag(tag)}
+                              className='ml-3 clickable'
+                            >
+                              <Icon name='trash-2' fill='#9DA4AE' />
+                            </div>
+                          </>
+                        )}
                     </Row>
                   </div>
                 ))}
