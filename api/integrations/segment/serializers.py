b47bebab51cc282739b1394f1c86a6c850a964a9
Zach Aysan <zachaysan@gmail.com>
2024-10-10 09:23:24 -0400

    feat: Set base url for segment (#4684)

--- api/integrations/segment/serializers.py
+++ api/integrations/segment/serializers.py
@@ -1,10 +1,23 @@
+from rest_framework import serializers
+
 from integrations.common.serializers import (
     BaseEnvironmentIntegrationModelSerializer,
 )
+from integrations.segment import constants
 from integrations.segment.models import SegmentConfiguration
 
 
 class SegmentConfigurationSerializer(BaseEnvironmentIntegrationModelSerializer):
+
+    base_url = serializers.ChoiceField(
+        choices=[
+            constants.DEFAULT_BASE_URL,
+            constants.DUBLIN_BASE_URL,
+        ],
+        required=False,
+        default="api.segment.io/",
+    )
+
     class Meta:
         model = SegmentConfiguration
-        fields = ("id", "api_key")
+        fields = ("id", "api_key", "base_url")
