2bc0bf4e09cf500d3bc9ffe246ce538dfc60632c
Zach Aysan <zachaysan@gmail.com>
2024-10-30 14:17:09 +0000

    Fix typing

--- api/organisations/subscriptions/metadata.py
+++ api/organisations/subscriptions/metadata.py
@@ -1,15 +1,12 @@
-import typing
-
-
 class BaseSubscriptionMetadata:
     payment_source = None
 
     def __init__(
         self,
         seats: int = 0,
-        api_calls: typing.Optional[int] = None,
-        projects: typing.Optional[int] = None,
-        chargebee_email: str = None,
+        api_calls: None | int = None,
+        projects: None | int = None,
+        chargebee_email: None | str = None,
     ):
         self.seats = seats
         self.api_calls = api_calls
