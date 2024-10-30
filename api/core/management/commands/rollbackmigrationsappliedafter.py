d4c909333dce7cca23aa3beac48332ba6a5a601c
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-10-30 15:11:42 +0000

    Add test coverage

--- api/core/management/commands/rollbackmigrationsappliedafter.py
+++ api/core/management/commands/rollbackmigrationsappliedafter.py
@@ -9,25 +9,36 @@ class Command(BaseCommand):
     """
     Rollback all migrations applied on or after a given datetime.
 
-    Usage: python manage.py rollbackmigrationsappliedafter --datetime 2024-10-24
+    Usage: python manage.py rollbackmigrationsappliedafter "2024-10-24 08:23:45"
     """
 
     def add_arguments(self, parser: ArgumentParser):
         parser.add_argument(
-            "--datetime",
+            "dt",
             type=str,
-            required=True,
             help="Rollback all migrations applied on or after this datetime (provided in ISO format)",
         )
 
-    def handle(self, *args, date: str, **kwargs) -> None:
+    def handle(self, *args, dt: str, **kwargs) -> None:
         try:
-            _date = datetime.fromisoformat(date)
+            _dt = datetime.fromisoformat(dt)
         except ValueError:
             raise CommandError("Date must be in ISO format")
 
-        applied_migrations = MigrationRecorder.Migration.objects.filter(applied__gte=_date)
+        applied_migrations = MigrationRecorder.Migration.objects.filter(applied__gte=_dt).order_by("applied")
+        if not applied_migrations.exists():
+            self.stdout.write(self.style.NOTICE("No migrations to rollback."))
+
+        # Since we've ordered by the date applied, we know that the first entry in the qs for each app
+        # is the earliest migration after the supplied date, i.e. we want to roll back to one migration
+        # earlier than this one.
+        earliest_migration_by_app = {}
         for migration in applied_migrations:
-            migration_number = migration.name.split("_", maxsplit=1)[0]
-            rollback_to = int(migration_number) - 1
-            call_command("migrate", migration.app, f"{rollback_to:04}")
+            if migration.app in earliest_migration_by_app:
+                continue
+            earliest_migration_by_app[migration.app] = migration.name
+
+        for app, migration_name in earliest_migration_by_app.items():
+            migration_number = int(migration_name.split("_", maxsplit=1)[0])
+            rollback_to = f"{migration_number - 1:04}" if migration_number > 1 else "zero"
+            call_command("migrate", app, rollback_to)
