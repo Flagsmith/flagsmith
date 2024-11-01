ecab02c4f30364e31888da5807441dd4d3357bc4
Zach Aysan <zachaysan@gmail.com>
2024-11-01 19:25:35 +0000

    Remove nested object TODO

--- api/organisations/subscriptions/licensing/licensing.py
+++ api/organisations/subscriptions/licensing/licensing.py
@@ -11,9 +11,8 @@ class LicenceInformation(BaseModel):
     department_name: typing.Optional[str] = None
     expiry_date: typing.Optional[datetime] = None
 
-    # TODO: should these live in a nested object?
     num_seats: int
     num_projects: int  # TODO: what about Flagsmith on Flagsmith project?
-    num_api_calls: typing.Optional[
-        int
-    ] = None  # required to support private cloud installs
+    num_api_calls: typing.Optional[int] = (
+        None  # required to support private cloud installs
+    )
