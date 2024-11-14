9bbfdf0d2bc4cb95fa0d029144c4bc61fb23f0bc
Kim Gustyr <kim.gustyr@flagsmith.com>
2024-11-14 08:00:09 +0000

    fix: Custom Gunicorn logger not sending StatsD stats (#4819)

--- api/tests/unit/util/test_logging.py
+++ api/tests/unit/util/test_logging.py
@@ -2,12 +2,13 @@ import json
 import logging
 import logging.config
 import os
+from datetime import datetime
 
 import pytest
-from gunicorn.config import Config
+from gunicorn.config import AccessLogFormat, Config
 from pytest_django.fixtures import SettingsWrapper
 
-from util.logging import JsonFormatter
+from util.logging import GunicornAccessLogJsonFormatter, JsonFormatter
 
 
 @pytest.mark.freeze_time("2023-12-08T06:05:47.320000+00:00")
@@ -28,7 +29,7 @@ def test_json_formatter__outputs_expected(
     expected_tb_string = (
         "Traceback (most recent call last):\n"
         f'  File "{expected_module_path}",'
-        " line 37, in _log_traceback\n"
+        " line 38, in _log_traceback\n"
         "    raise Exception()\nException"
     )
 
@@ -62,6 +63,66 @@ def test_json_formatter__outputs_expected(
             "exc_info": expected_tb_string,
         },
     ]
+
+
+@pytest.mark.freeze_time("2023-12-08T06:05:47.320000+00:00")
+def test_gunicorn_access_log_json_formatter__outputs_expected() -> None:
+    # Given
+    gunicorn_access_log_json_formatter = GunicornAccessLogJsonFormatter()
+    log_record = logging.LogRecord(
+        name="gunicorn.access",
+        level=logging.INFO,
+        pathname="",
+        lineno=1,
+        msg=AccessLogFormat.default,
+        args={
+            "a": "requests",
+            "b": "-",
+            "B": None,
+            "D": 1000000,
+            "f": "-",
+            "h": "192.168.0.1",
+            "H": None,
+            "l": "-",
+            "L": "1.0",
+            "m": "GET",
+            "M": 1000,
+            "p": "<42>",
+            "q": "foo=bar",
+            "r": "GET",
+            "s": 200,
+            "T": 1,
+            "t": datetime.fromisoformat("2023-12-08T06:05:47.320000+00:00").strftime(
+                "[%d/%b/%Y:%H:%M:%S %z]"
+            ),
+            "u": "-",
+            "U": "/test",
+        },
+        exc_info=None,
+    )
+    expected_pid = os.getpid()
+
+    # When
+    json_log = gunicorn_access_log_json_formatter.get_json_record(log_record)
+
+    # Then
+    assert json_log == {
+        "duration_in_ms": 1000,
+        "levelname": "INFO",
+        "logger_name": "gunicorn.access",
+        "message": '192.168.0.1 - - [08/Dec/2023:06:05:47 +0000] "GET" 200 - "-" "requests"',
+        "method": "GET",
+        "path": "/test?foo=bar",
+        "pid": "<42>",
+        "process_id": expected_pid,
+        "referer": "-",
+        "remote_ip": "192.168.0.1",
+        "status": "200",
+        "thread_name": "MainThread",
+        "time": "2023-12-08T06:05:47+00:00",
+        "timestamp": "2023-12-08 06:05:47,319",
+        "user_agent": "requests",
+    }
 
 
 def test_gunicorn_json_capable_logger__non_existent_setting__not_raises(
