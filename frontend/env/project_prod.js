8a1efa89dd17db0a45fdcdc8c0eb56394b52ace2
Kyle Johnson <kyle@solidstategroup.com>
2024-12-06 09:52:24 +0000

    chore: Send hubspot token to register endpoints (#4883)

--- frontend/env/project_prod.js
+++ frontend/env/project_prod.js
@@ -24,6 +24,7 @@ module.exports = global.Project = {
   hubspot: '//js-eu1.hs-scripts.com/143451822.js',
   // This is used for Sentry tracking
   maintenance: false,
+  linkedinConversionId: 16798338,
   plans: {
     scaleUp: { annual: 'scale-up-12-months-v2', monthly: 'scale-up-v2' },
     startup: { annual: 'start-up-12-months-v2', monthly: 'startup-v2' },
