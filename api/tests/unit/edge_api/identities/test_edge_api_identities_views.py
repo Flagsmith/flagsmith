001ca64b799b4c1b7b03680a5274c134a08d0eba
Zach Aysan <zachaysan@gmail.com>
2024-12-06 20:38:27 +0000

    Update call assertion

--- api/tests/unit/edge_api/identities/test_edge_api_identities_views.py
+++ api/tests/unit/edge_api/identities/test_edge_api_identities_views.py
@@ -161,7 +161,6 @@ def test_get_edge_identity_overrides_for_a_feature(
     mock_dynamodb_wrapper.get_identity_overrides_by_environment_id.assert_called_once_with(
         environment_id=environment.id,
         feature_id=feature.id,
-        feature_ids=None,
     )
 
 
