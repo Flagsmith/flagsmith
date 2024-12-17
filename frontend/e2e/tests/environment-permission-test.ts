3a59576122eafe9bb07da978ad2da522bd1225f8
kyle-ssg <kyle@solidstategroup.com>
2024-12-17 14:50:46 +0000

    Environment tests

--- frontend/e2e/tests/environment-permission-test.ts
+++ frontend/e2e/tests/environment-permission-test.ts
@@ -1,13 +1,13 @@
 import {
   byId,
-  click,
+  click, clickByText, createEnvironment,
   createFeature,
   gotoTraits,
   log,
-  login,
-  toggleFeature, waitForElementNotExist
-} from "../helpers.cafe";
-import { PASSWORD, E2E_NON_ADMIN_USER_WITH_ENV_PERMISSIONS } from '../config'
+  login, logout,
+  toggleFeature, waitForElementNotExist, waitForElementVisible,
+} from '../helpers.cafe';
+import { PASSWORD, E2E_NON_ADMIN_USER_WITH_ENV_PERMISSIONS, E2E_USER } from '../config';
 import { Selector, t } from 'testcafe'
 
 export default async function () {
@@ -18,13 +18,38 @@ export default async function () {
   await t
     .expect(Selector('#project-select-1').exists)
     .notOk('The element"#project-select-1" should not be present')
+  await logout()
+
   log('User with permissions can Handle the Features')
+  await login(E2E_NON_ADMIN_USER_WITH_ENV_PERMISSIONS, PASSWORD)
+  await click('#project-select-0')
   await createFeature(0, 'test_feature', false)
   await toggleFeature(0, true)
+  await logout()
+
   log('User without permissions cannot create traits')
+  await login(E2E_NON_ADMIN_USER_WITH_ENV_PERMISSIONS, PASSWORD)
+  await click('#project-select-0')
   await gotoTraits()
   const createTraitBtn = Selector(byId('add-trait'))
   await t.expect(createTraitBtn.hasAttribute('disabled')).ok()
+  await logout()
+
   log('User without permissions cannot see audit logs')
+  await login(E2E_NON_ADMIN_USER_WITH_ENV_PERMISSIONS, PASSWORD)
+  await click('#project-select-0')
   await waitForElementNotExist(byId('audit-log-link'))
+  await logout()
+
+  log('Create new environment')
+  await login(E2E_USER, PASSWORD)
+  await clickByText('My Test Project 6 Env Permission')
+  await createEnvironment('Production')
+  await logout()
+  log('User without permissions cannot see environment')
+  await waitForElementNotExist(byId('audit-log-link'))
+  await login(E2E_NON_ADMIN_USER_WITH_ENV_PERMISSIONS, PASSWORD)
+  await click('#project-select-0')
+  await waitForElementVisible(byId('switch-environment-development'))
+  await waitForElementNotExist(byId('switch-environment-production'))
 }
--- frontend/web/components/pages/EnvironmentSettingsPage.js
+++ frontend/web/components/pages/EnvironmentSettingsPage.js
@@ -67,20 +67,22 @@ const EnvironmentSettingsPage = class extends Component {
       { organisation_id: AccountStore.getOrganisation().id },
       { forceRefetch: true },
     ).then((roles) => {
-      getRoleEnvironmentPermissions(
-        getStore(),
-        {
-          env_id: env.id,
-          organisation_id: AccountStore.getOrganisation().id,
-          role_id: roles.data.results[0].id,
-        },
-        { forceRefetch: true },
-      ).then((res) => {
-        const matchingItems = roles.data.results.filter((item1) =>
-          res.data.results.some((item2) => item2.role === item1.id),
-        )
-        this.setState({ roles: matchingItems })
-      })
+      if (roles.data.results[0]) {
+        getRoleEnvironmentPermissions(
+          getStore(),
+          {
+            env_id: env.id,
+            organisation_id: AccountStore.getOrganisation().id,
+            role_id: roles.data.results[0].id,
+          },
+          { forceRefetch: true },
+        ).then((res) => {
+          const matchingItems = roles.data.results.filter((item1) =>
+            res.data.results.some((item2) => item2.role === item1.id),
+          )
+          this.setState({ roles: matchingItems })
+        })
+      }
     })
 
     if (Utils.getPlansPermission('METADATA')) {
