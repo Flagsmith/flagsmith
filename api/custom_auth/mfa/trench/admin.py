cfc951d5faa7f4aaa35bb92f8191718c2e9db4bb
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-01-14 14:08:27 +0000

    Add pragma no cover and make fields read only

--- api/custom_auth/mfa/trench/admin.py
+++ api/custom_auth/mfa/trench/admin.py
@@ -4,7 +4,7 @@ from custom_auth.mfa.trench.models import MFAMethod
 
 
 @admin.register(MFAMethod)
-class MFAMethodAdmin(admin.ModelAdmin):
+class MFAMethodAdmin(admin.ModelAdmin):  # pragma: no cover
     list_display = (
         "user_email",
         "name",
@@ -17,6 +17,23 @@ class MFAMethodAdmin(admin.ModelAdmin):
     list_filter = ("is_active", "is_primary")
 
     list_select_related = ("user",)
+
+    fields = (
+        "user",
+        "name",
+        "created_at",
+        "updated_at",
+        "is_active",
+        "is_primary",
+    )
+
+    readonly_fields = (
+        "user",
+        "name",
+        "created_at",
+        "updated_at",
+        "is_primary",
+    )
 
     def user_email(self, instance: MFAMethod) -> str:
         return instance.user.email
