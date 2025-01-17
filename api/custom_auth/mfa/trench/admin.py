13ff9d0abec6f55a53ade9437d6db95d8a7aa87c
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-01-17 11:57:25 +0000

    chore: MFA Method debugging / support improvements (#4989)

--- api/custom_auth/mfa/trench/admin.py
+++ api/custom_auth/mfa/trench/admin.py
@@ -4,5 +4,36 @@ from custom_auth.mfa.trench.models import MFAMethod
 
 
 @admin.register(MFAMethod)
-class MFAMethodAdmin(admin.ModelAdmin):
-    pass
+class MFAMethodAdmin(admin.ModelAdmin):  # pragma: no cover
+    list_display = (
+        "user_email",
+        "name",
+        "is_active",
+        "is_primary",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("user__email", "name")
+    list_filter = ("is_active", "is_primary")
+
+    list_select_related = ("user",)
+
+    fields = (
+        "user",
+        "name",
+        "created_at",
+        "updated_at",
+        "is_active",
+        "is_primary",
+    )
+
+    readonly_fields = (
+        "user",
+        "name",
+        "created_at",
+        "updated_at",
+        "is_primary",
+    )
+
+    def user_email(self, instance: MFAMethod) -> str:
+        return instance.user.email
