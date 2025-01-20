19e3a267823901e7c4af372fa907a704cce0003c
Tiago Fonseca <tiago@tiagopaiva.me>
2025-01-20 18:16:11 -0300

    fix: Disable invite button when email config is not set

--- api/app/utils.py
+++ api/app/utils.py
@@ -4,6 +4,12 @@ from functools import lru_cache
 from typing import TypedDict
 
 import shortuuid
+from app.settings.common import (
+    EMAIL_BACKEND,
+    EMAIL_HOST_USER,
+    SENDGRID_API_KEY,
+    AWS_SES_REGION_ENDPOINT,
+)
 
 UNKNOWN = "unknown"
 VERSIONS_INFO_FILE_LOCATION = ".versions.json"
@@ -12,6 +18,7 @@ VERSIONS_INFO_FILE_LOCATION = ".versions.json"
 class VersionInfo(TypedDict):
     ci_commit_sha: str
     image_tag: str
+    has_email_provider: bool
     is_enterprise: bool
     is_saas: bool
 
@@ -29,6 +36,17 @@ def is_saas() -> bool:
     return pathlib.Path("./SAAS_DEPLOYMENT").exists()
 
 
+def has_email_provider() -> bool:
+    match EMAIL_BACKEND:
+        case "django.core.mail.backends.smtp.EmailBackend":
+            return EMAIL_HOST_USER is not None
+        case "sgbackend.SendGridBackend":
+            return SENDGRID_API_KEY is not None
+        case "django_ses.SESBackend":
+            return AWS_SES_REGION_ENDPOINT is not None
+        case _:
+            return False
+
 @lru_cache
 def get_version_info() -> VersionInfo:
     """Reads the version info baked into src folder of the docker container"""
@@ -45,6 +63,7 @@ def get_version_info() -> VersionInfo:
     version_json = version_json | {
         "ci_commit_sha": _get_file_contents("./CI_COMMIT_SHA"),
         "image_tag": image_tag,
+        "has_email_provider": has_email_provider(),
         "is_enterprise": is_enterprise(),
         "is_saas": is_saas(),
     }
--- api/tests/unit/app/test_unit_app_utils.py
+++ api/tests/unit/app/test_unit_app_utils.py
@@ -45,6 +45,7 @@ def test_get_version_info(mocker: MockerFixture) -> None:
     assert result == {
         "ci_commit_sha": "some_sha",
         "image_tag": "2.66.2",
+        "has_email_provider": False,
         "is_enterprise": True,
         "is_saas": False,
         "package_versions": {".": "2.66.2"},
@@ -76,6 +77,75 @@ def test_get_version_info_with_missing_files(mocker: MockerFixture) -> None:
     assert result == {
         "ci_commit_sha": "unknown",
         "image_tag": "unknown",
+        "has_email_provider": False,
         "is_enterprise": True,
+        "is_saas": False,
+    }
+
+
+def test_get_version_info_with_email_config_smtp(mocker: MockerFixture) -> None:
+
+    mocker.patch(
+        "app.utils.EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend"
+    )
+    mocker.patch("app.utils.EMAIL_HOST_USER", "user")
+    # When
+    result = get_version_info()
+
+    # Then
+    assert result == {
+        "ci_commit_sha": "unknown",
+        "image_tag": "unknown",
+        "has_email_provider": True,
+        "is_enterprise": False,
+        "is_saas": False,
+    }
+
+
+def test_get_version_info_with_email_config_sendgrid(mocker: MockerFixture) -> None:
+
+    mocker.patch("app.utils.EMAIL_BACKEND", "sgbackend.SendGridBackend")
+    mocker.patch("app.utils.SENDGRID_API_KEY", "key")
+    # When
+    result = get_version_info()
+
+    # Then
+    assert result == {
+        "ci_commit_sha": "unknown",
+        "image_tag": "unknown",
+        "has_email_provider": True,
+        "is_enterprise": False,
+        "is_saas": False,
+    }
+
+
+def test_get_version_info_with_email_config_ses(mocker: MockerFixture) -> None:
+
+    mocker.patch("app.utils.EMAIL_BACKEND", "django_ses.SESBackend")
+    mocker.patch("app.utils.AWS_SES_REGION_ENDPOINT", "endpoint")
+    # When
+    result = get_version_info()
+
+    # Then
+    assert result == {
+        "ci_commit_sha": "unknown",
+        "image_tag": "unknown",
+        "has_email_provider": True,
+        "is_enterprise": False,
+        "is_saas": False,
+    }
+
+
+def test_get_version_info_without_email_config(mocker: MockerFixture) -> None:
+
+    # When
+    result = get_version_info()
+
+    # Then
+    assert result == {
+        "ci_commit_sha": "unknown",
+        "image_tag": "unknown",
+        "has_email_provider": False,
+        "is_enterprise": False,
         "is_saas": False,
     }
