44e8ee9c07e9d7a8449ed9e7014c0d9f6ce1ab67
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-01-22 16:46:44 +0000

    parametrise

--- api/tests/unit/app/test_unit_app_utils.py
+++ api/tests/unit/app/test_unit_app_utils.py
@@ -48,46 +48,25 @@ def test_get_version_info_with_missing_files(fs: FakeFilesystem) -> None:
     }
 
 
-def test_get_version_info_with_email_config_smtp(settings: SettingsWrapper) -> None:
+EMAIL_BACKENDS_AND_SETTINGS = [
+    ("django.core.mail.backends.smtp.EmailBackend", "EMAIL_HOST_USER"),
+    ("django_ses.SESBackend", "AWS_SES_REGION_ENDPOINT"),
+    ("sgbackend.SendGridBackend", "SENDGRID_API_KEY"),
+]
+
+
+@pytest.mark.parametrize(
+    "email_backend,expected_setting_name",
+    EMAIL_BACKENDS_AND_SETTINGS,
+)
+def test_get_version_info__email_config_enabled__return_expected(
+    settings: SettingsWrapper,
+    email_backend: str,
+    expected_setting_name: str,
+) -> None:
     # Given
-    settings.EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    settings.EMAIL_HOST_USER = "user"
-
-    # When
-    result = get_version_info()
-
-    # Then
-    assert result == {
-        "ci_commit_sha": "unknown",
-        "image_tag": "unknown",
-        "has_email_provider": True,
-        "is_enterprise": False,
-        "is_saas": False,
-    }
-
-
-def test_get_version_info_with_email_config_sendgrid(settings: SettingsWrapper) -> None:
-    # Given
-    settings.EMAIL_BACKEND = "sgbackend.SendGridBackend"
-    settings.SENDGRID_API_KEY = "key"
-
-    # When
-    result = get_version_info()
-
-    # Then
-    assert result == {
-        "ci_commit_sha": "unknown",
-        "image_tag": "unknown",
-        "has_email_provider": True,
-        "is_enterprise": False,
-        "is_saas": False,
-    }
-
-
-def test_get_version_info_with_email_config_ses(settings: SettingsWrapper) -> None:
-    # Given
-    settings.EMAIL_BACKEND = "django_ses.SESBackend"
-    settings.AWS_SES_REGION_ENDPOINT = "endpoint"
+    settings.EMAIL_BACKEND = email_backend
+    setattr(settings, expected_setting_name, "value")
 
     # When
     result = get_version_info()
@@ -97,23 +76,21 @@ def test_get_version_info_with_email_config_ses(settings: SettingsWrapper) -> No
 
 
 @pytest.mark.parametrize(
-    "email_backend,expected_empty_setting_name",
+    "email_backend,expected_setting_name",
     [
         (None, None),
-        ("django.core.mail.backends.smtp.EmailBackend", "EMAIL_HOST_USER"),
-        ("django_ses.SESBackend", "AWS_SES_REGION_ENDPOINT"),
-        ("sgbackend.SendGridBackend", "SENDGRID_API_KEY"),
+        *EMAIL_BACKENDS_AND_SETTINGS,
     ],
 )
-def test_get_version_info_without_email_config(
+def test_get_version_info__email_config_disabled__return_expected(
     settings: SettingsWrapper,
     email_backend: str | None,
-    expected_empty_setting_name: str | None,
+    expected_setting_name: str | None,
 ) -> None:
     # Given
     settings.EMAIL_BACKEND = email_backend
-    if expected_empty_setting_name:
-        setattr(settings, expected_empty_setting_name, None)
+    if expected_setting_name:
+        setattr(settings, expected_setting_name, None)
 
     # When
     result = get_version_info()
