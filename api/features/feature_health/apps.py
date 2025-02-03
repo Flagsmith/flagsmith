7ae54ec52bd86da7fb80676ffb1577acedd55958
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-02-03 15:51:15 +0000

    fix: Feature health event task not working (#5072)

--- api/features/feature_health/apps.py
+++ api/features/feature_health/apps.py
@@ -4,3 +4,10 @@ from core.apps import BaseAppConfig
 class FeatureHealthConfig(BaseAppConfig):
     name = "features.feature_health"
     default = True
+
+    def ready(self):
+        from features.feature_health.tasks import (  # noqa
+            update_feature_unhealthy_tag,
+        )
+
+        return super().ready()
