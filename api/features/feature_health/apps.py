95eca6b1f9ff5a64c0dae3b23f004e4c0b187a03
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-02-03 16:40:24 +0100

    fix: Feature health event creation task not working

--- api/features/feature_health/apps.py
+++ api/features/feature_health/apps.py
@@ -4,3 +4,10 @@ from core.apps import BaseAppConfig
 class FeatureHealthConfig(BaseAppConfig):
     name = "features.feature_health"
     default = True
+
+    def ready(self):
+        from features.feature_health.tasks import (  # noqa
+            send_feature_health_event,
+        )
+
+        return super().ready()
