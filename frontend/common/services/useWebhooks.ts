816ae82a0e0b1ea1ccd96ac31cfba9836fc02df8
Tiago Fonseca <tiago@tiagopaiva.me>
2025-02-11 17:51:13 -0300

    removes withWebhooks

--- frontend/common/services/useWebhooks.ts
+++ frontend/common/services/useWebhooks.ts
@@ -45,6 +45,49 @@ export const webhookService = service
     }),
   })
 
+export async function createWebhook(
+  store: any,
+  data: Req['createWebhook'],
+  options?: Parameters<
+    typeof webhookService.endpoints.createWebhook.initiate
+  >[1],
+) {
+  return store.dispatch(
+    webhookService.endpoints.createWebhook.initiate(data, options),
+  )
+}
+export async function getWebhooks(
+  store: any,
+  data: Req['getWebhooks'],
+  options?: Parameters<typeof webhookService.endpoints.getWebhooks.initiate>[1],
+) {
+  return store.dispatch(
+    webhookService.endpoints.getWebhooks.initiate(data, options),
+  )
+}
+export async function updateWebhook(
+  store: any,
+  data: Req['updateWebhook'],
+  options?: Parameters<
+    typeof webhookService.endpoints.updateWebhook.initiate
+  >[1],
+) {
+  return store.dispatch(
+    webhookService.endpoints.deleteWebhook.initiate(data, options),
+  )
+}
+export async function deleteWebhook(
+  store: any,
+  data: Req['deleteWebhook'],
+  options?: Parameters<
+    typeof webhookService.endpoints.deleteWebhook.initiate
+  >[1],
+) {
+  return store.dispatch(
+    webhookService.endpoints.deleteWebhook.initiate(data, options),
+  )
+}
+
 export const {
   useCreateWebhookMutation,
   useDeleteWebhookMutation,
