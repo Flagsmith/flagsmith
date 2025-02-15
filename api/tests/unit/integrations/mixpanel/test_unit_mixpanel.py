5ec27947e86969c4de0b908d93fcf7c91b234a6c
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-02-15 15:52:32 +0000

    improve test coverage, fix a bit of integrations typing

--- api/tests/unit/integrations/mixpanel/test_unit_mixpanel.py
+++ api/tests/unit/integrations/mixpanel/test_unit_mixpanel.py
@@ -1,8 +1,19 @@
+from logging import DEBUG
+from typing import TYPE_CHECKING
+
 from integrations.mixpanel.mixpanel import MIXPANEL_API_URL, MixpanelWrapper
 from integrations.mixpanel.models import MixpanelConfiguration
 
+if TYPE_CHECKING:
+    from pytest import LogCaptureFixture
+    from pytest_mock import MockerFixture
 
-def test_mixpanel_initialized_correctly():
+    from environments.identities.models import Identity
+    from features.models import Feature
+    from projects.models import Project
+
+
+def test_mixpanel_initialized_correctly() -> None:
     # Given
     config = MixpanelConfiguration(api_key="123key")
 
@@ -15,10 +26,58 @@ def test_mixpanel_initialized_correctly():
     assert mixpanel.api_key == config.api_key
 
 
-def test_mixpanel_generate_user_data(project, feature, identity):
+def test_mixpanel_identity_user__calls_expected(
+    mocker: "MockerFixture",
+    caplog: "LogCaptureFixture",
+    feature: "Feature",
+    identity: "Identity",
+) -> None:
+    # Given
+    caplog.set_level(DEBUG)
+    config = MixpanelConfiguration(api_key="123key")
+    feature_states = [*feature.feature_states.all()]
+
+    mixpanel = MixpanelWrapper(config)
+    expected_user_data = mixpanel.generate_user_data(
+        identity=identity,
+        feature_states=feature_states,
+        trait_models=[],
+    )
+    post_mock = mocker.patch("integrations.mixpanel.mixpanel.requests.post")
+    post_mock.return_value.status_code = 200
+    post_mock.return_value.text = expected_response_text = "test content"
+
+    # When
+    mixpanel._identify_user(expected_user_data)
+
+    # Then
+    post_mock.assert_called_once_with(
+        "https://api.mixpanel.com/engage#profile-set",
+        headers={"Accept": "text/plain", "X-Mixpanel-Integration-ID": "flagsmith"},
+        json=expected_user_data,
+    )
+    assert caplog.record_tuples == [
+        (
+            "integrations.mixpanel.mixpanel",
+            DEBUG,
+            "Sent event to Mixpanel. Response code was: 200",
+        ),
+        (
+            "integrations.mixpanel.mixpanel",
+            DEBUG,
+            f"Sent event to Mixpanel. Response content was: {expected_response_text}",
+        ),
+    ]
+
+
+def test_mixpanel_generate_user_data(
+    project: "Project",
+    feature: "Feature",
+    identity: "Identity",
+) -> None:
     # Given
     config = MixpanelConfiguration(api_key="123key")
-    feature_states = feature.feature_states.all()
+    feature_states = [*feature.feature_states.all()]
 
     mixpanel = MixpanelWrapper(config)
 
