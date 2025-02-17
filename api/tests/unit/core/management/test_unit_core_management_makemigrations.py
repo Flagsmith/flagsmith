d3e0bcc7841689b1ffba666d5c3edfa74254e6a4
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-02-17 12:26:42 +0000

    Fix tests

--- api/tests/unit/core/management/test_unit_core_management_makemigrations.py
+++ api/tests/unit/core/management/test_unit_core_management_makemigrations.py
@@ -7,13 +7,16 @@ def test_makemigrations_without_name_raises_error():
         call_command('makemigrations')
 
 
-def test_makemigrations_with_check_changes_runs_without_error(db: None):
+@pytest.mark.django_db(databases=["default", "analytics"])
+def test_makemigrations_with_check_changes_runs_without_error():
     call_command('makemigrations', check_changes=True)
 
 
-def test_makemigrations_with_dry_run_runs_without_error(db: None):
+@pytest.mark.django_db(databases=["default", "analytics"])
+def test_makemigrations_with_dry_run_runs_without_error():
     call_command('makemigrations', dry_run=True)
 
 
-def test_makemigrations_with_name_runs_without_error(db: None):
+@pytest.mark.django_db(databases=["default", "analytics"])
+def test_makemigrations_with_name_runs_without_error():
     call_command('makemigrations', name="some_useful_name")
