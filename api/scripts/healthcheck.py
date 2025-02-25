5933a6e1e38f26a91147e9e4cf28a724f45bda19
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-02-25 12:51:27 +0000

    feat: Switch existing task processor health checks to new liveness probe

--- api/scripts/healthcheck.py
+++ api/scripts/healthcheck.py
@@ -2,7 +2,13 @@ import sys
 
 import requests
 
-url = "http://localhost:8000/health/liveness"
-status = requests.get(url).status_code
 
-sys.exit(0 if 200 >= status < 300 else 1)
+def main() -> None:
+    url = "http://localhost:8000/health/liveness"
+    status = requests.get(url).status_code
+
+    sys.exit(0 if 200 >= status < 300 else 1)
+
+
+if __name__ == "__main__":
+    main()
