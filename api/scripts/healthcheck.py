ac086b47432db107bf25eda8511f46504563924d
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-04 13:47:40 +0000

    improve deprecated health check warning

--- api/scripts/healthcheck.py
+++ api/scripts/healthcheck.py
@@ -3,16 +3,23 @@ import sys
 
 import requests
 
+HEALTH_LIVENESS_URL = "http://localhost:8000/health/liveness"
+
+
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
-    logging.getLogger(__name__).warning(
-        "`python scripts/healthcheck.py` is deprecated. "
-        "Use the `health/liveness` endpoint instead."
+    logger.warning(
+        f"This healthcheck, invoked by {' '.join(sys.argv)}, is deprecated. "
+        f"Use the `{HEALTH_LIVENESS_URL}` endpoint instead."
     )
-    url = "http://localhost:8000/health/liveness"
-    status = requests.get(url).status_code
+    status_code = requests.get(HEALTH_LIVENESS_URL).status_code
 
-    sys.exit(0 if 200 >= status < 300 else 1)
+    if status_code != 200:
+        logger.error(f"Health check failed with status {status_code}")
+
+    sys.exit(0 if 200 >= status_code < 300 else 1)
 
 
 if __name__ == "__main__":
