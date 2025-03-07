9a9e2d139af5ea156f4ff3a32e725802b144240a
Tiago Fonseca <tiago@tiagopaiva.me>
2025-03-07 16:39:57 -0300

    feat: JWT token refresh
    discard frontend changes

--- api/custom_auth/jwt_cookie/authentication.py
+++ api/custom_auth/jwt_cookie/authentication.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.exceptions import (
 )
 from rest_framework_simplejwt.tokens import Token
 
-from custom_auth.jwt_cookie.constants import JWT_SLIDING_COOKIE_KEY
+from custom_auth.jwt_cookie.constants import ACCESS_TOKEN_COOKIE_KEY, REFRESH_TOKEN_COOKIE_KEY
 from users.models import FFAdminUser
 
 
@@ -16,10 +16,21 @@ class JWTCookieAuthentication(JWTAuthentication):
         return f'Cookie realm="{self.www_authenticate_realm}"'
 
     def authenticate(self, request: Request) -> tuple[FFAdminUser, Token] | None:
-        if raw_token := request.COOKIES.get(JWT_SLIDING_COOKIE_KEY):
+        raw_access_token = request.COOKIES.get(ACCESS_TOKEN_COOKIE_KEY)
+        raw_refresh_token = request.COOKIES.get(REFRESH_TOKEN_COOKIE_KEY)
+
+        if raw_access_token:
             try:
-                validated_token = self.get_validated_token(raw_token)  # type: ignore[arg-type]
-                return self.get_user(validated_token), validated_token  # type: ignore[return-value]
+                validated_access_token = self.get_validated_token(raw_access_token)  # type: ignore[arg-type]
+                return self.get_user(validated_access_token), validated_access_token  # type: ignore[return-value]
             except (InvalidToken, TokenError, AuthenticationFailed):
-                return None
+                pass
+
+        if raw_refresh_token:
+            try:
+                validated_refresh_token = self.get_validated_token(raw_refresh_token)  # type: ignore[arg-type]
+                return self.get_user(validated_refresh_token), validated_refresh_token  # type: ignore[return-value]
+            except (InvalidToken, TokenError, AuthenticationFailed):
+                pass
+
         return None
