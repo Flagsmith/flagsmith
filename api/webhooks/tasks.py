e8b203be0ab9e2547002fec7c651d32aa45a94cb
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-11 15:05:48 +0000

    fix: Some tasks are not being initialised (#5213)

---
+++ api/webhooks/tasks.py
@@ -0,0 +1,25 @@
+from task_processor.decorators import (  # type: ignore[import-untyped]
+    register_task_handler,
+)
+
+from webhooks.webhooks import (
+    call_environment_webhooks as call_environment_webhooks_service,
+)
+from webhooks.webhooks import (
+    call_organisation_webhooks as call_organisation_webhooks_service,
+)
+from webhooks.webhooks import (
+    call_webhook_with_failure_mail_after_retries as call_webhook_with_failure_mail_after_retries_service,
+)
+
+call_environment_webhooks = register_task_handler()(
+    call_environment_webhooks_service,
+)
+
+call_organisation_webhooks = register_task_handler()(
+    call_organisation_webhooks_service,
+)
+
+call_webhook_with_failure_mail_after_retries = register_task_handler()(
+    call_webhook_with_failure_mail_after_retries_service,
+)
