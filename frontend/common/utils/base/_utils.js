95235698039f2b7b5ba8fa8f29a965d0c9494019
Rodrigo López Dato <rodrigo.lopezdato@flagsmith.com>
2025-03-19 11:40:20 -0300

    Parse querystring using URL methods and not JSON

--- frontend/common/utils/base/_utils.js
+++ frontend/common/utils/base/_utils.js
@@ -393,19 +393,20 @@ const Utils = {
 emailRegex: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i,
 
   fromParam(str) {
-    // {min:100,max:200} <- ?min=100&max=200
-    const documentSearch =
-      typeof document === 'undefined' ? '' : document.location.search
+    const searchParams = new URLSearchParams(str || document?.location.search)
+    const result = {}
 
-    if (!str && !documentSearch) {
-      return {}
+    for (const [key, value] of searchParams.entries()) {
+      if (key) {
+        try {
+          result[key] = decodeURIComponent(value)
+        } catch (e) {
+          result[key] = value
+        }
+      }
     }
-    // eslint-disable-next-line
-        const urlString = (str || documentSearch).replace(/(^\?)/, '');
-    return JSON.parse(
-      `{"${urlString.replace(/&/g, '","').replace(/=/g, '":"')}"}`,
-      (key, value) => (key === '' ? value : decodeURIComponent(value)),
-    )
+
+    return result
   },
 
   getPaging(currentPage, totalPages, rangeAround) {
