3471d77833f08cde152e7af3de211e7a27fcffc7
Joscha Feth <joscha@feth.com>
2025-03-27 15:59:42 +0000

    fix: Syntax error in NodeJS "Try it" code snippets (#5252)

--- frontend/common/code-help/create-user/create-user-node.js
+++ frontend/common/code-help/create-user/create-user-node.js
@@ -2,24 +2,27 @@ import Constants from 'common/constants'
 
 module.exports = (
   envId,
-  { FEATURE_NAME, FEATURE_NAME_ALT, LIB_NAME, NPM_NODE_CLIENT, USER_ID },
+  { FEATURE_NAME, FEATURE_NAME_ALT, LIB_NAME, NPM_NODE_CLIENT, USER_ID, TRAIT_NAME },
   userId,
 ) => `import Flagsmith from "${NPM_NODE_CLIENT}"; // Add this line if you're using ${LIB_NAME} via npm
 
 const ${LIB_NAME} = new Flagsmith({${
-  Constants.isCustomFlagsmithUrl() &&
-  `\n    apiUrl: '${Constants.getFlagsmithSDKUrl()}',`
+  Constants.isCustomFlagsmithUrl()
+    ? `\n    apiUrl: '${Constants.getFlagsmithSDKUrl()}',`
+    : ''
 }
     environmentKey: '${envId}'
 });
 
+// Optional - set traits for this identity
+const traits = { ${TRAIT_NAME}: 42 };
 
 // Identify the user
 const flags = await flagsmith.getIdentityFlags('${
   userId || USER_ID
-}', traitList);
+}', traits);
 
 // get the state / value of the user's flags 
-var isEnabled = flags.isFeatureEnabled('${FEATURE_NAME}');
-var featureValue = flags.getFeatureValue('${FEATURE_NAME_ALT}');
+const isEnabled = flags.isFeatureEnabled('${FEATURE_NAME}');
+const featureValue = flags.getFeatureValue('${FEATURE_NAME_ALT}');
 `
