194acae3c6fcf3e88d354f90f551dbf13d2b03f6
kyle-ssg <kyle@solidstategroup.com>
2025-04-01 11:31:50 +0100

    Only run amplitude in dom

--- docs/plugins/amplitude.js
+++ docs/plugins/amplitude.js
@@ -1,2 +1,6 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import amplitude from '@amplitude/analytics-browser';
-amplitude.init('541e058ce750dcdeb3a39d48a85f3425');
+
+if (ExecutionEnvironment.canUseDOM) {
+    amplitude.init('541e058ce750dcdeb3a39d48a85f3425');
+}
