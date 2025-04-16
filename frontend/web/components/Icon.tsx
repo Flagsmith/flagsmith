5e9521cdf22be52a9443b629d280d74cb254b0f4
kyle-ssg <kyle@solidstategroup.com>
2025-04-16 16:38:00 +0100

    Getting started designs

--- frontend/web/components/Icon.tsx
+++ frontend/web/components/Icon.tsx
@@ -62,6 +62,7 @@ export type IconName =
   | 'settings-2'
   | 'shield'
   | 'star'
+  | 'link'
   | 'sun'
   | 'timer'
   | 'trash-2'
@@ -81,6 +82,27 @@ export type IconType = React.DetailedHTMLProps<
 
 const Icon: FC<IconType> = ({ fill, fill2, height, name, width, ...rest }) => {
   switch (name) {
+    case 'link': {
+      return (
+        <svg
+          width='16'
+          height='16'
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <g id='Outlined/external-link'>
+            <path
+              id='Icon'
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M19 18V12C19 11.448 19.447 11 20 11C20.553 11 21 11.448 21 12V18C21 19.654 19.654 21 18 21H6C4.346 21 3 19.654 3 18V6C3 4.346 4.346 3 6 3H12C12.553 3 13 3.448 13 4C13 4.552 12.553 5 12 5H6C5.448 5 5 5.449 5 6V18C5 18.551 5.448 19 6 19H18C18.552 19 19 18.551 19 18ZM17.5781 5.008L15.9951 5C15.4421 4.997 14.9971 4.547 15.0001 3.995C15.0031 3.444 15.4501 3 16.0001 3H16.0051L20.0021 3.02C20.5521 3.023 20.9971 3.469 20.9971 4.019L21.0001 7.999C21.0001 8.552 20.5531 9 20.0011 9H20.0001C19.4481 9 19.0001 8.553 19.0001 8.001L18.9991 6.415L12.7071 12.707C12.5121 12.902 12.2561 13 12.0001 13C11.7441 13 11.4881 12.902 11.2931 12.707C10.9021 12.316 10.9021 11.684 11.2931 11.293L17.5781 5.008Z'
+              fill='rgb(104, 55, 252)'
+            />
+          </g>
+        </svg>
+      )
+    }
     case 'shield': {
       return (
         <svg
--- frontend/web/components/pages/GettingStartedPage.tsx
+++ frontend/web/components/pages/GettingStartedPage.tsx
@@ -30,12 +30,12 @@ const GettingStartedItem: FC<GettingStartedItemType> = ({ data }) => {
   const { complete, cta, description, duration, link, title } = data
   return (
     <div className='col-md-4 col-lg-2'>
-      <div className='card bg-white h-100 shadow rounded'>
-        <div
-          className={classNames('p-3 d-flex h-100 flex-column', {
-            'opacity-50': complete,
-          })}
-        >
+      <div
+        className={classNames('card bg-card h-100 shadow rounded', {
+          'opacity-50': complete,
+        })}
+      >
+        <div className={classNames('p-3 d-flex h-100 flex-column mx-0')}>
           <div className='d-flex mb-2'>
             <span className='chip chip-secondary d-flex gap-1  align-items-center lh-1 chip chip--xs'>
               <Icon className='chip-svg-icon' width={14} name={'clock'} />
@@ -43,6 +43,7 @@ const GettingStartedItem: FC<GettingStartedItemType> = ({ data }) => {
             </span>
           </div>
           <h5
+            style={{ height: 50 }}
             className={`d-flex align-items-center gap-1 ${
               complete ? 'text-success' : ''
             }`}
@@ -79,13 +80,6 @@ const GettingStartedPage: FC<ResourcesPageType> = ({}) => {
   const items = [
     {
       complete: true,
-      description: 'Everything you need to get up-and-running with Flagsmith',
-      duration: 5,
-      link: '/',
-      title: 'Quick Start Guide',
-    },
-    {
-      complete: true,
       cta: 'Create Project',
       description:
         'Projects let you create and manage a set of features and configure them between multiple app environments',
@@ -94,12 +88,29 @@ const GettingStartedPage: FC<ResourcesPageType> = ({}) => {
       title: 'Create a Project',
     },
     {
+      complete: true,
       cta: 'Create Feature',
       description:
         'The first step to using feature flags is to create one in the features page',
       duration: 1,
       link: '/',
-      title: 'Create a Feature Flag',
+      title: 'Create a Feature',
+    },
+    {
+      complete: true,
+      cta: 'Create a Segment',
+      description:
+        'Once your features are setup you can target their rollout with segments',
+      duration: 1,
+      link: '/',
+      title: 'Create a Segment',
+    },
+    {
+      complete: true,
+      description: 'Everything you need to get up-and-running with Flagsmith',
+      duration: 5,
+      link: '/',
+      title: 'Quick Start Guide',
     },
     {
       description: "Familiarise yourself with Flagsmith's features",
@@ -116,7 +127,7 @@ const GettingStartedPage: FC<ResourcesPageType> = ({}) => {
     },
   ]
   return (
-    <div className='bg-light100'>
+    <div className='bg-light100 pb-5'>
       <div className='container-fluid px-3'>
         <PageTitle title={'Welcome to Flagsmith'}>
           Here's all the useful links to get you started, if you have any
@@ -131,42 +142,92 @@ const GettingStartedPage: FC<ResourcesPageType> = ({}) => {
           </Button>
           !
         </PageTitle>
-        <h6 className='mb-4'>Try things out</h6>
+        <h5 className='mb-4 mt-5'>Getting Started</h5>
         <div className='row '>
           {items.map((v) => (
             <GettingStartedItem data={v} />
           ))}
         </div>
-        <h6 className='my-4'>Resources</h6>
-        <div className='d-flex gap-4 flex-md-row flex-column'>
-          <div>
-            <div className='card bg-white p-3 h-100 shadow rounded'>
+        <h5 className='mb-4 mt-5'>Resources</h5>
+        <div className='row gap-4 '>
+          <div className='col-lg-2'>
+            <div className='card bg-card p-3  shadow rounded'>
               <h6 className='d-flex gap-1 mb-3 align-items-center'>
                 Community Links
               </h6>
               <div className='d-flex flex-column align-items-start gap-2'>
                 <Button theme='text' href='https://github.com/flagsmith'>
+                  <Icon name='link' />
                   Find us on GitHub
                 </Button>
                 <Button theme='text' href='https://github.com/flagsmith'>
+                  <Icon name='link' />
                   Contribution Guidelines
                 </Button>
                 <Button theme='text' href='https://github.com/flagsmith'>
+                  <Icon name='link' />
                   Chat with Developers on Discord
                 </Button>
                 <Button theme='text' href='https://github.com/flagsmith'>
-                  Listen to the Open Source podcast
+                  <Icon name='link' />
+                  Listen to our Podcast
                 </Button>
               </div>
             </div>
           </div>
-          <div className='row flex-1'>
-            <div className='col-md-6 h-100'>
-              <div className='card h-100 w-100 bg-white p-3 h-100 shadow rounded'></div>
-            </div>
-            <div className='col-md-6 h-100'>
-              <div className='card bg-white p-3 h-100 shadow rounded'></div>
-            </div>
+          <div className='col-lg-9 row'>
+            {[
+              {
+                'description':
+                  'Set yourself up for success with these best practices.',
+                'image': '/static/images/welcome/featured1.png',
+                'title':
+                  'eBook | Unlock Modern Software Development with Feature Flags',
+                url: 'https://www.flagsmith.com/ebook/flip-the-switch-on-modern-software-development-with-feature-flags?utm_source=app',
+              },
+              {
+                'description':
+                  'Set yourself up for success with these best practices.',
+                'image': '/static/images/welcome/featured2.png',
+                'title':
+                  'eBook | Unlock Modern Software Development with Feature Flags',
+                url: 'https://www.flagsmith.com/ebook/flip-the-switch-on-modern-software-development-with-feature-flags?utm_source=app',
+              },
+              {
+                'description':
+                  'Set yourself up for success with these best practices.',
+                'image': '/static/images/welcome/featured1.png',
+                'title':
+                  'eBook | Unlock Modern Software Development with Feature Flags',
+                url: 'https://www.flagsmith.com/ebook/flip-the-switch-on-modern-software-development-with-feature-flags?utm_source=app',
+              },
+            ].map((v) => (
+              <div className='col-lg-4 h-100'>
+                <div className='card bg-card p-0 h-100 shadow rounded'>
+                  <div className='row'>
+                    <div className='col-md-5'>
+                      <img className='img-fluid rounded' src={v.image} />
+                    </div>
+                    <div className='col-md-6 p-3'>
+                      <a
+                        href={v.url}
+                        target='_blank'
+                        className=''
+                        rel='noreferrer'
+                      >
+                        <h6 className={`d-flex align-items-center gap-1`}>
+                          {v.title}
+                        </h6>
+
+                        <h6 className='fw-normal d-flex text-muted flex-1'>
+                          {v.description}
+                        </h6>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
