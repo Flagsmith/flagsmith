7cf0cb032f71d9e2d08db539bfc151ce5123ad46
Evandro Myller <evandro.myller@flagsmith.com>
2025-04-23 11:35:00 -0300

    feat: Include current API version to all HTTP responses

---
+++ api/tests/unit/api/test_api_version_http_header.py
@@ -0,0 +1,69 @@
+import pytest
+from pyfakefs.fake_filesystem import FakeFilesystem
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from common.core.utils import get_file_contents, get_versions_from_manifest
+
+
+@pytest.fixture(autouse=True)
+def clear_lru_caches() -> None:
+    get_file_contents.cache_clear()
+    get_versions_from_manifest.cache_clear()
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "/robots.txt",
+        # TODO: Add other GET-only URLs
+    ],
+)
+@pytest.mark.parametrize(
+    "version_file_contents, expected_version",
+    [
+        ('{".": "v1.2.3"}', "v1.2.3"),
+        ('{"foo": "bar"}', "unknown"),
+        ('', "unknown"),
+    ],
+)
+def test_api_version_is_added_to_success_response_headers(
+    client: APIClient,
+    expected_version: str,
+    fs: FakeFilesystem,
+    url: str,
+    version_file_contents: str,
+) -> None:
+    fs.create_file(".versions.json", contents=version_file_contents)
+
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.headers["X-Flagsmith-Version"] == expected_version
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "/wat",
+    ],
+)
+@pytest.mark.parametrize(
+    "version_file_contents, expected_version",
+    [
+        ('{".": "v1.2.3"}', "v1.2.3"),
+        ('{"foo": "bar"}', "unknown"),
+        ('', "unknown"),
+    ],
+)
+def test_api_version_is_added_to_error_response_headers(
+    client: APIClient,
+    expected_version: str,
+    fs: FakeFilesystem,
+    url: str,
+    version_file_contents: str,
+) -> None:
+    fs.create_file(".versions.json", contents=version_file_contents)
+
+    response = client.get(url)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.headers["X-Flagsmith-Version"] == expected_version
