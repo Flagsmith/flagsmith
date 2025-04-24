711218a9765674fac259641c8033daf687d9b5ea
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-04-24 18:19:06 +0100

    `flagsmith healthcheck`

--- api/scripts/healthcheck.py
+++ api/scripts/healthcheck.py
@@ -1,32 +1,11 @@
-import argparse
-import socket
+import logging
+import sys
 
-parser = argparse.ArgumentParser(
-    description="Check if the API is able to accept local TCP connections.",
-)
-parser.add_argument(
-    "--port",
-    "-p",
-    type=int,
-    default=8000,
-    help="Port to check the API on (default: 8000)",
-)
-parser.add_argument(
-    "--timeout",
-    "-t",
-    type=int,
-    default=1,
-    help="Socket timeout for the connection attempt in seconds (default: 1)",
-)
-
-
-def main() -> None:
-    args = parser.parse_args()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(args.timeout)
-    sock.connect(("localhost", args.port))
-    sock.close()
-
+from common.core.main import main
 
 if __name__ == "__main__":
-    main()
+    logging.getLogger(__name__).warning(
+        f"This healthcheck, invoked by `{' '.join(sys.argv)}``, is deprecated. "
+        "Please use one of the `flagsmith healthcheck` commands instead."
+    )
+    main(["flagsmith", "healthcheck", "http"])
