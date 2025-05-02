f647e1751cb7c71c44074537e56b824ff62440fc
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-05-02 17:12:51 +0100

    tuple -> list

--- api/tests/integration/custom_auth/end_to_end/test_custom_auth_integration.py
+++ api/tests/integration/custom_auth/end_to_end/test_custom_auth_integration.py
@@ -704,7 +704,7 @@ def test_cannot_create_superuser_if_any_user_exists(
     assert FFAdminUser.objects.filter(email=email).exists() is False
 
 
-@pytest.mark.parametrize("marketing_consent_given", (None, True, False))
+@pytest.mark.parametrize("marketing_consent_given", [None, True, False])
 def test_marketing_consent_given_defaults_to_true(
     api_client: APIClient,
     marketing_consent_given: bool | None,
