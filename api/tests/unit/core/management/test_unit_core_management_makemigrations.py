af6e1af24283fa61e00a121e741f63b8c88ccb3f
Gagan <gagandeeptrivedi47@gmail.com>
2025-05-14 13:01:39 +0530

    fix: misc test fixes (#5450)

--- api/tests/unit/core/management/test_unit_core_management_makemigrations.py
+++ api/tests/unit/core/management/test_unit_core_management_makemigrations.py
@@ -1,5 +1,8 @@
 import pytest
+from django.conf import settings
 from django.core.management import CommandError, call_command
+
+DATABASES_LIST = list(settings.DATABASES.keys())
 
 
 def test_makemigrations_without_name_raises_error() -> None:
@@ -7,16 +10,16 @@ def test_makemigrations_without_name_raises_error() -> None:
         call_command("makemigrations")
 
 
-@pytest.mark.django_db(databases=["default", "analytics"])
+@pytest.mark.django_db(databases=DATABASES_LIST)
 def test_makemigrations_with_check_changes_runs_without_error() -> None:
     call_command("makemigrations", check_changes=True)
 
 
-@pytest.mark.django_db(databases=["default", "analytics"])
+@pytest.mark.django_db(databases=DATABASES_LIST)
 def test_makemigrations_with_dry_run_runs_without_error() -> None:
     call_command("makemigrations", dry_run=True)
 
 
-@pytest.mark.django_db(databases=["default", "analytics"])
+@pytest.mark.django_db(databases=DATABASES_LIST)
 def test_makemigrations_with_name_runs_without_error() -> None:
     call_command("makemigrations", name="some_useful_name")
