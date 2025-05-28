82ceeb5f7e3b873cd611595b6a1495fdbd16b3ab
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-05-28 18:38:08 +0100

    clarify types

--- api/integrations/launch_darkly/exceptions.py
+++ api/integrations/launch_darkly/exceptions.py
@@ -8,6 +8,6 @@ class LaunchDarklyAPIError(Exception):
 class LaunchDarklyRateLimitError(LaunchDarklyAPIError):
     """Exception raised when the LaunchDarkly API rate limit is exceeded."""
 
-    def __init__(self, retry_at: datetime | None = None):
+    def __init__(self, retry_at: datetime):
         super().__init__()
         self.retry_at = retry_at
