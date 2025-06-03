d4caa48e1af56a70376afc1861d9edd5b521f03b
Tiago Fonseca <tiago@tiagopaiva.me>
2025-06-03 01:45:28 -0300

    adds todo

--- frontend/web/components/release-pipelines/constants.ts
+++ frontend/web/components/release-pipelines/constants.ts
@@ -18,6 +18,7 @@ const FLAG_ACTION_OPTIONS = [
   },
 ]
 
+// TODO: Validate value to be sent in the request
 const TIME_UNIT_OPTIONS = [
   { label: 'Day(s)', value: 'day' },
   { label: 'Hour(s)', value: 'hour' },
