a1ad812214122561b84335ea560102cca73d671a
wadii <wadii.zaim@flagsmith.com>
2025-06-03 14:55:52 +0200

    feat: implemented-links-and-tooltip-content

--- frontend/web/components/metrics/EnvironmentMetric.tsx
+++ frontend/web/components/metrics/EnvironmentMetric.tsx
@@ -1,11 +1,38 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import Tooltip from 'components/Tooltip'
+import Icon from 'components/Icon'
 
 interface EnvironmentMetricProps {
   label: string
   value: string | number
+  link?: string
+  tooltip?: string
 }
 
-const EnvironmentMetric: FC<EnvironmentMetricProps> = ({ label, value }) => {
+const EnvironmentMetric: FC<EnvironmentMetricProps> = ({
+  label,
+  link,
+  tooltip,
+  value,
+}) => {
+  const LabelWithTooltip = () => {
+    return (
+      <>
+        {label}{' '}
+        {tooltip && (
+          <Tooltip
+            title={<Icon name='info-outlined' width={12} height={12} />}
+            place='top'
+            titleClassName='cursor-pointer'
+          >
+            {tooltip}
+          </Tooltip>
+        )}
+      </>
+    )
+  }
+
   return (
     <div
       style={{
@@ -17,7 +44,17 @@ const EnvironmentMetric: FC<EnvironmentMetricProps> = ({ label, value }) => {
         textAlign: 'center',
       }}
     >
-      <p className='metric-label'>{label}</p>
+      <div className='d-flex items-center gap-1'>
+        {link ? (
+          <Link to={link} className='metric-label'>
+            <LabelWithTooltip />
+          </Link>
+        ) : (
+          <p className='metric-label'>
+            <LabelWithTooltip />
+          </p>
+        )}
+      </div>
       <p className='metric-value'>{value}</p>
     </div>
   )
