f55ae79ff6e98482fbee7e05e6a120c1d02197a0
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-06-11 16:06:11 +0100

    Handle unrecognised analytics data

--- api/app_analytics/serializers.py
+++ api/app_analytics/serializers.py
@@ -1,6 +1,14 @@
 import typing
 
+from django.conf import settings
 from rest_framework import serializers
+
+from app_analytics.tasks import (
+    track_feature_evaluation_influxdb_v2,
+    track_feature_evaluation_v2,
+)
+from environments.models import Environment
+from features.models import FeatureState
 
 from .types import PERIOD_TYPE
 
@@ -37,3 +45,38 @@ class SDKAnalyticsFlagsSerializerDetail(serializers.Serializer):  # type: ignore
 
 class SDKAnalyticsFlagsSerializer(serializers.Serializer):  # type: ignore[type-arg]
     evaluations = SDKAnalyticsFlagsSerializerDetail(many=True)
+
+    def validate(self, attrs: dict[str, typing.Any]) -> dict[str, typing.Any]:
+        request = self.context["request"]
+        environment_feature_names = set(
+            FeatureState.objects.filter(
+                environment=request.environment,
+                feature_segment=None,
+                identity=None,
+            ).values_list("feature__name", flat=True)
+        )
+        return {
+            "evaluations": [
+                evaluation
+                for evaluation in attrs["evaluations"]
+                if evaluation["feature_name"] in environment_feature_names
+            ]
+        }
+
+    def save(self, **kwargs: typing.Any) -> typing.Any:
+        environment: Environment = kwargs["environment"]
+
+        if settings.USE_POSTGRES_FOR_ANALYTICS:
+            track_feature_evaluation_v2.delay(
+                args=(
+                    environment.id,
+                    self.validated_data["evaluations"],
+                )
+            )
+        elif settings.INFLUXDB_TOKEN:
+            track_feature_evaluation_influxdb_v2.delay(
+                args=(
+                    environment.id,
+                    self.validated_data["evaluations"],
+                )
+            )
