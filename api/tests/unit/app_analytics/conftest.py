f89e47dd11712aece4d6fd9ef2751c690e78f63b
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-06-19 17:15:20 +0100

    cleanup, improve coverage

--- api/tests/unit/app_analytics/conftest.py
+++ api/tests/unit/app_analytics/conftest.py
@@ -18,5 +18,5 @@ def skip_if_no_analytics_db_marked(request: pytest.FixtureRequest) -> None:
     Automatically skip tests that are marked with 'skip_if_no_analytics_db'.
     This allows for selective skipping of tests based on the database configuration.
     """
-    if request.node.get_closest_marker("skip_if_no_analytics_db"):
+    if request.node.get_closest_marker("skip_if_no_analytics_db"):  # pragma: no cover
         request.getfixturevalue("skip_if_no_analytics_db")
