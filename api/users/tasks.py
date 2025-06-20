b8fee4af70c63e6f3f7262170dba7d5aa9773b17
Zaimwa9 <wadii.zaim@flagsmith.com>
2025-06-20 12:17:21 +0200

    chore: api remove pipedrive (#5626)
    Co-authored-by: Matthew Elwell <matthew.elwell@flagsmith.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- api/users/tasks.py
+++ api/users/tasks.py
@@ -4,18 +4,6 @@ from task_processor.decorators import (
     register_task_handler,
 )
 
-from integrations.lead_tracking.pipedrive.lead_tracker import (
-    PipedriveLeadTracker,
-)
-from users.models import FFAdminUser
-
-
-@register_task_handler()
-def create_pipedrive_lead(user_id: int):  # type: ignore[no-untyped-def]
-    pipedrive = PipedriveLeadTracker()
-    user = FFAdminUser.objects.get(id=user_id)
-    pipedrive.create_organisation_lead(user)
-
 
 @register_task_handler()
 def send_email_changed_notification_email(  # type: ignore[no-untyped-def]
