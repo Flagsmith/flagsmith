58e01c06185d675b7133b1684d24fafa14b40c90
Robert Norrie <robert.norrie@flagsmith.com>
2025-06-23 09:34:38 +0100

    feat: Publish Release Pipeline
    docs: adding old banner image back to repo as it's referenced in SDK repo readme files (#5637)
    
    change action detail
    
    handle shallow comparison
    
    feat: Adds wait for trigger functionality to release pipelines
    
    wip
    
    creates release pipe detail components
    
    feat: Create and List Pipelines
    
    adds endpoints
    
    list release pipelines
    
    adds list view
    
    adds view state for release pipelines
    
    fix missing curly brace
    
    changed actions and trigger schema
    
    fix action body
    
    creation and list
    
    adds todo
    
    wait-for field
    
    feat: Publish Release Pipeline
    
    remove comments
    
    invalidate cache
    
    fix always true condition in CreateFlag.js
    
    final implementation
    
    fix permission
    
    set correct permission

---
+++ frontend/web/components/release-pipelines/AddToReleasePipelineModal.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from 'react'
+import {
+  useAddFeatureToReleasePipelineMutation,
+  useGetReleasePipelinesQuery,
+} from 'common/services/useReleasePipelines'
+import { ReleasePipeline } from 'common/types/responses'
+import Utils from 'common/utils/utils'
+import Button from 'components/base/forms/Button'
+import InputGroup from 'components/base/forms/InputGroup'
+
+type AddToReleasePipelineModalProps = {
+  projectId: string
+  featureId: number
+}
+
+const AddToReleasePipelineModal = ({
+  featureId,
+  projectId,
+}: AddToReleasePipelineModalProps) => {
+  const [selectedReleasePipeline, setSelectedReleasePipeline] =
+    useState<string>('')
+  const { data: releasePipelines } = useGetReleasePipelinesQuery({
+    page: 1,
+    page_size: 100,
+    projectId: Number(projectId),
+  })
+
+  const [
+    addFeatureToReleasePipeline,
+    { error, isError, isLoading, isSuccess },
+  ] = useAddFeatureToReleasePipelineMutation()
+
+  const releasePipelinesOptions = releasePipelines?.results?.map(
+    (pipeline: ReleasePipeline) => ({
+      label: pipeline.name,
+      value: pipeline.id,
+    }),
+  )
+
+  const handleSelect = (option: { label: string; value: string }) => {
+    setSelectedReleasePipeline(option.value)
+  }
+
+  const handleAddToReleasePipeline = () => {
+    const data = {
+      featureId: Number(featureId),
+      pipelineId: Number(selectedReleasePipeline),
+      projectId: Number(projectId),
+    }
+    addFeatureToReleasePipeline(data)
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      closeModal2()
+      toast('Feature added to release pipeline')
+      return
+    }
+
+    if (isError) {
+      toast(
+        error?.data?.detail ?? 'Error adding feature to release pipeline',
+        'danger',
+      )
+      return
+    }
+  }, [isSuccess, isError, error])
+
+  return (
+    <div className='p-4'>
+      <InputGroup
+        title='Release Pipeline'
+        component={
+          <Select
+            options={releasePipelinesOptions}
+            onChange={handleSelect}
+            value={Utils.toSelectedValue(
+              selectedReleasePipeline,
+              releasePipelinesOptions,
+              { label: 'Select Release Pipeline', value: '' },
+            )}
+          />
+        }
+      />
+      <div className='text-right mt-4'>
+        <Button
+          theme='secondary'
+          className='mr-2'
+          onClick={() => closeModal2()}
+        >
+          Cancel
+        </Button>
+        <Button onClick={handleAddToReleasePipeline} disabled={isLoading}>
+          Add to Release Pipeline
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export type { AddToReleasePipelineModalProps }
+export default AddToReleasePipelineModal
