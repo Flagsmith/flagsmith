6a2ba6dab3e56f8bc423d869377f5f0aa4d9dd4d
Kyle Johnson <kyle@solidstategroup.com>
2025-06-24 16:54:57 +0100

    fix: Panel styles (#5649)

--- frontend/web/components/release-pipelines/ReleasePipelinesList.tsx
+++ frontend/web/components/release-pipelines/ReleasePipelinesList.tsx
@@ -70,6 +70,7 @@ const ReleasePipelinesList = ({
       id='release-pipelines-list'
       isLoading={isLoading}
       items={pipelinesList}
+      className='no-pad'
       filterRow={(item, search) => {
         return item.name.toLowerCase().includes(search.toLowerCase())
       }}
