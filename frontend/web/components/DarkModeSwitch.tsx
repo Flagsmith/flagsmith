a07e8a21cc24f52ce92d0f192758e5db964f0c91
Kyle Johnson <kyle@solidstategroup.com>
2025-07-01 16:32:53 +0100

    feat: Make dark mode local (#5680)

--- frontend/web/components/DarkModeSwitch.tsx
+++ frontend/web/components/DarkModeSwitch.tsx
@@ -1,27 +1,23 @@
-import React, { FC } from 'react'
-import Utils from 'common/utils/utils'
+import React, { FC, useState } from 'react'
 import ConfigProvider from 'common/providers/ConfigProvider'
-import flagsmith from 'flagsmith'
-import Tooltip from './Tooltip'
 import Setting from './Setting'
+import { getDarkMode, setDarkMode as persistDarkMode } from 'project/darkMode'
 
 type DarkModeSwitchType = {}
 
 const DarkModeSwitch: FC<DarkModeSwitchType> = ({}) => {
+  const [darkModeLocal, setDarkModeLocal] = useState(getDarkMode())
+
   const toggleDarkMode = () => {
-    const newValue = !Utils.getFlagsmithHasFeature('dark_mode')
-    flagsmith.setTrait('dark_mode', newValue)
-    if (newValue) {
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-    }
+    const newDarkMode = !getDarkMode()
+    setDarkModeLocal(newDarkMode)
+    persistDarkMode(newDarkMode)
   }
   return (
     <Setting
       title='Dark Mode'
       description='Adjust the theme you see when using Flagsmith.'
-      checked={Utils.getFlagsmithHasFeature('dark_mode')}
+      checked={darkModeLocal}
       onChange={toggleDarkMode}
     />
   )
