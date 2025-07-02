b00b821d4c751bec5f3f1e37e2660792f66821d1
kyle-ssg <kyle@solidstategroup.com>
2025-07-02 09:16:39 +0100

    Local storage dark mode

--- frontend/web/project/darkMode.ts
+++ frontend/web/project/darkMode.ts
@@ -1,20 +1,21 @@
 import API from './api'
+import { storageGet, storageSet } from 'common/safeLocalStorage'
 
 export const getDarkMode = () => {
-  return API.getCookie('dark_mode') === 'true'
+  return storageGet('dark_mode') === 'true'
 }
 export const setDarkMode = (enabled: boolean) => {
   if (enabled) {
-    API.setCookie('dark_mode', true)
+    storageSet('dark_mode', 'true')
     document.body.classList.add('dark')
     document.documentElement.setAttribute('data-bs-theme', 'dark')
   } else {
-    API.setCookie('dark_mode', 'false')
+    storageSet('dark_mode', 'false')
     document.body.classList.remove('dark')
     document.documentElement.removeAttribute('data-bs-theme')
   }
 }
 
-if (API.getCookie('dark_mode')) {
+if (storageGet('dark_mode')) {
   setDarkMode(getDarkMode())
 }
