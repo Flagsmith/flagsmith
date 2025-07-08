4131a5627e9750b44e74008ab992153267c65a61
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-07-08 11:52:53 +0100

    Add issue URL in TODO

--- api/tests/unit/environments/conftest.py
+++ api/tests/unit/environments/conftest.py
@@ -27,6 +27,7 @@ def enable_v2_versioning_for_new_environments(
     TODO: How can we make this more generic so that we don't need to mock individual calling
      locations and instead we can have a fixture that somehow mocks a given flag or set of flags.
      This should probably be a part of the flagsmith-python-client repository itself.
+     See: https://github.com/Flagsmith/flagsmith-python-client/issues/135
     """
 
     def _enable_v2_versioning_for_new_environments() -> None:
