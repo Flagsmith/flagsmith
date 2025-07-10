a1cb9f4031d2c27223f2da43e4263d406eef6ef6
wadii <wadii.zaim@flagsmith.com>
2025-07-10 17:26:23 +0200

    feat: added-invalid-moment-date-fallback

--- frontend/web/components/users-permissions/OrganisationUsersTable/components/LastLogin.tsx
+++ frontend/web/components/users-permissions/OrganisationUsersTable/components/LastLogin.tsx
@@ -14,20 +14,25 @@ const LastLogin: React.FC<LastLoginProps> = ({ lastLogin }) => {
   } => {
     if (!last_login) return { relativeDate: 'Never' }
 
-    const diff = moment().diff(moment(last_login), 'days')
+    const loginMoment = moment(last_login)
+
+    if (!loginMoment.isValid()) {
+      return { relativeDate: 'Never' }
+    }
+
+    const diff = moment().diff(loginMoment, 'days')
 
     if (diff === 0) return { relativeDate: 'Today' }
     if (diff === 1) return { relativeDate: 'Yesterday' }
-    if (diff >= 30)
-      return {
-        date: moment(last_login).format('Do MMM YYYY'),
-        relativeDate: `${diff} days ago`,
-      }
 
-    return { relativeDate: `${diff} days ago` }
+    return {
+      date: diff > 30 ? moment(last_login).format('Do MMM YYYY') : undefined,
+      relativeDate: `${diff} days ago`,
+    }
   }
 
   const { date, relativeDate } = formatLastLogin(lastLogin)
+
   return (
     <div className='fs-small lh-sm'>
       <div className='mb-1'>
