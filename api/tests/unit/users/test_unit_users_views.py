4cc03e3e2802299e49afe8a91b0a11cadaa29e5c
wadii <wadii.zaim@flagsmith.com>
2025-07-14 22:18:16 +0200

    fix: included-testing-settings-threshold

--- api/tests/unit/users/test_unit_users_views.py
+++ api/tests/unit/users/test_unit_users_views.py
@@ -17,7 +17,7 @@ from freezegun import freeze_time
 from pytest_django import DjangoAssertNumQueries
 from rest_framework import status
 from rest_framework.test import APIClient
-
+from django.conf import settings
 from integrations.lead_tracking.hubspot.constants import HUBSPOT_COOKIE_NAME
 from organisations.invites.models import Invite, InviteLink
 from organisations.models import Organisation, OrganisationRole
@@ -937,18 +937,17 @@ def test_list_user_groups(
 @pytest.mark.django_db
 @freeze_time("2024-01-01T10:00:00Z")
 @pytest.mark.parametrize(
-    "last_login, expected_last_login",
+    "last_login",
     [
-        (None, lambda: timezone.now()),
-        ("2023-01-01T10:00:00Z", lambda: timezone.now()),
-        ("2024-01-01T09:59:00Z", lambda: timezone.now() - timedelta(minutes=1)),
+        None,
+        "2023-01-01T10:00:00Z",
+        "2024-01-01T09:59:00Z",
     ],
 )
 def test_get_me_view_updates_last_login(
     api_client: APIClient,
     test_user: FFAdminUser,
     last_login: datetime | None,
-    expected_last_login: Callable[[], datetime],
 ) -> None:
     # Given
     test_user.last_login = last_login
@@ -958,6 +957,10 @@ def test_get_me_view_updates_last_login(
     api_client.force_authenticate(test_user)
     assert test_user.last_login is None or test_user.last_login < timezone.now()
 
+    should_update_last_login = test_user.last_login is None or (
+        timezone.now() - test_user.last_login
+        > timedelta(minutes=settings.LAST_LOGIN_UPDATE_THRESHOLD_MINUTES)
+    )
     url = reverse("api-v1:custom_auth:ffadminuser-me")
 
     # When
@@ -966,4 +969,8 @@ def test_get_me_view_updates_last_login(
     # Then
     assert response.status_code == status.HTTP_200_OK
     test_user.refresh_from_db()
-    assert test_user.last_login == expected_last_login()
+
+    expected_last_login = (
+        timezone.now() if should_update_last_login else test_user.last_login
+    )
+    assert test_user.last_login == expected_last_login
