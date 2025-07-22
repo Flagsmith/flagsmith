cfb46a8d6602547b05046ee6f8b57e5bbfa7036a
Tiago Paiva <tiago@tiagopaiva.me>
2025-07-22 10:53:30 -0300

    feat: adds multiple actions to release pipeline stage (#5767)

--- frontend/web/components/release-pipelines/PipelineStageActions.tsx
+++ frontend/web/components/release-pipelines/PipelineStageActions.tsx
@@ -3,17 +3,28 @@ import { StageActionRequest } from 'common/types/requests'
 
 import { useMemo } from 'react'
 import SinglePipelineStageAction from './SinglePipelineStageAction'
+import Button from 'components/base/forms/Button'
 
 interface PipelineStageActionsProps {
   actions: StageActionRequest[]
   projectId: number
-  onActionChange: (option: { value: string; label: string }) => void
-  onSegmentChange: (option: { value: number; label: string }) => void
+  onAddAction: () => void
+  onRemoveAction: (actionIndex: number) => void
+  onActionChange: (
+    option: { value: string; label: string },
+    stageIndex: number,
+  ) => void
+  onSegmentChange: (
+    option: { value: number; label: string },
+    stageIndex: number,
+  ) => void
 }
 
 const PipelineStageActions = ({
   actions,
   onActionChange,
+  onAddAction,
+  onRemoveAction,
   onSegmentChange,
   projectId,
 }: PipelineStageActionsProps) => {
@@ -33,29 +44,23 @@ const PipelineStageActions = ({
     }))
   }, [segments])
 
-  if (!actions?.length) {
-    return (
-      <SinglePipelineStageAction
-        onActionChange={onActionChange}
-        onSegmentChange={onSegmentChange}
-        isSegmentsLoading={isSegmentsLoading}
-        segmentOptions={segmentOptions}
-      />
-    )
-  }
-
   return (
     <>
-      {actions.map((action, index) => (
+      {actions?.map((action, index) => (
         <SinglePipelineStageAction
           key={index}
+          actionIndex={index}
           action={action}
           onActionChange={onActionChange}
           onSegmentChange={onSegmentChange}
           isSegmentsLoading={isSegmentsLoading}
           segmentOptions={segmentOptions}
+          onRemoveAction={index > 0 ? onRemoveAction : undefined}
         />
       ))}
+      <Button iconLeft='plus' className='w-100' onClick={onAddAction}>
+        Add Flag Action
+      </Button>
     </>
   )
 }
