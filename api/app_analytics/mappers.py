0b7e0d88814be2c90eae85704f18c812e1f4a974
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-07-23 23:51:27 +0100

    fix test

--- api/app_analytics/mappers.py
+++ api/app_analytics/mappers.py
@@ -124,9 +124,8 @@ def map_input_labels_to_labels(input_labels: InputLabels) -> Labels:
             # fastuaparser classifies unrecognized UAs as "Other" — assume these to
             # represent server-side SDKs.
             parsed_ua_string: str = parse_ua(value)
-            is_server_side_sdk = parsed_ua_string.startswith("Other - ")
-
-            # Skip browser SDKs that don't send the special header
+            is_server_side_sdk = parsed_ua_string.startswith("Other")
+            # Skip browser SDKs that don't send the special header.
             if not is_server_side_sdk:
                 continue
         labels[label] = value
