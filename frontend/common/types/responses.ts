6b0a9859ebda8756af331f628c67a41ac9fa6997
Kyle Johnson <kyle@solidstategroup.com>
2025-07-28 15:41:52 +0100

    chore: typescript api (#5807)

--- frontend/common/types/responses.ts
+++ frontend/common/types/responses.ts
@@ -554,6 +554,7 @@ export type SubscriptionMeta = {
 export type Account = {
   first_name: string
   last_name: string
+  date_joined: string
   sign_up_type: SignupType
   id: number
   email: string
@@ -800,7 +801,7 @@ export type Webhook = {
   updated_at: string
 }
 
-export type AccountModel = User & {
+export type AccountModel = Account & {
   organisations: Organisation[]
 }
 
