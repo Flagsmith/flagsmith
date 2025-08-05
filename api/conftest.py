f7fd376ade3eadcb93890f64e9d4d7373f97afca
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-08-05 11:41:54 +0100

    Add new fixture

--- api/conftest.py
+++ api/conftest.py
@@ -1298,3 +1298,9 @@ def enable_features(
         )
 
     return _enable_features
+
+
+@pytest.fixture(autouse=True)
+def clear_content_type_cache(db: None) -> typing.Generator[None]:
+    yield
+    ContentType.objects.clear_cache()
