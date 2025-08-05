31340683215cf7e4e7b626c715ea418a2692c58c
kyle-ssg <kyle@solidstategroup.com>
2025-08-05 16:27:18 +0100

    Add segment page

---
+++ frontend/web/components/pages/SegmentPage.tsx
@@ -0,0 +1,80 @@
+import React, { FC } from 'react'
+import {
+  useDeleteSegmentMutation,
+  useGetSegmentQuery,
+} from 'common/services/useSegment'
+import { useHistory, useRouteMatch } from 'react-router-dom'
+import CreateSegment from 'components/modals/CreateSegment'
+import ProjectStore from 'common/stores/project-store'
+import { Environment } from 'common/types/responses'
+import ChangeRequestStore from 'common/stores/change-requests-store'
+import Breadcrumb from 'components/Breadcrumb'
+import PageTitle from 'components/PageTitle'
+import Button from 'components/base/forms/Button'
+import { useHasPermission } from 'common/providers/Permission'
+import { handleRemoveSegment } from 'components/segments/SegmentRow/SegmentRow'
+
+type SegmentPageType = {}
+
+const SegmentPage: FC<SegmentPageType> = ({}) => {
+  const route = useRouteMatch<{ id: string; projectId: string }>()
+  const { id, projectId } = route.params
+  const history = useHistory()
+  const environmentId = (
+    ProjectStore.getEnvironment() as unknown as Environment | undefined
+  )?.api_key
+  const { data: segment } = useGetSegmentQuery({ id, projectId })
+
+  const { permission: manageSegmentsPermission } = useHasPermission({
+    id: projectId,
+    level: 'project',
+    permission: 'MANAGE_SEGMENTS',
+  })
+
+  const [removeSegment] = useDeleteSegmentMutation()
+  const onRemoveSegment = () => {
+    handleRemoveSegment(projectId, segment!, removeSegment, () => {
+      history.replace(`/project/${projectId}/segments`)
+    })
+  }
+  return (
+    <div
+      style={{ opacity: ChangeRequestStore.isLoading ? 0.25 : 1 }}
+      data-test='segment-page'
+      id='segment-page'
+      className='app-container container-fluid mt-3'
+    >
+      <Breadcrumb
+        items={[
+          {
+            title: 'Segments',
+            url: `/project/${projectId}/segments/`,
+          },
+        ]}
+        currentPage={segment?.name}
+      />
+      <PageTitle
+        cta={
+          manageSegmentsPermission && (
+            <Row>
+              <Button onClick={onRemoveSegment} theme='secondary'>
+                Delete
+              </Button>
+            </Row>
+          )
+        }
+        title={segment?.name}
+      >
+        {segment?.description}
+      </PageTitle>
+
+      <CreateSegment
+        segment={parseInt(id)}
+        projectId={projectId}
+        environmentId={environmentId!}
+      />
+    </div>
+  )
+}
+
+export default SegmentPage
