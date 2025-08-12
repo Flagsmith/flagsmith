df8de6873f7c0fbfdb1b697882b66590d4d2480c
kyle-ssg <kyle@solidstategroup.com>
2025-08-12 13:58:19 +0100

    Migrate feature override row to component

--- frontend/web/components/modals/CreateSegment.tsx
+++ frontend/web/components/modals/CreateSegment.tsx
@@ -587,7 +587,7 @@ type LoadingCreateSegmentType = {
   readOnly?: boolean
   onSegmentRetrieved?: (segment: Segment) => void
   onComplete?: (segment: Segment) => void
-  projectId: string
+  projectId: string | number
   segment?: number
 }
 
