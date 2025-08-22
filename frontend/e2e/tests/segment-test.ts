3d921242a0d3a66fd341725282c6d802a4af556f
Zaimwa9 <wadii.zaim@gmail.com>
2025-08-22 13:04:01 +0200

    Revert "fix: reverted-tests (#5966)"
    This reverts commit 1c20aa5a8825793dca8f52f439a85c85de14e64d.

--- frontend/e2e/tests/segment-test.ts
+++ frontend/e2e/tests/segment-test.ts
@@ -98,27 +98,8 @@ export const testSegment1 = async (flagsmith: any) => {
   // (=== 18 || === 19) && (> 17 || < 19) && (!=20) && (<=18) && (>=18)
   // Rule 1- Age === 18 || Age === 19
 
-  log('Update segment')
-  await gotoSegments()
-  const lastRule = segmentRules[segmentRules.length - 1]
-  await createSegment(0, 'segment_to_update', [lastRule])
-  await closeModal()
-  await click(byId('segment-0-name'))
-  await setSegmentRule(0, 0, lastRule.name, lastRule.operator, lastRule.value + 1)
-  await click(byId('update-segment'))
-  await closeModal()
-  await gotoSegments()
-  await click(byId('segment-0-name'))
-  await assertInputValue(byId(`rule-${0}-value-0`), `${lastRule.value + 1}`)
-  await closeModal()
-  await deleteSegment(0, 'segment_to_update', !isCloneSegmentEnabled)
-  await waitAndRefresh()
-
-  log('Create segment')
   await createSegment(0, '18_or_19', segmentRules)
   await closeModal()
-
-
   log('Add segment trait for user')
   await gotoTraits()
   await createTrait(0, 'age', 18)
@@ -163,6 +144,21 @@ export const testSegment1 = async (flagsmith: any) => {
   await deleteSegment(0, '18_or_19', !isCloneSegmentEnabled)
   await gotoFeatures()
   await deleteFeature(0, 'mv_flag')
+  
+  // log('Update segment')
+  // const SEGMENT_TO_UPDATE_NAME = 'segment_to_update'
+  // await gotoSegments()
+  // const lastRule = segmentRules[segmentRules.length - 1]
+  // await createSegment(0, SEGMENT_TO_UPDATE_NAME, [lastRule])
+  // await waitAndRefresh()
+  // await clickSegmentByName(SEGMENT_TO_UPDATE_NAME)
+  // await setSegmentRule(0, 0, lastRule.name, lastRule.operator, lastRule.value + 1)
+  // await click(byId('update-segment'))
+  // await closeModal()
+  // await clickSegmentByName(SEGMENT_TO_UPDATE_NAME)
+  // await assertInputValue(byId(`rule-${0}-value-0`), `${lastRule.value + 1}`)
+  // await closeModal()
+  // await deleteSegment(0, SEGMENT_TO_UPDATE_NAME, !isCloneSegmentEnabled)
 }
 
 export const testSegment2 = async () => {
