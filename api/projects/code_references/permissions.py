70a652b2fa7beb6f374003d669566a6e294ceb10
Evandro Myller <22429+emyller@users.noreply.github.com>
2025-08-25 20:34:57 -0300

    fix(GitHub PoC): Fix API authentication (#5979)

--- api/projects/code_references/permissions.py
+++ api/projects/code_references/permissions.py
@@ -1,19 +1,17 @@
 from common.projects.permissions import VIEW_PROJECT
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
 from projects.models import Project
-from users.models import FFAdminUser
 
 
 class _BaseCodeReferencePermission(IsAuthenticated):
     def has_permission(self, request: Request, view: APIView) -> bool:
         if not super().has_permission(request, view):
             return False
-
-        if not isinstance(request.user, FFAdminUser):  # pragma: no cover
-            return False
+        assert not isinstance(request.user, AnonymousUser)
 
         project = Project.objects.get(id=view.kwargs["project_pk"])
         return request.user.has_project_permission(VIEW_PROJECT, project)
