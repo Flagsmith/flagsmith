24edb2acd9468531012da2ee2c276f3dc886940b
wadii <wadii.zaim@flagsmith.com>
2025-10-16 10:23:41 +0200

    feat: removed-unused-props-in-AppLoader

--- frontend/web/components/AppLoader.tsx
+++ frontend/web/components/AppLoader.tsx
@@ -1,22 +1,14 @@
 import React, { FC } from 'react'
 
-interface AppLoaderProps {
-  style?: React.CSSProperties
-}
-
-const AppLoader: FC<AppLoaderProps> = ({ style }) => {
+const AppLoader: FC = () => {
   return (
     <div
-      style={
-        style
-          ? style
-          : {
-              position: 'absolute',
-              textAlign: 'center',
-              top: '35%',
-              width: '100%',
-            }
-      }
+      style={{
+        position: 'absolute',
+        textAlign: 'center',
+        top: '35%',
+        width: '100%',
+      }}
     >
       <Loader />
     </div>
