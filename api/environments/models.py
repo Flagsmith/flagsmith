e40358962577412b5a0e997402521e9eba9cdce0
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2025-11-10 10:41:55 +0530

    use | operator instead of Union

--- api/environments/models.py
+++ api/environments/models.py
@@ -635,9 +635,9 @@ class Webhook(AbstractBaseExportableWebhookModel):
         environment: Environment,
         enabled: bool,
         value: typing.Union[str, int, bool, type(None)],  # type: ignore[valid-type]
-        identity_id: typing.Union[int, str, None] = None,
-        identity_identifier: typing.Union[str, None] = None,
-        feature_segment: typing.Union[FeatureSegment, None] = None,
+        identity_id: int | str | None = None,
+        identity_identifier: str | None = None,
+        feature_segment: FeatureSegment | None = None,
     ) -> dict:  # type: ignore[type-arg]
         if (identity_id or identity_identifier) and not (
             identity_id and identity_identifier
