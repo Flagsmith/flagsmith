1b905699f62c6ea1a1f488269eddea82b1eb1b19
kyle-ssg <kyle@solidstategroup.com>
2025-11-11 10:22:52 +0000

    Remove !

--- frontend/web/components/feature-override/FeatureOverrideCTA.tsx
+++ frontend/web/components/feature-override/FeatureOverrideCTA.tsx
@@ -59,7 +59,7 @@ const FeatureOverrideCTA: FC<FeatureOverrideCTAType> = ({
                   identifier: identifier,
                   identity: identity,
                   identityFlag: overrideFeatureState as IdentityFeatureState,
-                  projectFlag: projectFlag!,
+                  projectFlag: projectFlag,
                 })
               }}
             >
