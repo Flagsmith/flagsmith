74139cec26038f2844c67dba446be74c435f8717
Talisson Costa <talisson.odcosta@gmail.com>
2025-11-19 16:07:29 -0300

    refactor: simplify import tab passing project name and id

--- frontend/web/components/pages/project-settings/tabs/ImportTab.tsx
+++ frontend/web/components/pages/project-settings/tabs/ImportTab.tsx
@@ -1,26 +1,18 @@
 import ImportPage from 'components/import-export/ImportPage'
 import InfoMessage from 'components/InfoMessage'
 import React from 'react'
-import { useGetProjectQuery } from 'common/services/useProject'
 
 type ImportTabProps = {
-  projectId: number
+  projectName: string
+  projectId: string
   environmentId?: string
 }
 
-export const ImportTab = ({ environmentId, projectId }: ImportTabProps) => {
-  const { data: project, isLoading } = useGetProjectQuery({
-    id: String(projectId),
-  })
-
-  if (isLoading) {
-    return (
-      <div className='text-center mt-4'>
-        <Loader />
-      </div>
-    )
-  }
-
+export const ImportTab = ({
+  environmentId,
+  projectId,
+  projectName,
+}: ImportTabProps) => {
   if (!environmentId) {
     return (
       <div className='mt-4'>
@@ -35,7 +27,7 @@ export const ImportTab = ({ environmentId, projectId }: ImportTabProps) => {
     <ImportPage
       environmentId={environmentId}
       projectId={projectId}
-      projectName={project?.name || ''}
+      projectName={projectName}
     />
   )
 }
