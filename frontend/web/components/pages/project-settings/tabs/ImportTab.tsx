fc22a055c10e65358549e6096189237e826c304a
Zaimwa9 <wadii.zaim@flagsmith.com>
2025-12-01 11:37:17 +0100

    fix: project-settings-export-import-missing (#6343)

--- frontend/web/components/pages/project-settings/tabs/ImportTab.tsx
+++ frontend/web/components/pages/project-settings/tabs/ImportTab.tsx
@@ -1,33 +1,11 @@
-import ImportPage from 'components/import-export/ImportPage'
-import InfoMessage from 'components/InfoMessage'
 import React from 'react'
+import ImportPage from 'components/import-export/ImportPage'
 
 type ImportTabProps = {
   projectName: string
   projectId: string
-  environmentId?: string
 }
 
-export const ImportTab = ({
-  environmentId,
-  projectId,
-  projectName,
-}: ImportTabProps) => {
-  if (!environmentId) {
-    return (
-      <div className='mt-4'>
-        <InfoMessage>
-          Please select an environment to import features
-        </InfoMessage>
-      </div>
-    )
-  }
-
-  return (
-    <ImportPage
-      environmentId={environmentId}
-      projectId={projectId}
-      projectName={projectName}
-    />
-  )
+export const ImportTab = ({ projectId, projectName }: ImportTabProps) => {
+  return <ImportPage projectId={projectId} projectName={projectName} />
 }
