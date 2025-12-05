45d7e2a40e73edd6bd03430ec59b1f57653f57cc
wadii <wadii.zaim@flagsmith.com>
2025-12-05 11:28:33 +0100

    fix: temporarily-change-nothing-bis

--- frontend/e2e/init.cafe.js
+++ frontend/e2e/init.cafe.js
@@ -113,7 +113,7 @@ test('Environment', environmentTest).meta({ autoLogout: true, category: 'oss' })
 
 test('Project', projectTest).meta({ autoLogout: true, category: 'oss' })
 
-test('Organization', organisationTest).meta({ autoLogout: true, category: 'oss', skipEnterprise: true })
+test('Organization', organisationTest).meta({ autoLogout: true, category: 'oss', skipEnterprise: false })
 
 test('Versioning', versioningTests).meta({ autoLogout: true, category: 'oss' })
 
