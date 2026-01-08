872f0c92a11ec157dc5473c93fe190361f8e9522
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-01-08 18:49:31 +0000

    Circular import fix

--- api/core/throttling.py
+++ api/core/throttling.py
@@ -1,16 +1,20 @@
+import typing
+
 from django.conf import settings
 from django.core.cache import caches
 from redis.exceptions import RedisClusterException
 from rest_framework import throttling
-from rest_framework.request import Request
-from rest_framework.views import APIView
+
+if typing.TYPE_CHECKING:
+    from rest_framework.request import Request
+    from rest_framework.views import APIView
 
 
 class UserRateThrottle(throttling.UserRateThrottle):
     cache = caches[settings.USER_THROTTLE_CACHE_NAME]
 
     def allow_request(
-        self, request: Request, view: APIView
+        self, request: "Request", view: "APIView"
     ) -> bool:  # pragma: no cover
         try:
             return super(UserRateThrottle, self).allow_request(request, view)
