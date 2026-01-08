70b005f3517d646ea7e17a3d9ff74d04ebf3d1a0
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-01-08 18:40:17 +0000

    Handle intermittent redis connection failures

--- api/core/throttling.py
+++ api/core/throttling.py
@@ -1,7 +1,21 @@
 from django.conf import settings
 from django.core.cache import caches
+from redis.exceptions import RedisClusterException
 from rest_framework import throttling
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
 class UserRateThrottle(throttling.UserRateThrottle):
     cache = caches[settings.USER_THROTTLE_CACHE_NAME]
+
+    def allow_request(
+        self, request: Request, view: APIView
+    ) -> bool:  # pragma: no cover
+        try:
+            return super(UserRateThrottle, self).allow_request(request, view)
+        except RedisClusterException:
+            # Handle intermittent redis connection failures.
+            # Allow the request without calling throttle_success which
+            # would also try to establish a connection to Redis.
+            return True
