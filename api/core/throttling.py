8d73c532b35d653d5a6c37fb894e0f48cc6e63d6
Kim Gustyr <kim.gustyr@flagsmith.com>
2026-01-12 13:50:18 +0000

    consistency

--- api/core/throttling.py
+++ api/core/throttling.py
@@ -3,11 +3,11 @@ from typing import TYPE_CHECKING
 from django.conf import settings
 from django.core.cache import caches
 from rest_framework import throttling
-from rest_framework.request import Request
 
 from api_keys.user import APIKeyUser
 
 if TYPE_CHECKING:
+    from rest_framework.request import Request
     from rest_framework.views import APIView
 
 
@@ -18,7 +18,7 @@ class UserRateThrottle(throttling.UserRateThrottle):
 class MasterAPIKeyUserRateThrottle(throttling.UserRateThrottle):
     cache = caches[settings.USER_THROTTLE_CACHE_NAME]
 
-    def get_cache_key(self, request: Request, view: "APIView") -> str | None:
+    def get_cache_key(self, request: "Request", view: "APIView") -> str | None:
         if getattr(request.user, "is_master_api_key_user", False):
             assert isinstance(request.user, APIKeyUser)
             return self.cache_format % {
