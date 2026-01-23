Flagsmith/flagsmith

commit 00085cdefc6266174c5e1e10e8cfb36b402c4d8e

feat: Edit Change Requests (#6356)
Co-authored-by: Zaimwa9 <wadii.zaim@flagsmith.com>
Co-authored-by: Matthew Elwell <matthew.elwell@flagsmith.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

frontend/web/components/modals/create-feature/tabs/CreateFeature.tsx
@@ -1,0 +1,90 @@
+import React, { FC } from 'react'
+import { FeatureState, ProjectFlag } from 'common/types/responses'
+import FeatureValue from './FeatureValue'
+import FeatureSettings from './FeatureSettings'
+import ErrorMessage from 'components/ErrorMessage'
+import WarningMessage from 'components/WarningMessage'
+import { useHasPermission } from 'common/providers/Permission'
+
+type CreateFeatureTabProps = {
+  projectId: number
+  error: any
+  featureState: FeatureState
+  overrideFeatureState?: FeatureState
+  projectFlag: ProjectFlag | null
+  featureContentType: any
+  identity?: string
+  onEnvironmentFlagChange: (changes: FeatureState) => void
+  onProjectFlagChange: (changes: ProjectFlag) => void
+  onRemoveMultivariateOption?: (id: number) => void
+  onHasMetadataRequiredChange: (hasMetadataRequired: boolean) => void
+  featureError?: string
+  featureWarning?: string
+}
+
+const CreateFeature: FC<CreateFeatureTabProps> = ({
+  error,
+  featureContentType,
+  featureError,
+  featureState,
+  featureWarning,
+  identity,
+  onEnvironmentFlagChange,
+  onHasMetadataRequiredChange,
+  onProjectFlagChange,
+  onRemoveMultivariateOption,
+  overrideFeatureState,
+  projectFlag,
+  projectId,
+}) => {
+  const { permission: createFeature } = useHasPermission({
+    id: projectId,
+    level: 'project',
+    permission: 'CREATE_FEATURE',
+  })
+
+  const { permission: projectAdmin } = useHasPermission({
+    id: projectId,
+    level: 'project',
+    permission: 'ADMIN',
+  })
+
+  const noPermissions = !createFeature && !projectAdmin
+
+  return (
+    <>
+      <ErrorMessage error={featureError} />
+      <WarningMessage warningMessage={featureWarning} />
+      {!!projectFlag && (
+        <>
+          <FeatureValue
+            error={error}
+            createFeature={createFeature}
+            hideValue={false}
+            isEdit={false}
+            identity={identity}
+            noPermissions={noPermissions}
+            featureState={overrideFeatureState || featureState}
+            projectFlag={projectFlag}
+            onEnvironmentFlagChange={onEnvironmentFlagChange}
+            onProjectFlagChange={onProjectFlagChange}
+            onRemoveMultivariateOption={onRemoveMultivariateOption}
+          />
+          <FeatureSettings
+            projectAdmin={projectAdmin}
+            createFeature={createFeature}
+            featureContentType={featureContentType}
+            identity={identity}
+            isEdit={false}
+            projectId={projectId}
+            projectFlag={projectFlag}
+            onChange={onProjectFlagChange}
+            onHasMetadataRequiredChange={onHasMetadataRequiredChange}
+          />
+        </>
+      )}
+    </>
+  )
+}
+
+export default CreateFeature

frontend/common/providers/withSegmentOverrides.js
@@ -1,6 +1,7 @@
 import data from 'common/data/base/_data'
 import ProjectStore from 'common/stores/project-store'
 import FeatureListStore from 'common/stores/feature-list-store'
+import { mergeChangeSets } from 'common/services/useChangeRequest'
 
 export default (WrappedComponent) => {
   class HOC extends React.Component {
@@ -28,6 +29,7 @@ export default (WrappedComponent) => {
     getOverrides = () => {
       if (this.props.projectFlag) {
         //todo: migrate to useSegmentFeatureState
+        const projectId = this.props.projectFlag.project
         Promise.all([
           data.get(
             `${
@@ -43,7 +45,12 @@ export default (WrappedComponent) => {
               this.props.environmentId,
             )}&feature=${this.props.projectFlag.id}`,
           ),
-        ]).then(([res, res2]) => {
+          this.props.changeRequest?.change_sets
+            ? data.get(
+                `${Project.api}projects/${projectId}/segments/?page_size=1000`,
+              )
+            : Promise.resolve({ results: [] }),
+        ]).then(([res, res2, segmentsRes]) => {
           const results = res.results
           const featureStates = res2.results
           const environmentOverride = res2.results.find(
@@ -72,18 +79,115 @@ export default (WrappedComponent) => {
               }
             }
           })
-          const resResults = res.results || []
-          const segmentOverrides = results
-            .concat(
+
+          let segmentOverrides
+          if (this.props.changeRequest?.change_sets) {
+            // Add changesets to existing segment overrides
+            const mergedFeatureStates = mergeChangeSets(
+              this.props.changeRequest.change_sets,
+              featureStates,
+              this.props.changeRequest.conflicts,
+            )
+
+            // Get segment IDs marked for deletion
+            const segmentIdsToDelete =
+              this.props.changeRequest.change_sets?.flatMap(
+                (changeSet) => changeSet.segment_ids_to_delete_overrides || [],
+              ) || []
+
+            segmentOverrides = results.map((currentSegmentOverride) => {
+              const changedFeatureState = mergedFeatureStates.find(
+                (featureState) =>
+                  featureState.feature_segment?.segment ===
+                  currentSegmentOverride.segment,
+              )
+
+              // Any segment_ids_to_delete_overrides should be marked as toRemove
+              const toRemove = segmentIdsToDelete.includes(
+                currentSegmentOverride.segment,
+              )
+
+              if (changedFeatureState) {
+                return {
+                  ...currentSegmentOverride,
+                  ...changedFeatureState,
+                  id: changedFeatureState.id || currentSegmentOverride.id,
+                  is_feature_specific:
+                    changedFeatureState.feature_segment?.is_feature_specific,
+                  multivariate_options:
+                    changedFeatureState.multivariate_feature_state_values || [],
+                  priority: changedFeatureState.feature_segment?.priority || 0,
+                  segment: changedFeatureState.feature_segment?.segment,
+                  segment_name:
+                    changedFeatureState.feature_segment?.segment_name ||
+                    currentSegmentOverride.segment_name,
+                  toRemove,
+                  uuid:
+                    changedFeatureState.feature_segment?.uuid ||
+                    currentSegmentOverride.uuid,
+                  value: Utils.featureStateToValue(
+                    changedFeatureState.feature_state_value,
+                  ),
+                }
+              }
+
+              return toRemove
+                ? { ...currentSegmentOverride, toRemove }
+                : currentSegmentOverride
+            })
+
+            // Add any new segment overrides from the changesets
+            mergedFeatureStates
+              .filter(
+                (featureState) =>
+                  !!featureState.feature_segment?.segment &&
+                  !results.find(
+                    (currentOverride) =>
+                      currentOverride.segment ===
+                      featureState.feature_segment.segment,
+                  ),
+              )
+              .forEach((newFeatureState) => {
+                // Look up segment metadata from segments API to get segment name
+                const segmentMetadata = segmentsRes.results?.find(
+                  (segment) =>
+                    segment.id === newFeatureState.feature_segment?.segment,
+                )
+
+                segmentOverrides.push({
+                  enabled: newFeatureState.enabled,
+                  environment: newFeatureState.environment,
+                  feature: newFeatureState.feature,
+                  id: newFeatureState.id,
+                  is_feature_specific:
+                    newFeatureState.feature_segment?.is_feature_specific,
+                  multivariate_options:
+                    newFeatureState.multivariate_feature_state_values || [],
+                  priority: newFeatureState.feature_segment?.priority || 0,
+                  segment: newFeatureState.feature_segment?.segment,
+                  segment_name:
+                    newFeatureState.feature_segment?.segment_name ||
+                    segmentMetadata?.name ||
+                    'Unknown Segment',
+                  uuid: newFeatureState.feature_segment?.uuid,
+                  value: Utils.featureStateToValue(
+                    newFeatureState.feature_state_value,
+                  ),
+                })
+              })
+          } else {
+            segmentOverrides = results.concat(
               (this.props.newSegmentOverrides || []).map((v, i) => ({
                 ...v,
               })),
             )
-            .map((v, i) => ({
-              ...v,
-              originalPriority: i,
-              priority: i,
-            }))
+          }
+          segmentOverrides = segmentOverrides.map((v, i) => ({
+            ...v,
+            originalPriority: i,
+            priority: i,
+          }))
+
           const originalSegmentOverrides = _.cloneDeep(segmentOverrides)
           this.setState({
             environmentVariations:

frontend/web/components/Feature.js
@@ -1,209 +1,0 @@
-// import propTypes from 'prop-types';
-import React, { PureComponent } from 'react'
-import ValueEditor from './ValueEditor'
-import Constants from 'common/constants'
-import { VariationOptions } from './mv/VariationOptions'
-import { AddVariationButton } from './mv/AddVariationButton'
-import ErrorMessage from './ErrorMessage'
-import Tooltip from './Tooltip'
-import Icon from './Icon'
-import InputGroup from './base/forms/InputGroup'
-import WarningMessage from './WarningMessage'
-
-function isNegativeNumberString(str) {
-  if (typeof Utils.getTypedValue(str) !== 'number') {
-    return false
-  }
-  if (typeof str !== 'string') {
-    return false
-  }
-  const num = parseFloat(str)
-  return !isNaN(num) && num < 0
-}
-
-export default class Feature extends PureComponent {
-  static displayName = 'Feature'
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      isNegativeNumberString: isNegativeNumberString(
-        props.environmentFlag?.feature_state_value,
-      ),
-    }
-  }
-  removeVariation = (i) => {
-    const idToRemove = this.props.multivariate_options[i].id
-
-    if (idToRemove) {
-      openConfirm({
-        body: 'This will remove the variation on your feature for all environments, if you wish to turn it off just for this environment you can set the % value to 0.',
-        destructive: true,
-        onYes: () => {
-          this.props.removeVariation(i)
-        },
-        title: 'Delete variation',
-        yesText: 'Confirm',
-      })
-    } else {
-      this.props.removeVariation(i)
-    }
-  }
-
-  render() {
-    const {
-      checked,
-      environmentFlag,
-      environmentVariations,
-      error,
-      identity,
-      isEdit,
-      multivariate_options,
-      onCheckedChange,
-      onValueChange,
-      projectFlag,
-      readOnly,
-      value,
-    } = this.props
-
-    const enabledString = isEdit ? 'Enabled' : 'Enabled by default'
-    const controlPercentage = Utils.calculateControl(multivariate_options)
-    const valueString = identity
-      ? 'User override'
-      : !!multivariate_options && multivariate_options.length
-      ? `Control Value - ${controlPercentage}%`
-      : `Value`
-
-    const showValue = !(
-      !!identity &&
-      multivariate_options &&
-      !!multivariate_options.length
-    )
-    return (
-      <div>
-        <FormGroup className='mb-4'>
-          <Tooltip
-            title={
-              <div className='flex-row'>
-                <Switch
-                  data-test='toggle-feature-button'
-                  defaultChecked={checked}
-                  disabled={readOnly}
-                  checked={checked}
-                  onChange={onCheckedChange}
-                  className='ml-0'
-                />
-                <div className='label-switch ml-3 mr-1'>
-                  {enabledString || 'Enabled'}
-                </div>
-                {!isEdit && <Icon name='info-outlined' />}
-              </div>
-            }
-          >
-            {!isEdit &&
-              'This will determine the initial enabled state for all environments. You can edit the this individually for each environment once the feature is created.'}
-          </Tooltip>
-        </FormGroup>
-
-        {showValue && (
-          <FormGroup className='mb-4'>
-            <InputGroup
-              component={
-                <ValueEditor
-                  data-test='featureValue'
-                  name='featureValue'
-                  className='full-width'
-                  value={`${
-                    typeof value === 'undefined' || value === null ? '' : value
-                  }`}
-                  onChange={onValueChange}
-                  disabled={readOnly}
-                  placeholder="e.g. 'big' "
-                />
-              }
-              tooltip={`${Constants.strings.REMOTE_CONFIG_DESCRIPTION}${
-                !isEdit
-                  ? '<br/>Setting this when creating a feature will set the value for all environments. You can edit this individually for each environment once the feature is created.'
-                  : ''
-              }`}
-              title={`${valueString}`}
-            />
-          </FormGroup>
-        )}
-        {this.state.isNegativeNumberString && (
-          <WarningMessage
-            warningMessage={
-              <div>
-                This feature currently has the value of{' '}
-                <strong>"{environmentFlag?.feature_state_value}"</strong>.
-                Saving this feature will convert its value from a string to a
-                number. If you wish to preserve this value as a string, please
-                save it using the{' '}
-                <a href='https://api.flagsmith.com/api/v1/docs/#/api/api_v1_environments_featurestates_update'>
-                  API
-                </a>
-                .
-              </div>
-            }
-          />
-        )}
-
-        {!!error && (
-          <div className='mx-2 mt-2'>
-            <ErrorMessage error={error} />
-          </div>
-        )}
-        {!!identity && (
-          <div>
-            <FormGroup className='mb-4'>
-              <VariationOptions
-                disabled
-                select
-                controlValue={environmentFlag?.feature_state_value}
-                controlPercentage={controlPercentage}
-                variationOverrides={this.props.identityVariations}
-                setVariations={this.props.onChangeIdentityVariations}
-                updateVariation={() => {}}
-                weightTitle='Override Weight %'
-                projectFlag={projectFlag}
-                multivariateOptions={projectFlag.multivariate_options}
-                removeVariation={() => {}}
-              />
-            </FormGroup>
-          </div>
-        )}
-        {!identity && (
-          <div>
-            <FormGroup className='mb-0'>
-              {(!!environmentVariations || !isEdit) && (
-                <VariationOptions
-                  canCreateFeature={this.props.canCreateFeature}
-                  disabled={!!identity || readOnly}
-                  controlValue={environmentFlag?.feature_state_value}
-                  controlPercentage={controlPercentage}
-                  variationOverrides={environmentVariations}
-                  updateVariation={this.props.updateVariation}
-                  weightTitle={
-                    isEdit ? 'Environment Weight %' : 'Default Weight %'
-                  }
-                  multivariateOptions={multivariate_options}
-                  removeVariation={this.removeVariation}
-                />
-              )}
-            </FormGroup>
-            {!this.props.hideAddVariation &&
-              Utils.renderWithPermission(
-                this.props.canCreateFeature,
-                Constants.projectPermissions('Create Feature'),
-                <AddVariationButton
-                  multivariateOptions={multivariate_options}
-                  disabled={!this.props.canCreateFeature || readOnly}
-                  onClick={this.props.addVariation}
-                />,
-              )}
-          </div>
-        )}
-      </div>
-    )
-  }
-}

frontend/web/components/SegmentOverrides.js
@@ -302,8 +302,7 @@ const SegmentOverrideInner = class Override extends React.Component {
         {!!multivariateOptions?.length && (
           <div className='px-3'>
             <VariationOptions
-              preventRemove
-              readOnlyValue
+              readOnly
               disabled={readOnly}
               controlValue={controlValue}
               controlPercentage={controlPercent}

frontend/web/components/Switch.js
@@ -1,55 +1,0 @@
-// import propTypes from 'prop-types';
-import React, { PureComponent } from 'react'
-import RCSwitch from 'rc-switch'
-import Icon from './Icon'
-
-export default class Switch extends PureComponent {
-  static displayName = 'Switch'
-
-  static propTypes = {}
-
-  render() {
-    const { checked, darkMode, offMarkup, onChange, onMarkup } = this.props
-    if (E2E) {
-      return (
-        <div style={{ display: 'inline-block', height: '28px' }}>
-          <button
-            role='switch'
-            type='button'
-            style={{
-              color: 'black',
-              pointerEvents: 'all',
-              position: 'relative',
-            }}
-            className={checked ? 'switch-checked' : 'switch-unchecked'}
-            {...this.props}
-            onClick={() => {
-              onChange(!this.props.checked)
-            }}
-          >
-            {checked ? offMarkup || 'On' : onMarkup || 'Off'}
-          </button>
-        </div>
-      )
-    }
-    if (darkMode) {
-      return (
-        <button
-          role='switch'
-          type='button'
-          className={`rc-switch flex-row justify-content-center gap-3 ${
-            checked ? 'rc-switch-checked' : 'rc-switch-unchecked'
-          }`}
-          {...this.props}
-          onClick={() => {
-            onChange(!checked)
-          }}
-        >
-          <Icon name='sun' fill={checked ? '#656D7B' : '#1A2634'} />
-          <Icon name='moon' fill={checked ? '#FFFFFF' : '#9DA4AE'} />
-        </button>
-      )
-    }
-    return <RCSwitch {...this.props} />
-  }
-}

frontend/web/components/Switch.tsx
@@ -1,0 +1,68 @@
+import React, { FC } from 'react'
+import RCSwitch, { Props as RCSwitchProps } from 'rc-switch'
+import Icon from './Icon'
+
+export type SwitchProps = RCSwitchProps & {
+  checked?: boolean
+  darkMode?: boolean
+  offMarkup?: React.ReactNode
+  onMarkup?: React.ReactNode
+  onChange?: (checked: boolean) => void
+}
+
+const Switch: FC<SwitchProps> = ({
+  checked,
+  darkMode,
+  offMarkup,
+  onChange,
+  onMarkup,
+  ...rest
+}) => {
+  if (E2E) {
+    return (
+      <div style={{ display: 'inline-block', height: '28px' }}>
+        <button
+          role='switch'
+          type='button'
+          style={{
+            color: 'black',
+            pointerEvents: 'all',
+            position: 'relative',
+          }}
+          className={checked ? 'switch-checked' : 'switch-unchecked'}
+          {...rest}
+          onClick={() => {
+            onChange?.(!checked)
+          }}
+        >
+          {checked ? offMarkup || 'On' : onMarkup || 'Off'}
+        </button>
+      </div>
+    )
+  }
+
+  if (darkMode) {
+    return (
+      <button
+        role='switch'
+        type='button'
+        className={`rc-switch flex-row justify-content-center gap-3 ${
+          checked ? 'rc-switch-checked' : 'rc-switch-unchecked'
+        }`}
+        {...rest}
+        onClick={() => {
+          onChange?.(!checked)
+        }}
+      >
+        <Icon name='sun' fill={checked ? '#656D7B' : '#1A2634'} />
+        <Icon name='moon' fill={checked ? '#FFFFFF' : '#9DA4AE'} />
+      </button>
+    )
+  }
+
+  return <RCSwitch checked={checked} onChange={onChange} {...rest} />
+}
+
+Switch.displayName = 'Switch'
+
+export default Switch

frontend/web/components/base/forms/Button.tsx
@@ -23,6 +23,7 @@ export const sizeClassNames = {
   large: 'btn-lg',
   small: 'btn-sm',
   xSmall: 'btn-xsm',
+  xxSmall: 'btn-xxsm',
 }
 
 export type ButtonType = ButtonHTMLAttributes<HTMLButtonElement> & {

frontend/web/components/base/grid/FormGroup.js
@@ -1,18 +1,0 @@
-import { PureComponent } from 'react'
-const FormGroup = class extends PureComponent {
-  static displayName = 'FormGroup'
-
-  render() {
-    return (
-      <div className={`form-group ${this.props.className || ''}`}>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-FormGroup.displayName = 'FormGroup'
-FormGroup.propTypes = {
-  children: OptionalNode,
-}
-module.exports = FormGroup

commit 66d356b71e39a1bbcafa7941bc955f05e5262f23

fix(frontend): hide 'Enabled by default' toggle when editing identity override
The 'Enabled by default' toggle and its tooltip were incorrectly shown
when editing an identity override. This was a regression introduced in
PR #6356 where the CreateFeature component was extracted with isEdit
hardcoded to false.

When editing an identity override, isEdit should be true so that:
- The toggle shows 'Enabled' instead of 'Enabled by default'
- The tooltip about initial enabled state is not shown

Fixes #6580

Co-Authored-By: Claude Opus 4.5 <noreply@anthropic.com>

frontend/web/components/modals/create-feature/tabs/CreateFeature.tsx
@@ -61,7 +61,7 @@ const CreateFeature: FC<CreateFeatureTabProps> = ({
             error={error}
             createFeature={createFeature}
             hideValue={false}
-            isEdit={false}
+            isEdit={!!identity}
             identity={identity}
             noPermissions={noPermissions}
             featureState={overrideFeatureState || featureState}
@@ -75,7 +75,7 @@ const CreateFeature: FC<CreateFeatureTabProps> = ({
             createFeature={createFeature}
             featureContentType={featureContentType}
             identity={identity}
-            isEdit={false}
+            isEdit={!!identity}
             projectId={projectId}
             projectFlag={projectFlag}
             onChange={onProjectFlagChange}