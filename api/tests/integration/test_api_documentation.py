7cd8a685df38991b757de72da0c0c2eca152a8c7
wadii <wadii.zaim@flagsmith.com>
2026-01-28 17:33:21 +0100

    docs: coverage

--- api/tests/integration/test_api_documentation.py
+++ api/tests/integration/test_api_documentation.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import Client
 
 
@@ -11,3 +12,7 @@ def test_api_documentation_specification_loads(
     # Then
     assert response.status_code == 200
     assert response.json()["info"]["title"] == "Flagsmith API"
+
+
+def test_spectacular_settings__component_no_read_only_required__is_enabled() -> None:
+    assert settings.SPECTACULAR_SETTINGS["COMPONENT_NO_READ_ONLY_REQUIRED"] is True
