9a677ee03da9bf70688355c2b1b0463d31f34dbd
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-03-06 17:58:39 +0000

    fix: `/api/v1/environment-document` N+1 query issue related to the number of segment overrides in a version (#6865)

--- api/environments/managers.py
+++ api/environments/managers.py
@@ -34,7 +34,10 @@ class EnvironmentManager(SoftDeleteManager):
                 Prefetch(
                     "project__segments__feature_segments__feature_states",
                     queryset=FeatureState.objects.select_related(
-                        "feature", "feature_state_value", "environment"
+                        "feature",
+                        "feature_state_value",
+                        "environment",
+                        "environment_feature_version",
                     ),
                 ),
                 Prefetch(
