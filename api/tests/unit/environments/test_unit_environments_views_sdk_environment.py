a0311cacfcdfc2e1f03bc13dd361ebae1c488574
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-03-09 16:13:57 +0000

    Remove new tests

--- api/tests/unit/environments/test_unit_environments_views_sdk_environment.py
+++ api/tests/unit/environments/test_unit_environments_views_sdk_environment.py
@@ -1,14 +1,9 @@
-import time
 from typing import TYPE_CHECKING
-from unittest.mock import ANY
 
 import pytest
 from core.constants import FLAGSMITH_UPDATED_AT_HEADER
 from django.urls import reverse
-from django.utils import timezone
-from django.utils.http import http_date
 from flag_engine.segments.constants import EQUAL
-from pytest_lazyfixture import lazy_fixture  # type: ignore[import-untyped]
 from rest_framework import status
 from rest_framework.test import APIClient
 
@@ -154,105 +149,6 @@ def test_get_environment_document(
     )
 
 
-@pytest.mark.parametrize(
-    "environment_",
-    (
-        lazy_fixture("environment"),
-        lazy_fixture("environment_v2_versioning"),
-    ),
-)
-def test_get_environment_document__identity_overrides(
-    project: Project,
-    environment_: Environment,
-) -> None:
-    # Given
-    api_key = EnvironmentAPIKey.objects.create(environment=environment_)
-    client = APIClient()
-    client.credentials(HTTP_X_ENVIRONMENT_KEY=api_key.key)
-
-    feature = Feature.objects.create(name="test_feature", project=project)
-    for i in range(2):
-        identity = Identity.objects.create(
-            environment=environment_,
-            identifier=f"identity_{i}",
-        )
-        identity_feature_state = FeatureState.objects.create(
-            identity=identity,
-            feature=feature,
-            environment=environment_,
-        )
-        FeatureStateValue.objects.filter(feature_state=identity_feature_state).update(
-            string_value="overridden",
-            type=STRING,
-        )
-
-    url = reverse("api-v1:environment-document")
-
-    # When
-    response = client.get(url)
-
-    # Then
-    assert response.status_code == status.HTTP_200_OK
-    assert sorted(
-        response.data["identity_overrides"],
-        key=lambda x: x["identifier"],
-    ) == [
-        {
-            "composite_key": ANY,
-            "created_date": ANY,
-            "django_id": ANY,
-            "environment_api_key": environment_.api_key,
-            "identifier": "identity_0",
-            "dashboard_alias": None,
-            "identity_features": [
-                {
-                    "django_id": ANY,
-                    "enabled": False,
-                    "feature": {
-                        "id": feature.id,
-                        "name": "test_feature",
-                        "type": "STANDARD",
-                    },
-                    "feature_segment": None,
-                    "feature_state_value": "overridden",
-                    "featurestate_uuid": ANY,
-                    "multivariate_feature_state_values": [],
-                }
-            ],
-            "identity_traits": [],
-            "identity_uuid": ANY,
-        },
-        {
-            "composite_key": ANY,
-            "created_date": ANY,
-            "django_id": ANY,
-            "environment_api_key": environment_.api_key,
-            "identifier": "identity_1",
-            "dashboard_alias": None,
-            "identity_features": [
-                {
-                    "django_id": ANY,
-                    "enabled": False,
-                    "feature": {
-                        "id": feature.id,
-                        "name": "test_feature",
-                        "type": "STANDARD",
-                    },
-                    "feature_segment": None,
-                    "feature_state_value": "overridden",
-                    "featurestate_uuid": ANY,
-                    "multivariate_feature_state_values": [],
-                }
-            ],
-            "identity_traits": [],
-            "identity_uuid": ANY,
-        },
-    ]
-    assert response.headers[FLAGSMITH_UPDATED_AT_HEADER] == str(
-        environment_.updated_at.timestamp()
-    )
-
-
 def test_get_environment_document_fails_with_invalid_key(
     organisation_one: "Organisation",
     organisation_one_project_one: "Project",
@@ -277,62 +173,3 @@ def test_get_environment_document_fails_with_invalid_key(
     # We get a 403 since only the server side API keys are able to access the
     # environment document
     assert response.status_code == status.HTTP_403_FORBIDDEN
-
-
-def test_environment_document_if_modified_since(
-    organisation_one: "Organisation",
-    organisation_one_project_one: "Project",
-) -> None:
-    # Given
-    project = organisation_one_project_one
-    environment = Environment.objects.create(name="Test Environment", project=project)
-    api_key = EnvironmentAPIKey.objects.create(environment=environment).key
-
-    client = APIClient()
-    client.credentials(HTTP_X_ENVIRONMENT_KEY=api_key)
-    url = reverse("api-v1:environment-document")
-
-    # When - first request
-    response1 = client.get(url)
-
-    # Then - first request should return 200 and include Last-Modified header
-    assert response1.status_code == status.HTTP_200_OK
-    last_modified = response1.headers["Last-Modified"]
-    assert last_modified == http_date(environment.updated_at.timestamp())
-
-    # When - second request with If-Modified-Since header
-    client.credentials(
-        HTTP_X_ENVIRONMENT_KEY=api_key,
-        HTTP_IF_MODIFIED_SINCE=last_modified,
-    )
-    response2 = client.get(url)
-
-    # Then - second request should return 304 Not Modified
-    assert response2.status_code == status.HTTP_304_NOT_MODIFIED
-    assert len(response2.content) == 0
-
-    # sleep for 1s since If-Modified-Since is only accurate to the nearest second
-    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/If-Modified-Since
-    time.sleep(1)
-
-    # When - environment is updated
-    environment.updated_at = timezone.now()
-    environment.save()
-
-    # Then - request with same If-Modified-Since header should return 200
-    response3 = client.get(url)
-    assert response3.status_code == status.HTTP_200_OK
-    assert response3.headers["Last-Modified"] == http_date(
-        environment.updated_at.timestamp()
-    )
-
-    # When - request without If-Modified-Since header
-    client.credentials(
-        HTTP_X_ENVIRONMENT_KEY=api_key,
-        HTTP_IF_MODIFIED_SINCE="",
-    )
-    response4 = client.get(url)
-
-    # Then - actual environment is returned with a 200
-    assert response4.status_code == status.HTTP_200_OK
-    assert len(response4.content) > 0
