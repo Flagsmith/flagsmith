b1dab48c085f867072ca33e76534eb95d9301c9c
Kim Gustyr <kim.gustyr@flagsmith.com>
2026-03-09 16:34:40 +0000

    fix typing

--- api/features/feature_types.py
+++ api/features/feature_types.py
@@ -2,6 +2,15 @@ from typing import get_args
 
 from flagsmith_schemas.types import FeatureType
 
+__all__ = (
+    "FeatureType",
+    "CONFIG",
+    "FEATURE_TYPE_CHOICES",
+    "FLAG",
+    "MULTIVARIATE",
+    "STANDARD",
+)
+
 MULTIVARIATE: FeatureType = "MULTIVARIATE"
 STANDARD: FeatureType = "STANDARD"
 
@@ -10,4 +19,6 @@ FEATURE_TYPE_CHOICES = [(v, v) for v in get_args(FeatureType)]
 # the following two types have been merged in terms of functionality
 # but kept for now until the FE is updated
 CONFIG = "CONFIG"
+"""Deprecated."""
 FLAG = "FLAG"
+"""Deprecated."""
