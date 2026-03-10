b8edc1df4326deab3302adc02d90d8379d3d38ad
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2026-03-10 15:00:21 +0530

    fix(6905): replace dynamic serializer with SDKAnalyticsFlagsV1Serializer
    The V1 SDKAnalyticsFlags view dynamically generated a DRF serializer
    inside get_serializer_class. Extract this into a proper
    SDKAnalyticsFlagsV1Serializer so the view mirrors the clean V2 pattern
    of serializer_class / get_serializer / is_valid / save.

--- api/app_analytics/views.py
+++ api/app_analytics/views.py
@@ -5,31 +5,26 @@ from common.core.utils import using_database_replica
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes, throttle_classes
-from rest_framework.fields import IntegerField
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer
 
 from app_analytics.analytics_db_service import (
     get_total_events_count,
     get_usage_data,
 )
 from app_analytics.cache import FeatureEvaluationCache
-from app_analytics.mappers import (
-    map_request_to_labels,
-)
 from app_analytics.throttles import InfluxQueryThrottle
 from environments.authentication import EnvironmentKeyAuthentication
 from environments.permissions.permissions import EnvironmentKeyPermissions
-from features.models import FeatureState
 from organisations.models import Organisation
 from telemetry.serializers import TelemetrySerializer
 
 from .permissions import UsageDataPermission
 from .serializers import (
     SDKAnalyticsFlagsSerializer,
+    SDKAnalyticsFlagsV1Serializer,
     UsageDataQuerySerializer,
     UsageDataSerializer,
     UsageTotalCountSerializer,
@@ -63,55 +58,17 @@ class SDKAnalyticsFlags(CreateAPIView):  # type: ignore[type-arg]
 
     permission_classes = (EnvironmentKeyPermissions,)
     authentication_classes = (EnvironmentKeyAuthentication,)
+    serializer_class = SDKAnalyticsFlagsV1Serializer
     throttle_classes = []
-    format_kwarg = None
-
-    def get_serializer_class(self):  # type: ignore[no-untyped-def]
-        environment_feature_names = set(
-            using_database_replica(FeatureState.objects)
-            .filter(
-                environment=self.request.environment,
-                feature_segment=None,
-                identity=None,
-            )
-            .values_list("feature__name", flat=True)
-        )
-
-        class _AnalyticsSerializer(Serializer):  # type: ignore[type-arg]
-            def get_fields(self):  # type: ignore[no-untyped-def]
-                return {
-                    feature_name: IntegerField(required=False)
-                    for feature_name in environment_feature_names
-                }
-
-            def save(self, **kwargs: typing.Any) -> None:
-                request = self.context["request"]
-                for feature_name, evaluation_count in self.validated_data.items():
-                    feature_evaluation_cache.track_feature_evaluation(
-                        environment_id=request.environment.id,
-                        feature_name=feature_name,
-                        evaluation_count=evaluation_count,
-                        labels=map_request_to_labels(request),
-                    )
-
-        return _AnalyticsSerializer
 
     @extend_schema(
         request=SDKAnalyticsFlagsSerializer,
         responses={200: None},
     )
-    def create(
-        self, request: Request, *args: typing.Any, **kwargs: typing.Any
-    ) -> Response:
-        """
-        Send flag evaluation events from the SDK back to the API for reporting.
-
-        TODO: Eventually replace this with the v2 version of
-              this endpoint once SDKs have been updated.
-        """
+    def create(self, request: Request, *args: typing.Any, **kwargs: typing.Any) -> Response:
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid()
-        serializer.save(environment=self.request.environment)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(environment=request.environment, cache=feature_evaluation_cache)
         return Response(status=status.HTTP_200_OK)
 
 
