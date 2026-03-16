30bdfa7633e66d9bcb31d17097ddcabdc819f96d
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-03-16 07:14:39 +0100

    Update go client version in docs and codehelp

--- frontend/common/code-help/install/install-go.js
+++ frontend/common/code-help/install/install-go.js
@@ -1,4 +1,4 @@
-module.exports = () => `go get github.com/flagsmith/flagsmith-go-client
+module.exports = () => `go get github.com/flagsmith/flagsmith-go-client/v5
 
 import (
   "github.com/flagsmith/flagsmith-go-client"
