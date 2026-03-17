a6a34bde13efa0ef54d7770c1a5809b613d40c69
Pwongchaiya <Pwongchaiya1@gmail.com>
2026-03-17 15:38:14 -0700

    added validation for incoming enviornment cannot be null

--- api/features/serializers.py
+++ api/features/serializers.py
@@ -588,7 +588,7 @@ class FeatureStateSerializerBasic(WritableNestedModelSerializer):
 
         except django.core.exceptions.ValidationError as e:
             raise serializers.ValidationError(str(e))
-
+        
     def validate_feature(self, feature):  # type: ignore[no-untyped-def]
         if self.instance and self.instance.feature_id != feature.id:  # type: ignore[union-attr]
             raise serializers.ValidationError(
@@ -597,6 +597,11 @@ class FeatureStateSerializerBasic(WritableNestedModelSerializer):
         return feature
 
     def validate_environment(self, environment):  # type: ignore[no-untyped-def]
+        if environment is None:
+            raise serializers.ValidationError(
+                "Environment cannot be null"
+            )
+
         if self.instance and self.instance.environment_id != environment.id:  # type: ignore[union-attr]
             raise serializers.ValidationError(
                 "Cannot change the environment of a feature state"
