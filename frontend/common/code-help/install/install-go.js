cf43e1b9fe7a5be245bad5977d2de11e2b660c0c
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2026-03-20 14:45:53 +0530

    fix: update import path to v5 and fix casing in install-go.js

--- frontend/common/code-help/install/install-go.js
+++ frontend/common/code-help/install/install-go.js
@@ -1,6 +1,6 @@
-module.exports = () => `go get github.com/flagsmith/flagsmith-go-client/v5
+module.exports = () => `go get github.com/Flagsmith/flagsmith-go-client/v5
 
 import (
-  "github.com/flagsmith/flagsmith-go-client"
+  "github.com/Flagsmith/flagsmith-go-client/v5"
 )
 `
