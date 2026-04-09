9dcde586e08a73c32aa2b122bc034797f8a685a6
Kyle Johnson <kyle@solidstategroup.com>
2026-04-09 08:53:44 +0100

    refactor: Codehelp TypeScript migration (#7170)

--- frontend/web/components/icons/Icon.tsx
+++ frontend/web/components/icons/Icon.tsx
@@ -225,8 +225,8 @@ const Icon: FC<IconType> = ({
     case 'github': {
       return (
         <svg
-          width='20'
-          height='20'
+          width={width || '20'}
+          height={height || '20'}
           viewBox='0 0 20 20'
           fill='none'
           xmlns='http://www.w3.org/2000/svg'
