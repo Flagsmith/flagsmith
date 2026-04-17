1b4b870df774ab7a3c7f5de13cdd94e49ab1a9e6
Evandro Myller <evandro.myller@flagsmith.com>
2026-04-17 16:08:59 -0300

    Fix URL building

--- frontend/common/services/useGitlab.ts
+++ frontend/common/services/useGitlab.ts
@@ -1,29 +1,25 @@
 import { Res } from 'common/types/responses'
 import { Req } from 'common/types/requests'
 import { service } from 'common/service'
+import Utils from 'common/utils/utils'
 
 export const gitlabService = service
   .enhanceEndpoints({ addTagTypes: ['GitLab'] })
   .injectEndpoints({
     endpoints: (builder) => ({
-      getGitLabProjects: builder.query<
-        Res['gitlabProjects'],
-        Req['getGitLabProjects']
-      >({
-        providesTags: [{ id: 'LIST', type: 'GitLab' }],
-        query: (query: Req['getGitLabProjects']) => ({
-          url: `projects/${query.project_id}/gitlab/projects/?page_size=${query.page_size ?? 100}&page=${query.page ?? 1}`,
-        }),
-      }),
       getGitLabIssues: builder.query<
         Res['gitlabIssues'],
         Req['getGitLabIssues']
       >({
         providesTags: [{ id: 'LIST', type: 'GitLab' }],
         query: (query: Req['getGitLabIssues']) => ({
-          url:
-            `projects/${query.project_id}/gitlab/issues/` +
-            `?gitlab_project_id=${query.gitlab_project_id}&page_size=${query.page_size ?? 100}&page=${query.page ?? 1}&search_text=${query.q || ''}&state=opened`,
+          url: `projects/${query.project_id}/gitlab/issues/?${Utils.toParam({
+            gitlab_project_id: query.gitlab_project_id,
+            page: query.page ?? 1,
+            page_size: query.page_size ?? 100,
+            search_text: query.q || undefined,
+            state: 'opened',
+          })}`,
         }),
       }),
       getGitLabMergeRequests: builder.query<
@@ -32,9 +28,27 @@ export const gitlabService = service
       >({
         providesTags: [{ id: 'LIST', type: 'GitLab' }],
         query: (query: Req['getGitLabMergeRequests']) => ({
-          url:
-            `projects/${query.project_id}/gitlab/merge-requests/` +
-            `?gitlab_project_id=${query.gitlab_project_id}&page_size=${query.page_size ?? 100}&page=${query.page ?? 1}&search_text=${query.q || ''}&state=opened`,
+          url: `projects/${
+            query.project_id
+          }/gitlab/merge-requests/?${Utils.toParam({
+            gitlab_project_id: query.gitlab_project_id,
+            page: query.page ?? 1,
+            page_size: query.page_size ?? 100,
+            search_text: query.q || undefined,
+            state: 'opened',
+          })}`,
+        }),
+      }),
+      getGitLabProjects: builder.query<
+        Res['gitlabProjects'],
+        Req['getGitLabProjects']
+      >({
+        providesTags: [{ id: 'LIST', type: 'GitLab' }],
+        query: (query: Req['getGitLabProjects']) => ({
+          url: `projects/${query.project_id}/gitlab/projects/?${Utils.toParam({
+            page: query.page ?? 1,
+            page_size: query.page_size ?? 100,
+          })}`,
         }),
       }),
       // END OF ENDPOINTS
@@ -77,8 +91,8 @@ export async function getGitLabMergeRequests(
 // END OF FUNCTION_EXPORTS
 
 export const {
-  useGetGitLabProjectsQuery,
   useGetGitLabIssuesQuery,
   useGetGitLabMergeRequestsQuery,
+  useGetGitLabProjectsQuery,
   // END OF EXPORTS
 } = gitlabService
