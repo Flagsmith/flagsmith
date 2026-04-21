86782a5755590bf19bdd96f88847c83c919c7a38
wadii <wadii.zaim@flagsmith.com>
2026-04-21 14:32:42 +0200

    feat: add tagging enabled configuration field

---
+++ api/integrations/gitlab/migrations/0002_gitlabconfiguration_tagging_enabled.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("gitlab", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="gitlabconfiguration",
+            name="tagging_enabled",
+            field=models.BooleanField(default=False),
+        ),
+    ]
