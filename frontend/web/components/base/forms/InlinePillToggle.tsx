cacb97c1d3902f49efe4644c86e92d22eaf169a4
wadii <wadii.zaim@flagsmith.com>
2026-05-05 12:42:28 +0200

    feat: implemented toggle any and all rules with correct text diff

---
+++ frontend/web/components/base/forms/InlinePillToggle.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import cn from 'classnames'
+
+type InlinePillToggleSize = 'small' | 'medium' | 'large'
+
+type Option<T extends string> = {
+  label: string
+  value: T
+}
+
+type InlinePillToggleProps<T extends string> = {
+  options: Option<T>[]
+  value: T
+  onChange: (value: T) => void
+  size?: InlinePillToggleSize
+  className?: string
+  'data-test'?: string
+}
+
+function InlinePillToggle<T extends string>({
+  className,
+  'data-test': dataTest,
+  onChange,
+  options,
+  size = 'medium',
+  value,
+}: InlinePillToggleProps<T>) {
+  return (
+    <div
+      className={cn(
+        'inline-pill-toggle',
+        `inline-pill-toggle--${size}`,
+        className,
+      )}
+      data-test={dataTest}
+      role='radiogroup'
+    >
+      {options.map((option) => (
+        <button
+          key={option.value}
+          type='button'
+          role='radio'
+          aria-checked={value === option.value}
+          className={cn('inline-pill-toggle__option', {
+            'inline-pill-toggle__option--active': value === option.value,
+          })}
+          onClick={() => onChange(option.value)}
+        >
+          {option.label}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default InlinePillToggle
