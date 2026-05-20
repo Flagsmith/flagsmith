c77dde905605ab6dfa886e1fda4f7ff994ed3afb
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-05-20 09:35:28 +0100

    Hide exception details from webhook test response

--- api/webhooks/views.py
+++ api/webhooks/views.py
@@ -63,11 +63,11 @@ class WebhookViewSet(viewsets.ViewSet):
                 },
                 status=status.HTTP_200_OK,
             )
-        except requests.exceptions.RequestException as e:
+        except requests.exceptions.RequestException:
             return Response(
                 {
                     "detail": "Could not connect to webhook URL",
-                    "body": str(e),
+                    "body": "Please check the URL, and ensure it is valid and accessible from the server.",
                 },
                 status=status.HTTP_400_BAD_REQUEST,
             )
