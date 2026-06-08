282c32f086a82f73219a24e4f635e1ceb549f2ee
wadii <wadii.zaim@flagsmith.com>
2026-06-08 10:08:12 +0200

    chore: drop-stray-frontend-utils-reorder-from-backend-branch

--- frontend/common/utils/utils.tsx
+++ frontend/common/utils/utils.tsx
@@ -145,6 +145,16 @@ const Utils = Object.assign({}, BaseUtils, {
     return res
   },
 
+  getContrastColour(backgroundColor: string | null | undefined): string {
+    if (!backgroundColor) return 'white'
+
+    try {
+      return Color(backgroundColor).luminosity() > 0.179 ? 'black' : 'white'
+    } catch {
+      return 'white'
+    }
+  },
+
   copyToClipboard: async (
     value: string,
     successMessage?: string,
@@ -158,7 +168,6 @@ const Utils = Object.assign({}, BaseUtils, {
       throw error
     }
   },
-
   displayLimitAlert(type: string, percentage: number | undefined) {
     const envOrProject =
       type === 'segment overrides' ? 'environment' : 'project'
@@ -201,6 +210,7 @@ const Utils = Object.assign({}, BaseUtils, {
         return featureState.string_value
     }
   },
+
   findOperator(
     operator: SegmentCondition['operator'],
     value: string,
@@ -217,29 +227,18 @@ const Utils = Object.assign({}, BaseUtils, {
 
     return conditions.find((v) => v.value === operator)
   },
-
   /** Checks whether the specified flag exists, which is different from the flag being enabled or not. This is used to
    *  only add behaviour to Flagsmith-on-Flagsmith flags that have been explicitly created by customers.
    */
   flagsmithFeatureExists(flag: string) {
     return Object.prototype.hasOwnProperty.call(flagsmith.getAllFlags(), flag)
   },
-
   getContentType(
     contentTypes: ContentType[] | undefined,
     model: string,
     type: string,
   ) {
     return contentTypes?.find((c: ContentType) => c[model] === type) || null
-  },
-  getContrastColour(backgroundColor: string | null | undefined): string {
-    if (!backgroundColor) return 'white'
-
-    try {
-      return Color(backgroundColor).luminosity() > 0.179 ? 'black' : 'white'
-    } catch {
-      return 'white'
-    }
   },
   getCreateProjectPermission(organisation: Organisation) {
     if (organisation?.restrict_project_create_to_admin) {
