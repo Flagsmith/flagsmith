6d6f0ce244188bfa39b127650d96db9e839ac5aa
afsuyadi <afsuyadi@gmail.com>
2026-07-06 10:25:07 +0700

    feat(integrations): add method to retrieve latest integration health

--- api/integrations/common/services.py
+++ api/integrations/common/services.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
 
 from integrations.common.models import IntegrationHealthRecord
 
@@ -11,3 +12,20 @@ def record_integration_health(
         content_object=integration_config,
         status_code=status_code,
     )
+
+def get_latest_integration_health(
+    integration_config: models.Model
+) -> dict | None:
+    latest_record = IntegrationHealthRecord.objects.filter(
+        content_type=ContentType.objects.get_for_model(integration_config),
+        object_id=integration_config.id,
+    ).order_by("-created_at").first()
+
+    if not latest_record:
+        return None
+
+    return {
+        "status_code": latest_record.status_code,
+        "is_healthy": 200 <= latest_record.status_code < 300,
+        "created_at": latest_record.created_at,
+    }
