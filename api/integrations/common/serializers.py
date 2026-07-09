590d7e424e516422682008919caee6eb35bdd01e
afsuyadi <afsuyadi@gmail.com>
2026-07-09 07:00:34 +0700

    fix(serializer): add type notation

--- api/integrations/common/serializers.py
+++ api/integrations/common/serializers.py
@@ -36,7 +36,7 @@ class BaseEnvironmentIntegrationModelSerializer(_BaseIntegrationModelSerializer)
     one_to_one_field_name = "environment"  # type: ignore[assignment]
     latest_health = SerializerMethodField()
 
-    def get_latest_health(self, obj):
+    def get_latest_health(self, obj: Model) -> dict | None:  
         return get_latest_integration_health(obj)
 
 
