99c6e798d7058b57dd9eedadb46f95b3fba271fb
Evandro Myller <evandro.myller@flagsmith.com>
2026-07-21 11:51:18 -0300

    red stuff

---
+++ api/tests/unit/environments/onboarding/test_unit_environments_views.py
@@ -0,0 +1,154 @@
+import freezegun
+import pytest
+from django.utils import timezone
+from pytest_django.fixtures import DjangoAssertNumQueries
+from pytest_lazyfixture import lazy_fixture  # type: ignore[import-untyped]
+from pytest_mock import MockerFixture
+from rest_framework.test import APIClient
+
+from environments.models import Environment
+from environments.onboarding import views
+
+pytestmark = pytest.mark.freeze_time("2077-07-07T07:07:07Z")
+
+
+@pytest.fixture
+def onboarded_environment(environment: Environment) -> Environment:
+    environment.first_evaluated_at = timezone.now()
+    environment.first_evaluated_sdk_label = "flagsmith-python-sdk"
+    environment.save(update_fields=["first_evaluated_at", "first_evaluated_sdk_label"])
+    return environment
+
+
+def test_get_onboarding_status__never_evaluated__responds_200(
+    api_client: APIClient,
+    environment: Environment,
+) -> None:
+    # Given / When
+    response = api_client.get(
+        f"/api/v1/environments/{environment.api_key}/onboarding-status/"
+    )
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {
+        "first_evaluated_at": None,
+        "first_evaluated_sdk_label": None,
+    }
+
+
+def test_get_onboarding_status__evaluated__responds_200(
+    api_client: APIClient,
+    onboarded_environment: Environment,
+) -> None:
+    # Given / When
+    response = api_client.get(
+        f"/api/v1/environments/{onboarded_environment.api_key}/onboarding-status/"
+    )
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {
+        "first_evaluated_at": "2077-07-07T07:07:07Z",
+        "first_evaluated_sdk_label": "flagsmith-python-sdk",
+    }
+
+
+@pytest.mark.parametrize(
+    "either_environment",
+    (lazy_fixture("environment"), lazy_fixture("onboarded_environment")),
+)
+def test_put_onboarding_status_async__regardless_evaluated__enqueues_and_responds_204(
+    api_client: APIClient,
+    django_assert_num_queries: DjangoAssertNumQueries,
+    either_environment: Environment,
+    mocker: MockerFixture,
+) -> None:
+    # Given
+    task = mocker.patch.object(views, "record_environment_first_evaluation")
+
+    # When
+    with django_assert_num_queries(0):
+        response = api_client.put(
+            f"/api/v1/environments/{either_environment.api_key}/onboarding-status/",
+            data={
+                "first_evaluated_sdk_label": "flagsmith-python-sdk",
+            },
+            format="json",
+        )
+
+    # Then
+    assert response.status_code == 204
+    task.delay.assert_called_once_with(
+        args=(either_environment.api_key, "flagsmith-python-sdk"),
+    )
+
+
+def test_put_onboarding_status_sync__never_evaluated__updates_environment(
+    api_client: APIClient,
+    environment: Environment,
+    mocker: MockerFixture,
+) -> None:
+    # Given
+    assert environment.first_evaluated_at is None
+    assert environment.first_evaluated_sdk_label is None
+
+    # When
+    response = api_client.put(
+        f"/api/v1/environments/{environment.api_key}/onboarding-status/",
+        data={
+            "first_evaluated_sdk_label": "flagsmith-python-sdk",
+        },
+        format="json",
+    )
+
+    # Then
+    assert response.status_code == 204
+    environment.refresh_from_db()
+    assert environment.first_evaluated_at == timezone.now()
+    assert environment.first_evaluated_sdk_label == "flagsmith-python-sdk"
+
+
+def test_put_onboarding_status_sync__evaluated__does_not_update_environment(
+    api_client: APIClient,
+    onboarded_environment: Environment,
+) -> None:
+    # Given
+    assert onboarded_environment.first_evaluated_at is not None
+    assert onboarded_environment.first_evaluated_sdk_label is not None
+
+    # When
+    with freezegun.freeze_time("2088-08-08T08:08:08Z"):
+        response = api_client.put(
+            f"/api/v1/environments/{onboarded_environment.api_key}/onboarding-status/",
+            data={
+                "first_evaluated_sdk_label": "flagsmith-php-sdk",
+            },
+            format="json",
+        )
+
+    # Then
+    assert response.status_code == 204
+    onboarded_environment.refresh_from_db()
+    assert "2077" in onboarded_environment.first_evaluated_at.isoformat()
+    assert "python" in onboarded_environment.first_evaluated_sdk_label
+
+
+def test_put_onboarding_status__invalid_sdk_label__responds_400(
+    api_client: APIClient,
+    environment: Environment,
+) -> None:
+    # Given / When
+    response = api_client.put(
+        f"/api/v1/environments/{environment.api_key}/onboarding-status/",
+        data={
+            "first_evaluated_sdk_label": "invalid-sdk-label",
+        },
+        format="json",
+    )
+
+    # Then
+    assert response.status_code == 400
+    assert response.json() == {
+        "first_evaluated_sdk_label": ['"invalid-sdk-label" is not a valid choice.'],
+    }
