1400751314debed54c2b695d57899bed53b751f2
Srijan Tripathi <srijantrpth@gmail.com>
2026-08-06 15:58:22 +0000

    fix: rename project view tests to match FT003 linting convention

--- api/tests/unit/projects/test_unit_projects_views.py
+++ api/tests/unit/projects/test_unit_projects_views.py
@@ -1074,7 +1074,7 @@ def test_list_projects__default_enforce_feature_owners__returns_false(
     assert response.json()[0]["enforce_feature_owners"] is False
 
 
-def test_create_project_creates_audit_log(admin_client, organisation):
+def test_create_project__valid_request__creates_audit_log(admin_client, organisation):
     # Given
     url = reverse("api-v1:projects:project-list")
     project_name = "New Audit Log Project"
@@ -1095,7 +1095,9 @@ def test_create_project_creates_audit_log(admin_client, organisation):
     assert audit_log.project_id == response.data["id"]
 
 
-def test_delete_project_creates_audit_log(admin_client, project, organisation):
+def test_delete_project__valid_request__creates_audit_log(
+    admin_client, project, organisation
+):
     # Given
     url = reverse("api-v1:projects:project-detail", args=[project.id])
     project_name = project.name
