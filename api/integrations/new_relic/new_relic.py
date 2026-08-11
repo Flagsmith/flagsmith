612c5bca3aea74cbf4b67857a0c2e182543d979a
Bunlong Heng <bheng.code@gmail.com>
2026-08-11 15:14:46 -0400

    fix: add request timeout to new_relic integration

--- api/integrations/new_relic/new_relic.py
+++ api/integrations/new_relic/new_relic.py
@@ -3,6 +3,10 @@ import logging
 
 import requests
 
+from integrations.common.constants import (
+    INTEGRATION_REQUEST_TIMEOUT_SECONDS,
+)
+
 from audit.models import AuditLog
 from integrations.common.wrapper import AbstractBaseEventIntegrationWrapper
 
@@ -20,7 +24,10 @@ class NewRelicWrapper(AbstractBaseEventIntegrationWrapper):
 
     def _track_event(self, event: dict) -> None:  # type: ignore[type-arg]
         response = requests.post(
-            self.url, headers=self._headers(), data=json.dumps(event)
+            self.url,
+            headers=self._headers(),
+            data=json.dumps(event),
+            timeout=INTEGRATION_REQUEST_TIMEOUT_SECONDS,
         )
         logger.debug(
             "Sent event to NewRelic. Response code was %s" % response.status_code
