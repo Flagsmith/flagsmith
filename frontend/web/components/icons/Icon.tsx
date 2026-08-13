dc99d654ae6649a44912bf52f2900cdcbb94e7a2
Zaimwa9 <wadii.zaim@gmail.com>
2026-08-13 10:20:49 +0200

    feat: create segment sources fake door modal (#8272)

--- frontend/web/components/icons/Icon.tsx
+++ frontend/web/components/icons/Icon.tsx
@@ -17,6 +17,7 @@ export type IconName =
   | 'clock'
   | 'close'
   | 'close-circle'
+  | 'cloud-upload'
   | 'code'
   | 'copy'
   | 'copy-outlined'
@@ -86,6 +87,33 @@ const Icon: FC<IconType> = ({
   ...rest
 }) => {
   switch (name) {
+    case 'cloud-upload': {
+      return (
+        <svg
+          width={width || '24'}
+          height={height || width || '24'}
+          viewBox='0 0 512 512'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          {...rest}
+        >
+          <path
+            d='M320 367.79h76c55 0 100-29.21 100-83.6s-53-81.47-96-83.6c-8.89-85.06-71-136.8-144-136.8-69 0-113.44 45.79-128 91.2-60 5.7-112 43.88-112 106.4s54 106.4 120 106.4h56'
+            stroke={fill || 'currentColor'}
+            strokeWidth='32'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+          <path
+            d='M320 255.79l-64-64-64 64M256 448.21V207.79'
+            stroke={fill || 'currentColor'}
+            strokeWidth='32'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      )
+    }
     case 'code': {
       return (
         <svg
