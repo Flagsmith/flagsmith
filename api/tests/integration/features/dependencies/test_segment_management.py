f7b8ac7b01b8953f8e05ef1bfe2d4db68b3b690e
Evandro Myller <evandro.myller@flagsmith.com>
2026-09-18 13:10:14 -0300

    fix test coverage

--- api/tests/integration/features/dependencies/test_segment_management.py
+++ api/tests/integration/features/dependencies/test_segment_management.py
@@ -314,6 +314,7 @@ def test_delete_segment__flag_dependency__indexes_removed(
 
 def test_update_segment_add_override__circular_flag_dependency__responds_400(
     admin_client: APIClient,
+    create_segment_override: CreateSegmentOverrideFixture,
     environment: int,
     environment_api_key: str,
     environment_name: str,
@@ -331,10 +332,10 @@ def test_update_segment_add_override__circular_flag_dependency__responds_400(
         prerequisite_feature=egg,
         condition_json_path="$[0].conditions[1]",
     )
-    FeatureSegment.objects.create(
-        segment=segment1,
-        feature=chicken,
-        environment_id=environment,
+    create_segment_override(
+        environment_api_key=environment_api_key,
+        feature_id=chicken.id,
+        segment_id=segment1.id,
     )
     SegmentFlagReference.objects.create(
         segment=segment2,
@@ -928,84 +929,4 @@ def test_update_segment_update_rules__shared_prerequisite__responds_200(
         environment__key=environment_api_key,
         feature__name="chicken",
         prerequisite_feature__name="egg",
-    )
-
-
-def test_create_feature_segment__circular_flag_dependency__responds_400(
-    admin_client: APIClient,
-    create_segment_override: CreateSegmentOverrideFixture,
-    environment: int,
-    environment_api_key: str,
-    environment_name: str,
-    log: StructuredLogCapture,
-    organisation: int,
-    project: int,
-) -> None:
-    # Given
-    chicken = Feature.objects.create(name="chicken", project_id=project)
-    egg = Feature.objects.create(name="egg", project_id=project)
-    chicken_segment = Segment.objects.create(name="chicken_segment", project_id=project)
-    egg_segment = Segment.objects.create(name="egg_segment", project_id=project)
-    SegmentFlagReference.objects.create(
-        segment=chicken_segment,
-        prerequisite_feature=egg,
-        condition_json_path="$[0].conditions[1]",
-    )
-    create_segment_override(
-        environment_api_key=environment_api_key,
-        feature_id=chicken.id,
-        segment_id=chicken_segment.id,
-    )
-    SegmentFlagReference.objects.create(
-        segment=egg_segment,
-        prerequisite_feature=chicken,
-        condition_json_path="$[1].conditions[2]",
-    )
-
-    # When
-    response = admin_client.post(
-        "/api/v1/features/feature-segments/",
-        data={
-            "feature": egg.id,
-            "segment": egg_segment.id,
-            "environment": environment,
-        },
-        format="json",
-    )
-
-    # Then
-    assert response.status_code == 400
-    assert response.json() == {
-        "code": "circular_dependency",
-        "environment": {"key": environment_api_key, "name": environment_name},
-        "path": [
-            {
-                "feature": "egg",
-                "needs": "chicken",
-                "segment": {
-                    "id": egg_segment.id,
-                    "name": egg_segment.name,
-                    "condition_json_path": "$[1].conditions[2]",
-                },
-            },
-            {
-                "feature": "chicken",
-                "needs": "egg",
-                "segment": {
-                    "id": chicken_segment.id,
-                    "name": chicken_segment.name,
-                    "condition_json_path": "$[0].conditions[1]",
-                },
-            },
-        ],
-    }
-    assert not FeatureSegment.objects.filter(segment=egg_segment, feature=egg).exists()
-    assert log.has(
-        "dependencies.create_failed",
-        level="info",
-        organisation__id=organisation,
-        project__id=project,
-        environment__key=environment_api_key,
-        feature__name="egg",
-        prerequisite_feature__name="chicken",
     )
