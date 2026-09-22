0d9136890a2612dca9ad458ccdbd9bb887eadccd
Matthew Elwell <matthew.elwell@flagsmith.com>
2026-09-22 22:54:22 +0100

    fix(chargebee): Handle different billing period units (#8568)

--- api/organisations/chargebee/constants.py
+++ api/organisations/chargebee/constants.py
@@ -1,8 +1,9 @@
 ADDITIONAL_API_START_UP_ADDON_ID = "additional-api-start-up-monthly"
 ADDITIONAL_API_SCALE_UP_ADDON_ID = "additional-api-scale-up-monthly"
 
-SEAT_SCALE_UP_V2_ADDON_BY_BILLING_PERIOD: dict[int, str] = {
-    1: "additional-team-members-scale-up-v2-monthly",
-    6: "additional-team-members-scale-up-v2-semiannual",
-    12: "additional-team-members-scale-up-v2-annual",
+SEAT_SCALE_UP_V2_ADDON_BY_BILLING_PERIOD: dict[tuple[int, str], str] = {
+    (1, "month"): "additional-team-members-scale-up-v2-monthly",
+    (6, "month"): "additional-team-members-scale-up-v2-semiannual",
+    (12, "month"): "additional-team-members-scale-up-v2-annual",
+    (1, "year"): "additional-team-members-scale-up-v2-annual",
 }
